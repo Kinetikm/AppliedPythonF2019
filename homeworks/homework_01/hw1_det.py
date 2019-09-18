d6b76e8a6206518ea5ba75d4eacc83394fd69ec9
verbarius <verbarius@mail.ru>
2019-09-18 22:37:26 +0300

    calculate the determinant of matrix

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,23 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    A = list_of_lists
+    n = len(A)
+    m = len(A[0])
+    if n!=m:
+        return None
+    if n == 1:
+        return A[0]
+    else:
+        for i in range(n): #номер шага и номер вычитаемой строки
+            for j in range(n-1, i, -1):  #номер строки n-1..0
+                if A[i][i]!=0:
+                    koeff = (A[j][i]/A[i][i])
+                else:
+                    koeff = 0
+                for k in range(n):   #индексы в j строке
+                    A[j][k] = A[j][k] - koeff * A[i][k]
+        det = 1
+        for i in range(n):
+            det*=A[i][i]
+        return det
