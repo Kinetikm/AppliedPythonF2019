6856c660f05878deed92c92f5496e45c2e24f31f
ChesnovAE <aechesnov@yandex.ru>
2019-09-18 16:46:41 +0300

    List inversion. Add return input_lst in function...

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,4 +8,14 @@ def word_inversion(input_lst):
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-    raise NotImplementedError
+    idx = 0
+    input_lst = input_lst[::-1]
+    for i in range(len(input_lst)):
+        if (input_lst[i] == ' '):
+            if (i - idx + 1 != 2):
+                input_lst[idx:i] = input_lst[idx:i][::-1]
+                idx = i + 1
+            else:
+                idx = i + 1
+    input_lst[idx:] = input_lst[idx:][::-1]
+    return input_lst
