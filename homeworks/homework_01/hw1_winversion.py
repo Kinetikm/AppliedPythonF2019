45f5e423e48538d6acaf2825ffcf9fa9fb1d50c6
verbarius <verbarius@mail.ru>
2019-09-18 23:40:18 +0300

    invert words in list

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,4 +8,14 @@ def word_inversion(input_lst):
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-    raise NotImplementedError
+    B = input_lst
+    B = B[::-1]
+    l = 0
+    r = 0
+    for i in range(len(B)):
+        if B[i] == ' ':
+            r = i
+            B[l:r]=B[l:r][::-1]
+            l = i+1
+    B[l:]=B[l:][::-1]
+    return B
