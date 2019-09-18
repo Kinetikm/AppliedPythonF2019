658b11da396215e91d125c50c598af70b929ab21
francuz-v <vera.frantsuzova@yandex.ru>
2019-09-18 23:05:39 +0300

    word_inversion solved

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,4 +8,17 @@ def word_inversion(input_lst):
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-    raise NotImplementedError
+    if ' ' in input_lst:
+        s = ''
+        for letter in input_lst:
+            s += letter
+            #print(s)
+        del input_lst[::]
+        input_lst += [i for i in s.split()]
+        input_lst.reverse()
+        for i in range(len(input_lst)):
+            input_lst += input_lst[0]
+            input_lst.append(' ')
+            del input_lst[0]
+        input_lst.pop()
+    return input_lst
