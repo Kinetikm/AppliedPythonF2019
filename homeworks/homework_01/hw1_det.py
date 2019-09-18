1bd9c146ddca8d781e8ee293352e2efe46989f6c
ChesnovAE <aechesnov@yandex.ru>
2019-09-18 21:45:06 +0300

    Calculation matrix's determinant

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,22 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    n = len(list_of_lists)
+    m = len(list_of_lists[0])
+    if n != m:
+        return None
+    if n == 1:
+        return list_of_lists[0][0]
+    # приводим матрицу к верхнетреугольному виду
+    for i in range(n):
+        for j in range(m - 1, i, -1):
+            if list_of_lists[i][i] != 0:
+                koef = list_of_lists[j][i] / list_of_lists[i][i]
+            else:
+                koef = 0
+            temp_list = [koef * item for item in list_of_lists[i]]
+            list_of_lists[j] = [b - a for a, b in zip(temp_list, list_of_lists[j])]
+    det = 1
+    for i in range(n):
+        det *= list_of_lists[i][i]
+    return det
