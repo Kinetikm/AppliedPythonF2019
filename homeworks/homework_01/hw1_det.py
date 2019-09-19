4461593af838c1c2e710d9edfaf5c6145d0a378f
francuz-v <vera.frantsuzova@yandex.ru>
2019-09-19 22:55:35 +0300

    calculate determinant solved

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,18 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    if len(list_of_lists) != len(list_of_lists[0]):
+        return None
+    elif len(list_of_lists) == 1:
+        return list_of_lists[0][0]
+    elif len(list_of_lists) == 2:
+        return list_of_lists[0][0]*list_of_lists[1][1] - list_of_lists[1][0]*list_of_lists[0][1]
+    else:
+        sum = 0
+        for i in range(len(list_of_lists)):
+            sublist = []
+            for j in range(len(list_of_lists)):
+                if i != j:
+                    sublist += [list_of_lists[j][1::]]
+            sum += ((-1)**(2+i))*list_of_lists[i][0]*calculate_determinant(sublist)
+        return sum
