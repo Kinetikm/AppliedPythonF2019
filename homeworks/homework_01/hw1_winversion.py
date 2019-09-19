074576e9dc0f5d1bf881cbdc12008847a52a135a
Anatoly Rubanov <anatolijrubanov@gmail.com>
2019-09-19 12:56:32 +0300

    modified hw1_winversion for working inplace

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -9,14 +9,18 @@ def word_inversion(input_lst):
     :return: None Все изменения в input_lst проходят
     """
     temp = list()
-    new_list = list()
+    write_index = 0
     for _ in reversed(range(len(input_lst))):
         symbol = input_lst.pop()
         if symbol != ' ':
             temp.insert(0, symbol)
         else:
             temp.append(symbol)
-            new_list += temp
+            for element in temp:
+                input_lst.insert(write_index, element)
+                write_index += 1
             temp = list()
-    new_list += temp
-    return new_list
+    for element in temp:
+        input_lst.insert(write_index, element)
+        write_index += 1
+    return input_lst
