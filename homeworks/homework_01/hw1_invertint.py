28609ef02b0922c5df43a570e0404c56d6d1e43c
Ramil.Chermanteev <ramil.chermanteev@x5.ru>
2019-09-19 22:37:02 +0300

    Добавлено решение

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -2,11 +2,13 @@
 # coding: utf-8
 
 
-def reverse(number):
-    '''
+def reverse(number: int) -> int:
+    """
     Метод, принимающий на вход int и
     возвращающий инвертированный int
     :param number: исходное число
     :return: инвертированное число
-    '''
-    raise NotImplementedError
+    """
+    return (
+        int(str(number)[::-1]) if number >= 0 else int(str(number * (-1))[::-1]) * (-1)
+    )
