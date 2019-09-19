e4aa65873a5b5af1932f36c5e7475e1694cb10f2
Ilnur Zinnurov <vip.zinnurov@mail.ru>
2019-09-19 15:04:21 +0300

    All done v5

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -13,7 +13,7 @@ def calculate_determinant(list_of_lists):
     '''
     if len(list_of_lists) == 0 or len(list_of_lists) != len(list_of_lists[0]):
         return None
-    elif len(list_of_lists)==1:
+    elif len(list_of_lists) == 1:
         return list_of_lists[0][0]
     # elif len(list_of_lists) == 2:
     #     return list_of_lists[0][0] * list_of_lists[1][1] - list_of_lists[0][1] * list_of_lists[1][0]
