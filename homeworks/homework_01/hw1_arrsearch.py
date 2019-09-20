a28fdeeabcffacfcccef6637c1c84e48e535fb43
Илья Филатов <ilya_fiatovv@mail.ru>
2019-09-20 19:08:00 +0300

    теперь вроде end

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -1,16 +1,15 @@
 def find_indices(input_list, target):
     if len(input_list) == 0:
         return None
-    b = input_list[::]
-    b.sort()
+    b = sorted(input_list[::])
     k = 0
-    m = len(input_list)-1
+    m = len(input_list) - 1
     while True:
         if k == m:
             return None
-        elif b[k]+b[m] > target:
-            m = m-1
-        elif b[k]+b[m] < target:
+        elif b[k] + b[m] > target:
+            m = m - 1
+        elif b[k] + b[m] < target:
             k = k + 1
         else:
             break
@@ -21,4 +20,3 @@ def find_indices(input_list, target):
     else:
         p = (input_list.index(b[m]), input_list.index(b[k]))
     return p
-
--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,31 +1,33 @@
 def calculate_determinant(list_of_lists):
     matrix = list_of_lists
     try:
-        if type(matrix[0]) == list:
+        if isinstance(matrix[0], list):
             for i in range(len(matrix)):
-                if len(matrix) != len(matrix[i]):    # проверяем, является ли матрица квадратной
+                if len(matrix) != len(
+                        matrix[i]):    # проверяем, является ли матрица квадратной
                     return None
         det = 0
         k = 1
         if len(matrix) > 2:
             for i in range(len(matrix)):
                 minor = list()
-                for j in range(len(matrix)-1):
-                    minor.append(matrix[j+1][::])
-                for j in range(len(matrix)-1):
+                for j in range(len(matrix) - 1):
+                    minor.append(matrix[j + 1][::])
+                for j in range(len(matrix) - 1):
                     del(minor[j][i])
-                det = det + matrix[0][i]*k*calculate_determinant(minor)  # рекурсивно вычисляем определитель
+                # рекурсивно вычисляем определитель
+                det = det + matrix[0][i] * k * calculate_determinant(minor)
                 k = -k
             return det
         if len(matrix) == 2:
-            det = matrix[0][0]*matrix[1][1] - matrix[1][0]*matrix[0][1]
+            det = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
             return det
         if len(matrix) == 1:
-            if type(matrix[0]) == list:
-                if len(matrix[0]) == 1:      # граничные условия для [[k]] и [k]
+            if isinstance(matrix[0], list):
+                if len(
+                        matrix[0]) == 1:      # граничные условия для [[k]] и [k]
                     return matrix[0][0]
-            if type(matrix[0]) == int:
+            if isinstance(matrix[0], int):
                 return matrix[0]
     except TypeError:
         return None
-
