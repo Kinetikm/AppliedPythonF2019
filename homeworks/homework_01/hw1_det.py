5dcf74072f5a8a88b135f8ab650b39f8875dfd15
Ramil.Chermanteev <ramil.chermanteev@x5.ru>
2019-09-20 19:34:19 +0300

    Добавлено решение

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,12 +3,24 @@
 
 
 def calculate_determinant(list_of_lists):
-    '''
+    """
     Метод, считающий детерминант входной матрицы,
     если это возможно, если невозможно, то возвращается
     None
     Гарантируется, что в матрице float
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    """
+    import numpy as np
+
+    if len(list_of_lists) != len(list_of_lists[0]):  # Матрица не квадратная
+        return None
+    else:
+        for i in range(len(list_of_lists) - 1):
+            for k in range(i + 1, len(list_of_lists)):
+                if list_of_lists[i][i] != 0:
+                    a = list_of_lists[k][i] / list_of_lists[i][i]
+                    for j in range(i, len(list_of_lists)):
+                        list_of_lists[k][j] -= a * list_of_lists[i][j]
+    print(list_of_lists)
+    return np.multiply.reduce(np.diagonal(list_of_lists))
