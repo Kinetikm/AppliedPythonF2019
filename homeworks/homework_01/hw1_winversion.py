c392a2734943655e4cdc2636de9b47e3edcf9607
Mikhail <zojloverr@gmail.com>
2019-09-21 00:46:28 +0300

    hdh

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -1,11 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
+def find_comma(cur_pos, input_lst):
+    i = cur_pos
+    while (i != len(input_lst)) and (input_lst[i] != ","):
+        i += 1
+    return i
 
 
 def word_inversion(input_lst):
-    '''
-    Метод инвертирующий порядок слов в строке inplace (без выделения доп памяти)
-    :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
-    :return: None Все изменения в input_lst проходят
-    '''
-    raise NotImplementedError
+    # reverse array
+    for i in range(len(input_lst) // 2):
+        input_lst[i], input_lst[-i - 1] = input_lst[-i - 1], input_lst[i]
+
+    # reverse words
+    next_comma_pos = -1
+    cur_comma_pos = -1
+    while next_comma_pos < (len(input_lst)):
+        cur_comma_pos = next_comma_pos
+        next_comma_pos = find_comma(cur_comma_pos + 1, input_lst)
+        for i in range((next_comma_pos - cur_comma_pos) // 2):
+            input_lst[cur_comma_pos + i + 1], input_lst[next_comma_pos - i - 1] = input_lst[next_comma_pos - i - 1], \
+                                                                                  input_lst[
+                                                                                      cur_comma_pos + i + 1]  # swap
+    return input_lst
