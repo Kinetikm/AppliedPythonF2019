9b624984d3584e40b5c34e5f3f10d963b5293f40
fatkullinalina <alina2469@gmail.com>
2019-09-20 13:21:45 +0300

    invertint3

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -9,9 +9,9 @@ def reverse(number):
     :param number: исходное число
     :return: инвертированное число
     '''
-b = 0
-while number > 0:
-    b = b * 10 + number % 10
-    number //= 10
-return b
+    b = 0
+    while number > 0:
+        b = b * 10 + number % 10
+        number //= 10
+    return b
     raise NotImplementedError
