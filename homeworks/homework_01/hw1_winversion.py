02c6a31c0b2dc0e12576e57ebbe678c8c801bfbe
denisimus97 <super-denis97@yandex.ru>
2019-09-20 23:21:51 +0300

    hw_1

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -2,10 +2,16 @@
 # coding: utf-8
 
 
-def word_inversion(input_lst):
+def word_inversion(input_list):
     '''
     Метод инвертирующий порядок слов в строке inplace (без выделения доп памяти)
-    :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
+    :param input_list: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-    raise NotImplementedError
+    c = 0
+    for i in range(len(input_list)):
+        if (c < len(input_list)) and (input_list[i] == " "):
+            input_list[c:i] = input_list[c:i][::-1]
+            c = i + 1
+    input_list[c:] = input_list[c:][::-1]
+    return input_list[::-1]
