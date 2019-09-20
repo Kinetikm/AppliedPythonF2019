356efd56fa0d169f72e52eaabafb8891972a76d7
Ramil.Chermanteev <ramil.chermanteev@x5.ru>
2019-09-20 18:52:54 +0300

    Добавлено решение

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -3,12 +3,21 @@
 
 
 def find_subarr(input_lst, num):
-    '''
+    """
     Метод, находящий подмассив, сумма чисел которого равна заданному числу
     O(n) по времени
     :param input_lst: массив
     :param num: искомое число
     :return: два индекса (начала и конца подмассива). Пустой tuple, если таких нет
     Пример: find_subarr([1, 2, 3, 4, 5, -1], 4) может вернуть (3, 3) или (4, 5)
-    '''
-    raise NotImplementedError
+    """
+
+    sols_dict = dict()
+    temp_sum = 0
+    for i, val in enumerate(input_lst):
+        temp_sum += val
+        if temp_sum - num in sols_dict:
+            return sols_dict[temp_sum - num] + 1, i
+        else:
+            sols_dict[temp_sum] = i
+    return ()
