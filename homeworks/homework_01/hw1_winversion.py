b649bfe414e1b17a9e5c0be49f7ed0870e54d005
EvgeniyMist <wertygeny@gmail.com>
2019-09-20 08:49:04 +0300

    winversion & subarr first try

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -3,9 +3,19 @@
 
 
 def word_inversion(input_lst):
-    '''
-    Метод инвертирующий порядок слов в строке inplace (без выделения доп памяти)
-    :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
-    :return: None Все изменения в input_lst проходят
-    '''
-    raise NotImplementedError
+    input_lst.append(' ')
+    word = ''
+    for i in range(len(input_lst)):
+        ch = input_lst.pop(0)
+        word += ch
+        if ch == ' ':
+            input_lst.append(word[:-1])
+            word = ''
+    input_lst.reverse()
+    for i in range(len(input_lst)):
+        word = input_lst.pop(0)
+        for ch in word:
+            input_lst.append(ch)
+        input_lst.append(' ')
+    input_lst.pop()
+    return input_lst
