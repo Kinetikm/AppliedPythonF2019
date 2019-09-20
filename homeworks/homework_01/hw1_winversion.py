7e750f9a1368c47db59cffa8a0ad6021c3488087
Ilya Utkin <ilyaaayou@gmail.com>
2019-09-20 23:29:06 +0300

    swap funciton

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -2,20 +2,21 @@
 # coding: utf-8
 
 
+def swap(array, i, j):
+    tmp = array[i]
+    array[i] = array[j]
+    array[j] = tmp
+
+
 def word_inversion(input_lst):
     n = len(input_lst)
-    a = input_lst
     l = 0
     r = n - 1
     # reverse array
     while l < r:
-
-        tmp = a[l]
-        a[l] = a[r]
-        a[r] = tmp
+        swap(input_lst, l, r)
         l += 1
         r -= 1
-
     # pointer to a word START
     s = 0
     # pointer to a word END
@@ -23,29 +24,22 @@ def word_inversion(input_lst):
 
     while e < n:
         if e == n - 1:
-
             while s < e:
-                tmp = a[s]
-                a[s] = a[e]
-                a[e] = tmp
+                swap(input_lst, s, e)
                 s += 1
                 e -= 1
-
             break
 
-        if a[e] == ' ':
-            space = e
+        if input_lst[e] == ' ':
+            space_index = e
             e -= 1
-
             while s < e:
-                tmp = a[s]
-                a[s] = a[e]
-                a[e] = tmp
+                swap(input_lst, s, e)
                 s += 1
                 e -= 1
 
-            e = space
-            s = space + 1
+            e = space_index
+            s = space_index + 1
 
         e += 1
     return input_lst
