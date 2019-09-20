6d10549110186cd083690c5716528a16864508eb
EvgeniyMist <wertygeny@gmail.com>
2019-09-20 08:11:57 +0300

    invertint update

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -6,11 +6,5 @@ def reverse(number):
     minus = 1 if number < 0 else 0
     num_list = list(str(abs(number)))
     num_list.reverse()
-    while num_list[0] == '0':
-        num_list.pop(0)
-        if not num_list:
-            return 0
-    result_str = ''
-    for sym in num_list:
-        result_str += sym
+    result_str = ''.join(num_list)
     return -int(result_str) if minus else int(result_str)
