77c2d471a9ba85595f66168575c38bd2b0f0669e
Mikhail <zojloverr@gmail.com>
2019-09-21 00:14:49 +0300

    hdh

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
+import copy
+import random
+
+
+def new_matrix(o_matr,column):
+    n_matr = copy.deepcopy(o_matr)
+    del n_matr[column:column + 1:1]
+    for i in range(len(n_matr)):
+        del n_matr[i][:1]
+    return n_matr
+
+
+def recrs(matr):
+    det=0
+    if len(matr) == 2:
+        return matr[0][0]*matr[1][1]-matr[1][0]*matr[0][1]
+    else:
+        for pos in range(len(matr)):
+            print(matr)
+            if pos % 2 == 0:
+                det += matr[pos][0]*recrs(new_matrix(matr,pos))
+            else:
+                det -= matr[pos][0]*recrs(new_matrix(matr,pos))
+        print(det)
+        return det
 
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    if len(list_of_lists) != len(list_of_lists[0]):
+        return None
+    else:
+        return recrs(list_of_lists)
