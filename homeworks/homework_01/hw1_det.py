3754907cbd1ba7f44c1216734a235c69b5c5e6ea
getkekked <gettopkekked@gmail.com>
2019-09-21 00:50:55 +0300

    Added det and palindrom

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 def calculate_determinant(list_of_lists):
     '''
     Метод, считающий детерминант входной матрицы,
@@ -11,4 +7,14 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    det = 0
+    if len(list_of_lists) != len(list_of_lists[0]):
+        return None
+    if len(list_of_lists) == 2:
+        return (list_of_lists[0][0] * list_of_lists[1][1] - list_of_lists[0][1] * list_of_lists[1][0])
+    for i in range (len(list_of_lists[0])):
+        a = []
+        for j in range (1,len(list_of_lists)):
+            a.append(list_of_lists[j][0:i] + list_of_lists[j][i+1:])
+        det += (-1) ** i * list_of_lists[0][i] * calculate_determinant(a)
+    return(det)
