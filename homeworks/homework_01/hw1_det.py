00bdb1d9949ce91525fc0e4ce9722de33896dba3
AlexKay28 <Kaygorodow@mail.ru>
2019-09-21 17:30:44 +0300

    fixed det

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,20 +2,24 @@
 # coding: utf-8
 
 def calculate_determinant(A):
-    import copy
-    M, N, Det = len(A), len(A[0]), 0
+    M, N, D = len(A), len(A[0]), 0
     if M != N: return None
     if N == 1: return A[0][0]
     for i in range(N):
-        Minor = copy.deepcopy(A)
+        Minor = []
+        for p in range(len(A)):
+            Minor.append([])
+            for k in range(len(A[0])):
+                Minor[p].append(A[p][k])
         del Minor[0]
         for j in range(1, len(A[0])):
             del Minor[j - 1][i]
         if i % 2 == 0:
-            Det += A[0][i] * calculate_determinant(Minor)
+            D += A[0][i] * calculate_determinant(Minor)
         else:
-            Det -= A[0][i] * calculate_determinant(Minor)
-    return Det
+            D -= A[0][i] * calculate_determinant(Minor)
+    return D
+
 
 
 
