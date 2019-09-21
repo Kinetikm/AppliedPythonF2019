f19b16eab25a94c27c136e7ffe01821d94a0041e
Sinitcyn Dmitriy <sinitcyn.dmitriy@mail.ru>
2019-09-21 14:57:20 +0300

    my first commit

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,24 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    res = 0
+    A = list_of_lists
+    if (len(A) != len(A[0])):
+        return None
+    if (len(A) == 2):
+        return A[0][0]*A[1][1] - A[1][0]*A[0][1]
+    if (len(A) == 1):
+        return A[0][0]
+
+    for i in range(len(A)): # иду по строке, i-номер столбца
+        res += (-1)**(2+i)* A[0][i] *calculate_determinant(minor(A, 0, i))
+    return res
+
+import copy
+def minor(A, i, j):
+    M = copy.deepcopy(A)
+    del M[i]      #удалил текущую строку
+    for i in range(len(A[0]) - 1):
+        del M[i][j]    # удалил текущий столбец
+    return M
+    
--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -9,4 +9,15 @@ def reverse(number):
     :param number: исходное число
     :return: инвертированное число
     '''
-    raise NotImplementedError
+    negative = False
+    if(number < 0):
+        negative = True
+    s = str(number)
+    s =  s.replace('-', '')
+    s = s[::-1]
+    number = int(s)
+    if negative: 
+        number = number*-1
+    return number
+    
+    
