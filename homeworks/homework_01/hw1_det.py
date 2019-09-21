15f319f332160d141400b3d8d8e08491ab4b80bc
kipdurron <ishunin97@gmail.com>
2019-09-22 01:53:06 +0300

    was added task of determinant

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,25 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    if not is_square_matrix_check(list_of_lists):
+        return None
+    elif len(list_of_lists) == 1:
+        return list_of_lists[0][0]
+    elif len(list_of_lists) == 2:
+        return list_of_lists[0][0] * list_of_lists[1][1] - list_of_lists[1][0] * list_of_lists[0][1]
+    else:
+        determinant = 0
+        for i in range(len(list_of_lists)):
+            sub_matrix = []
+            for j in range(len(list_of_lists)):
+                if i != j:
+                    sub_matrix.append(list_of_lists[j][1:])
+            minor = calculate_determinant(sub_matrix)
+            determinant += list_of_lists[i][0] * ((-1) ** (0 + i)) * minor
+        return determinant
+
+def is_square_matrix_check(matrix):
+    for i, val in enumerate(matrix):
+        if len(matrix) != len(matrix[i]):
+            return False
+    return True
