58f0abe9883cbcd5e9f829a0d6fe11c22b25406b
Konstantin Kuritsyn <kuritsyn.1998@inbox.ru>
2019-09-21 17:46:46 +0300

    delete hw1_palindrom

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,15 +11,4 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    det = 0
-    for i in range(len(list_of_lists)):
-        if len(list_of_lists) != len(list_of_lists[i]):
-            return None
-    if len(list_of_lists) == 2:
-        return (list_of_lists[0][0] * list_of_lists[1][1] - list_of_lists[0][1] * list_of_lists[1][0])
-    for i in range(len(list_of_lists)):
-        a = []
-        for j in range(1, len(list_of_lists)):
-            a.append(list_of_lists[j][0:i] + list_of_lists[j][i + 1:])
-        det += (-1) ** i * list_of_lists[0][i] * calculate_determinant(a)
-    return(det)
+    raise NotImplementedError
