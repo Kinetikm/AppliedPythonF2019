d1e194fa2e90c0333f1efa5caa50e4071fba1556
fatkullinalina <alina2469@gmail.com>
2019-09-21 08:38:39 +0300

    win

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -10,7 +10,7 @@ def word_inversion(input_lst):
     '''
     i = 0
     k = 0
-    while i != len(input_lst):
+    while i <= len(input_lst):
         if input_lst[i] is " ":
             input_lst[k:i:] = input_lst[k:i:][::-1]
             k = i + 1
