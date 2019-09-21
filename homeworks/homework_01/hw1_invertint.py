6c537dcfd9609a43d1875cccf3ce08baa6b0e349
ArtNeos <55402014+ArtNeos@users.noreply.github.com>
2019-09-21 18:02:51 +0300

    Add files via upload

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 def reverse(number):
-    '''
-    Метод, принимающий на вход int и
-    возвращающий инвертированный int
-    :param number: исходное число
-    :return: инвертированное число
-    '''
-    raise NotImplementedError
+    result = 0
+    sign = 1
+    if number < 0 :
+        sign = -1
+        number *= -1
+    while number > 0 :
+        result = result * 10 + number % 10
+        number = number // 10
+    result *= sign
+    return result
