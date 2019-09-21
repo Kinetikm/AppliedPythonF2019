0d7c23ac631f4bd0142ac2d8760cda14818cea63
Mikhail <zojloverr@gmail.com>
2019-09-21 16:46:41 +0300

    hdh

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -1,14 +1,16 @@
 def find_subarr(input_lst, num):
+    if len(input_lst) == 0:
+        return()
     cur_sum = 0
     sums = dict()
-    # fill set
+    # fill dict
     for i in range(len(input_lst)):
         cur_sum += input_lst[i]
         if cur_sum == num:
             return 0, i
-        sums[cur_sum] = i
+        if cur_sum - num in sums:
+            return sums[cur_sum - num] + 1, i
+        else:
+            sums[cur_sum] = i
 
-    for key in sums:
-        if key - num in sums:
-            return sums[key - num] + 1, sums[key]
     return()
