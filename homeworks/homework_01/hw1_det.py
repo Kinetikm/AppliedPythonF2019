36266127f8c5f0ccd200a94827da77fb1ba8aa01
MaximAbramenko <gotosleepmcht@gmail.com>
2019-09-21 17:51:32 +0300

    ddt

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,6 +2,27 @@
 # coding: utf-8
 
 
+def calculate_minor(num, a):
+    m = []
+    for i in range(1, len(a)):
+        row = []
+        if num == 0:
+            row = a[i][1:]
+        elif num == len(a) - 1:
+            row = a[i][:(len(a) - 1)]
+        else:
+            row = a[i][:num] + a[i][num + 1:]
+        m.append(row)
+    if len(m) == 1:
+        return m[0][0]
+    if len(m) == 2:
+        return m[0][0] * m[1][1] - m[0][1] * m[1][0]
+    minor = 0
+    for i in range(0, len(m)):
+        minor += (-1) ** i * m[0][i] * calculate_minor(i, m)
+    return minor
+
+
 def calculate_determinant(list_of_lists):
     '''
     Метод, считающий детерминант входной матрицы,
@@ -11,4 +32,17 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    n = len(list_of_lists[0])
+    m = len(list_of_lists)
+    for i in range(0, len(list_of_lists)):
+        if m != len(list_of_lists[i]):
+            return None
+    if n == 1:
+        return list_of_lists[0][0]
+    determinant = 0
+    k = 1
+    for i in range(n):
+        determinant += list_of_lists[0][i] * \
+                       calculate_minor(i, list_of_lists) * k
+        k *= -1
+    return determinant
