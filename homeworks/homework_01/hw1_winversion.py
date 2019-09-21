63f41d5d3d86fa51f53018e236b808886039e363
MaximAbramenko <gotosleepmcht@gmail.com>
2019-09-21 21:41:53 +0300

    last

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -11,8 +11,8 @@ def word_inversion(input_lst):
     input_lst = input_lst[::-1]
     k = 0
     for i in range(len(input_lst)):
-        if input_lst[i] == ' ':
-            input_lst[k:i] = input_lst[k:i:-1]
-            k = i+1
-    input_lst[k:] = input_lst[k::-1]
+        if input_lst[i] == " ":
+            input_lst[k:i] = input_lst[k:i][::-1]
+            k = i + 1
+    input_lst[k:] = input_lst[k:][::-1]
     return input_lst
