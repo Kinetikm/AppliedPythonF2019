4d40a7963641bf68c757853a2aa1f705e91dd6cd
volodindimitrii <dmvolodin@inbox.ru>
2019-09-21 21:07:45 +0300

    First homework, 5 tasks

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -9,4 +9,15 @@ def reverse(number):
     :param number: исходное число
     :return: инвертированное число
     '''
-    raise NotImplementedError
+    sign = 1
+    if number < 0:
+        sign = -1
+        number *= -1
+    str_num = str(number)
+    str_num = str_num[(len(str_num)-1)::-1]
+    new_number = sign * int(str_num)
+    return new_number
+
+
+
+
--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,4 +8,16 @@ def word_inversion(input_lst):
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-    raise NotImplementedError
+
+    input_lst = input_lst[len(input_lst)-1::-1]
+    j = 0
+    for i in range(len(input_lst)+1):
+        if i == len(input_lst) or input_lst[i] == " ":
+            if j == 0:
+                input_lst[j:i:1] = input_lst[i-1::-1]
+            else:
+                input_lst[j:i:1] = input_lst[i-1:j-1:-1]
+            j = i+1
+
+    return input_lst
+
