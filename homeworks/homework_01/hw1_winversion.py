8fc3b09c325cd3e1df2c440d076e60a99346762f
Sinitcyn Dmitriy <sinitcyn.dmitriy@mail.ru>
2019-09-21 17:08:49 +0300

    pep8 hvatit

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -11,7 +11,7 @@ def word_inversion(input_lst):
     input_lst = input_lst[::-1]  # сначала перевернём всё
     beg = 0
     for i in range(len(input_lst)):
-        if ( input_lst[i] == ' ' ):  # теперь каждое слово по отдельности
+        if (input_lst[i] == ' '):  # теперь каждое слово по отдельности
             input_lst[beg:i] = input_lst[beg:i][::-1]
             beg = i+1
     input_lst[beg:] = input_lst[beg:][::-1]  # и последнее слово
