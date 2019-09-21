e0017a6db00df455cd004385942f9fbf2944e38e
Mikhail <zojloverr@gmail.com>
2019-09-21 17:07:42 +0300

    hdh

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
+# comment
 def check_palindrom(input_string):
     for i in range(len(input_string)//2):
         if input_string[i] != input_string[-i - 1]:
