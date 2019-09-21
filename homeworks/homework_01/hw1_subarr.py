67027cb71193ac654732cf56cafa52f55f85c669
Ilia Koltsa <koltsa.ilya@mail.ru>
2019-09-21 16:28:15 +0300

    Изменил subarr

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -3,26 +3,13 @@
 
 
 def find_subarr(input_lst, num):
-    if len(input_lst) > 0:
-        dc = dict()
-        dc2 = dict()
-        if num == input_lst[0]:
-            return(0, 0)
+    dc = dict()
+    for i in range(len(input_lst)):
+        k = sum(input_lst[:i + 1])
+        if (k - num == 0):
+            return (0, i)
+        elif (k - num) in dc:
+            return(dc[k - num] + 1, i)
         else:
-            dc[input_lst[0]] = 0
-        for i in range(1, len(input_lst)):
-            if num == input_lst[i]:
-                return(i, i)
-            # проверяем, есть ли подходящая последовательность
-            elif (dc.get(num - input_lst[i]) is not None):
-                return (dc.get(num - input_lst[i]), i)
-            else:
-                # формируем все возможные последовательности
-                for j in list(dc.keys()):
-                    dc2[j + input_lst[i]] = dc[j]
-                # удаляем ненужные пары, не дающие последовательность
-                # со следующими элементами
-                dc = dc2
-                dc2 = dict()
-                dc[input_lst[i]] = i
+            dc[k] = i
     return ()
