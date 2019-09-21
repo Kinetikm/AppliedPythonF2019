da119c404c60239405e8e6ee1e0f97ea6d0bcde8
advatrix <trojan.advatrix@gmail.com>
2019-09-21 22:26:51 +0300

    msg

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -2,10 +2,35 @@
 # coding: utf-8
 
 
+def reverse_word(input_lst, begin, end):
+    tmp_b = begin
+    tmp_e = end
+    while tmp_b < tmp_e:
+        input_lst[tmp_b], input_lst[tmp_e] = input_lst[tmp_e], input_lst[tmp_b]
+        tmp_b += 1
+        tmp_e -= 1
+
 def word_inversion(input_lst):
     '''
     Метод инвертирующий порядок слов в строке inplace (без выделения доп памяти)
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-    raise NotImplementedError
+    begin = 0
+    try:
+        space = input_lst.index(' ')
+    except ValueError:
+        return
+    while space < len(input_lst):
+        end = space - 1
+        reverse_word(input_lst, begin, end)
+        begin = end + 2
+        try:
+            space = input_lst.index(' ', begin)
+        except ValueError:
+            reverse_word(input_lst, begin, len(input_lst)-1)
+            break
+    input_lst.reverse()
+
+
+   # raise NotImplementedError
