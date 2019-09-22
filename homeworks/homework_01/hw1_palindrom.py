9433a0f894890f7ef3e33fe46040fa0498d0ea8e
Daniil <tretyakov.daniil.edcuation@gmail.com>
2019-09-22 15:13:08 +0300

    small fix in palindrom.py

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -12,7 +12,7 @@ def check_palindrom(input_string):
     '''
 
     for i in range(len(input_string) // 2):
-        if input_string[i].lower() != input_string[len(input_string) - i - 1]:
+        if input_string[i].lower() != input_string[len(input_string) - i - 1].lower():
             return False
 
     return True
