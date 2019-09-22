c5640d073cce0a1a4e2b192061fa11eac0eb90df
Kirill <vk1r1ll@yandex.ru>
2019-09-22 20:19:07 +0300

    subaa

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -4,11 +4,12 @@ import copy
 
 
 def minor(M0, i, j):
-    M = copy.deepcopy(M0) #чтобы не менять основу
+    M = copy.deepcopy(M0)#чтобы не менять основу
     del M[i]
     for i in range(len(M0[0]) - 1):
-        del M[i][j] #убираем столбец
+        del M[i][j]#убираем столбец
     return M
+
 
 def calculate_determinant(list_of_lists):
     '''
@@ -22,18 +23,18 @@ def calculate_determinant(list_of_lists):
     if not list_of_lists:
         return None
 
-    len = len(list_of_lists[0])
+    r = len(list_of_lists[0])
 
-    if len(list_of_lists) != len:
+    if len(list_of_lists) != r:
         return None
-    if len == 1:
+    if r == 1:
         return list_of_lists[0][0]
 
     sign = 1
     det = 0
     # разложение по первой строке
-    for j in range(len):
+    for j in range(r):
         det += list_of_lists[0][j] * sign * calculate_determinant(minor(list_of_lists, 0, j))
         sign *= -1
 
-    return 
+    return det
