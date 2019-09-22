e400bd6d976b1532c7b79f5358cbd2bb36228afb
Vladislav <48061257+VladislavKuzevanov@users.noreply.github.com>
2019-09-22 22:02:40 +0300

    Update hw1_invertint.py

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,10 +3,18 @@
 
 
 def reverse(number):
-    '''
-    Метод, принимающий на вход int и
-    возвращающий инвертированный int
-    :param number: исходное число
-    :return: инвертированное число
-    '''
-    raise NotImplementedError
+    temp = 0
+    znak = 0
+    if number < 0:
+        znak = -1
+    else:
+        znak = 1
+    number = abs(number)
+    while True:
+        temp = (temp * 10) + (number % 10)
+        number = number // 10
+        if (number // 10) == 0 :
+            temp = (temp * 10) + (number % 10)
+            break
+    number =  znak * temp
+    return number
