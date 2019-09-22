64cacc366bfa36252ebe95d7a91d97909465376f
volkovalexeydm <volkovalexeydm@gmail.com>
2019-09-23 02:20:22 +0300

    qwerty

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -2,4 +2,10 @@ def reverse(number):
     d = number
     d = str(d)
     d = d[::-1]
-    return int(d)
+    if '-' in d:
+        d = d.rstrip('-')
+        d = '-'+d
+        return(int(d))
+    else:
+        return(int(d))
+
