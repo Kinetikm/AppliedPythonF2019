4f790a9189810389e9539a4f1a5d5e3f4017569f
supermartynov <55408414+supermartynov@users.noreply.github.com>
2019-09-23 01:08:22 +0300

    Update hw1_winversion.py

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 
-def word_inversion(input_lst): 
+def word_inversion(input_lst):
     input_lst = input_lst[::-1]
     z = 0
     length = len(input_lst)
@@ -10,17 +10,16 @@ def word_inversion(input_lst):
         if input_lst[i] == ' ':
             mark = i
             tmp = mark
-            for j in range(z,z +((mark - z + 1 ))//2):
-                input_lst[0],input_lst[tmp-1] = input_lst[tmp-1],input_lst[0]
+            for j in range(z, z + ((mark - z + 1))//2):
+                input_lst[0], input_lst[tmp-1] = input_lst[tmp-1], input_lst[0]
                 tmp -= 1
                 z += 1
             z = mark + 1
         if i == (length - 1):
             mark = i
             tmp = mark
-            for l in range(z,z +((mark - z + 1 ))//2):
+            for l in range(z, z + ((mark - z + 1))//2):
                 input_lst[z], input_lst[tmp] = input_lst[tmp], input_lst[z]
                 tmp -= 1
                 z += 1
     return input_lst
-
