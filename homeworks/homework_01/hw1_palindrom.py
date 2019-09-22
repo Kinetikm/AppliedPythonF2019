804f78842cbaa41cbc4f7a33158be9d3947defac
Vladislav <48061257+VladislavKuzevanov@users.noreply.github.com>
2019-09-23 00:02:47 +0300

    Update hw1_palindrom.py

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -3,6 +3,8 @@
 
 
 def check_palindrom(input_string):
+    if len(input_string) == 0:
+        return False
     for i in range(len//2):
         if input_string[i] != input_string[-1-i]:
             return False
