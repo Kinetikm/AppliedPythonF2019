2304e9ddbbc77d1ac3583dd21e4d9d177a255b30
Alexander Zimokos <alexander.zimokos@mail.ru>
2019-09-22 22:58:06 +0300

    add subarr

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 def find_subarr(input_lst, num):
-    '''
-    Метод, находящий подмассив, сумма чисел которого равна заданному числу
-    O(n) по времени
-    :param input_lst: массив
-    :param num: искомое число
-    :return: два индекса (начала и конца подмассива). Пустой tuple, если таких нет
-    Пример: find_subarr([1, 2, 3, 4, 5, -1], 4) может вернуть (3, 3) или (4, 5)
-    '''
-    raise NotImplementedError
+    dict = {}
+    sum = 0
+    for i, var in enumerate(input_lst):
+        sum += var
+        if sum == num:
+            return (0, i)
+        elif sum - num in dict:
+            return (dict[sum - num] + 1, i)
+        dict[sum] = i
+    return ()
