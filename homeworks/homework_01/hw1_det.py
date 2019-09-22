39ecb27e792608c849ba1bb7ced35878737c6ad7
Ivan.Protopopov <Afchi31@gmail.com>
2019-09-23 01:54:28 +0300

    all_done

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,6 +3,30 @@
 
 
 def calculate_determinant(list_of_lists):
+    import copy
+
+    def minor(list_of_lists, i, j):
+        M = copy.deepcopy(list_of_lists)
+        del M[i]
+        for i in range(len(list_of_lists[0]) - 1):
+            del M[i][j]
+        return M
+
+    def det(list_of_lists):
+        m = len(list_of_lists)
+        n = len(list_of_lists[0])
+        if m != n:
+            return None
+        if n == 1:
+            return list_of_lists[0][0]
+        signum = 1
+        determinant = 0
+        for j in range(n):
+            determinant += list_of_lists[0][j] * signum * det(minor(list_of_lists, 0, j))
+            signum *= -1
+        return determinant
+
+    print(det(list_of_lists))
     '''
     Метод, считающий детерминант входной матрицы,
     если это возможно, если невозможно, то возвращается
