cd249f8af69f047ca84ecf23b841fe090af9149a
Emtseva Sofya <sofochkaemtseva@gmail.com>
2019-09-22 18:28:16 +0300

    Задание 1

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -4,9 +4,6 @@
 # In[78]:
 
 
-#!/usr/bin/env python
-# coding: utf-8
-
 def word_inversion(input_lst):
     input_lst.append(" ")
     input_lst.reverse()
@@ -14,28 +11,24 @@ def word_inversion(input_lst):
 
     i = 1
     gap = 0
-    
+
     while i < len(input_lst):
-        if input_lst[i]==" ":
-            word_len=i-gap-1
+        if input_lst[i] == " ":
+            word_len = i - gap - 1
             gap = i
-            j = i-word_len
-            k=1
-            while j < i-word_len + word_len / 2:
+            j = i - word_len
+            k = 1
+            while j < i - word_len + word_len / 2:
                 temp = input_lst[j]
                 input_lst[j] = input_lst[i - k]
                 input_lst[i - k] = temp
                 k = k + 1
                 j = j + 1
-        i = i+1
+        i = i + 1
     del input_lst[0]
     del input_lst[-1]
-    #print(input_lst)
+    # print(input_lst)
     return input_lst
 
 
 # In[ ]:
-
-
-
-
