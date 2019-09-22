783131eeb265e754081a19b246a2702a5cf8f338
Kirill <vk1r1ll@yandex.ru>
2019-09-22 15:19:33 +0300

    subaa

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -13,10 +13,3 @@ def find_subarr(input_lst, num):
             return (i-1, i)
         t = input_lst[i]
     return ()
-a = []
-b = int(input())
-print(find_subarr(a,b))
-
-a = [1,-2,0,-1,1]
-b = int(input())
-print(find_subarr(a,b))
