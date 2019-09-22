917901b1a081a97befb52f87be1d1f7fb08f4ff3
Артём <artem@Artems-MacBook-Pro.local>
2019-09-22 16:22:38 +0300

    Some fix

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -5,6 +5,11 @@
 def reverse(number):
     res = 0
     residue = 0
+    flag = False
+
+    if number < 0:
+        number *= -1
+        flag = True
 
     while number != 0:
         residue = number % 10
@@ -15,5 +20,8 @@ def reverse(number):
             res *= 10
         else:
             break
+        
+        if flag:
+            res *= -1
 
-    return res
+return res
--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -2,8 +2,12 @@
 # coding: utf-8
 
 
-def find_subarr(input_lst, num):
+def find_subarr(input_list, num):
+    if not input_list:
+        return None
+
     res_contain = []
+
     for index in range(1, len(input_list)):
         if input_list[index - 1] == num:
             res_contain.append([index - 1, index - 1])
@@ -12,4 +16,4 @@ def find_subarr(input_lst, num):
 
     res = (res_contain[0][0], res_contain[0][1])
 
-    return res
+return res
