f1243f3d41e4eff9da09b10116827df2618ef21f
Vladislav <48061257+VladislavKuzevanov@users.noreply.github.com>
2019-09-23 00:00:02 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -4,23 +4,25 @@
 
 import copy
 
-def minor( A, i, j ):
+
+def minor(A, i, j):
     temp = copy.deepcopy(A)
-    del temp [i]
-    for i in range( len( A[0] ) - 1 ):
-        del temp [i][j]
+    del temp[i]
+    for i in range(len(A[0]) - 1):
+        del temp[i][j]
     return temp
 
+
 def calculate_determinant(list_of_lists):
-    lines = len( list_of_lists )
-    columns = len( list_of_lists[0] )
+    lines = len(list_of_lists)
+    columns = len(list_of_lists[0])
     if lines != columns:
         return None
     if columns == 1:
         return list_of_lists[0][0]
     sign = 1
     det = 0
-    for j in range( columns ):
-        det += list_of_lists[0][j] * sign * calculate_determinant( minor( list_of_lists, 0, j ) )
+    for j in range(columns):
+        det += list_of_lists[0][j] * sign * calculate_determinant(minor(list_of_lists, 0, j))
         signum *= -1
     return det
