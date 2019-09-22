dedfeb7535663a9434cbce4ff9ee5cca9bd16dba
Kirill <vk1r1ll@yandex.ru>
2019-09-22 18:24:06 +0300

    subaa

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -3,7 +3,11 @@
 
 
 def check_palindrom(input_string):
-    if (input_string == input_string[::-1]):
-        return True
-    else:
-        return False
+    '''
+    Метод проверяющий строку на то, является ли
+    она палиндромом.
+    :param input_string: строка
+    :return: True, если строка являестя палиндромом
+    False иначе
+    '''
+    return input_string == input_string[::-1]
--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,4 +8,17 @@ def word_inversion(input_lst):
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-    raise NotImplementedError
+    input_lst.reverse()
+    start = 0
+    len_lst = len(input_lst)
+
+    for end in range(len_lst):
+        if input_lst[end] == ' ':
+            if start == 0:
+                input_lst[:end] = input_lst[end - 1::-1]
+            else:
+                input_lst[start-1:end] = input_lst[end:start-1:-1]
+
+            start = end + 1
+    input_lst[start:] = input_lst[end:start - 1:-1]
+    return None
