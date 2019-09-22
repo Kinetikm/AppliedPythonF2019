a22c8976832551f8c9b537e3a3ae079ef1cc1999
Артём <artem@Artems-MacBook-Pro.local>
2019-09-22 21:49:03 +0300

    Run all

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
-import random
 
 
 def find_subarr(input_list, num):
-    if not any(input_list):
-        return None
+    res_contain = {}
+    curr_sum = 0
 
-    res_contain = []
+    for index in range(len(input_list)):
+        curr_sum = curr_sum + input_list[index]
 
-    for index in range(1, len(input_list)):
-        if input_list[index - 1] == num:
-            res_contain.append((index - 1, index - 1))
-        if input_list[index - 1] + input_list[index] == num:
-            res_contain.append((index - 1, index))
+        if curr_sum == num:
+            return 0, index
+        if (curr_sum - num) in res_contain:
+            return res_contain[curr_sum - num] + 1, index
 
-    if res_contain:
-        return res_contain[random.randrange(0, len(res_contain), 1)]
-    else:
-        return None
+        res_contain[curr_sum] = index
