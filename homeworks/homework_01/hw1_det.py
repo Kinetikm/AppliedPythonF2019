17e9f06f552344127851dee87b635eadd6a476bb
Emtseva Sofya <sofochkaemtseva@gmail.com>
2019-09-22 18:14:31 +0300

    HW_01. Задание 5. Попытка 1

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,14 +1,51 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[51]:
+
+
+# определитель матрицы
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    matrix = list_of_lists
+    is_matrix = True
+    det = 0
+
+    i = 0
+    while i < len(matrix):
+        if len(matrix) != len(matrix[i]):
+            is_matrix = False
+            #print("no matrix")
+            return None
+        i += 1
+    if is_matrix:
+        if len(matrix) > 2:
+            i = 0
+            k = 1
+            while i < len(matrix):
+                minor = list()
+                j = 0
+                while j < (len(matrix) - 1):
+                    minor.append(matrix[j + 1][::])
+                    j += 1
+                j = 0
+                while j < (len(matrix) - 1):
+                    del(minor[j][i])
+                    j += 1
+                det += matrix[0][i] * k * calculate_determinant(minor)
+                k = k * (-1)
+                i += 1
+            return det
+
+        if len(matrix) == 2:
+            det = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+            return det
+
+        if len(matrix) == 1:
+            det = matrix[0]
+            if isinstance(det, list):
+                det = matrix[0][0]
+            return det
+
+
+# In[ ]:
