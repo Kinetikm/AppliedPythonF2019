29d27a24f6782b39162236fa0603d8047d2e4b04
Vladislav <48061257+VladislavKuzevanov@users.noreply.github.com>
2019-09-22 22:04:30 +0300

    Update hw1_winversion.py

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -2,10 +2,21 @@
 # coding: utf-8
 
 
-def word_inversion(input_lst):
-    '''
-    Метод инвертирующий порядок слов в строке inplace (без выделения доп памяти)
-    :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
-    :return: None Все изменения в input_lst проходят
-    '''
-    raise NotImplementedError
+def word_inversion (input_lst):
+    word_len = 0
+    index = 0
+    probelnum = 0
+    probelcount = input_lst.count(' ')
+    for i in range(len(input_lst)):
+        while (input_lst[len(input_lst) - 1] != " ")  and (i != (len(input_lst))) and (probelnum != probelcount):
+            word_len += 1
+            input_lst.insert(index, input_lst.pop())
+            i += 1
+        if probelnum != probelcount:
+            i += 1
+            input_lst.insert(word_len, input_lst.pop())
+            probelnum += 1
+            word_len += 1
+            index = word_len
+
+    return input_lst
