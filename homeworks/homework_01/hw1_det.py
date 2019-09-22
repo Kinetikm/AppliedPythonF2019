20bb33b14a2eec776cf18a83d6b2dfd05d2d735b
artemg1297 <55649576+artemg1297@users.noreply.github.com>
2019-09-22 23:38:43 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,13 +2,28 @@
 # coding: utf-8
 
 
-def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+def calculate_determinant(b):
+    if len(b) != len(b[0]):
+        return
+    if len(b) == 2:
+        de = b[0][0] * b[1][1] - b[0][1] * b[1][0]
+        return de
+    else:
+        sum = 0
+        for j in range(len(b)):
+            c = [[0 for i in range(len(b)-1)] for j in range(len(b)-1)]
+            i = 0
+            for l in range(i+1, len(b)):
+                for k in range(0, j):
+                    c[l-i-1][k] = b[l][k]
+                if j+1 == len(b):
+                    for m in range(j+1, len(b)):
+                        c[l-i-1][m-j+1] = b[l][m]
+                elif j == 0:
+                    for m in range(j+1, len(b)):
+                        c[l-i-1][m-j-1] = b[l][m]
+                else:
+                    for m in range(j+1, len(b)):
+                        c[l-i-1][m-1] = b[l][m]
+            sum += b[i][j]*((-1)**j)*calculate_determinant(c)
+        return sum
