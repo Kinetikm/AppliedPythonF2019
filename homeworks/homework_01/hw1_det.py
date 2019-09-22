19eac14fb3c69a91e0f5056a6897e675b9cdfa7c
Aigul <sirazovaaigul@gmail.com>
2019-09-23 01:10:04 +0300

    det changed

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
+import copy
+
+def minor(M, i, j):
+    copy_M = copy.deepcopy(M)
+    del copy_M[i]
+    for i in range(len(M[0]) - 1):
+        del copy_M[i][j]
+    return copy_M
 
 
 def calculate_determinant(list_of_lists):
@@ -11,4 +19,17 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+
+    n = len(list_of_lists[0])
+
+    if len(list_of_lists) != n:
+        return None
+    if n == 1:
+        return list_of_lists[0][0]
+
+    k = 1
+    det = 0
+    for j in range(n):
+        det += list_of_lists[0][j] * k * calculate_determinant(minor(list_of_lists, 0, j))
+        k *= -1
+    return det
