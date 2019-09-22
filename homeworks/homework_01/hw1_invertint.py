2448ff0296c4f1f3f26bc8cb2720e77ab99249bb
Vasoskirko <vasoskirko@gmail.com>
2019-09-22 23:45:25 +0300

    second commit

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -1,20 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 def reverse(number):
-    '''
-    Метод, принимающий на вход int и
-    возвращающий инвертированный int
-    :param number: исходное число
-    :return: инвертированное число
-    '''
     number_2 = 0
     while number != 0:
         number_2 = number_2 * 10
         number_2 += number % 10
         number = number // 10
-        
     return number_2
-
-l = reverse(123456789)
