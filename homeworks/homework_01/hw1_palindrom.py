513cbcaae80162627f864ba812f6ff82d40bd85e
vlad1m1r_17 <zamyatovskiy.vi@gmail.com>
2019-09-22 10:03:51 +0300

    hw1_palindrom done

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -10,4 +10,9 @@ def check_palindrom(input_string):
     :return: True, если строка являестя палиндромом
     False иначе
     '''
-    raise NotImplementedError
+    ret_val = True
+    for i in range(len(input_string) // 2):
+        if input_string[i] != input_string[len(input_string) - 1 - i]:
+            ret_val = ret_val & False
+            break
+    return ret_val
