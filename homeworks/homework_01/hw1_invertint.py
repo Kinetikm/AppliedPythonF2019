b1454ae456b1712569d8e9cdee9ed0f6619d2d63
ciplatov <siplatov-kg@mail.ru>
2019-09-23 00:47:47 +0300

    inv_1

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,6 +3,19 @@
 
 
 def reverse(number):
+    n = len(number)
+    if n % 2 == 0:
+        print(number[:-int(n/2)-1:-1])
+        if number.startswith(number[:-int(n/2)-1:-1]):
+            print(True)
+        else:
+            print(False)
+    else:
+        print(number[:-int(n//2)-1:-1])
+        if number.startswith(s[:-int(n//2)-1:-1]):
+            print(True)
+        else:
+            print(False)
     '''
     Метод, принимающий на вход int и
     возвращающий инвертированный int
