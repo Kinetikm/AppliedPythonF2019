ba3816da3658a4099f865bd86da28bdade5c1ca5
artemg1297 <55649576+artemg1297@users.noreply.github.com>
2019-09-23 00:52:15 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,10 +3,11 @@
 
 
 def calculate_determinant(b):
+    k=0
     if len(b) <= 1:
-        return 0
+        k=1
     if len(b) != len(b[0]):
-        return 0
+        k=1
     if len(b) == 2:
         return b[0][0] * b[1][1] - b[0][1] * b[1][0]
     else:
@@ -19,12 +20,13 @@ def calculate_determinant(b):
                     c[l - i - 1][k] = b[l][k]
                 if j + 1 == len(b):
                     for m in range(j + 1, len(b)):
-                        c[l - i -1 ][m - j + 1] = b[l][m]
+                        c[l - i - 1][m - j + 1] = b[l][m]
                 elif j == 0:
                     for m in range(j+1, len(b)):
                         c[l - i - 1][m - j - 1] = b[l][m]
                 else:
                     for m in range(j+1, len(b)):
                         c[l - i - 1][m - 1] = b[l][m]
-            sum += b[i][j]*(( - 1)**j)*calculate_determinant(c)
-        return sum
+            sum += b[i][j]*((- 1)**j)*calculate_determinant(c)
+        if k=0: return
+        else: return sum
