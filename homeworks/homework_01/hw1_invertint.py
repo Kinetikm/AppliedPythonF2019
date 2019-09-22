fb02375f8698bc4c97e20d6c81b9c8d120296cff
volkovalexeydm <volkovalexeydm@gmail.com>
2019-09-23 00:50:31 +0300

    zxcv

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -1,12 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 def reverse(number):
-    '''
-    Метод, принимающий на вход int и
-    возвращающий инвертированный int
-    :param number: исходное число
-    :return: инвертированное число
-    '''
-    raise NotImplementedError
+    d = number
+    d = str(d)
+    d = d[::-1]
+    return int(d)
