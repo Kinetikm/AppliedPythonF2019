6672b3766285dbb0622973ed01ed0a63776a429e
Diroman <roman.dubatov@gmail.com>
2019-09-22 18:37:09 +0300

    homework1

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,22 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    def calc_matrix(mass, ind):
+        result = []
+        for line in range(1, len(mass)):
+            result.append(mass[line][:ind] + mass[line][ind+1:])
+        return result
+
+    if len(list_of_lists) == 0 or len(list_of_lists) != len(list_of_lists[0]):
+        return None
+    if len(list_of_lists) == 1:
+        return list_of_lists[0]
+
+    if len(list_of_lists[0]) == 2:
+        return list_of_lists[0][0] * list_of_lists[1][1] - list_of_lists[0][1] * list_of_lists[1][0]
+
+    res = 0
+    for j in range(len(list_of_lists[0])):
+        i = 1 if j%2 == 0 else -1
+        res += i * list_of_lists[0][j] * calculate_determinant(calc_matrix(list_of_lists, j))
+    return res
--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,4 +8,12 @@ def word_inversion(input_lst):
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-    raise NotImplementedError
+    ptr = 0
+    for i, n in enumerate(input_lst):
+        if n == ' ':
+            input_lst = input_lst[:ptr] + input_lst[ptr:i][::-1] + input_lst[i:]
+            ptr = i+1
+        if i == len(input_lst)-1:
+            input_lst = input_lst[:ptr] + input_lst[ptr:][::-1]
+
+    return input_lst[::-1]
