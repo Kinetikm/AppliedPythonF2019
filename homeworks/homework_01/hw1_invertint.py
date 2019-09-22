95d7833a0bd40b0011f437d7e9ba7a527b42cab3
Emtseva Sofya <sofochkaemtseva@gmail.com>
2019-09-22 18:28:55 +0300

    Задание 3

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[ ]:
 
-
-#!/usr/bin/env python
-# coding: utf-8
 
 def reverse(number):
-    minus=0
-    if number<0:
+    minus = 0
+    if number < 0:
         minus = True
     num_inv = 0
     str_num = str(abs(number))
     i = 0
     while i < len(str_num):
-        num_inv = num_inv + int(str_num[-i-1]) * (10 ** (len(str_num)-i-1))
+        num_inv = num_inv + int(str_num[-i - 1]) * \
+            (10 ** (len(str_num) - i - 1))
         i += 1
     if minus:
         num_inv = -num_inv
@@ -23,7 +21,3 @@ def reverse(number):
 
 
 # In[ ]:
-
-
-
-
