684122de7bcdf52fdf3b6b72d1b91a72ceecf965
volodindimitrii <dmvolodin@inbox.ru>
2019-09-22 13:24:11 +0300

    The foutht commit

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -2,37 +2,22 @@
 # coding: utf-8
 
 
-def invert_dict(source_dict):
+def reverse(number):
     '''
-    Функция которая разворачивает словарь, т.е.
-    каждому значению ставит в соответствие ключ.
-    :param source_dict: dict
-    :return: new_dict: dict
+    Метод, принимающий на вход int и
+    возвращающий инвертированный int
+    :param number: исходное число
+    :return: инвертированное число
     '''
+    sign = 1
+    if number < 0:
+        sign = -1
+        number *= -1
+    str_num = str(number)
+    str_num = str_num[(len(str_num)-1)::-1]
+    new_number = sign * int(str_num)
+    return new_number
 
-    def add_to_dict(d, key, value):
-        if isinstance(key, (list, tuple, set)):
-            for i in key:
-                add_to_dict(d, i, value)
-        else:
-            if '__hash__' in dir(key) and key.__hash__ is not None:
-                if key in d:
-                    if isinstance(d[key], list):
-                        d[key].append(value)
-                    else:
-                        d[key] = [d[key], value]
-                else:
-                    d[key] = value
-            else:
-                print('error!')
 
-    new_dict = {}
-    for key in source_dict:
-        if isinstance(source_dict[key], (dict)):
-            continue
-        if isinstance(source_dict[key], (list, tuple, set)):
-            for i in source_dict[key]:
-                add_to_dict(new_dict, i, key)
-        else:
-            add_to_dict(new_dict, source_dict[key], key)
-    return new_dict
+
+
