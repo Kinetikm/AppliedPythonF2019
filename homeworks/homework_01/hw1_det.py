e88313848500555f6c8d6425fdbfa52624131b14
ttimur2 <55452138+ttimur2@users.noreply.github.com>
2019-09-22 20:32:12 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -10,20 +10,20 @@ def calculate_determinant(list_of_lists):
         if len(list_of_lists[i]) != n:
             return None
     for i in range(n):
-        s = i
-        for k in range(i + 1, n):
-            if abs(list_of_lists[k][i] > abs(list_of_lists[s][i])):
-                s = k
-        if i != s:
-            list_of_lists[i], list_of_lists[j] = list_of_lists[j], list_of_lists[i]
-            res *= -1
-        if list_of_lists[i][i] < eps:
+        m = i
+        for j in range(i + 1, n):
+            if abs(list_of_lists[j][i]) > abs(list_of_lists[m][i]):
+                m = j
+        if abs(list_of_lists[m][i]) < eps:
             return 0
+        list_of_lists[i], list_of_lists[m] = list_of_lists[m], list_of_lists[i]
+        if i != m:
+            res *= -1
         res *= list_of_lists[i][i]
         for j in range(i + 1, n):
             list_of_lists[i][j] /= list_of_lists[i][i]
         for j in range(n):
             if j != i and abs(list_of_lists[j][i]) > eps:
                 for k in range(i + 1, n):
-                    list_of_lists[j][k] -= list_of_lists[i][k] * b
+                    list_of_lists[j][k] -= list_of_lists[i][k] * list_of_lists[j][i]
     return res
