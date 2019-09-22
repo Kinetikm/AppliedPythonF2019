9444af5ace3f20434098846691b5d9acb45905ca
Konstantin Kuritsyn <kuritsyn.1998@inbox.ru>
2019-09-22 22:24:18 +0300

    Add det

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,12 +3,22 @@
 
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    det = 1
+    for i in range(len(list_of_lists)):
+        if len(list_of_lists[i]) != len(list_of_lists):
+            return None
+    for i in range(len(list_of_lists)):
+        imax = i
+        cs = 1
+        for j in range(i+1, len(list_of_lists)):
+            if abs(list_of_lists[j][i]) > abs(list_of_lists[imax][i]):
+                imax = j
+        list_of_lists[i], list_of_lists[imax] = list_of_lists[imax], list_of_lists[i]
+        if imax != i:
+            cs = cs * (-1)
+        for j in range(i+1, len(list_of_lists)):
+            coef = -list_of_lists[j][i] / list_of_lists[i][i]
+            for q in range(len(list_of_lists) - 1, i - 1, -1):
+                list_of_lists[j][q] += coef * list_of_lists[i][q]
+        det *= cs * list_of_lists[i][i]
+    return det
