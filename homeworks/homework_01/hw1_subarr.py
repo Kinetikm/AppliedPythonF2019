ed9bf6e22edaa175cc26ffe63280b7bc1ca1b9ff
artemg1297 <55649576+artemg1297@users.noreply.github.com>
2019-09-22 20:28:44 +0300

    Update hw1_subarr.py

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 def find_subarr(a, b):
-    p=tuple()
+    p = tuple()
     for i in range(len(a)):
         for j in range(len(a)):
             if sum(a[i:j + 1]) == b:
