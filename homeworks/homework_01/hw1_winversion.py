53fb9450f4b04024ae2bbace9c7bbd4f828a19d0
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2019-09-22 14:36:17 +0300

    homework_01 winversion is done

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -11,10 +11,28 @@ def word_inversion(input_lst):
 
     input_lst.reverse()
 
+    # print("".join(input_lst))
+
     word_start = 0
     for (i, elem) in enumerate(input_lst):
-        if elem == ' ':
-            input_lst[word_start:i] = input_lst[word_start:i:-1]
+        is_last_iteration = i == len(input_lst) - 1
+        if elem == ' ' or is_last_iteration:
+            if is_last_iteration:
+                i += 1
+            # print("sswitching")
+            # print(input_lst[word_start:i])
+            # print("to")
+            # switch = input_lst[i-1:(word_start-1 if word_start-1 > 0 else None):-1]
+            # print(switch)
+
+            for j in range(word_start, int((word_start+i)/2)):
+                tmp = input_lst[j]
+                rev_idx = i - (j-word_start) - 1
+                input_lst[j] = input_lst[rev_idx]
+                input_lst[rev_idx] = tmp
+
+            # print("switched\n", input_lst[word_start:i])
+
             word_start = i + 1
 
     return input_lst
