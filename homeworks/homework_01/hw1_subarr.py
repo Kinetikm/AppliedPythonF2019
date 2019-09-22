bcf7f226a0955574df7615010d465facffdd142b
artemg1297 <55649576+artemg1297@users.noreply.github.com>
2019-09-22 20:20:31 +0300

    Update hw1_subarr.py

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -5,6 +5,5 @@ def find_subarr(a, b):
     for i in range(len(a)):
         for j in range(len(a)):
             if sum(a[i:j]) == b:
-                print(a[i:j])
                 return i, j - 1
     return p
