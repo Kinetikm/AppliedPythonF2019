732f0b563bce5cf5bf33483b6c56d5bc1299f0bd
Maximenko Daniil <engmaxidan@gmail.com>
2019-09-22 23:11:53 +0400

    hw1_subarr

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -11,4 +11,15 @@ def find_subarr(input_lst, num):
     :return: два индекса (начала и конца подмассива). Пустой tuple, если таких нет
     Пример: find_subarr([1, 2, 3, 4, 5, -1], 4) может вернуть (3, 3) или (4, 5)
     '''
-    raise NotImplementedError
+    s = 0
+    sols_dict = dict()
+    t = tuple()
+    for i, val in enumerate(input_lst):
+        s += val
+        if s == num:
+            return (0, i)
+        if s - num in sols_dict:
+            t = (sols_dict[s - num] + 1, i)
+            return t
+        sols_dict[s] = i
+    return t
