a47e00ef913eda0c2d3c918e19f0c39059f19be7
artemg1297 <55649576+artemg1297@users.noreply.github.com>
2019-09-22 17:13:57 +0300

    “Сообщение”

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,10 +3,4 @@
 
 
 def reverse(number):
-    '''
-    Метод, принимающий на вход int и
-    возвращающий инвертированный int
-    :param number: исходное число
-    :return: инвертированное число
-    '''
     raise NotImplementedError
