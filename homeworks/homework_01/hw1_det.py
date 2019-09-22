1f8fd1dbd95a434648b3519a2f6679472f3c0057
ttimur2 <hunter220900@gmail.com>
2019-09-22 22:30:19 +0300

    push homework_01 1-5

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,12 +3,27 @@
 
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    res = 1
+    eps = 1e-15
+    n = len(list_of_lists)
+    for i in range(n):
+        if len(list_of_lists[i]) != n:
+            return None
+    for i in range(n):
+        m = i
+        for j in range(i + 1, n):
+            if abs(list_of_lists[j][i]) > abs(list_of_lists[m][i]):
+                m = j
+        if abs(list_of_lists[m][i]) < eps:
+            return 0
+        list_of_lists[i], list_of_lists[m] = list_of_lists[m], list_of_lists[i]
+        if i != m:
+            res *= -1
+        res *= list_of_lists[i][i]
+        for j in range(i + 1, n):
+            list_of_lists[i][j] /= list_of_lists[i][i]
+        for j in range(n):
+            if j != i and abs(list_of_lists[j][i]) > eps:
+                for k in range(i + 1, n):
+                    list_of_lists[j][k] -= list_of_lists[i][k] * list_of_lists[j][i]
+    return res
--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -1,11 +1,16 @@
+
 #!/usr/bin/env python
 # coding: utf-8
 
 
 def word_inversion(input_lst):
-    '''
-    Метод инвертирующий порядок слов в строке inplace (без выделения доп памяти)
-    :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
-    :return: None Все изменения в input_lst проходят
-    '''
-    raise NotImplementedError
+    start = 0
+    for i in range(len(input_lst)):
+        a = input_lst.pop()
+        if (a == " "):
+            start = i
+            input_lst.insert(start, " ")
+            start += 1
+        else:
+            input_lst.insert(start, a)
+    return input_lst
