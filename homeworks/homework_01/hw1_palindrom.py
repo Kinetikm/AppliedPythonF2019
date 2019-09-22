1c1e3fd7a58d60ca9e05c4acd035fdc89e438a67
Kirill <vk1r1ll@yandex.ru>
2019-09-22 16:56:26 +0300

    subaa

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -3,14 +3,15 @@
 
 
 def check_palindrom(input_string):
-	'''
+	
+    if (input_string == input_string[::-1]):
+        return True
+    else:
+        return False
+    '''
     Метод проверяющий строку на то, является ли
     она палиндромом.
     :param input_string: строка
     :return: True, если строка являестя палиндромом
     False иначе
-    '''
-    if (input_string == input_string[::-1]):
-        return True
-    else:
-        return False
+    '''    
