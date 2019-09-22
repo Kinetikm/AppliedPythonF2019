23bfe9857414cce3032c947cd524fc3bcf1a7fe1
ProfffUndo <55656519+ProfffUndo@users.noreply.github.com>
2019-09-22 18:32:28 +0300

    Update hw1_palindrom.py

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -10,4 +10,8 @@ def check_palindrom(input_string):
     :return: True, если строка являестя палиндромом
     False иначе
     '''
+    if (input_string==input_string[::-1]):
+        return True
+    else:
+        return False
     raise NotImplementedError
