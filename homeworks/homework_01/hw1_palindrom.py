f5a7b311edd1421897a74820d8fd1e770ff01fb7
Emtseva Sofya <sofochkaemtseva@gmail.com>
2019-09-22 13:49:06 +0300

    HW_01. Задание 4. Попытка 1

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[8]:
+
 
 def check_palindrom(input_string):
-    '''
-    Метод проверяющий строку на то, является ли
-    она палиндромом.
-    :param input_string: строка
-    :return: True, если строка являестя палиндромом
-    False иначе
-    '''
-    raise NotImplementedError
+    i = 0
+    while i < len(input_string) / 2:
+        if input_string[i] == input_string[-i - 1]:
+            return True
+        else:
+            return False
+        i += 1
+
+
+# In[ ]:
