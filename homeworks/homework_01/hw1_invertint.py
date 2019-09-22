1543de270c9f5fd5e72f53a2a3fde4a455d745c2
cash57 <al.shafiev.sam@gmail.com>
2019-09-22 18:38:25 +0300

    int 1

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -9,4 +9,7 @@ def reverse(number):
     :param number: исходное число
     :return: инвертированное число
     '''
-    raise NotImplementedError
+    s = str(number)
+    invert_number = int(s[::-1])
+    return invert_number
+    #raise NotImplementedError
