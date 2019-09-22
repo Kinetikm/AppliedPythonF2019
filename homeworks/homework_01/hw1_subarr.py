f9944f254b223740eec236c1ca93d3eea061a950
LoskutovPD <48155308+LoskutovPD@users.noreply.github.com>
2019-09-22 20:35:30 +0300

    Update subarr

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -10,6 +10,8 @@ def find_subarr(input_lst, num):
         sum += el
         if sum == num:
             return 0, i
+        if el == num:
+            return i, i
         if sum - num in dct:
             return dct[sum-num] + 1, i
         dct[sum] = i
