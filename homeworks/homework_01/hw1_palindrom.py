b8c5429b2d5818dd1f8771615283bd2daed4318e
Yakovlev Egor <egorka.yakovlev.00@mail.ru>
2019-09-22 16:45:49 +0300

    palindrom_new

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -10,13 +10,12 @@ def check_palindrom(input_string):
     :return: True, если строка являестя палиндромом
     False иначе
     '''
-    raise NotImplementedError
     i = 0
     flag = True
     while i < len(a)//2:
-    if a[i] != a[len(a)-1-i]:
-        flag = False
-        break
-    else:
-        i = i + 1
+        if a[i] != a[len(a)-1-i]:
+            flag = False
+            break
+        else:
+            i = i + 1
     return flag
