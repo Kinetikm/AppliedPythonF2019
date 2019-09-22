724b449e9093e383cd91b65f002fdb67b835a988
artemg1297 <55649576+artemg1297@users.noreply.github.com>
2019-09-23 00:48:19 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -4,9 +4,9 @@
 
 def calculate_determinant(b):
     if len(b) <= 1:
-        return
+        return 0
     if len(b) != len(b[0]):
-        return
+        return 0
     if len(b) == 2:
         return b[0][0] * b[1][1] - b[0][1] * b[1][0]
     else:
