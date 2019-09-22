f1eefafe9eb1b2eedbec0d1b36fb58b1988f57e1
Vladislav <48061257+VladislavKuzevanov@users.noreply.github.com>
2019-09-23 00:00:54 +0300

    Update hw1_invertint.py

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -13,8 +13,8 @@ def reverse(number):
     while True:
         temp = (temp * 10) + (number % 10)
         number = number // 10
-        if (number // 10) == 0 :
+        if (number // 10) == 0:
             temp = (temp * 10) + (number % 10)
             break
-    number =  znak * temp
+    number = znak * temp
     return number
