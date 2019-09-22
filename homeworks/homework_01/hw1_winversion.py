6926850a99f7e1d2d2fe6f301a77a82b25c45428
Artem Kugushev <artemkugushev@inbox.ru>
2019-09-22 23:23:14 +0300

    Change 23:23

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,18 +8,18 @@ def word_inversion(input_lst):
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-input_lst.append(' ')
-k = 0
-for i in range(len(input_lst)):
-    if input_lst[i] == ' ':
-        for s in range(i, k, -1):
-            input_lst[s] = input_lst[k]
-            for l in range(k, s):
-                input_lst[l] = input_lst[l+1]
-        for n in range(k, i):
-            input_lst[n] = input_lst[len(input_lst+1]
-        input_lst[i] = ' '
-        k = i+1
-del input_lst[len(input_lst)-1]
-input_lst.reverse()
-print(input_lst)
+    input_lst.append(' ')
+    k = 0
+    for i in range(len(input_lst)):
+        if input_lst[i] == ' ':
+            for s in range(i, k, -1):
+                input_lst[s] = input_lst[k]
+                for l in range(k, s):
+                    input_lst[l] = input_lst[l+1]
+            for n in range(k, i):
+                input_lst[n] = input_lst[len(input_lst+1]
+            input_lst[i] = ' '
+            k = i+1
+    del input_lst[len(input_lst)-1]
+    input_lst.reverse()
+    print(input_lst)
