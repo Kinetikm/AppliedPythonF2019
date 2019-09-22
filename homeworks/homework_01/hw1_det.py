9ba9a3bdd49c232dca304ca7445c0b2fb476a267
Vasoskirko <vasoskirko@gmail.com>
2019-09-23 02:06:52 +0300

    4 commit

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,14 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
+import copy
+
+
+def minor(list_of_lists, i, j):
+    new_list = copy.deepcopy(list_of_lists)
+    for k in range(len(list_of_lists)):
+        del new_list[k][j]
+    del new_list[i]
+    return new_list
 
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    for i in range(len(list_of_lists)):
+        if len(list_of_lists) != len(list_of_lists[i]):
+            return None
+    if len(list_of_lists) == 1:
+        return list_of_lists[0][0]
+    det = 0
+    for j in range(len(list_of_lists)):
+        det += ((-1)**j * list_of_lists[0][j] *
+                calculate_determinant(minor(list_of_lists, 0, j)))
+    return det
--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 def find_subarr(input_lst, num):
-    '''
-    Метод, находящий подмассив, сумма чисел которого равна заданному числу
-    O(n) по времени
-    :param input_lst: массив
-    :param num: искомое число
-    :return: два индекса (начала и конца подмассива). Пустой tuple, если таких нет
-    Пример: find_subarr([1, 2, 3, 4, 5, -1], 4) может вернуть (3, 3) или (4, 5)
-    '''
-    raise NotImplementedError
+    dict = {}
+    summ = 0
+    for i in range(len(input_lst)):
+        summ += input_lst[i]
+        if (summ - num) in dict:
+            return (dict[summ - num] + 1, i)
+        if summ == num:
+            return (0, i)
+        dict[summ] = i
+    return ()
