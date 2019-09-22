6603fa610ffd379a934a2660a24e7c5d3d5341ab
supermartynov <55408414+supermartynov@users.noreply.github.com>
2019-09-23 01:17:27 +0300

    Update hw1_invertint.py

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -2,12 +2,11 @@
 # coding: utf-8
 
 
-def reverse(number): 
-    if number >= 0 :
+def reverse(number):
+    if number >= 0:
         stringnumber = str(number)
         number1 = int(stringnumber[::-1])
     else:
         stringnumber = str(abs(number))
         number1 = (int(stringnumber[::-1])) * -1
     return number1
-
