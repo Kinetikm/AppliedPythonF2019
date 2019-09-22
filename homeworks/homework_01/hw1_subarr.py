9dc09a7c1c2bfebb46b8216671bc7fd1f40add68
denisimus97 <super-denis97@yandex.ru>
2019-09-22 23:34:45 +0300

    +2задачи

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
 def find_subarr(input_lst, num):
     '''
     Метод, находящий подмассив, сумма чисел которого равна заданному числу
@@ -11,4 +9,13 @@ def find_subarr(input_lst, num):
     :return: два индекса (начала и конца подмассива). Пустой tuple, если таких нет
     Пример: find_subarr([1, 2, 3, 4, 5, -1], 4) может вернуть (3, 3) или (4, 5)
     '''
-    raise NotImplementedError
+    indeces = {}
+    sum = 0
+    for i in range(len(input_lst)):
+        sum += input_lst[i]
+        if sum == num:
+            return 0, i
+        if sum - num in indeces:
+            return [sum - num] + 1, i
+        indeces[sum] = i
+    return ()
