940ac28854eb0e7c04d2dbea85673aa55da7ee18
mnpks <fuglu@mail.ru>
2019-09-22 23:51:04 +0300

    subbaray improved4

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -11,8 +11,8 @@ def find_subarr(input_lst, num):
         if sum == num:
             return (start, end)
         if end < l - 1:
-            sum += input_lst[end]
             end += 1
+            sum += input_lst[end]
         elif end == l - 1:
             start += 1
             sum = input_lst[start]
