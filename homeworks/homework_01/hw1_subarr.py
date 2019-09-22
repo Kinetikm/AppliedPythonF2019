b3f3ac63786ea4c0215b7e44149ab8e4dd460799
Kirill <vk1r1ll@yandex.ru>
2019-09-22 14:59:38 +0300

    subar

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -3,10 +3,13 @@
 
 
 def find_subarr(input_lst, num):
+	if not input_lst:
+		return ()
     t = input_lst[0]
-    for i in range(1,len(input_lst)):
+    for i in range(1, len(input_lst)):
         if (num == input_lst[i]):
-            return (i,i)
+            return (i, i)
         elif (num == t + input_lst[i]):
-            return (i-1,i)
+            return (i-1, i)
         t = input_lst[i]
+    return ()
