4d743cd49c3ba314970ab31f8668c68ec7e66426
ProfffUndo <55656519+ProfffUndo@users.noreply.github.com>
2019-09-22 19:45:20 +0300

    Create hw1_invertint.py

---
+++ homeworks/homework_01/hw1_invertint.py
@@ -0,0 +1,17 @@
+def reverse(number):
+    '''
+    Метод, принимающий на вход int и
+    возвращающий инвертированный int
+    :param number: исходное число
+    :return: инвертированное число
+    '''
+    sign = 1      
+    s = str(number)
+    if s[0] == '-':
+        sign = -1
+        s = s[-1::]
+    s = s[::-1]
+    while (s[0] == "0"): 
+        s=s[-1::]
+    return int(s)*sign
+    raise NotImplementedError
