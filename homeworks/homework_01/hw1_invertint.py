b4ac35568765c8c7326f5fa8cbd30a7ec7b3d4f2
Emtseva Sofya <sofochkaemtseva@gmail.com>
2019-09-22 11:58:02 +0300

    HW_01. Задание 2. Попытка 1

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[14]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
 
 def reverse(number):
-    '''
-    Метод, принимающий на вход int и
-    возвращающий инвертированный int
-    :param number: исходное число
-    :return: инвертированное число
-    '''
-    raise NotImplementedError
+    minus=0
+    if number<0:
+        minus = True
+    num_inv = 0
+    str_num = str(abs(number))
+    i = 0
+    while i < len(str_num):
+        num_inv = num_inv + int(str_num[-i-1]) * (10 ** (len(str_num)-i-1))
+        i += 1
+    if minus:
+        num_inv = -num_inv
+    return num_inv
+
+
+# In[ ]:
+
+
+
+
