7489fa105ee30b563799b8a37db808187696828b
ttimur2 <55452138+ttimur2@users.noreply.github.com>
2019-09-22 20:03:54 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -10,18 +10,20 @@ def calculate_determinant(list_of_lists):
         if list_of_lists[i] != n:
             return None
     for i in range(n):
-        j = i
+        s = i
         for k in range(i + 1, n):
-            if abs(list_of_lists[k][i] > abs(list_of_lists[j][i])):
-                j = k
-        if i != j:
+            if abs(list_of_lists[k][i] > abs(list_of_lists[s][i])):
+                s = k
+        if i != s:
             list_of_lists[i], list_of_lists[j] = list_of_lists[j], list_of_lists[i]
             res *= -1
         if list_of_lists[i][i] < eps:
             return 0
         res *= list_of_lists[i][i]
         for j in range(i + 1, n):
-            b = list_of_lists[j][i] / list_of_lists[i][i]
-            for k in range(i + 1, n):
-                list_of_lists[j][k] -= list_of_lists[i][k] * b
+            list_of_lists[i][j] /= list_of_lists[i][i]
+        for j in range(n)
+            if j != i and abs(list_of_lists[j][i]) > eps
+                for k in range(i + 1, n):
+                    list_of_lists[j][k] -= list_of_lists[i][k] * b
     return res
