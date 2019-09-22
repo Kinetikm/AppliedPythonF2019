f9e8f9df211506f82df9c6c1bd04040bbfba76a0
advatrix <trojan.advatrix@gmail.com>
2019-09-22 14:25:04 +0300

    done

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from copy import deepcopy as copy
+
 
 def calculate_determinant(list_of_lists):
     '''
@@ -11,4 +13,24 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    n = len(list_of_lists)
+    if n == 0 or n != len(list_of_lists[0]):
+        return None
+    if n == 1:
+        return list_of_lists[0][0]
+    if n == 2:
+        return list_of_lists[0][0] * list_of_lists[1][1] - list_of_lists[0][1] * list_of_lists[1][0]
+    mx = copy(list_of_lists)
+    for i in range(n):
+        for j in range(n - 1, i, -1):
+            if mx[0][0]:
+                k = mx[j][i] / mx[i][i]
+            else:
+                k = 0
+            tmp = [k * a for a in mx[i]]
+            # mx[j] = [b - a for a, b in (tmp, mx[j])]
+            mx[j] = [mx[j][a] - tmp[a] for a in range(n)]
+    d = 1
+    for i in range(n):
+        d *= mx[i][i]
+    return d
