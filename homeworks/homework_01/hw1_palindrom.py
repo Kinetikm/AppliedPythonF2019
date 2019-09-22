47b2c2962c27db514c67a306327b23b6b54ffc65
ciplatov <siplatov-kg@mail.ru>
2019-09-23 00:21:29 +0300

    pol

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -10,8 +10,8 @@ def check_palindrom(input_string):
     :return: True, если строка являестя палиндромом
     False иначе
     '''
-    if input_string==input_string[::-1]:
-	    return True
+    if input_string == input_string[::-1]:
+        return True
     else:
-	    return False
+        return False
     raise NotImplementedError
