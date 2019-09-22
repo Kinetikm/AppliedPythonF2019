583a860f1d9a757ccb5ba863ec3de22213085b7b
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-09-23 00:01:35 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,4 +1,4 @@
-def calculate_determinant(list_of_lists,m):
+def calculate_determinant(list_of_lists, m):
     def GetMatr(mas, p, i, j, m):
         di = 0
         for ki in range(m - 1):
@@ -19,24 +19,15 @@ def calculate_determinant(list_of_lists,m):
     if (m < 1):
         print('Определитель вычислить невозможно!')
     if (m == 1):
-        d = list_of_lists[0][0];
-        return (d);
+        d = list_of_lists[0][0]
+        return (d)
     if (m == 2):
-        d = (list_of_lists[0][0] * list_of_lists[1][1]) - (list_of_lists[1][0] * list_of_lists[0][1]);
+        d = (list_of_lists[0][0] * list_of_lists[1][1]) - (list_of_lists[1][0] * list_of_lists[0][1])
         return (d)
     if (m > 2):
         for i in range(m):
             GetMatr(list_of_lists, p, i, 0, m)
-            #PrintMatr(p);
-            d = d + k * list_of_lists[i][0] * calculate_determinant(p, n);
-            k = -k;
+            d = d + k * list_of_lists[i][0] * calculate_determinant(p, n)
+            k = -k
     return (d)
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
     raise NotImplementedError
