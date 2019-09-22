232c2081d516738ba27632a3ee64efa229eb4e6f
Kirill <vk1r1ll@yandex.ru>
2019-09-22 15:04:51 +0300

    suba

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -3,8 +3,8 @@
 
 
 def find_subarr(input_lst, num):
-	if not input_lst:
-		return ()
+    if not input_lst:
+        return ()
     t = input_lst[0]
     for i in range(1, len(input_lst)):
         if (num == input_lst[i]):
