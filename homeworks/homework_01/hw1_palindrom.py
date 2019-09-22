6c25cd95ec98703d0045e428bffb1cbc5899b834
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-09-22 23:58:07 +0300

    Update hw1_palindrom.py

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -4,20 +4,7 @@
 
 def check_palindrom(input_string):
     l = len(input_string)
-
-
     for i in range(l // 2):
         if input_string[i] != input_string[-1 - i]:
-            #print("It's not palindrome")
             return False
-
-    #print("It's palindrome")
-    '''
-    Метод проверяющий строку на то, является ли
-    она палиндромом.
-    :param input_string: строка
-    :return: True, если строка являестя палиндромом
-    False иначе
-    '''
     return True
-    raise NotImplementedError
