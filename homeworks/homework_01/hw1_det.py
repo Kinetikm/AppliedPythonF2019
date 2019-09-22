4967d0dc87ba8f3becd0dd0e311e69ad738da9be
Nickolai427 <nick.daryin2015@yandex.ru>
2019-09-22 17:07:48 +0600

    correction of hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,17 +11,27 @@ def minor(lol, k, n):
     return b
 
 
+def check(list_of_lists):
+    if list_of_lists == []:
+        return False
+    lenth = len(list_of_lists)
+    for i in range(len(list_of_lists)):
+        if len(list_of_lists[i]) != lenth:
+            return False
+    return True
+
+
+def rec_func(lol):
+    if len(lol) == 2:
+        return lol[0][0] * lol[1][1] - lol[0][1] * lol[1][0]
+    s = 0
+    for i in range(len(lol)):
+        s += lol[0][i]*calculate_determinant(minor(lol, 0, i)) * (-1)**i
+    return s
+
+
 def calculate_determinant(list_of_lists):
-    if len(list_of_lists) == len(list_of_lists[0]):
-        prm = True
+    if check(list_of_lists):
+        return rec_func(list_of_lists)
     else:
-        prm = False
-    if prm:
-        lol = copy.deepcopy(list_of_lists)
-        if len(lol) == 2:
-            return lol[0][0] * lol[1][1] - lol[0][1] * lol[1][0]
-        s = 0
-        for i in range(len(lol)):
-            s += lol[0][i]*calculate_determinant(minor(lol, 0, i)) * (-1)**i
-        return s
-    return None
+        return None
