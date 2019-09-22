3a336b13b7e6c6c1156f54ab2d3c268c4d4a9fd5
danilsmirn <45015971+danilsmirn@users.noreply.github.com>
2019-09-22 17:37:21 +0300

    Update hw1_palindrom.py

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -3,11 +3,4 @@
 
 
 def check_palindrom(input_string):
-    '''
-    Метод проверяющий строку на то, является ли
-    она палиндромом.
-    :param input_string: строка
-    :return: True, если строка являестя палиндромом
-    False иначе
-    '''
-    raise NotImplementedError
+    return input_string == input_string[::-1]
