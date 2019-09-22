822521ade1983b15a98f5e6a986a560859e2179d
Ivan.Protopopov <Afchi31@gmail.com>
2019-09-23 02:24:33 +0300

    all_done

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -12,6 +12,7 @@ def calculate_determinant(list_of_lists):
             del M[i][j]
         return M
 
+
     def det(list_of_lists):
         m = len(list_of_lists)
         n = len(list_of_lists[0])
