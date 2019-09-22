943b79dc520e0160e4e7b6ed25a1ad66cfacf92b
Maximenko Daniil <engmaxidan@gmail.com>
2019-09-22 18:30:33 +0400

    palindrom changes

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -10,7 +10,7 @@ def check_palindrom(input_string):
     :return: True, если строка являестя палиндромом
     False иначе
     '''
-    def palindrome(input_string):
+
     l = len(input_string)
     for i in range(l//2):
         if input_string[i] != input_string[l - i - 1]:
