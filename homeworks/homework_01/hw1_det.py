0b8e9ae897ccb674c90b2c8901fdf9f7a3c0e2d5
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-09-22 22:47:14 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,8 +1,36 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-def calculate_determinant(list_of_lists):
+def calculate_determinant(list_of_lists,m):
+    def GetMatr(mas, p, i, j, m):
+        di = 0
+        for ki in range(m - 1):
+            if (ki == i):
+                di = 1
+            dj = 0
+            for kj in range(m - 1):
+                if (kj == j):
+                    dj = 1
+                p[ki][kj] = mas[ki + di][kj + dj]
+    p = [0] * m
+    for i in range(m):
+        p[i] = [0] * m
+    j = 0
+    d = 0
+    k = 1
+    n = m - 1
+    if (m < 1):
+        print('Определитель вычислить невозможно!')
+    if (m == 1):
+        d = list_of_lists[0][0];
+        return (d);
+    if (m == 2):
+        d = (list_of_lists[0][0] * list_of_lists[1][1]) - (list_of_lists[1][0] * list_of_lists[0][1]);
+        return (d)
+    if (m > 2):
+        for i in range(m):
+            GetMatr(list_of_lists, p, i, 0, m)
+            #PrintMatr(p);
+            d = d + k * list_of_lists[i][0] * calculate_determinant(p, n);
+            k = -k;
+    return (d)
     '''
     Метод, считающий детерминант входной матрицы,
     если это возможно, если невозможно, то возвращается
