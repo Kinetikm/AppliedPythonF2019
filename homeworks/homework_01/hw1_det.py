e829033dfd15a85f8c4bbefb46cfff68295fe389
Misha945 <mix.zaharov@gmail.com>
2019-09-22 22:57:50 +0300

    Det, first

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,13 +2,33 @@
 # coding: utf-8
 
 
+def decrease(list_of_lists, i, j):
+    L = []
+    n = len(list_of_lists)
+    ik = 0
+    for k in range(n):
+        if k == i:
+            ik -= 1
+            continue
+        L.append([])
+        for q in range(n):
+            if q == j:
+                continue
+            L[k+ik].append(list_of_lists[k][q])
+    return L
+
+
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    if list_of_lists == []:
+        return None
+    for i in range(len(list_of_lists)):
+        if len(list_of_lists) != len(list_of_lists[i]):
+            return None
+    if len(list_of_lists) == 1:
+        return list_of_lists[0][0]
+    else:
+        summ = 0
+        for i in range(len(list_of_lists)):
+            summ += ((-1)**(i)*list_of_lists[i][0] *
+                     calculate_determinant(decrease(list_of_lists, i, 0)))
+    return summ
