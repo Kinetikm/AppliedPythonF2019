0b779f6bb07c149a3c062cb4fc429c93d1817951
volkovalexeydm <volkovalexeydm@gmail.com>
2019-09-23 04:03:39 +0300

    popil6

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -1,24 +1,11 @@
-def word_inversion(input_list):
-    list = tuple(input_list)
-    k = len(list)
-    i = 0
-    j = 0
-    z = -1
-    b = 0
-    for i in range(k):
-        z += 1
-        if (list[i] == ' ' or i == (k - 1)):
-            if (i == (k-1)):
-                z += 1
-            for j in range(z):
-                if (i == (k-1)):
-                    i += 1
-                input_list[k - i + j] = list[b]
-                b += 1
-            if (i == k):
-                i = i - 1
-            if (list[i] == ' '):
-                input_list[k - 1 - i] = list[b]
-                b += 1
-            z = -1
-    return None
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def word_inversion(input_lst):
+    '''
+    Метод инвертирующий порядок слов в строке inplace (без выделения доп памяти)
+    :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
+    :return: None Все изменения в input_lst проходят
+    '''
+    raise NotImplementedError
