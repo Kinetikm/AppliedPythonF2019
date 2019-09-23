cec32db05bb6093ef56fafa3f965f9a0fd0d7a62
cash57 <al.shafiev.sam@gmail.com>
2019-09-23 06:55:30 +0300

    det 4

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -12,11 +12,11 @@ def calculate_determinant(s):
     :return: значение определителя или None
     '''
     if s == [[]]:
-        return
+        return 0
     res = 1
     N = len(s)
     for i in range(N):
-        j = max(range(i, N), key=lambda k: abs(s[k][i]))
+        j = max(range(i, N+1), key=lambda k: abs(s[k][i]))
         if len(s) != len(s[i]):
             return
         if i != j:
@@ -25,12 +25,7 @@ def calculate_determinant(s):
         if s[i][i] == 0:
             return 0
         res *= s[i][i]
-        for j in range(i + 1, N):
+        for j in range(i + 1, N+1):
             b = s[j][i] / s[i][i]
             s[j] = [s[j][k] - b * s[i][k] for k in range(N)]
     return res
-
-
-
-s = [[]]
-print(calculate_determinant(s))
