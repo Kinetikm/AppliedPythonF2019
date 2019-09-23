3a5662e41aa01c8cdc4c81b26dd2b78414f34bc3
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-09-23 09:50:24 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,39 +3,27 @@
 
 
 def calculate_determinant(list_of_lists):
-    m = len(list_of_lists)
-    
-    def determinant(list_of_lists1, m):
-        def GetMatr(mas, p, i, j, m):
-            di = 0
-            for ki in range(m - 1):
-                if (ki == i):
-                    di = 1
-                dj = 0
-                for kj in range(m - 1):
-                    if (kj == j):
-                        dj = 1
-                    p[ki][kj] = mas[ki + di][kj + dj]
-        p = [0] * m
-        for i in range(m):
-            p[i] = [0] * m
-        j = 0
-        d = 0
-        k = 1
-        n = m - 1
-        if (m < 1):
-            print('Определитель вычислить невозможно!')
-        if (m == 1):
-            d = list_of_lists1[0]
-            return (d)
-        if (m == 2):
-            d = (list_of_lists1[0][0] * list_of_lists1[1][1]) - (list_of_lists1[1][0] * list_of_lists1[0][1])
-            return (d)
-        if (m > 2):
-            for i in range(m):
-                GetMatr(list_of_lists, p, i, 0, m)
-                d = d + k * list_of_lists1[i][0] * determinant(p, n)
-                k = -k
-        return (d)
-        raise NotImplementedError
-    return determinant(list_of_lists, m)
+    res = 1
+    eps = 1e-15
+    n = len(list_of_lists)
+    for i in range(n):
+        if len(list_of_lists[i]) != n:
+            return None
+    for i in range(n):
+        m = i
+        for j in range(i + 1, n):
+            if abs(list_of_lists[j][i]) > abs(list_of_lists[m][i]):
+                m = j
+        if abs(list_of_lists[m][i]) < eps:
+            return 0
+        list_of_lists[i], list_of_lists[m] = list_of_lists[m], list_of_lists[i]
+        if i != m:
+            res *= -1
+        res *= list_of_lists[i][i]
+        for j in range(i + 1, n):
+            list_of_lists[i][j] /= list_of_lists[i][i]
+        for j in range(n):
+            if j != i and abs(list_of_lists[j][i]) > eps:
+                for k in range(i + 1, n):
+                    list_of_lists[j][k] -= list_of_lists[i][k] * list_of_lists[j][i]
+    return res
