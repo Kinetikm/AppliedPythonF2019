fa3e8b25b260ad236d4190a816edd6e74856cac0
sergeiklochkov <43850350+sergeiklochkov@users.noreply.github.com>
2019-09-23 07:05:56 +0300

    Update hw1_winversion.py

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,4 +8,25 @@ def word_inversion(input_lst):
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
+    
+    k = 0
+    n = 0
+    l = 0
+    for i in range(0, input_lst.count(" "), 1):
+        k = k + n
+        input_lst.insert(len(input_lst) - k, ' ')
+        k = k + l
+        n = 0
+        while input_lst[0] != ' ':
+            input_lst.insert(len(input_lst) - k, input_lst[0])
+            input_lst.pop(0)
+            #print(input_lst)
+            n = n + 1
+        input_lst.pop(0)
+        n = n
+        l = 1
+        
+    
+    return input_lst
+    
     raise NotImplementedError
