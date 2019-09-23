aad26a8a343c37c43b911f87ba27c7db56486fb9
Mariia Makarova <43655254+Mashmallow5@users.noreply.github.com>
2019-09-23 16:48:57 +0300

    subarray

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -3,12 +3,13 @@
 
 
 def find_subarr(input_lst, num):
-    '''
-    Метод, находящий подмассив, сумма чисел которого равна заданному числу
-    O(n) по времени
-    :param input_lst: массив
-    :param num: искомое число
-    :return: два индекса (начала и конца подмассива). Пустой tuple, если таких нет
-    Пример: find_subarr([1, 2, 3, 4, 5, -1], 4) может вернуть (3, 3) или (4, 5)
-    '''
+    _dict = {}   
+    curr_sum = 0 
+    for i in range(0,len(input_lst)):
+        curr_sum = curr_sum + arr[i]
+        if curr_sum == num:  
+            return 0, i
+        if (curr_sum - num) in _dict:  
+            return _dict[curr_sum - num] + 1, i
+        _dict[curr_sum] = i   
     raise NotImplementedError
