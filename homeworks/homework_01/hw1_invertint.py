92c33f0da353e8c2df98dca3e2253d09ef0dd6cb
Mariia Makarova <43655254+Mashmallow5@users.noreply.github.com>
2019-09-23 16:47:20 +0300

    invert int

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -8,5 +8,16 @@ def reverse(number):
     возвращающий инвертированный int
     :param number: исходное число
     :return: инвертированное число
-    '''
+	'''
+    sign = 1
+    if number == 0:
+    	return 0
+    num = str(number)
+    if num[0] == '-':
+        sign = -1
+        num = num[1:]
+    num = num[::-1]
+    while num[0] == '0':
+        num = num[1:]
+    return sign * int(num)
     raise NotImplementedError
