588e6721d42e20a0506a3e61174cd13c65561656
cash57 <al.shafiev.sam@gmail.com>
2019-09-23 03:54:42 +0300

    word_inversion 2

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -2,10 +2,27 @@
 # coding: utf-8
 
 
-def word_inversion(input_lst):
+def word_inversion(s):
     '''
     Метод инвертирующий порядок слов в строке inplace (без выделения доп памяти)
-    :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
-    :return: None Все изменения в input_lst проходят
+    :param s: строка-массив букв (['H', 'i']). Пробелы одиночные
+    :return: None Все изменения в s проходят
     '''
-    raise NotImplementedError
+    N = len(s)-1
+    i: int
+    i, omega = N, 0
+    print(s)
+    s.reverse()
+    print(s)
+    while i > -1:
+        if s[i] == ' ':
+            print(i)
+            s[i+1:i + omega+1] = s[i + omega:i:-1]
+            omega = 0
+            i -= 1
+        elif i == 0:
+            s[i:omega+1] = s[omega-N-1:-N-2:-1]
+            return s
+        else:
+            omega += 1
+            i -= 1
