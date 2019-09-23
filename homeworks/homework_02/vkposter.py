27eb1deffdba25195e287161f62b473f3b28e219
Zarina Sayfullina <z.sayfullina@corp.mail.ru>
2019-09-23 14:26:28 +0300

    added hw02 and lecture02

---
+++ homeworks/homework_02/vkposter.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from homeworks.homework_02.heap import MaxHeap
+from homeworks.homework_02.fastmerger import FastSortedListMerger
+
+
+class VKPoster:
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def user_posted_post(self, user_id: int, post_id: int):
+        '''
+        Метод который вызывается когда пользователь user_id
+        выложил пост post_id.
+        :param user_id: id пользователя. Число.
+        :param post_id: id поста. Число.
+        :return: ничего
+        '''
+        pass
+
+    def user_read_post(self, user_id: int, post_id: int):
+        '''
+        Метод который вызывается когда пользователь user_id
+        прочитал пост post_id.
+        :param user_id: id пользователя. Число.
+        :param post_id: id поста. Число.
+        :return: ничего
+        '''
+        pass
+
+    def user_follow_for(self, follower_user_id: int, followee_user_id: int):
+        '''
+        Метод который вызывается когда пользователь follower_user_id
+        подписался на пользователя followee_user_id.
+        :param follower_user_id: id пользователя. Число.
+        :param followee_user_id: id пользователя. Число.
+        :return: ничего
+        '''
+        pass
+
+    def get_recent_posts(self, user_id: int, k: int)-> list:
+        '''
+        Метод который вызывается когда пользователь user_id
+        запрашивает k свежих постов людей на которых он подписан.
+        :param user_id: id пользователя. Число.
+        :param k: Сколько самых свежих постов необходимо вывести. Число.
+        :return: Список из post_id размером К из свежих постов в
+        ленте пользователя. list
+        '''
+        pass
+
+    def get_most_popular_posts(self, k: int) -> list:
+        '''
+        Метод который возвращает список k самых популярных постов за все время,
+        остортированных по свежести.
+        :param k: Сколько самых свежих популярных постов
+        необходимо вывести. Число.
+        :return: Список из post_id размером К из популярных постов. list
+        '''
+        pass
