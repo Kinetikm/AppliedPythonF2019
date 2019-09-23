4b8259247dfb9a3883dcb8ae457e4301f91ed5c8
volkovalexeydm <volkovalexeydm@gmail.com>
2019-09-23 09:46:09 +0300

    popil22

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,14 +1,36 @@
-#!/usr/bin/env python
-# coding: utf-8
+def delete(c, a):
+    g = []
+    i = 1
+    while(i < len(c)):
+        p = []
+        for j in range(len(c)):
+            if (j < a or j > a and j < len(c)):
+                p.append(c[i][j])
+            else:
+                continue
+        g.append(p)
+        i += 1
+    return g
+
+
+def norm(a):
+    for i in a:
+        if len(i) != len(a):
+            return False
+    return True
 
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+    width = len(list_of_lists)
+    if norm(list_of_lists):
+        if (len(list_of_lists) == 2):
+            return list_of_lists[0][0] * list_of_lists[1][1] - list_of_lists[0][1] * list_of_lists[1][0]
+        elif (len(list_of_lists) == 1):
+            return list_of_lists[0][0]
+        else:
+            sum = 0
+            for i in range(len(list_of_lists)):
+                sum += (-1)**(i) * list_of_lists[0][i] * calculate_determinant(delete(list_of_lists, i))
+            return sum
+    else:
+        return None
