9c6898be08fcbaa1012273ec4e92c6f1e6361f7b
BatoNam <bnamzhilov@gmail.com>
2019-09-23 03:07:59 +0300

    6

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 
-def calculate_determinant(list_of_lists):
+def calculate_determinant(m):
     '''
     Метод, считающий детерминант входной матрицы,
     если это возможно, если невозможно, то возвращается
@@ -11,4 +11,18 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    if len(m) <= 1:
+        return None
+    elif len(m) != len(m[0]):
+        return None
+    elif len(m) == 2:
+        return m[0][0]*m[1][1] - m[0][1]*m[1][0]
+    else:
+        det = 1
+        for k in range(len(m)):
+            if m[k][k] == 0:
+                return 0
+            det *= m[k][k]
+            for i in range(k+1, len(m)):
+                m[i] = [m[i][j]-m[k][j]*m[i][k]/m[k][k] for j in range(len(m))]
+        return det
