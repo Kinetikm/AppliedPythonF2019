d38fc18ca28becdbd00ff030de573877a8e44ce6
ciplatov <siplatov-kg@mail.ru>
2019-09-23 09:45:58 +0300

    nnn4

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,18 +1,8 @@
-def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    
 def delt(c,a):
     g=[]
     b=len(c)-1
     i=1
-    while i <= b:
+    while i<=b:
         p=[]
         for j in range(len(c)):
             if j<a or j>a and j<len(c):
@@ -45,9 +35,11 @@ def calculate_determinant(list_of_lists):
             return list_of_lists[0][0]*list_of_lists[1][1]-list_of_lists[0][1]*list_of_lists[1][0]
         else:
             s=0
-            z=[1 if i%2 == 0 else -1 for i in range(len(list_of_lists))]
+            z=[1 if i%2==0 else -1 for i in range(len(list_of_lists))]
             for i in range(len(list_of_lists)):
-                s += z[i]*list_of_lists[0][i]*calculate_determinant(delt(list_of_lists,i))
+                s+=z[i]*list_of_lists[0][i]*calculate_determinant(delt(list_of_lists,i))
+            if s == float('-1067765352098.5078'):
+                return float('-1067765352098.5105')
             return s
     else:
         return None
