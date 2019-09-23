e6b722b50a9daa1a19b9dc58ccd2558eaa8681dc
cash57 <al.shafiev.sam@gmail.com>
2019-09-23 04:05:27 +0300

    word_inversion 4

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -10,18 +10,15 @@ def word_inversion(s):
     '''
     N = len(s)-1
     i, omega = N, 0
-    print(s)
     s.reverse()
-    print(s)
     while i > -1:
         if s[i] == ' ':
-            print(i)
             s[i + 1:i + omega+1] = s[i + omega:i:-1]
             omega = 0
             i -= 1
-        elif:
-            omega += 1
-            i -= 1
-        else i == 0:
+        elif i == 0:
             s[i:omega+1] = s[omega-N-1:-N-2:-1]
             return s
+        else:
+            omega += 1
+            i -= 1
