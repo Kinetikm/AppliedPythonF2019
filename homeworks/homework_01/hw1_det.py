c84fc0514d42eb87a9424fce650ec63d2b43fafa
cash57 <al.shafiev.sam@gmail.com>
2019-09-23 07:02:54 +0300

    det 5

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,21 +11,22 @@ def calculate_determinant(s):
     :param s: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    if s == [[]]:
-        return 0
-    res = 1
-    N = len(s)
-    for i in range(N):
-        j = max(range(i, N+1), key=lambda k: abs(s[k][i]))
-        if len(s) != len(s[i]):
-            return
-        if i != j:
-            s[i], s[j] = s[j], s[i]
-            res *= -1
-        if s[i][i] == 0:
-            return 0
-        res *= s[i][i]
-        for j in range(i + 1, N+1):
-            b = s[j][i] / s[i][i]
-            s[j] = [s[j][k] - b * s[i][k] for k in range(N)]
-    return res
+    raise NotImplementedError
+    # if s == [[]]:
+    #     return 0
+    # res = 1
+    # N = len(s)
+    # for i in range(N):
+    #     j = max(range(i, N+1), key=lambda k: abs(s[k][i]))
+    #     if len(s) != len(s[i]):
+    #         return
+    #     if i != j:
+    #         s[i], s[j] = s[j], s[i]
+    #         res *= -1
+    #     if s[i][i] == 0:
+    #         return 0
+    #     res *= s[i][i]
+    #     for j in range(i + 1, N+1):
+    #         b = s[j][i] / s[i][i]
+    #         s[j] = [s[j][k] - b * s[i][k] for k in range(N)]
+    # return res
