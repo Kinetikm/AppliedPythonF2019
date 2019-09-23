3b85e86a9dca3b95bef9e4c5c48cd12bf33eefda
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-09-23 09:41:19 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 def calculate_determinant(list_of_lists):
     m = len(list_of_lists)
     
-    def determinant(list_of_lists, m):
+    def determinant(list_of_lists1, m):
         def GetMatr(mas, p, i, j, m):
             di = 0
             for ki in range(m - 1):
@@ -26,15 +25,15 @@ def calculate_determinant(list_of_lists):
         if (m < 1):
             print('Определитель вычислить невозможно!')
         if (m == 1):
-            d = list_of_lists[0][0]
+            d = list_of_lists1[0][0]
             return (d)
         if (m == 2):
-            d = (list_of_lists[0][0] * list_of_lists[1][1]) - (list_of_lists[1][0] * list_of_lists[0][1])
+            d = (list_of_lists1[0][0] * list_of_lists1[1][1]) - (list_of_lists1[1][0] * list_of_lists1[0][1])
             return (d)
         if (m > 2):
             for i in range(m):
                 GetMatr(list_of_lists, p, i, 0, m)
-                d = d + k * list_of_lists[i][0] * determinant(p, n)
+                d = d + k * list_of_lists1[i][0] * determinant(p, n)
                 k = -k
         return (d)
         raise NotImplementedError
