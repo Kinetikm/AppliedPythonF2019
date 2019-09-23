57492c31ab41cda09a0b3e9e5507503c510bdcd7
Mariia Makarova <43655254+Mashmallow5@users.noreply.github.com>
2019-09-23 16:46:29 +0300

    determinant

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -3,12 +3,21 @@
 
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
-    raise NotImplementedError
+	indices = list(range(len(list_of_lists)))
+	total = 0
+	if len(list_of_lists) != len(list_of_lists[0]) or len(list_of_lists) == 0:
+		return None
+	if len(list_of_lists) == 1:
+		return list_of_lists[0][0]
+	if len(list_of_lists) == 2 or len(list_of_lists[0]) == 2:
+		return list_of_lists[0][0] * list_of_lists[1][1] - list_of_lists[0][1] * list_of_lists[1][0]
+	for fc in indices:
+		matrix = list_of_lists
+		matrix = matrix[1:]
+		height = len(matrix)
+		for i in range(height):
+			matrix[i] = matrix[i][0:fc] + matrix[i][fc+1:]
+		sign = (-1) ** (fc % 2)
+		sub_det = calculate_determinant(matrix)
+		total += sign * list_of_lists[0][fc] * sub_det
+	return total
