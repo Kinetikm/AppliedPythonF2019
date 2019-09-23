998402a95c134acfb52772a15b8e010f1d189a98
Artem Kugushev <artemkugushev@inbox.ru>
2019-09-23 03:35:44 +0300

    change 3:35

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,18 +11,20 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    m=len(list_of_lists)
-    n=len(list_of_lists[1])
+    m = len(list_of_lists)
+    n = len(list_of_lists[0])
     if m == n:
-        det=1
+        det = 1
         b = 1
         k = 1
+        e = 1
         for i in range(m):
             for j in range(i+1, n):
                 if list_of_lists[j][i] != 0:
                     b = list_of_lists[i][i] / list_of_lists[j][i]
                     for s in range(m):
-                        list_of_lists[j][s] = list_of_lists[j][s] * b - list_of_lists[i][s]
+                        e = list_of_lists[j][s] * b
+                        list_of_lists[j][s] = e * b - list_of_lists[i][s]
                 k = k / b
         for i in range(n):
             det = det * list_of_lists[i][i]
