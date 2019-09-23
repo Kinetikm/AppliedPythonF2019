f5e4cac3f7ef42378aa6f0439563406937b300fb
Karina1408 <55440723+Karina1408@users.noreply.github.com>
2019-09-23 08:37:32 +0300

    Update hw1_palindrom.py

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
 def check_palindrom(input_string):
      if input_string == input_string[::-1]:
         return "True"
     else:
-        return "False"
-   
+        return "False" 
