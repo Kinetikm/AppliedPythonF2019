a9a144ab03e0837d7e9b27028d99897e97e02915
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-09-23 09:33:54 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -20,8 +20,8 @@ def calculate_determinant(list_of_lists):
         d = 0
         k = 1
         n = m - 1
-        if (m < 1 ):
-            return None
+        if (m < 1):
+            print('Определитель вычислить невозможно!')
         if (m == 1):
             d = list_of_lists[0][0]
             return (d)
@@ -35,3 +35,4 @@ def calculate_determinant(list_of_lists):
                 k = -k
         return (d)
         raise NotImplementedError
+    return determinant(a, m)
