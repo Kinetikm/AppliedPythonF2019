5029054603379fb62f6c35d9c63e9e225cdfd6d4
Ilia Koltsa <koltsa.ilya@mail.ru>
2019-09-25 03:00:41 +0300

    VKposter

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -5,55 +5,52 @@
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self._posts = {}
+        self._nr = {}
+        self._followers = {}
+        self._news = {}
+
+    def form_news(self, follower_user_id, followee_user_id, new):
+        if follower_user_id not in self._news:
+            self._news[follower_user_id] = []
+        if not isinstance(new, list):
+            new = [new]
+        self._news[follower_user_id].extend(new)
 
     def user_posted_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        выложил пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+        if user_id not in self._posts:
+            self._posts[user_id] = []
+        self._posts[user_id].append(post_id)
+        if user_id in self._followers:
+            for i in self._followers[user_id]:
+                self.form_news(i, user_id, post_id)
 
     def user_read_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        прочитал пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+        if post_id not in self._nr:
+            self._nr[post_id] = set()
+        self._nr[post_id].add(user_id)
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
-        '''
-        Метод который вызывается когда пользователь follower_user_id
-        подписался на пользователя followee_user_id.
-        :param follower_user_id: id пользователя. Число.
-        :param followee_user_id: id пользователя. Число.
-        :return: ничего
-        '''
-        pass
+        if followee_user_id not in self._followers:
+            self._followers[followee_user_id] = []
+        self._followers[followee_user_id].append(follower_user_id)
+        x = followee_user_id
+        if followee_user_id in self._posts:
+            self.form_news(follower_user_id, followee_user_id, self._posts[x])
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
-        '''
-        Метод который вызывается когда пользователь user_id
-        запрашивает k свежих постов людей на которых он подписан.
-        :param user_id: id пользователя. Число.
-        :param k: Сколько самых свежих постов необходимо вывести. Число.
-        :return: Список из post_id размером К из свежих постов в
-        ленте пользователя. list
-        '''
-        pass
+        if user_id in self._news:
+            result = sorted(self._news[user_id])
+            if k >= len(result):
+                return result[len(result) - 1::-1]
+            else:
+                return result[len(result) - 1:len(result) - k - 1: -1]
+        return []
 
     def get_most_popular_posts(self, k: int) -> list:
-        '''
-        Метод который возвращает список k самых популярных постов за все время,
-        остортированных по свежести.
-        :param k: Сколько самых свежих популярных постов
-        необходимо вывести. Число.
-        :return: Список из post_id размером К из популярных постов. list
-        '''
-        pass
+        res = sorted(self._nr.items(), key=lambda x: (len(x[1]), x[0]))
+        res = list(map(lambda x: x[0], res))
+        if k >= len(res):
+            return res[len(result) - 1::-1]
+        else:
+            return res[len(res) - 1:len(res) - k - 1: -1]
