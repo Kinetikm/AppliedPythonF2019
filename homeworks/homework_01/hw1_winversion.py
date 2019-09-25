811413b06a61652183649c9ee9339afe858d593f
Nickolai427 <nick.daryin2015@yandex.ru>
2019-09-26 01:24:16 +0600

    correction of hw1_winversion.py

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -2,21 +2,19 @@
 # coding: utf-8
 
 
+def swap(i, j, lst):
+    for k in range(len(lst[i + 1:j])//2):
+        lst[i + 1 + k], lst[j - 1 - k] = lst[j - 1 - k], lst[i + 1 + k]
+
+
 def word_inversion(input_lst):
-    word = ''
-    for i in range(len(input_lst)):
-        symbol = input_lst.pop(0)
-        if symbol == ' ':
-            input_lst.append(word)
-            word = ''
-            continue
-        word += symbol
-    input_lst.append(word)
     input_lst.reverse()
+    prm1 = -1
+    prm2 = 0
     for i in range(len(input_lst)):
-        word = input_lst.pop(0)
-        for symbol in word:
-            input_lst.append(symbol)
-        input_lst.append(' ')
-    input_lst.pop()
+        if input_lst[i] == ' ':
+            prm2 = i
+            swap(prm1, prm2, input_lst)
+            prm1 = prm2
+    swap(prm1, len(input_lst), input_lst)
     return input_lst
