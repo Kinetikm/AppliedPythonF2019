8c3d8591a6572e706f1d56fb504d53e93be67e05
Pavel Lomtev <pavellomtev2012@yandex.ru>
2019-09-25 21:21:02 +0300

    1-ая попытка дет и суб

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import copy
+
 
 def calculate_determinant(list_of_lists):
-    '''
-    Метод, считающий детерминант входной матрицы,
-    если это возможно, если невозможно, то возвращается
-    None
-    Гарантируется, что в матрице float
-    :param list_of_lists: список списков - исходная матрица
-    :return: значение определителя или None
-    '''
+    return(determinant(list_of_lists))
+
+
+def minor(list_of_lists, row, col):
+    copy_list = copy.deepcopy(list_of_lists)
+    for k in range(len(list_of_lists)):
+        del copy_list[k][col]
+    del copy_list[row]
+    return copy_list
+
+
+def determinant(list_of_list):
+    m = len(list_of_list)
+    n = len(list_of_list[0])
+    if (m != n):
+        return None
+    elif (n == 1):
+        return list_of_list[0][0]
+    coeff = 1
+    det = 0
+    for i in range(n):
+        det += list_of_list[0][i] * coeff * determinant(minor(list_of_list, 0, i))
+        coeff *= (-1)
+    return det
+
     raise NotImplementedError
+
--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -3,12 +3,16 @@
 
 
 def find_subarr(input_lst, num):
-    '''
-    Метод, находящий подмассив, сумма чисел которого равна заданному числу
-    O(n) по времени
-    :param input_lst: массив
-    :param num: искомое число
-    :return: два индекса (начала и конца подмассива). Пустой tuple, если таких нет
-    Пример: find_subarr([1, 2, 3, 4, 5, -1], 4) может вернуть (3, 3) или (4, 5)
-    '''
+    input_dict = {}
+    sum = 0
+    for i in range(len(input_lst)):
+        sum += input_lst[i]
+        if (sum == num):
+            return (0, i)
+        if (sum - num) in input_dict:
+            return (input_dict[sum - num] + 1, i)
+        else:
+            input_dict[sum] = i
+    return()
+    
     raise NotImplementedError
