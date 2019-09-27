891627c64c8c71429875bb4469b2e7d67de787fa
Ilya-Verendeev <55409291+Ilya-Verendeev@users.noreply.github.com>
2019-09-27 20:25:31 +0300

    Update hw1_subarr.py

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -2,22 +2,17 @@
 # coding: utf-8
 
 
-def find_subarr(a, sum):
+def find_subarr(a, s):
     index1 = 0
     index2 = 0
     while index1 + index2 < 2*len(a) - 2:
         midsum = a[index1] + a[index2]
-        if midsum != sum:
+        if midsum != s:
             if index1 >= index2:
                 index2 += 1
             else:
                 index1 += 1
         else:
-            return (index1, index2)
+            return index1, index2
     return ()
-    raise NotImplementedError
-#метод нахождения подмассива с заданной суммой
-
-
-
-
+# метод нахождения подмассива с заданной суммой
