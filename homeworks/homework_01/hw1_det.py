615fe9bfaccac4e2435b5b1c8ccc92cca8e85070
Ilya-Verendeev <55409291+Ilya-Verendeev@users.noreply.github.com>
2019-09-27 20:24:57 +0300

    Update hw1_det.py

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -2,31 +2,30 @@
 # coding: utf-8
 import copy
 
+
 def minor(a, i, j):
     m = copy.deepcopy(a)
     for k in range(len(a)):
         del m[k][j]
     del m[i]
     return m
-#метод по созданию минора матрицы по данным строке и столбцу
-#копирует матрицу, и в ней удаляет строку и столбец
+# метод по созданию минора матрицы по данным строке и столбцу
+# копирует матрицу, и в ней удаляет строку и столбец
+
 
 def calculate_determinant(a):
     m = len(a)
     n = len(a[0])
     if m != n:
-        return "matrix isn't quadratic"
+        return None
     elif n == 1:
         return a[0][0]
     sign = 1
-    #при вычислении алгебраических дополнений знак в зависимости
-    #от четности строки и столбца изменяется
-    DET = 0
+    # при вычислении алгебраических дополнений знак в зависимости
+    # от четности строки и столбца изменяется
+    det = 0
     for i in range(n):
-        DET += a[0][i] * sign * calculate_determinant(minor(a, 0, i))
+        det += a[0][i] * sign * calculate_determinant(minor(a, 0, i))
         sign *= -1
-    return DET
-    raise NotImplementedError
-#метод нахождения определителя матрицы методом разложения по строке
-
-
+    return det
+# метод нахождения определителя матрицы методом разложения по строке
