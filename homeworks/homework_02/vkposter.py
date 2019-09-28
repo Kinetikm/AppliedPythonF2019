bf21bf6101cc8fb32c674b9f30df2c608e27b99f
Ilya-Verendeev <55409291+Ilya-Verendeev@users.noreply.github.com>
2019-09-28 22:30:21 +0300

    Update vkposter.py

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -5,37 +5,25 @@
 class VKPoster:
     posts = {}
     # каждому посту сопоставляем список людей, его посмотревших
-    create_post = {}
-    # каждому пользователю сопоставляем список постов, просмотренных им
     follow = {}
-    # зователю сопоставляем список его подписок
-    fresh_posts = []
-    # список из самых свежих постов
-    rate_of_post = {}
-    # количество просмотров каждого поста
+    # каждому пользователю сопоставляем список его подписок
+    post_stats = {}
+    # статистика чтения поста
 
     def __init__(self):
         return
 
     def user_posted_post(self, user_id: int, post_id: int):
-        if user_id not in self.create_post:
-            self.create_post[user_id] = []
-        # если по данному пользователю не числится создание поста - начинаем список его постов
-        self.create_post[user_id] += [post_id]
-        # добавляем элемент в список данному user_id
-        self.posts[post_id] = [user_id]
-        # для нового поста создаем список людей его посмотревших
-        self.fresh_posts.insert(0, post_id)
-        # пополняем список свежих постов спереди
-        self.rate_of_post[post_id] = 1
-        # рейтинг нового поста автоматически становится равен единице - пользователь, его посмотревший
+        self.posts[post_id] = user_id
+        # каждому посту сопоставляем его создателя
         return
 
     def user_read_post(self, user_id: int, post_id: int):
-        self.posts[post_id] += [user_id]
-        # пополняем список посмотревших пользователей для post_id
-        self.rate_of_post[post_id] += 1
-        # рейтинг поста тем временем пополняется
+        if post_id not in self.post_stats:
+            self.post_stats[post_id] = set()
+        # если у поста еще нет пользователей, его посмотревших, то создаем новую коллекцию
+        self.post_stats[post_id].add(user_id)
+        # добавляем в соответствующую посту коллекцию нового посмотревшего пост человека
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         if follower_user_id not in self.follow:
@@ -45,23 +33,24 @@ class VKPoster:
         # пополняем список людей, на которых подписан follower_id
 
     def get_recent_posts(self, user_id: int, k: int) -> list:
+        fresh_posts = sorted(self.posts.keys())
+        fresh_posts.reverse()
         fresh_for_id = []
-        for post in self.fresh_posts:
-            for folowee in self.follow[user_id]:
-                if post in self.create_post[folowee]:
-                    fresh_for_id += [post]
-                    break
+        for post in fresh_posts:
+            if self.posts[post] in self.follow[user_id]:
+                fresh_for_id += [post]
             if len(fresh_for_id) == k:
                 return fresh_for_id
 
     def get_most_popular_posts(self, k: int) -> list:
+        new_dict = {key: len(value) for key, value in self.post_stats.items()}
+        most_popular = sorted(new_dict.values())
+        most_popular.reverse()
         popular_posts = []
-        count = len(self.rate_of_post)
-        for post in self.fresh_posts:
-            count = 0
-            for other_post in self.rate_of_post.keys():
-                if post > other_post:
-                    count += 1
-            if count > len(self.fresh_posts) - k:
-                popular_posts += [post]
+        for value in most_popular[:k]:
+            for key in sorted(new_dict.keys(), reverse=True):
+                if new_dict[key] == value:
+                    popular_posts.append(key)
+                    new_dict.pop(key)
+                    break
         return popular_posts
