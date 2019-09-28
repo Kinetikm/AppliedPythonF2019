530fbabb0d9c159b58d0ced9233433e8552dfd84
Pavlov Ilya <79216023478@yandex.ru>
2019-09-29 01:07:59 +0300

    vk1.2

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -66,12 +66,10 @@ class VKPoster:
         :return: Список из post_id размером К из свежих постов в
         ленте пользователя. list
         '''
+        if user_id not in self.subscriptions:
+            return None
         lst = list()
         for cur in self.subscriptions[user_id]:
-            if cur not in self.posts:
-                # если у текущего пользователя (cur), на которого
-                # подписан user_id, нет постов
-                continue
             lst += self.posts[cur]
         lst.reverse()
         return lst[:k]
