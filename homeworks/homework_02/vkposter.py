68144a48e956380d17d30b9f6e8c86cffd9924de
Mikhail <zojloverr@gmail.com>
2019-09-28 23:57:27 +0300

    HW2

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -10,50 +10,50 @@ class VKPoster(object):
         self.user_added_posts = {}    # key user_id,info - added posts
 
 
-def user_posted_post(self, user_id: int, post_id: int):
-    if user_id in self.user_added_posts:
-        self.user_added_posts[user_id].append(post_id)
-    else:
-        self.user_added_posts[user_id] = [post_id]
+    def user_posted_post(self, user_id: int, post_id: int):
+        if user_id in self.user_added_posts:
+            self.user_added_posts[user_id].append(post_id)
+        else:
+            self.user_added_posts[user_id] = [post_id]
 
 
-def user_read_post(self, user_id: int, post_id: int):
-    if post_id in self.read_users:
-        if user_id not in self.read_users[post_id]:
-            self.read_amount[post_id] += 1
-        self.read_users[post_id].append(user_id)
-    else:
-        self.read_users[post_id] = [user_id]
-        self.read_amount[post_id] = 1
+    def user_read_post(self, user_id: int, post_id: int):
+        if post_id in self.read_users:
+            if user_id not in self.read_users[post_id]:
+                self.read_amount[post_id] += 1
+            self.read_users[post_id].append(user_id)
+        else:
+            self.read_users[post_id] = [user_id]
+            self.read_amount[post_id] = 1
 
 
-def user_follow_for(self, follower_user_id: int, followee_user_id: int):
-    if follower_user_id in self.subscribes:
-        self.subscribes[follower_user_id].add(followee_user_id)
-    else:
-        s = set()
-        s.add(followee_user_id)
-        self.subscribes[follower_user_id] = s
+    def user_follow_for(self, follower_user_id: int, followee_user_id: int):
+        if follower_user_id in self.subscribes:
+            self.subscribes[follower_user_id].add(followee_user_id)
+        else:
+            s = set()
+            s.add(followee_user_id)
+            self.subscribes[follower_user_id] = s
 
 
-def get_recent_posts(self, user_id: int, k: int) -> list:
-    out_list = []
-    for val in self.subscribes[user_id]:
-        out_list += self.user_added_posts[val]
-        out_list.sort()
-        if len(out_list) > k:
-            del out_list[:len(out_list) - k]
-    return out_list
+    def get_recent_posts(self, user_id: int, k: int) -> list:
+        out_list = []
+        for val in self.subscribes[user_id]:
+            out_list += self.user_added_posts[val]
+            out_list.sort()
+            if len(out_list) > k:
+                del out_list[:len(out_list) - k]
+        return out_list
 
 
-def get_most_popular_posts(self, k: int) -> list:
-    sort_list = []
-    out_list = []
-    for key in self.read_amount:
-        sort_list.append((key, self.read_amount[key]))
-        sort_list.sort(key=lambda x: x[1])
-        if len(sort_list) > k:
-            del sort_list[:len(out_list) - k]
-    for i in range(k):
-        out_list.append(sort_list[i][0])
-    return sorted(out_list)
+    def get_most_popular_posts(self, k: int) -> list:
+        sort_list = []
+        out_list = []
+        for key in self.read_amount:
+            sort_list.append((key, self.read_amount[key]))
+            sort_list.sort(key=lambda x: x[1])
+            if len(sort_list) > k:
+                del sort_list[:len(out_list) - k]
+        for i in range(k):
+            out_list.append(sort_list[i][0])
+        return sorted(out_list)
