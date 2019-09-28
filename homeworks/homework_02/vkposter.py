240c4c2cae4d50f98e55e546655dbd99b914e839
Pavlov Ilya <79216023478@yandex.ru>
2019-09-29 00:49:52 +0300

    vk1.2

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -31,7 +31,7 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        if post_id in self.viewers: 
+        if post_id in self.viewers:
             # если это условие выполняется, то
             # условие (if post_id in self.views)
             # тоже выполняется, так как
@@ -87,7 +87,7 @@ class VKPoster:
         :return: Список из post_id размером К из популярных постов. list
         '''
         lst = list(self.views.items())
-        lst = sort(key = lambda i: i[1], reverse = True)
+        lst = sort(key=lambda i: i[1], reverse=True)
         for i in range(len(lst)):
             lst[i] = lst[i][1]
         return lst[:k]
