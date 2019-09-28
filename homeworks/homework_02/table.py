09fd2b4b597da43a2ecb99891b635f076c62b564
verbarius <verbarius@mail.ru>
2019-09-28 15:31:28 +0300

    add MyException

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -5,16 +5,26 @@ from read import read_file
 from format_table import print_table
 from encode import isvalid
 
+
+class MyError(Exception):
+    pass
+
 if __name__ == '__main__':
     filename = sys.argv[1]
 
 # Ваш код
 
+
 try:
     correct_encoding = isvalid(filename)
     data = read_file(filename, correct_encoding)
-    print_table(data)
+    len_ = len(data[0])
+    for line in data:
+        if len(line) != len_:
+            raise MyError
 except FileNotFoundError:
     print("Файл не валиден")
-except (TypeError, UnicodeError, SyntaxError, AttributeError):
+except (TypeError, UnicodeError, SyntaxError, AttributeError, IndexError, MyError):
     print('Формат не валиден')
+else:
+    print_table(data)
