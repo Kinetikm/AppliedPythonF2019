7e322b8ae02f9ed1fda02524eb68828c9b85ebf7
Зайцева Ирина <zaiceva.irinka@mail.ru>
2019-09-28 23:03:28 +0300

    fixed4

--- homeworks/homework_02/module1_open.py
+++ homeworks/homework_02/module1_open.py
@@ -1,18 +1,16 @@
 def open_file(filename):
-    code = ['utf-8', 'utf-16', 'cp1251']
+    code = ['utf-16', 'utf-8', 'cp1251']
 
     for i in code:
         try:
             with open(file=filename, encoding=i) as f:
                 data = f.read()
                 return data
+        # except (TypeError,  SyntaxError, AttributeError, UnicodeDecodeError, UnicodeError):
+        #     continue
         except FileNotFoundError:
             print("Файл не валиден")
             return -1
-        except UnicodeDecodeError:
-            continue
-        except UnicodeError:
-            continue
-        except (TypeError, UnicodeError, SyntaxError, AttributeError):
-            print("Формат не валиден")
+
+    print("Формат не валиден")
     return -1
--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
 import sys
 
 import module1_open as m1
@@ -6,8 +10,8 @@ import module3_print as m3
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-
     open_result = m1.open_file(filename)  # определение кодировки
+
     if open_result != -1:
         format_data = m2.format_data(data=open_result, filename=filename)  # определение формта
         m3.print_table(data=open_result, format_data=format_data)  # форматированный вывод
