93d0984ce4c5da67526ea0288308a19fef0cbc7e
Vladislav <48061257+VladislavKuzevanov@users.noreply.github.com>
2019-09-29 00:00:14 +0300

    Update vkposter.py

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -2,58 +2,52 @@
 # coding: utf-8
 
 
+#первый лист - собственный пост; второй лист - подписчики; третий - прочитанные посты; четвертый - подписки
+
+
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.post_relationship = {}
+        self.posts = {}
+
 
     def user_posted_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        выложил пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+        self.post_id = post_id
+        self.user_id = user_id
+        self.posts[post_id] = []
+        if user_id not in self.post_relationship:
+            self.post_relationship[user_id] = [[],[],[],[]]
+            self.post_relationship[user_id][0].append(post_id)
+        else:
+            self.post_relationship[user_id][0].append(post_id)
+
 
     def user_read_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        прочитал пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+        self.post_relationship[user_id][2].append(post_id)
+        if user_id not in self.posts[post_id]:
+            self.posts[post_id].append(user_id)
+
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
-        '''
-        Метод который вызывается когда пользователь follower_user_id
-        подписался на пользователя followee_user_id.
-        :param follower_user_id: id пользователя. Число.
-        :param followee_user_id: id пользователя. Число.
-        :return: ничего
-        '''
-        pass
+        self.post_relationship[follower_user_id][3].append(followee_user_id)
+
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
-        '''
-        Метод который вызывается когда пользователь user_id
-        запрашивает k свежих постов людей на которых он подписан.
-        :param user_id: id пользователя. Число.
-        :param k: Сколько самых свежих постов необходимо вывести. Число.
-        :return: Список из post_id размером К из свежих постов в
-        ленте пользователя. list
-        '''
-        pass
+        temp = []
+        for i in self.post_relationship[user_id][3]:
+            for j in self.post_relationship[i][0]:
+                temp.append(j)
+        temp.sort()
+        return temp[len(temp)-1-k::]
+
 
     def get_most_popular_posts(self, k: int) -> list:
-        '''
-        Метод который возвращает список k самых популярных постов за все время,
-        остортированных по свежести.
-        :param k: Сколько самых свежих популярных постов
-        необходимо вывести. Число.
-        :return: Список из post_id размером К из популярных постов. list
-        '''
-        pass
+        list_d = list(self.posts.items())
+        list_d.sort(key=lambda x: len(x[1]), reverse=True)
+        temp = [i[0] for i in list_d[:k]]
+        return temp
+
+
+    def print_VK(self):
+        print("self.post_relationship: ", self.post_relationship, "\n", "self.posts: ", self.posts, "\n\n")
