abf92e7c9d929ebc0325ed669a06f376d4df9ebf
danilsmirn <dddssasd@mail.ru>
2019-09-28 17:56:18 +0300

    update

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -5,10 +5,10 @@
 class VKPoster:
 
     def __init__(self):
-        self.user_posts={}
-        self.user_follows={}
-        self.user_read={}
-        self.most_popular={}
+        self.user_posts = {}
+        self.user_follows = {}
+        self.user_read = {}
+        self.most_popular = {}
         raise NotImplementedError
 
     def user_posted_post(self, user_id: int, post_id: int):
@@ -24,9 +24,9 @@ class VKPoster:
             if user_id in self.user_read:
                 self.user_read[user_id].append(post_id)
             else:
-                self.user_read[user_id]=[post_id]
+                self.user_read[user_id] = [post_id]
             if post_id not in self.most_popular:
-                self.most_popular[post_id]=1
+                self.most_popular[post_id] = 1
             else:
                 self.most_popular[post_id] += 1
             pass
@@ -35,10 +35,10 @@ class VKPoster:
         if (follower_user_id in self.user_follows) and (followee_user_id != follower_user_id):
             self.user_follows[follower_user_id].append(followee_user_id)
         else:
-            self.user_follows[follower_user_id]=[followee_user_id]
+            self.user_follows[follower_user_id] = [followee_user_id]
         pass
 
-    def get_recent_posts(self, user_id: int, k: int)-> list:
+    def get_recent_posts(self, user_id: int, k: int) -> list:
         spisok = []
         for i in self.user_follows[user_id]:
             if i in self.user_posts:
@@ -47,16 +47,14 @@ class VKPoster:
         spisok.reverse()
         return spisok[:k]
 
-
-
     def get_most_popular_posts(self, k: int) -> list:
         spis = []
         for i, v in self.most_popular:
-            spis.append([v,i])
+            spis.append([v, i])
         spis.sort()
         spis.reverse()
         vozv = spis[0:k]
-        c=[]
-        for  i in vozv:
+        c = []
+        for i in vozv:
             c.append(i[1])
         return c
