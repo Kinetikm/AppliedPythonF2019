60a70a0cb00d924d4501caf7615a54114a331761
ChesnovAE <aechesnov@yandex.ru>
2019-09-28 03:20:01 +0300

    Fixes invalid format

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,9 +1,11 @@
 import sys
+
 from gtable import GoodTable
 from methods import json_to_list, tsv_to_list
 from reading import read_file
 
-
+class MyException(Exception):
+    pass
 # Ваши импорты
 
 if __name__ == '__main__':
@@ -16,9 +18,16 @@ if __name__ == '__main__':
             data = json_to_list(data)
         else:
             data = tsv_to_list(data)
+        col_numb = max([len(i) for i in data])
+        for item in data:
+            if len(item) != col_numb:
+                raise MyException
+
         table = GoodTable(data)
         table.print_table()
     except FileNotFoundError:
         print('Файл не валиден')
     except TypeError:
+        print('Формат не валиден')
+    except MyException:
         print('Формат не валиден')
