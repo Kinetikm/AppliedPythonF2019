77370d5ab381f6059ed07a585ad4d3b03529ba66
Mikhail <zojloverr@gmail.com>
2019-09-28 21:13:09 +0300

    HW2

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -1,40 +1,50 @@
-from homeworks.homework_02.heap import MaxHeap
-from homeworks.homework_02.fastmerger import FastSortedListMerger
-
-
-class VKPoster:
+class VKPoster(object):
     def __init__(self):
         self.posts = dict([list[int], int])   # key - post_id, info - list if users who read
         self.users = dict([set[int], list[int]])    # key user_id,info - (set of subscribes, list of added posts)
 
 
-    def user_posted_post(self, user_id: int, post_id: int):
-        self.users[user_id][1].append(post_id)
+def user_posted_post(self, user_id: int, post_id: int):
+    self.users[user_id][1].append(post_id)
 
 
-    def user_read_post(self, user_id: int, post_id: int):
+def user_read_post(self, user_id: int, post_id: int):
+    if self.posts.get(post_id) is not None:
         if user_id not in self.posts[post_id][0]:
-            self.posts[post_id][1] + 1
+            self.posts[post_id][1] += 1
         self.posts[post_id][0].append(user_id)
 
 
-    def user_follow_for(self, follower_user_id: int, followee_user_id: int):
-        self.users[follower_user_id][0].add(followee_user_id)
+def user_follow_for(self, follower_user_id: int, followee_user_id: int):
+    self.users[follower_user_id][0].add(followee_user_id)
 
 
-    def get_recent_posts(self, user_id: int, k: int) -> list:
-        lol = []  # list of lists
-        for val in self.users[user_id][0]:
-            lol.append(self.users[val][1])
-        return FastSortedListMerger(lol, k)
+def get_recent_posts(self, user_id: int, k: int) -> list:
+    out_list = []
+    for val in self.users[user_id][0]:
+        out_list += self.users[val][1]
+        out_list.sort()
+        if len(out_list) > k:
+            del out_list[:k]
+    return out_list
 
 
-    def get_most_popular_posts(self, k: int) -> list:
-        arr = []
-        out_arr = []
-        for key in self.posts:
-            arr.append(self.posts[key][1], key)
-        mh = MaxHeap(arr)
-        while len(out_arr) < k and len(mh.array) > 0:
-            out_arr.append(mh.extract_maximum()[1])
-        return sorted(out_arr)
+def get_most_popular_posts(self, k: int) -> list:
+    sort_list = []
+    out_list = []
+    for key in self.posts:
+        sort_list.append((key, self.posts[key][1]))
+        sort_list.sort(key=lambda x: x[1])
+        if len(sort_list) > k:
+            del sort_list[:k]
+    for i in range(k):
+        out_list.append(sort_list[i][0])
+    return sorted(out_list)
+
+
+
+
+
+
+
+
