fe148918552b98b2f359473e2a7e57f505c6f4ec
MaximAbramenko <gotosleepmcht@gmail.com>
2019-09-30 02:33:46 +0300

    peredelal1

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,11 +8,22 @@ def word_inversion(input_lst):
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-    input_lst[::-1]
-    k = 0
-    for i in range(len(input_lst)):
-        if input_lst[i] == " ":
-            input_lst[k:i][::-1]
-            k = i + 1
-    input_lst[k:][::-1]
-    return input_lst
+    def find_space(cur_pos, input_lst):
+        i = cur_pos
+        while (i != len(input_lst)) and (input_lst[i] != " "):
+            i += 1
+        return i
+
+
+    def word_inversion(input_lst):
+        for i in range(len(input_lst) // 2):
+            input_lst[i], input_lst[-i - 1] = input_lst[-i - 1], input_lst[i]
+        next_space_pos = -1
+        cur_space_pos = -1
+        while next_space_pos < (len(input_lst)):
+            cur_space_pos = next_space_pos
+            next_space_pos = find_space(cur_space_pos + 1, input_lst)
+            for i in range((next_space_pos - cur_space_pos) // 2):
+                input_lst[cur_space_pos + i + 1], input_lst[next_space_pos - i - 1] = input_lst[next_space_pos - i - 1], \
+                                                                                      input_lst[cur_space_pos + i + 1]
+        return input_lst
