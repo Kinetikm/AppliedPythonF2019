169daa0bbda690c20c594adcf4f4ebc321fb66bc
MaximAbramenko <gotosleepmcht@gmail.com>
2019-09-30 00:50:40 +0300

    vkp

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -5,7 +5,10 @@
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.posts = dict()
+        self.reads = dict()
+        self.readers = dict()
+        self.followers = dict()
 
     def user_posted_post(self, user_id: int, post_id: int):
         '''
@@ -15,7 +18,10 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
+        if user_id in self.posts:
+            self.posts[user_id].append(post_id)
+        else:
+            self.posts[user_id] = [post_id]
 
     def user_read_post(self, user_id: int, post_id: int):
         '''
@@ -25,7 +31,11 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
+        if post_id in self.readers:
+            self.readers[post_id].append(user_id)
+        else:
+            self.readers[post_id] = [user_id]
+        self.reads[post_id] = len(self.readers[post_id])
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         '''
@@ -35,9 +45,12 @@ class VKPoster:
         :param followee_user_id: id пользователя. Число.
         :return: ничего
         '''
-        pass
+        if follower_user_id in self.followers:
+            self.followers[follower_user_id].append(followee_user_id)
+        else:
+            self.followers[follower_user_id] = [followee_user_id]
 
-    def get_recent_posts(self, user_id: int, k: int)-> list:
+    def get_recent_posts(self, user_id: int, k: int) -> list:
         '''
         Метод который вызывается когда пользователь user_id
         запрашивает k свежих постов людей на которых он подписан.
@@ -46,8 +59,15 @@ class VKPoster:
         :return: Список из post_id размером К из свежих постов в
         ленте пользователя. list
         '''
-        pass
+        out_list = list()
+        for th in self.followers[user_id]:
+            if th in self.posts:
+                out_list += self.posts[th]
+                out_list.sort()
+        out_list = out_list[::-1]
+        return out_list[:k]
 
+       
     def get_most_popular_posts(self, k: int) -> list:
         '''
         Метод который возвращает список k самых популярных постов за все время,
@@ -56,4 +76,12 @@ class VKPoster:
         необходимо вывести. Число.
         :return: Список из post_id размером К из популярных постов. list
         '''
-        pass
+        use_list = list()
+        out_list = list()
+        for key, val in self.reads.items():
+            use_list.append([val, key])
+        use_list.sort(reverse=True)
+        for i in use_list[:k]:
+            out_list.append(i[1])
+        return out_list
+
