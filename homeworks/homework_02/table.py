8874b34bfa60be6c4a5d25ee1e424d9b577a766e
Maksim Agafonov <maksim.agafonofff@gmail.com>
2019-09-29 22:24:37 +0300

    upd

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,21 @@
-import sys
+#!/usr/bin/env python
+# coding: utf-8
 
-# Ваши импорты
+import sys
+from data import read_data, print_data
+from file_functions import open_file, json_or_csv, find_encode
+
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-
-    # Ваш код
+    try:
+        open_file(filename)
+        type_f = find_encode(filename)
+        encoding = json_or_csv(filename, type_f)
+        print_data(read_data(filename, type_f, encoding))
+    except FileNotFoundError:
+        print("Файл не валиден")
+    except UnicodeDecodeError:
+        print("Формат не валиден")
+    except AssertionError:
+        print("Формат не валиден")
