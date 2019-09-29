58d8050814003fdc780d37be8efae4525a4a501c
Ivan.Protopopov <Afchi31@gmail.com>
2019-09-29 20:57:58 +0300

    test2

---
+++ homeworks/homework_02/encoding_define.py
@@ -0,0 +1,17 @@
+def encoding_define(path_2_file):
+    try:
+        with open(path_2_file, 'r', encoding='utf-8') as f:
+            f.readline()
+            return 'utf-8'
+    except:
+        try:
+            with open(path_2_file, 'r', encoding='utf-16') as f:
+                f.readline()
+                return 'utf-16'
+        except:
+            try:
+                with open(path_2_file, 'r', encoding='windows-1251') as f:
+                    f.readline()
+                    return 'windows-1251'
+            except:
+                return
--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -1,78 +1,71 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-user_posts = {}
-user_views = {}
-user_follows = {}
-sequence_of_user_posts = []
-post_views = {}
-recent_posts = []
-lst = []
-
-
 class VKPoster:
 
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self,):
+        self.user_posts = {}
+        self.user_views = {}
+        self.user_follows = {}
+        self.sequence_of_user_posts = []
+        self.post_views = {}
+        self.recent_posts = []
+        self.lst = []
 
     def user_posted_post(self, user_id: int, post_id: int):
         try:
-            sequence_of_user_posts.index(post_id)
+            self.sequence_of_user_posts.index(post_id)
         except ValueError:
-            sequence_of_user_posts.append(post_id)
+            self.sequence_of_user_posts.append(post_id)
         '''
         sequence_of_user_posts --> последовательность добавленных постов(list)
         '''
         try:
-            user_posts[user_id].add(post_id)
+            self.user_posts[user_id].add(post_id)
         except KeyError:
-            user_posts[user_id] = {post_id}
+            self.user_posts[user_id] = {post_id}
         return
 
     def user_read_post(self, user_id: int, post_id: int):
-        if post_id not in post_views:
-            post_views[post_id] = 1
+        if post_id not in self.post_views:
+            self.post_views[post_id] = 1
         else:
-            post_views[post_id] = post_views[post_id] + 1
+            self.post_views[post_id] = self.post_views[post_id] + 1
         '''
         post --> число просмотров для постов
         '''
         try:
-            user_views[user_id].add(post_id)
+            self.user_views[user_id].add(post_id)
         except KeyError:
-            user_views[user_id] = {post_id}
+            self.user_views[user_id] = {post_id}
         return
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         try:
-            if followee_user_id not in user_follows[follower_user_id]\
+            if followee_user_id not in self.user_follows[follower_user_id]\
                     or follower_user_id != followee_user_id:
-                user_follows[follower_user_id].add(followee_user_id)
+                self.user_follows[follower_user_id].add(followee_user_id)
         except KeyError:
             if follower_user_id != followee_user_id:
-                user_follows[follower_user_id] = {followee_user_id}
+                self.user_follows[follower_user_id] = {followee_user_id}
         return
 
     def get_recent_posts(self, user_id: int, k: int) -> list:
         for j in range(k):
-            for i in user_follows[user_id]:
-                if sequence_of_user_posts[::-1][j] in user_posts[i]:
-                    if sequence_of_user_posts[::-1][j] in recent_posts:
+            for i in self.user_follows[user_id]:
+                if self.sequence_of_user_posts[::-1][j] in self.user_posts[i]:
+                    if self.sequence_of_user_posts[::-1][j] in self.recent_posts:
                         continue
                     else:
-                        recent_posts.append(sequence_of_user_posts[::-1][j])
-        return recent_posts
+                        self.recent_posts.append(self.sequence_of_user_posts[::-1][j])
+        return self.recent_posts
 
     def get_most_popular_posts(self, k: int) -> list:
         q = []
         ql = []
-        if k > len(post_views):
-            k = len(post_views)
+        if k > len(self.post_views):
+            k = len(self.post_views)
         for x in range(k):
-            q.append(list(post_views.items())[x][::-1])
+            q.append(list(self.post_views.items())[x][::-1])
         q.sort()
         q.reverse()
         for x in range(k):
             ql.append(q[x][1])
-        return lst
+        return ql
