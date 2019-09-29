40cf9babc20479949eaa4fda76b868b45152bcb1
fatkullinalina <alina2469@gmail.com>
2019-09-29 18:57:01 +0300

    hm

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -2,18 +2,4 @@
 # coding: utf-8
 
 
-import sys
-from terminaltables import AsciiTable
-from encode import encoding
-from check import checking
-from read import reading
-
-
-if __name__ == '__main__':
-    filename = sys.argv[1]
-
-    enc = encoding(file_name)
-    check = checking(file_name, enc)
-    read = reading(file_name, enc, check)
-    table = AsciiTable(read)
-    print(table.table)
+if smth
