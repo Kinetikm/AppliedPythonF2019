4f8fda18690c45eeb84481004a9ff1b3e6275e3e
Smirnov Ilya <box53i@mail.ru>
2019-09-30 01:45:31 +0300

    new

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,16 @@
 import sys
-
+import table_printer
 # Ваши импорты
 
 if __name__ == '__main__':
     filename = sys.argv[1]
 
-    # Ваш код
+
+def table(file_name):
+    table_printer.print_table(file_name)
+
+
+if filename is not None:
+    table(filename)
+else:
+    print("Файл не валиден")
--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -1,59 +1,45 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import heapq
+
 
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.dict_of_posts = {}
+        self.dict_of_users = {}
+        self.dict_of_followers = {}
+        self.dict_of_readers = {}
+
 
     def user_posted_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        выложил пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+        self.dict_of_users.setdefault(user_id, []).append(post_id)
+        self.dict_of_posts[post_id] = []
+
 
     def user_read_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        прочитал пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+        if user_id not in self.dict_of_posts.setdefault(post_id, []):
+            self.dict_of_posts[post_id].append(user_id)
+
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
-        '''
-        Метод который вызывается когда пользователь follower_user_id
-        подписался на пользователя followee_user_id.
-        :param follower_user_id: id пользователя. Число.
-        :param followee_user_id: id пользователя. Число.
-        :return: ничего
-        '''
-        pass
+            self.dict_of_followers.setdefault(follower_user_id,[]).append(followee_user_id)
+
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
-        '''
-        Метод который вызывается когда пользователь user_id
-        запрашивает k свежих постов людей на которых он подписан.
-        :param user_id: id пользователя. Число.
-        :param k: Сколько самых свежих постов необходимо вывести. Число.
-        :return: Список из post_id размером К из свежих постов в
-        ленте пользователя. list
-        '''
-        pass
+        list = []
+        for i in self.dict_of_followers[user_id]:
+            list.extend(self.dict_of_users[i])
+        list.sort()
+        list.reverse()
+        list = list[:k:]
+        return list
+
 
     def get_most_popular_posts(self, k: int) -> list:
-        '''
-        Метод который возвращает список k самых популярных постов за все время,
-        остортированных по свежести.
-        :param k: Сколько самых свежих популярных постов
-        необходимо вывести. Число.
-        :return: Список из post_id размером К из популярных постов. list
-        '''
-        pass
+        tmp_list = []
+        for id, val in self.dict_of_posts.items:
+            heapq.heappush(tmp_list, (len(val), id))
+        list = [i[1] for i in heap.nlargest(k, tmp_list)]
+        return list
