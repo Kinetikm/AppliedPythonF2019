e49786f55d6fb3237440a982b889896e15e5284d
Artem Kugushev <artemkugushev@inbox.ru>
2019-09-29 23:05:16 +0300

    change 23:05

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -5,7 +5,9 @@
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.pp = dict()
+        self.pr = dict()
+        self.pf = dict()
 
     def user_posted_post(self, user_id: int, post_id: int):
         '''
@@ -15,6 +17,8 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
+        self.pp.setdefault(user_id, [])
+        self.pp[user_id].append(post_id)
         pass
 
     def user_read_post(self, user_id: int, post_id: int):
@@ -25,6 +29,8 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
+        self.pr.setdefault(post_id, [])
+        self.pr[post_id].append(user_id)
         pass
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
@@ -35,6 +41,8 @@ class VKPoster:
         :param followee_user_id: id пользователя. Число.
         :return: ничего
         '''
+        self.pf.setdefault(follower_user_id, [])
+        self.pf[follower_user_id].append(followee_user_id)
         pass
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
@@ -46,6 +54,14 @@ class VKPoster:
         :return: Список из post_id размером К из свежих постов в
         ленте пользователя. list
         '''
+        q = list()
+        s = self.pf[user_id]
+        for i in range(len(s)):
+            l1 = self.pp[s[i]]
+            for j in range(len(l1)):
+                q.append(l1[j])
+        q = sorted(q, reverse=True)
+        return q[:k]
         pass
 
     def get_most_popular_posts(self, k: int) -> list:
@@ -56,4 +72,21 @@ class VKPoster:
         необходимо вывести. Число.
         :return: Список из post_id размером К из популярных постов. list
         '''
+        kd = {}
+        q = []
+        ks = []
+        for key in self.pr:
+            self.pr[key] = list(set(self.pr[key]))
+            s = len(self.pr[key])
+            kd.setdefault(key, s)
+            q.append(s)
+        q = list(set(q))
+        q = sorted(q, reverse=True)
+        for i in range(len(q)):
+            for k1, v in kd.items():
+                if q[i] == v:
+                    ks.append(k1)
+        ks = ks[:k]
+        ks = sorted(ks, reverse=True)
+        return ks
         pass
