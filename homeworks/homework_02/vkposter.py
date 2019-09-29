386f5bb40bcdfa932f3f00c9b535b634c38139a2
denisimus97 <super-denis97@yandex.ru>
2019-09-30 02:32:00 +0300

    vk poster 1.0

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -2,10 +2,13 @@
 # coding: utf-8
 
 
+
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.user_post = {}
+        self.user_subs = {}
+        self.post_watched = {}
 
     def user_posted_post(self, user_id: int, post_id: int):
         '''
@@ -15,6 +18,10 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
+        if user_id in self.user_post:
+            self.user_post[user_id].insert(0, post_id)
+        else:
+            self.user_post[user_id] = [post_id]
         pass
 
     def user_read_post(self, user_id: int, post_id: int):
@@ -25,6 +32,11 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
+        if post_id in self.post_watched:
+            if user_id not in self.post_watched[post_id]:
+                self.post_watched[post_id].append(user_id)
+        else:
+            self.post_watched[post_id] = [user_id]
         pass
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
@@ -35,6 +47,11 @@ class VKPoster:
         :param followee_user_id: id пользователя. Число.
         :return: ничего
         '''
+        if follower_user_id in self.user_subs:
+            if followee_user_id not in self.user_subs[follower_user_id]:
+                self.user_subs[follower_user_id].append(followee_user_id)
+        else:
+            self.user_subs[follower_user_id] = [followee_user_id]
         pass
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
@@ -46,6 +63,17 @@ class VKPoster:
         :return: Список из post_id размером К из свежих постов в
         ленте пользователя. list
         '''
+        posts = []
+        if user_id in self.user_subs:
+            for sub in self.user_subs[user_id]:
+                    if sub in self.user_post:
+                        posts = posts + self.user_post[sub]
+            for post in posts:
+                if post in self.post_watched:
+                    if user_id in self.post_watched[post]:
+                        posts.remove(post)
+            posts.sort(reverse=True)
+        return posts[:k]
         pass
 
     def get_most_popular_posts(self, k: int) -> list:
@@ -56,4 +84,20 @@ class VKPoster:
         необходимо вывести. Число.
         :return: Список из post_id размером К из популярных постов. list
         '''
+        posts = self.post_watched.copy()
+        for key in posts:
+            posts[key] = len(posts[key])
+        views = sorted(set(posts.values()), reverse=True)
+        out = {}
+        for view in views:
+            out[view] = []
+            for post in posts:
+                if posts[post] == view:
+                    out[view].append(post)
+                    out[view].sort(reverse=True)
+        result = []
+        for key in out:
+            result = result + out[key]
+        if len(result) >= k:
+            return result[:k]
         pass
