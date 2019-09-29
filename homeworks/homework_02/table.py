cbba1262269c2bdd0726859d2431e13cadcc0123
danilsmirn <dddssasd@mail.ru>
2019-09-29 21:02:59 +0300

    upd_table

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -6,17 +6,17 @@ from table_print import print_table
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-try:
-    f = open(filename, "r")
-    f.close()
-except:
-    print('Файл не валиден')
-    sys.exit()
-en = enc(filename)
-if en not in ('utf-8', 'utf-16', 'cp1251'):
-    print("Формат не валиден")
-    sys.exit()
-if json_check(filename, en):
-    print_table(json_read(filename, en))
-if tsv_check(filename, en):
-    print_table(tsv_read(filename, en))
+    try:
+        f = open(filename, "r")
+        f.close()
+    except:
+        print('Файл не валиден')
+        sys.exit()
+    en = enc(filename)
+    if en not in ('utf-8', 'utf-16', 'cp1251'):
+        print("Формат не валиден")
+        sys.exit()
+    if json_check(filename, en):
+        print_table(json_read(filename, en))
+    if tsv_check(filename, en):
+        print_table(tsv_read(filename, en))
