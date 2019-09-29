40512a3bdf62f2133a522b25fba9ff3a19719051
Maksim Agafonov <maksim.agafonofff@gmail.com>
2019-09-29 21:43:30 +0300

    upd

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -38,7 +38,7 @@ class VKPoster:
 
     def get_most_popular_posts(self, k: int) -> list:
         tmp = []
-        for key, value in enumerate(self.posts):
+        for key, value in self.posts.items():
             tmp.append(key, len(value))
         tmp.sort(key=lambda tmp: tmp[1], reverse=True)
         tmp.sort(key=lambda tmp: tmp[0], reverse=True)
