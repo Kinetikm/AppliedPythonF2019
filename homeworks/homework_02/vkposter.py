0a6617c69cfb017471087e93da8ebf4eb1169c3f
Maximenko Daniil <engmaxidan@gmail.com>
2019-09-29 17:47:01 +0400

    vkposter changes

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -3,9 +3,11 @@
 
 
 class VKPoster:
-
+    posts = dict()
+    users = dict()
+    follow = dict()
     def __init__(self):
-        raise NotImplementedError
+        pass
 
     def user_posted_post(self, user_id: int, post_id: int):
         '''
@@ -15,7 +17,11 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
+        VKPoster.posts[post_id] = []
+        try:
+            VKPoster.users[user_id].append(post_id)
+        except KeyError:
+            VKPoster.users[user_id] = [post_id]
 
     def user_read_post(self, user_id: int, post_id: int):
         '''
@@ -25,7 +31,13 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
+        if user_id in VKPoster.posts[post_id]:
+            return None
+        else:
+            try:
+                VKPoster.posts[post_id].append(user_id)
+            except KeyError:
+                VKPoster.posts[post_id] = [user_id]
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         '''
@@ -35,7 +47,10 @@ class VKPoster:
         :param followee_user_id: id пользователя. Число.
         :return: ничего
         '''
-        pass
+        try:
+            VKPoster.follow[followee_user_id].append(follower_user_id)
+        except KeyError:
+            VKPoster.follow[followee_user_id] = [follower_user_id]
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
         '''
@@ -46,7 +61,13 @@ class VKPoster:
         :return: Список из post_id размером К из свежих постов в
         ленте пользователя. list
         '''
-        pass
+        sort_list = list()
+        for i in VKPoster.follow[user_id]:
+            for j in VKPoster.users[i]:
+                sort_list.append(j)
+        sort_list.sort()
+        sort_list.reverse()
+        return sort_list[:k]
 
     def get_most_popular_posts(self, k: int) -> list:
         '''
@@ -56,4 +77,22 @@ class VKPoster:
         необходимо вывести. Число.
         :return: Список из post_id размером К из популярных постов. list
         '''
-        pass
+        ret_list = list()
+        dict_posts = dict()
+        for key, value in VKPoster.posts.items():
+            try:
+                dict_posts[len(value)].append(key)
+            except KeyError:
+                dict_posts[len(value)] = [key]
+        list_items = list(dict_posts.items())
+        list_items.sort()
+        for i in list_items:
+            i[1].sort()
+        for i in range(len(list_items)):
+            for j in range(len(list_items[i][1])):
+                ret_list.append(list_items[i][1][j])
+        ret_list.reverse()
+        ret_list = ret_list[:k]
+        ret_list.sort()
+        ret_list.reverse()
+        return ret_list
