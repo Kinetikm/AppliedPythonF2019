df769df9260ce284d58029710222cf047d39903e
DanilTremasov <48184229+DanilTremasov@users.noreply.github.com>
2019-09-29 17:08:13 +0300

    Update hw1_winversion.py

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -1,23 +1,22 @@
+  
 #!/usr/bin/env python
 # coding: utf-8
 
 
 def word_inversion(input_lst):
-    input_lst = input_lst[::-1]
+    input_lst.reverse()
     n = 0
     for i in range(len(input_lst)):
         if input_lst[i] == ' ':
-            check = i
-            tmp = check
-            for k in range(n, n + ((check - n + 1) // 2)):
+            tmp = i
+            for k in range(n, n + ((i - n + 1) // 2)):
                 input_lst[n], input_lst[tmp-1] = input_lst[tmp-1], input_lst[n]
                 tmp = tmp - 1
                 n = n + 1
-            n = check + 1
+            n = i + 1
         if i == (len(input_lst)-1):
             tmp = i
-            check = tmp
-            for j in range(n, n + ((check - n + 1) // 2)):
+            for j in range(n, n + ((i - n + 1) // 2)):
                 input_lst[n], input_lst[tmp] = input_lst[tmp], input_lst[n]
                 tmp = tmp - 1
                 n = n + 1
