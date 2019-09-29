88306b67ca519121bd69a531fe276687494aec56
Maksim Agafonov <maksim.agafonofff@gmail.com>
2019-09-29 21:05:08 +0300

    upd

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -12,56 +12,41 @@ class VKPoster:
         if user_id in self.users.keys():
             self.users[user_id].append(post_id)
         else:
-            self.users.update({user_id: [post_id]})
+            self.users[user_id] = [post_id]
 
     def user_read_post(self, user_id: int, post_id: int):
         if user_id in self.users.keys() and post_id in self.posts.keys():
-            self.posts[post_id].append(user_id)
+            if user_id not in self.posts[post_id]:
+                self.posts[post_id].append(user_id)
         elif user_id in self.users.keys():
-            self.posts.update({post_id: [user_id]})
+            self.posts[post_id] = [user_id]
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         if follower_user_id in self.subs.keys():
             self.subs[follower_user_id].append(followee_user_id)
         else:
-            self.subs.update({follower_user_id: [followee_user_id]})
+            self.subs[follower_user_id] = [followee_user_id]
 
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
-        '''
-        Метод который вызывается когда пользователь user_id
-        запрашивает k свежих постов людей на которых он подписан.
-        :param user_id: id пользователя. Число.
-        :param k: Сколько самых свежих постов необходимо вывести. Число.
-        :return: Список из post_id размером К из свежих постов в
-        ленте пользователя. list
-        '''
-        tmp = ()
+        tmp = []
         if user_id in self.subs.keys():
             for i in range(len(self.subs[user_id])):
-                sorted(self.users[i])
+                self.users[i].sort()
                 for j in range(k):
                     tmp.append(self.users[i][j])
         return tmp
 
 
     def get_most_popular_posts(self, k: int) -> list:
-        '''
-        Метод который возвращает список k самых популярных постов за все время,
-        остортированных по свежести.
-        :param k: Сколько самых свежих популярных постов
-        необходимо вывести. Число.
-        :return: Список из post_id размером К из популярных постов. list
-        '''
-        tmp = ()
+        tmp = []
         for key,value in enumerate(self.posts):
             tmp.append(key, len(value))
-        sorted(tmp, key = lambda tmp: tmp[1])
-        sorted(tmp, key = lambda tmp: tmp[0], reverse = True)
-        res = ()
+        tmp.sort(tmp, key = lambda tmp: tmp[1], reverse = True)
+        tmp.sort(tmp, key = lambda tmp: tmp[0], reverse = True)
+        res = []
         for i in range(k):
             res.append(tmp[i][0])
-        tmp = ()
         return res
 
 
