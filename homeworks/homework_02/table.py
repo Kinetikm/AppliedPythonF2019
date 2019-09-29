262ff2e1178f4317a5ae1ed78a75e197a8994833
vlad1m1r_17 <zamyatovskiy.vi@gmail.com>
2019-09-29 09:56:09 +0300

    Printing table done

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,24 @@
 import sys
 
 # Ваши импорты
+from homeworks.homework_02.table_view import Table
+from homeworks.homework_02.file_reader import read_file
+from homeworks.homework_02.json_tsv_reader import extract_data
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-
-    # Ваш код
+    table = Table()
+    ret_code, data = read_file(filename)
+    if ret_code == 1:
+        print("Файл не валиден")
+    elif ret_code == 2:
+        print("Формат не валиден")
+    else:
+        (ret_code, header, rows) = extract_data(data)
+        if ret_code == 1:
+            print("Формат не валиден")
+        else:
+            table.add_header(header)
+            for row in rows:
+                table.add_row(row)
+            table.print_table()
