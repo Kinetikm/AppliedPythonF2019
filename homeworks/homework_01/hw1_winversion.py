1af3c57b78739024b1f533d25c3f14a585072b0e
Bazzer5678904 <48682371+Bazzer5678904@users.noreply.github.com>
2019-09-29 20:51:12 +0300

    Update hw1_winversion.py
    some fixes

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
+ 
+ 
 def word_inversion(input_lst):
-    start = 0
+    input_lst.reverse()
+    k = 0
     for i in range(len(input_lst)):
-        a = input_lst.pop()
-        if (a == " "):
-            start = i
-            input_lst.insert(start, " ")
-            start += 1
-        else:
-            input_lst.insert(start, a)
+        if input_lst[i] == ' ':
+            tmp = i
+            for k in range(k, k + ((i - k + 1) // 2)):
+                input_lst[k], input_lst[tmp-1] = input_lst[tmp-1], input_lst[k]
+                tmp = tmp - 1
+                k = k + 1
+            k = i + 1
+        if i == (len(input_lst)-1):
+            tmp = i
+            for j in range(k, k + ((i - k + 1) // 2)):
+                input_lst[k], input_lst[tmp] = input_lst[tmp], input_lst[k]
+                tmp = tmp - 1
+                k = k + 1
     return input_lst
