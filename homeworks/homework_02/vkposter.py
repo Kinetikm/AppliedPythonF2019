b021458041eaa9d372a9c9220f46ec10c630ea31
Smirnov Ilya <box53i@mail.ru>
2019-09-30 01:57:48 +0300

    vk

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -12,20 +12,16 @@ class VKPoster:
         self.dict_of_followers = {}
         self.dict_of_readers = {}
 
-
     def user_posted_post(self, user_id: int, post_id: int):
         self.dict_of_users.setdefault(user_id, []).append(post_id)
         self.dict_of_posts[post_id] = []
-
 
     def user_read_post(self, user_id: int, post_id: int):
         if user_id not in self.dict_of_posts.setdefault(post_id, []):
             self.dict_of_posts[post_id].append(user_id)
 
-
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
-            self.dict_of_followers.setdefault(follower_user_id,[]).append(followee_user_id)
-
+            self.dict_of_followers.setdefault(follower_user_id, []).append(followee_user_id)
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
         list = []
@@ -35,7 +31,6 @@ class VKPoster:
         list.reverse()
         list = list[:k:]
         return list
-
 
     def get_most_popular_posts(self, k: int) -> list:
         tmp_list = []
