67c68ad8a307e35d516ab464d1da4d7fbf8c4b14
vlad1m1r_17 <zamyatovskiy.vi@gmail.com>
2019-09-29 09:55:03 +0300

    File reading utils added

--- homeworks/homework_02/file_reader.py
+++ homeworks/homework_02/file_reader.py
@@ -4,16 +4,28 @@ import os.path
 
 def read_file(filename):
     if not os.path.exists(filename):
-        print("Файл не валиден")
-        return
-    file = None
+        return 1, None
     try:
         file = open(filename, encoding='utf-8')
-    except UnicodeError as err:
+        data = file.read()
+    except (FileNotFoundError, FileExistsError) as err:
+        return 1, None
+    except UnicodeError:
+        print('utf8 failed')
+        file.close()
         try:
             file = open(filename, encoding='utf-16')
-        except UnicodeError as err:
+            data = file.read()
+        except UnicodeError:
+            print('utf16 failed')
+            file.close()
             try:
                 file = open(filename, encoding='cp1251')
-            except UnicodeTranslateError as err:
-                print("Формат не валиден")
+                data = file.read()
+            except ValueError:
+                print('cp1251 failed')
+                file.close()
+                return 2, None
+    finally:
+        file.close()
+    return 0, data
