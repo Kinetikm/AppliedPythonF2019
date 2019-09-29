6e307570049aa7c636169e1276334b2296713340
VladislavKuzevanov <vlad.kuzevanov@mail.ru>
2019-09-29 14:18:07 +0300

    Домашка на 2 неделю

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -5,7 +5,7 @@
 import heapq
 
 
-# первый лист - собственный пост; второй лист - подписчики; третий - прочитанные посты; четвертый - подписки
+# первый лист - собственный пост; второй - прочитанные посты; третий - подписки
 
 
 class VKPoster:
@@ -17,45 +17,46 @@ class VKPoster:
     def user_posted_post(self, user_id: int, post_id: int):
         self.post_id = post_id
         self.user_id = user_id
-        self.posts[post_id] = []
+        self.posts[post_id] = 0
         if user_id not in self.post_relationship:
-            self.post_relationship[user_id] = [[], [], [], []]
+            self.post_relationship[user_id] = [[], [], []]
             self.post_relationship[user_id][0].append(post_id)
         else:
             self.post_relationship[user_id][0].append(post_id)
 
     def user_read_post(self, user_id: int, post_id: int):
         if user_id not in self.post_relationship.keys():
-            self.post_relationship[user_id] = [[], [], [], []]
-            self.post_relationship[user_id][2] = [post_id]
+            self.post_relationship[user_id] = [[], [], []]
+            self.post_relationship[user_id][1] = [post_id]
         else:
-            self.post_relationship[user_id][2].append(post_id)
+            self.post_relationship[user_id][1].append(post_id)
         if post_id not in self.posts.keys():
             self.posts[post_id] = []
         if user_id not in self.posts[post_id]:
             self.posts[post_id].append(user_id)
+        if post_id in self.post_relationship[user_id][1]:
+            self.posts[post_id] += 1
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         if follower_user_id in self.post_relationship.keys():
-            self.post_relationship[follower_user_id][3].append(followee_user_id)
+            self.post_relationship[follower_user_id][2].append(followee_user_id)
         else:
             self.post_relationship[follower_user_id] = [[], [], [], []]
-            self.post_relationship[follower_user_id][3] = [followee_user_id]
+            self.post_relationship[follower_user_id][2] = [followee_user_id]
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
         temp = []
-        for i in self.post_relationship[user_id][3]:
+        for i in self.post_relationship[user_id][2]:
             for j in self.post_relationship[i][0]:
                 temp.append(j)
         temp.sort(reverse=True)
         return temp[:k:]
 
     def get_most_popular_posts(self, k: int) -> list:
-        list_d = list(self.posts.items())
-        list_d.sort(key=lambda x: len(x[1]), reverse=True)
-        temp = [i[0] for i in list_d[:k]]
-        # temp.sort(reverse=True)
-        return temp
+        list = []
+        for key, val in self.posts.items():
+            heapq.heappush(list, (val, key))
+        return [s[1] for s in heapq.nlargest(k, list)]
 
     def print_VK(self):
         print("self.post_relationship: ", self.post_relationship, "\n", "self.posts: ", self.posts, "\n\n")
