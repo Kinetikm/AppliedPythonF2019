933b1379e1af8d071c6443aa73ad6af252561b72
Parviz Jamilov <parviz.jamilov@mail.ru>
2019-09-29 22:06:05 +0300

    first task fix

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -7,12 +7,12 @@ def word_inversion(input_lst):
     lastcheck = 0
     for i in range(0, len(input_lst)):
         if input_lst[i] == ' ':
-            cycl = lastcheck + (i - lastcheck) // 2
+            cycl = lastcheck + (i - lastcheck + 1) // 2
             for j in range(lastcheck, cycl):
                 input_lst[j], input_lst[i-1-j+lastcheck] = input_lst[i-1-j+lastcheck], input_lst[j]
             lastcheck = i + 1
         if i == (len(input_lst) - 1):
-            cycl = lastcheck + (i - lastcheck) // 2
+            cycl = lastcheck + (i - lastcheck + 1) // 2
             for j in range(lastcheck, cycl):
                 input_lst[j], input_lst[i - j + lastcheck] = input_lst[i - j + lastcheck], input_lst[j]
     return input_lst
