eb885f2ed1e1e9306f3fd435a094b2b86e92d0ff
vlad1m1r_17 <zamyatovskiy.vi@gmail.com>
2019-09-29 10:28:45 +0300

    Optimized structure of file reader utils

--- homeworks/homework_02/file_reader.py
+++ homeworks/homework_02/file_reader.py
@@ -5,27 +5,21 @@ import os.path
 def read_file(filename):
     if not os.path.exists(filename):
         return 1, None
-    try:
-        file = open(filename, encoding='utf-8')
-        data = file.read()
-    except (FileNotFoundError, FileExistsError) as err:
-        return 1, None
-    except UnicodeError:
-        print('utf8 failed')
-        file.close()
+    encodings = ['utf-8', 'utf-16', 'cp1251']
+    file = None
+    data = None
+    for encoding in encodings:
         try:
-            file = open(filename, encoding='utf-16')
+            file = open(filename, encoding=encoding)
             data = file.read()
-        except UnicodeError:
-            print('utf16 failed')
             file.close()
-            try:
-                file = open(filename, encoding='cp1251')
-                data = file.read()
-            except ValueError:
-                print('cp1251 failed')
+            break
+        except (FileNotFoundError, FileExistsError):
+            return 1, None
+        except UnicodeError:
+            if file:
                 file.close()
-                return 2, None
-    finally:
-        file.close()
+                file = None
+    if data is None:
+        return 2, None
     return 0, data
