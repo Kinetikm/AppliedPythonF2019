e54f124b7ca0daf0cff76e4718483475acf8e149
Misha945 <mix.zaharov@gmail.com>
2019-09-29 21:57:12 +0300

    first commit

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -5,9 +5,15 @@
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.users = {}
+        # Ключи - айдишники пользователей.
+        # Элементы - списки из их подписчиков.
+        self.posts = {}
+        # Ключи - айдишники постов
+        # Элементы - списки из айдишников просмотревших (создатель первый).
 
     def user_posted_post(self, user_id: int, post_id: int):
+        self.posts.update({post_id: [user_id]})
         '''
         Метод который вызывается когда пользователь user_id
         выложил пост post_id.
@@ -15,9 +21,9 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
 
     def user_read_post(self, user_id: int, post_id: int):
+        self.posts[post_id].append(user_id)
         '''
         Метод который вызывается когда пользователь user_id
         прочитал пост post_id.
@@ -25,9 +31,9 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
+        self.users[followee_user_id].append(follower_user_id)
         '''
         Метод который вызывается когда пользователь follower_user_id
         подписался на пользователя followee_user_id.
@@ -35,9 +41,21 @@ class VKPoster:
         :param followee_user_id: id пользователя. Число.
         :return: ничего
         '''
-        pass
 
-    def get_recent_posts(self, user_id: int, k: int)-> list:
+    def get_recent_posts(self, user_id: int, k: int) -> list:
+        following = []
+        for i in self.users.keys():
+            if user_id in self.users[i]:
+                following.append(i)
+        posts = []
+        for i in self.posts.keys():
+            if self.posts[i][0] in following:
+                posts.append(i)
+        posts.sort()
+        posts.reverse()
+        while len(posts) > k:
+            posts.pop()
+        return posts
         '''
         Метод который вызывается когда пользователь user_id
         запрашивает k свежих постов людей на которых он подписан.
@@ -46,9 +64,25 @@ class VKPoster:
         :return: Список из post_id размером К из свежих постов в
         ленте пользователя. list
         '''
-        pass
 
     def get_most_popular_posts(self, k: int) -> list:
+        popularity = {}
+        # Ключи - кол-во просмотревших, элементы - айди постов (всех)
+        for i in self.posts.keys():
+            wc = []
+            for k in self.posts[i]:
+                if k not in wc:
+                    wc.append(k)
+            popularity.update({len(wc): i})
+        L = list(popularity.keys)
+        L.sort()
+        L.reverse()
+        Result = []
+        for i in L:
+            Result.append(popularity[i])
+            if len(Result) == k:
+                break
+        return Result
         '''
         Метод который возвращает список k самых популярных постов за все время,
         остортированных по свежести.
@@ -56,4 +90,3 @@ class VKPoster:
         необходимо вывести. Число.
         :return: Список из post_id размером К из популярных постов. list
         '''
-        pass
