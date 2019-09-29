ed3cb4a9bbd9a8bf4a85ccbf8e8a52ba99efcf78
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-09-30 00:56:17 +0300

    Update vkposter.py

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -12,17 +12,17 @@ class VKPoster:
     def user_posted_post(self, user_id: int, post_id: int):
         if user_id not in self.users:
             self.users[user_id] = []
-            self.users[user_id].append(post_id)
+        self.users[user_id].append(post_id)
 
     def user_read_post(self, user_id, post_id):
         if post_id not in self.post_views:
             self.post_views[post_id] = set()
-            self.post_views[post_id].add(user_id)
+        self.post_views[post_id].add(user_id)
 
     def user_follow_for(self, follower_user_id, followee_user_id):
         if follower_user_id not in self.follow:
             self.follow[follower_user_id] = set()
-            self.follow[follower_user_id].add(followee_user_id)
+        self.follow[follower_user_id].add(followee_user_id)
 
     def get_recent_posts(self, user_id, k)-> list:
         list_a = list(self.follow[user_id])
