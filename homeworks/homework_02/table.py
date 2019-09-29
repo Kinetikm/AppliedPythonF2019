7219190083709f9fb3d6532c05b0c364f2bb6ac2
Mikhail <zojloverr@gmail.com>
2019-09-30 00:52:39 +0300

    hw2 Parshin Mikhail

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,18 @@
 import sys
+from read_file import read_file
+from decode import read_json
+from decode import read_csv
+from draw_table import  draw_table
 
-# Ваши импорты
 
 if __name__ == '__main__':
     filename = sys.argv[1]
 
-    # Ваш код
+text = read_file(filename)
+if text is None:
+    print("Not valid format")
+else:
+    text_dict = read_json(text)
+    if text_dict is None:
+        text_dict = read_csv(text)           # we get list of dicts
+return draw_table(text_dict)
