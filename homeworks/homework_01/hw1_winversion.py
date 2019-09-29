fe5d3f7a064e15e8afc949a75b301b11e5db5c1c
MaximAbramenko <gotosleepmcht@gmail.com>
2019-09-29 16:37:59 +0300

    peredel

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,11 +8,11 @@ def word_inversion(input_lst):
     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
     :return: None Все изменения в input_lst проходят
     '''
-    input_lst = input_lst[::-1]
+    input_lst[::-1]
     k = 0
     for i in range(len(input_lst)):
         if input_lst[i] == " ":
-            input_lst[k:i] = input_lst[k:i][::-1]
+            input_lst[k:i][::-1]
             k = i + 1
-    input_lst[k:] = input_lst[k:][::-1]
+    input_lst[k:][::-1]
     return input_lst
