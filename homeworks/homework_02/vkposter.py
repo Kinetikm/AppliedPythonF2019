ebd91431f86b5842e369ebe10a752a81b90c1a3e
fatkullinalina <alina2469@gmail.com>
2019-09-29 17:57:55 +0300

    vkposter

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -5,7 +5,9 @@
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.post = {}
+        self.read = {}
+        self.watched = {}
 
     def user_posted_post(self, user_id: int, post_id: int):
         '''
@@ -15,7 +17,9 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
+        if user_id not in self.post:
+            self.post[user_id] = {0: [], 1: []}
+        self.post[user_id][0].append(post_id)
 
     def user_read_post(self, user_id: int, post_id: int):
         '''
@@ -25,7 +29,12 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
+        if post_id not in self.watched:
+            self.watched[post_id] = []
+            self.read[post_id] = 0
+        if user_id not in self.watched[post_id]:
+            self.watched[post_id].append(user_id)
+            self.read[post_id] += 1
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         '''
@@ -35,7 +44,9 @@ class VKPoster:
         :param followee_user_id: id пользователя. Число.
         :return: ничего
         '''
-        pass
+        if follower_user_id not in self.post:
+            self.post[follower_user_id] = {0: [], 1: []}
+        self.post[follower_user_id][1].append(followee_user_id)
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
         '''
@@ -46,7 +57,11 @@ class VKPoster:
         :return: Список из post_id размером К из свежих постов в
         ленте пользователя. list
         '''
-        pass
+        a = list()
+        for user in self.post[user_id][1]:
+            a += self.post[user][0]
+        a = sorted(a, reverse=True)
+        return a[:k]
 
     def get_most_popular_posts(self, k: int) -> list:
         '''
@@ -56,4 +71,11 @@ class VKPoster:
         необходимо вывести. Число.
         :return: Список из post_id размером К из популярных постов. list
         '''
-        pass
+        b = list()
+        r = list(self.read.items())
+        r.sort()
+        r.sort(key=lambda i: i[1])
+        r = r[::-1]
+        for i in range(0, k, 1):
+            b.append(r[i][0])
+        return b
