1a7dc490d9c40a65306d22caef8f35857f197ae0
Parviz Jamilov <parviz_jamilov@MacBook-Pro-Parviz.local>
2019-09-29 21:32:58 +0300

    final task is done

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,18 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    if any(len(raw) != len(list_of_lists) for raw in list_of_lists):
+        return None
+    if len(list_of_lists) == 2:
+        return list_of_lists[0][0] * list_of_lists[1][1] - \
+               list_of_lists[0][1] * list_of_lists[1][0]
+    if len(list_of_lists) == 1:
+        return list_of_lists[0][0]
+    det = 0
+    for i in range(len(list_of_lists)):
+        det += (-1) ** i * list_of_lists[0][i] * calculate_determinant(
+            [[list_of_lists[k][j] for j in range(len(
+                list_of_lists)) if j != i] for k in range(
+                len(list_of_lists)) if k != 0]
+        )
+    return det
