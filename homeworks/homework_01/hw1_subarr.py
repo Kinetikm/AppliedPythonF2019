8ab8dbf2d9593e6084dbaf0fd9d4b13ef1d5b2c3
Misha945 <mix.zaharov@gmail.com>
2019-09-30 03:17:17 +0300

    fixed

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -5,8 +5,8 @@
 def find_subarr(input_lst, num):
     for i in range(len(input_lst)):
         summ = 0
-        for j in range(len(input_lst) - i):
-            summ += input_lst[i+j]
+        for k in range(len(input_lst) - i):
+            summ += input_lst[i + k]
             if summ == num:
-                return (i, i + j)
+                return (i, i + k)
     return ()
