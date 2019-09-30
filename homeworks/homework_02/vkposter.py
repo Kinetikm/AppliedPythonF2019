2d36f0f165f79f20a97cd6b4955c3beea1e35207
MrKaliostro <49442839+MrKaliostro@users.noreply.github.com>
2019-09-30 08:08:27 +0300

    Update vkposter.py

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -3,7 +3,6 @@
 
 import heapq
 
-
 class VKPoster:
 
     def __init__(self):
@@ -12,27 +11,23 @@ class VKPoster:
         self.user_follows = {}
         self.read_by_dict = {}
 
-
     def user_posted_post(self, user_id: int, post_id: int):
         self.all_posts.setdefault(user_id, []).append(post_id)
         self.read_by_dict[post_id] = []
-
 
     def user_read_post(self, user_id: int, post_id: int):
         if user_id not in self.all_posts:
             self.all_posts[post_id].append(user_id)
 
-
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         self.user_follows.setdefault(follower_user_id, []).append(followee_user_id)
-
 
     def get_recent_posts(self, user_id: int, k: int)-> list:
         list = []
         for i in self.user_follows:
             list.extend(self.user_follows[i])
-        return list.sort()[:k:-1]
-
+            list.sort()
+        return list[:k:-1]
 
     def get_most_popular_posts(self, k: int) -> list:
         list = []
