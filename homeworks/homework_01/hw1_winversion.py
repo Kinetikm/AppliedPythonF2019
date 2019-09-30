ea6a3c483cc52aaed506d17ed64a37cd7e3b6000
Misha945 <mix.zaharov@gmail.com>
2019-09-30 03:21:58 +0300

    fixed winvers

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -2,28 +2,19 @@
 # coding: utf-8
 
 
+def swap(i, j, lst):
+    for k in range(len(lst[i + 1:j])//2):
+        lst[i + 1 + k], lst[j - 1 - k] = lst[j - 1 - k], lst[i + 1 + k]
+
+
 def word_inversion(input_lst):
-    if input_lst == []:
-        return []
-    List = []
-    List.append(input_lst.pop(0))
-    k = 0
-    while True:
-        if input_lst == []:
-            break
-        if input_lst[0] == ' ':
-            k += 1
-            input_lst.pop(0)
-            List.append(input_lst.pop(0))
-        else:
-            List[k] = List[k] + input_lst.pop(0)
-    List.reverse()
-    while True:
-        if List == []:
-            break
-        w = List.pop(0)
-        for c in w:
-            input_lst.append(c)
-        input_lst.append(' ')
-    input_lst.pop()
+    input_lst.reverse()
+    a = -1
+    b = 0
+    for i in range(len(input_lst)):
+        if input_lst[i] == ' ':
+            b = i
+            swap(a, b, input_lst)
+            a = b
+    swap(a, len(input_lst), input_lst)
     return input_lst
