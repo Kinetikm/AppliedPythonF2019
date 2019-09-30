8f3029b91792460514e9a18baae7018ae3af124f
asyadyoma <asyadyoma@yandex.ru>
2019-09-30 09:51:44 +0300

    Демина Анастасия HW_02

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,12 +1,8 @@
-# -*- coding: utf-8 -*-
 import sys
 
 # Ваши импорты
-from json_file import json_data
-from formater_text import text_formater
-from tsv_file import tsv_data
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    
+
     # Ваш код
