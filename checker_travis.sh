f2b59fbe03c531bb6f917dc1f4a3e03ef20c3771
Mikhail Baranov <mikhail.baranov@corp.mail.ru>
2019-09-30 11:29:04 +0300

    Checker update

--- checker_travis.sh
+++ checker_travis.sh
@@ -5,9 +5,11 @@ function check_python_code(){
 function check_branch_name(){
  IFS=', ' read -r -a branch_data <<< "$(git show -s --pretty=%d HEAD | tr -d '()')"
  if [ "$1" != "${branch_data[2]}" ]; then
+   echo "Bad branch name your value: ${branch_data[2]}, asked: $1}"
    exit 1
  fi
  if [ "origin/${branch_data[2]}" != "${branch_data[1]}" ]; then
+   echo "Bad pull request, should be from origin/$1 not from ${branch_data[1]}"
    exit 1
  fi
 }
@@ -18,5 +20,5 @@ if [[ $# -lt 1 ]]; then
    exit 1
 fi
 
-check_branch_name ${args[0]}
+check_branch_name "${args[0]}"
 check_python_code
