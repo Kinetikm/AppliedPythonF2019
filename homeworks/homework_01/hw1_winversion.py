3462c313127da1a19c0068f6b6655dba7fae8dea
Vasoskirko <vasoskirko@gmail.com>
2019-09-30 03:27:34 +0300

    fixed

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -1,24 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def change(i, j, list_):
+    for l in range(len(list_[i + 1:j])//2):
+        list_[i + 1 + l], list_[j - 1 - l] = list_[j - 1 - l], list_[i + 1 + l]
+
+
 def word_inversion(input_lst):
-    w = ''
-    for i in range(len(input_lst)):
-        char = input_lst.pop(0)
-        if char == ' ':
-            input_lst.append(w)
-            w = ''
-            continue
-        w += char
-    input_lst.append(w)
     input_lst.reverse()
+    arg1 = -1
+    arg2 = 0
     for i in range(len(input_lst)):
-        if i == len(input_lst)-1:
-            w = input_lst.pop(0)
-            input_lst.append(w)
-        else:
-            w = input_lst.pop(0)
-            input_lst.append(w+' ')
-    w = ''
-    for i in range(len(input_lst)):
-        c = input_lst.pop(0)
-        w += c
-    input_lst.append(w)
-    return list(input_lst[0])
+        if input_lst[i] == ' ':
+            arg2 = i
+            change(arg1, arg2, input_lst)
+            arg1 = arg2
+    change(arg1, len(input_lst), input_lst)
+    return input_lst
