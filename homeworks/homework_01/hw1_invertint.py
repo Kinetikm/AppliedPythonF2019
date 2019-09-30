f2a86fd446f2f9db63f7117606e5fc6fecee79bc
sergeiklochkov <43850350+sergeiklochkov@users.noreply.github.com>
2019-09-30 03:55:44 +0300

    Update hw1_invertint.py

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -9,4 +9,16 @@ def reverse(number):
     :param number: исходное число
     :return: инвертированное число
     '''
+    k = 1
+    n = 0
+    r = 0
+    if number < 0:
+        number = -number
+        k = -1
+    while number >= 1:
+        n = number % 10
+        number = number // 10
+        r = r * 10 + n
+    r = r * k
+    return r
     raise NotImplementedError
