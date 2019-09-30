6fec7e01a944386cd9ee73f3175d8e12a3fd9b19
MrKaliostro <49442839+MrKaliostro@users.noreply.github.com>
2019-09-30 09:26:40 +0300

    Create rawinput.py

---
+++ homeworks/homework_02/rawinput.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import csv
+import json
+import encode as ecd
+
+
+def read_jfile(file_name, encode):
+    with open(file_name, "r", encoding=encode) as file:
+        return json.load(file, object_hook=dict, parse_int=str)
+
+
+def read_tfile(file_name, encode):
+    with open(file_name, "r", encoding=encode) as file:
+        str = [i.split("\t") for i in file.read().split("\n")]
+        ret = [{key: data for key, data in zip(str[0], str[j])}
+                    for j in range(1, len(str) - 1)]
+        return ret
+
+
+def open_data(file_name):
+    encode = ecd.ch_encod(file_name)
+    if encode is not None:
+        try:
+            return read_file(file_name, encode)
+        except Exception:
+            return read_tfile(file_name, encode)
+    else:
+        print('Формат не валиден')
