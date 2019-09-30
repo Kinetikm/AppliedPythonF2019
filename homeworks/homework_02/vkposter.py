93bb79870910218c5da22cd4cd6a7d79866428bc
unknown <enzhelia@gmail.com>
2019-09-30 09:36:24 +0300

    vkposter

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -1,59 +1,33 @@
-#!/usr/bin/env python
-# coding: utf-8
+from collections import defaultdict
+from itertools import chain
 
 
 class VKPoster:
-
     def __init__(self):
-        raise NotImplementedError
+        self.user_posts = defaultdict(set)
+        self.user_followees = defaultdict(set)
+        self.post_views = defaultdict(set)
 
-    def user_posted_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        выложил пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+    def user_posted_post(self, user_id, post_id):
+        self.user_posts[user_id].add(post_id)
 
-    def user_read_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        прочитал пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+    def user_read_post(self, user_id, post_id):
+        self.post_views[post_id].add(user_id)
 
-    def user_follow_for(self, follower_user_id: int, followee_user_id: int):
-        '''
-        Метод который вызывается когда пользователь follower_user_id
-        подписался на пользователя followee_user_id.
-        :param follower_user_id: id пользователя. Число.
-        :param followee_user_id: id пользователя. Число.
-        :return: ничего
-        '''
-        pass
+    def user_follow_for(self, user_id, followee_id):
+        self.user_followees[user_id].add(followee_id)
 
-    def get_recent_posts(self, user_id: int, k: int)-> list:
-        '''
-        Метод который вызывается когда пользователь user_id
-        запрашивает k свежих постов людей на которых он подписан.
-        :param user_id: id пользователя. Число.
-        :param k: Сколько самых свежих постов необходимо вывести. Число.
-        :return: Список из post_id размером К из свежих постов в
-        ленте пользователя. list
-        '''
-        pass
+    def fresh_posts_for(self, user_id, limit=5):
+        followees = self.user_followees[user_id]
+        followees_posts = map(self.user_posts.__getitem__, followees)
+        posts = list(chain.from_iterable(followees_posts))
+        posts.sort(reverse=True)
+        return posts[:limit]
 
-    def get_most_popular_posts(self, k: int) -> list:
-        '''
-        Метод который возвращает список k самых популярных постов за все время,
-        остортированных по свежести.
-        :param k: Сколько самых свежих популярных постов
-        необходимо вывести. Число.
-        :return: Список из post_id размером К из популярных постов. list
-        '''
-        pass
+    def top_posts(self, limit=5):
+        posts = self.post_views.keys()
+        posts.sort(
+            key=lambda post_id: len(self.post_views[post_id]),
+            reverse=True,
+        )
+        return posts[:limit]
