5692fdb5b3689ff65117eb2244698e80d196d158
AlexKay28 <Kaygorodow@mail.ru>
2019-09-30 09:39:32 +0300

    rabota nad oshibkami

--- homeworks/homework_01/hw1_subarr.py
+++ homeworks/homework_01/hw1_subarr.py
@@ -11,17 +11,6 @@ def find_subarr(input_lst, num):
     :return: два индекса (начала и конца подмассива). Пустой tuple, если таких нет
     Пример: find_subarr([1, 2, 3, 4, 5, -1], 4) может вернуть (3, 3) или (4, 5)
     '''
-    # summ = 0
-    # tup = ()
-    # for i in range(len(input_lst)):
-    #     for k in range(i, len(input_lst)):
-    #         summ += input_lst[k]
-    #         if summ == num:
-    #             tup = (i, k)
-    #             return tup
-    #     summ = 0
-    # return tup
-
     sum, table = 0, {}
 
     for i, value in enumerate(input_lst):
