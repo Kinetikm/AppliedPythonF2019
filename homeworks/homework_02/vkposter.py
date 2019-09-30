2f5565e1d443e5e30c5e1bbd4a7edb17a774df8b
MrKaliostro <49442839+MrKaliostro@users.noreply.github.com>
2019-09-30 08:34:11 +0300

    Update vkposter.py

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -10,15 +10,16 @@ class VKPoster:
         self.post_viewes = {}
         self.all_posts = {}
         self.user_follows = {}
-        self.read_by_dict = {}
 
     def user_posted_post(self, user_id: int, post_id: int):
         self.all_posts.setdefault(user_id, []).append(post_id)
         self.read_by_dict[post_id] = []
 
     def user_read_post(self, user_id: int, post_id: int):
-        if user_id not in self.all_posts:
-            self.all_posts[post_id].append(user_id)
+        if post_id in self.all_posts.keys():
+            self.all_posts[post_id] += 1
+        else:
+            self.all_posts[post_id] = 1
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         self.user_follows.setdefault(follower_user_id, []).append(followee_user_id)
@@ -26,7 +27,7 @@ class VKPoster:
     def get_recent_posts(self, user_id: int, k: int)-> list:
         list = []
         for i in self.user_follows[user_id]:
-            list.extend(self.user_follows[i])
+            list.extend(self.post_viewes[i])
         list.sort()
         return list[:k:-1]
 
