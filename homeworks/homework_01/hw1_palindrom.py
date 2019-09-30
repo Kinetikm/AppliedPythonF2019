6331d8cf2d173a04caf43c92ce600048ad641dbc
sergeiklochkov <43850350+sergeiklochkov@users.noreply.github.com>
2019-09-30 03:56:22 +0300

    Update hw1_palindrom.py

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -10,4 +10,10 @@ def check_palindrom(input_string):
     :return: True, если строка являестя палиндромом
     False иначе
     '''
+    l = len(input_string)
+    n = l // 2
+    for i in range(0, n, 1):
+        if input_string[i] != input_string[l - i-1]:
+            return False
+    return True
     raise NotImplementedError
