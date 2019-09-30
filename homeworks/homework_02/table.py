ec9f19b191e843ea19de4cb8747648f2c352579b
unknown <enzhelia@gmail.com>
2019-09-30 09:41:40 +0300

    hw2

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,36 @@
-import sys
+from argparse import ArgumentParser
+from table_parser import JSONTableParser, TSVTableParser
+from prettytable import PrettyTable
 
-# Ваши импорты
 
-if __name__ == '__main__':
-    filename = sys.argv[1]
+arg_parser = ArgumentParser()
+arg_parser.add_argument('file_name', type=str)
+arg_parser.add_argument('--encoding', type=str, default='utf-8')
 
-    # Ваш код
+args = arg_parser.parse_args()
+
+text = ''
+try:
+    with open(args.file_name, 'r', encoding=args.encoding) as file:
+        text = file.read()
+except:
+    exit('Файл не валиден')
+
+table_parser_class = None
+if text.lstrip().startswith('['):
+    table_parser_class = JSONTableParser
+else:
+    table_parser_class = TSVTableParser
+
+table_parser = table_parser_class(text)
+
+try:
+    columns, rows = table_parser.parse()
+except:
+    exit('Формат не валиден')
+
+table = PrettyTable(columns)
+for row in rows:
+    table.add_row(row)
+
+print(table)
---
+++ homeworks/homework_02/table_parser.py
@@ -0,0 +1,7 @@
+
+class TableParser:
+    def __init__(self, text):
+        self.text = text
+
+    def parse(self):
+        raise NotImplementedError()
