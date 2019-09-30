fa2acb47127e6f74d848755ee4d3af34acaf0743
sergeiklochkov <43850350+sergeiklochkov@users.noreply.github.com>
2019-09-30 04:04:18 +0300

    Update table.py

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,25 @@
 import sys
-
-# Ваши импорты
+import reader as a
+import writer as c
+import parсer as b
 
 if __name__ == '__main__':
     filename = sys.argv[1]
 
-    # Ваш код
+    filename = "files/posts-cp1251.json"
+    ind = True
+    try:
+        l = a.reader(filename)
+    except FileNotFoundError:
+        print("Файл не валиден")
+        ind = False
+    if l == None and ind:
+        print("Формат не валиден")
+    else:
+        if ind:
+            ll = b.parcer(l)
+    if ll == None and ind:
+        print("Формат не валиден")
+    else:
+        if ind:
+            c.writer(ll)
