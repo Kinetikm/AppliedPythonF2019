fac306e69366a79161edcbf303dd0c234a39e5e0
Filatov Ilya <ilya_filatovv@mail.ru>
2019-10-03 20:57:36 +0300

    HashTable

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -1,82 +1,86 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 class HashMap:
-    """
-    Давайте сделаем все объектненько,
-     поэтому внутри хешмапы у нас будет Entry
-    """
+
     class Entry:
         def __init__(self, key, value):
-            """
-            Сущность, которая хранит пары ключ-значение
-            :param key: ключ
-            :param value: значение
-            """
+            self.key = key
+            self.value = value
 
         def get_key(self):
-            # TODO возвращаем ключ
-            raise NotImplementedError
+            return self.key
 
         def get_value(self):
-            # TODO возвращаем значение
-            raise NotImplementedError
+            return self.value
 
         def __eq__(self, other):
-            # TODO реализовать функцию сравнения
-            raise NotImplementedError
+            return self.key == other.get_key
 
     def __init__(self, bucket_num=64):
-        """
-        Реализуем метод цепочек
-        :param bucket_num: число бакетов при инициализации
-        """
-        raise NotImplementedError
+        self.array = [[] for _ in range(bucket_num)]
+        self.table_size = bucket_num
+        self.fill_factor = 20
 
     def get(self, key, default_value=None):
-        # TODO метод get, возвращающий значение,
-        #  если оно присутствует, иначе default_value
-        raise NotImplementedError
+        idx = self._get_index(self._get_hash(key))
+        for i in self.array[idx]:
+            if i.get_key() == key:
+                default_value = i.get_value()
+        return default_value
 
     def put(self, key, value):
-        # TODO метод put, кладет значение по ключу,
-        #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
+        idx = self._get_index(self._get_hash(key))
+        for i in self.array[idx]:
+            if i.get_key() == key:
+                i.value = value
+                return
+        self.array[idx].append(self.Entry(key, value))
+        if self.__len__() / self.table_size > self.fill_factor:
+            self._resize()
 
     def __len__(self):
-        # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        len_ = 0
+        for i in self.array:
+            len_ += len(i)
+        return len_
 
     def _get_hash(self, key):
-        # TODO Вернуть хеш от ключа,
-        #  по которому он кладется в бакет
-        raise NotImplementedError
+        return hash(key)
 
     def _get_index(self, hash_value):
-        # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+        return hash_value % self.table_size
 
     def values(self):
-        # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+        return (item.get_value() for bucket in self.array for item in bucket)
 
     def keys(self):
-        # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+        return (item.get_key() for bucket in self.array for item in bucket)
 
     def items(self):
-        # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+        return ((item.get_key(), item.get_value()) for bucket in self.array for item in bucket)
 
     def _resize(self):
-        # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+        buckets_tmp = self.array[::]
+        self.table_size = self.table_size * 2
+        self.array = [[] for _ in range(self.table_size)]
+        for bucket in buckets_tmp:
+            for item in range(len(bucket)):
+                self.put(bucket[item].get_key(), bucket[item].get_value())
 
     def __str__(self):
-        # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        buckets = "{"
+        items = '{'
+        for i in range(self.table_size):
+            if len(self.array[i]) != 0:
+                buckets += str(i) + ': '
+                for item in self.array[i]:
+                    buckets += str(item.get_key()) + ', '
+                    items += str(item.get_key()) + ': ' + str(item.get_value()) + '; '
+                buckets = buckets[:-2:]
+                buckets += '; '
+        buckets = buckets[:-2:]
+        buckets += '}'
+        items = items[:-2:]
+        items += '}'
+        return 'buckets: {buckets1}\nitems: {items1}'.format(buckets1=buckets, items1=items)
 
     def __contains__(self, item):
-        # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+        return self.get(item.get_key()) == item.get_value()
