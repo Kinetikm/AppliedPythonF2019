17bf25ff25a279dd96c2a569e45388a9c760a6e2
Pavel Lomtev <pavellomtev2012@yandex.ru>
2019-10-03 22:04:45 +0300

    1 попытка

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,22 @@
 import sys
 
 # Ваши импорты
+from print_x import *
+from homeworks.homework_02.encode_x import *
+from type_x import *
+
 
 if __name__ == '__main__':
     filename = sys.argv[1]
 
     # Ваш код
+try:
+    input_data = ch(filename)  # функция ch пытается открытть файл в различных кодировках
+    input_data = type_x(input_data)
+    print_x(input_data)
+except FileNotFoundError:
+        print("Файл не валиден")
+except UnicodeDecodeError:
+        print("Файл не валиден")
+else:
+        print("Формат не валиден")
