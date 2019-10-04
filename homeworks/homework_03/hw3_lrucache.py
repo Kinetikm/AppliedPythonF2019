24f89994e65bb07c46c566e6869b3c59cd1edad6
verbarius <verbarius@mail.ru>
2019-10-04 21:22:41 +0300

    lrucache

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from time import time
+
 
 class LRUCacheDecorator:
 
@@ -10,10 +12,32 @@ class LRUCacheDecorator:
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cach_args = {}
+        self.cach_time = {}
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, func):
+        def cach(*args, **kwargs):
+            temp = ((args, str(kwargs)))
+            if temp in self.cach_args:
+                if self.ttl is not None and (time() - self.cach_time[temp])*1000 > self.ttl:
+                    result = func(*args, **kwargs)
+                    self.cach_args[temp] = result
+                    self.cach_time[temp] = time()
+                else:
+                    result = self.cach_args[temp]
+            else:
+                result = func(*args, **kwargs)
+                if len(self.cach_args) >= self.maxsize:
+                    for arg, time_ in self.cach_time.items():
+                        max_ = time_
+                        key = arg
+                        if time_ < max_:
+                            max_ = time_
+                            key = arg
+                    self.cach_args.pop(key)
+                self.cach_args[temp] = result
+                self.cach_time[temp] = time()
+            return result
+        return cach
