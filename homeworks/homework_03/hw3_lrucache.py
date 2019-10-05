c12847f7b46bad4a0bc9acc1dd2c971899685cdf
Maximenko Daniil <engmaxidan@gmail.com>
2019-10-05 22:53:09 +0400

    lrucache changes

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -3,7 +3,9 @@
 
 import time
 
+
 class LRUCacheDecorator:
+
 
     def __init__(self, maxsize, ttl):
         ''' 
@@ -15,39 +17,39 @@ class LRUCacheDecorator:
         self.ttl = ttl
         self.cache_bank = dict()
 
-        def __call__(self, f):
-            def wrapped(*args, **kwargs):
-                cache = str(args) + str(kwargs)
-                if cache in self.cache_bank.keys():
-                    if self.ttl:
-                        if (time.time() - self.cache_bank[cache][1]) * 1000 < self.ttl:
-                            print('from cache_bank')
-                            return self.cache_bank[cache][0]
-                        else:
-                            if len(self.cache_bank.keys()) == self.maxsize:
-                                print('from function')
-                                del [self.cache_bank[min(self.cache_bank.items(), key=lambda x: x[1][1])[0]]]
-                                res = f(*args, **kwargs)
-                                self.cache_bank[cache] = [res, time.time()]
-                                return res
-                            else:
-                                print('from function')
-                                res = f(*args, **kwargs)
-                                self.cache_bank[cache] = [res, time.time()]
-                                return res
-                    else:
+    def __call__(self, f):
+        def wrapped(*args, **kwargs):
+            cache = str(args) + str(kwargs)
+            if cache in self.cache_bank.keys():
+                if self.ttl:
+                    if (time.time() - self.cache_bank[cache][1]) * 1000 < self.ttl:
                         print('from cache_bank')
                         return self.cache_bank[cache][0]
-                else:
-                    if len(self.cache_bank.keys()) == self.maxsize:
-                        print('from function')
-                        del[self.cache_bank[min(self.cache_bank.items(), key=lambda x: x[1][1])[0]]]
-                        res = f(*args, **kwargs)
-                        self.cache_bank[cache] = [res, time.time()]
-                        return res
                     else:
-                        print('from function')
-                        res = f(*args, **kwargs)
-                        self.cache_bank[cache] = [res, time.time()]
-                        return res
-            return wrapped
+                        if len(self.cache_bank.keys()) == self.maxsize:
+                            print('from function')
+                            del [self.cache_bank[min(self.cache_bank.items(), key=lambda x: x[1][1])[0]]]
+                            res = f(*args, **kwargs)
+                            self.cache_bank[cache] = [res, time.time()]
+                            return res
+                        else:
+                            print('from function')
+                            res = f(*args, **kwargs)
+                            self.cache_bank[cache] = [res, time.time()]
+                            return res
+                else:
+                    print('from cache_bank')
+                    return self.cache_bank[cache][0]
+            else:
+                if len(self.cache_bank.keys()) == self.maxsize:
+                    print('from function')
+                    del[self.cache_bank[min(self.cache_bank.items(), key=lambda x: x[1][1])[0]]]
+                    res = f(*args, **kwargs)
+                    self.cache_bank[cache] = [res, time.time()]
+                    return res
+                else:
+                    print('from function')
+                    res = f(*args, **kwargs)
+                    self.cache_bank[cache] = [res, time.time()]
+                    return res
+        return wrapped
