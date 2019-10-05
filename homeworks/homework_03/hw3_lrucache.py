48abc3d2ace0fb686c16a99b1ca36b27b511bf56
Yakovlev Egor <egorka.yakovlev.00@mail.ru>
2019-10-06 00:42:24 +0300

    try2

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -15,18 +15,19 @@ class LRUCacheDecorator:
     def __call__(self, func):
         def _dec(*args, **kwargs):
             number = str(args) + str(*kwargs)
-            if  number in self.cache: 
-                if (time.time() - self.cache_time[number]) * 1000 < self.ttl and self.ttl is not None:
-                    return self.cache[number]
+            num_ = number[0]
+            if  num_ in self.cache: 
+                if (time.time() - self.cache_time[num_]) * 1000 < self.ttl and self.ttl is not None:
+                    return self.cache[num_]
                 else:
-                    self.cache[number] = func(*args, **kwargs)
-                    self.cache_time[number] = time.time()
+                    self.cache[num_] = func(*args, **kwargs)
+                    self.cache_time[num_] = time.time()
             if len(self.cache) >= self.maxsize:
                 for num in self.cache_time:
                     if self.cache_time[num] == sorted(self.cache_time.values(), reserve=True)[0]:
                         self.cache_time.pop(num)
                         self.cache.pop(num)
-                        self.cache[number] = func(*args, **kwargs)
-                        self.cache_time[number] = time.time()
-            return self.cache[number]
+                        self.cache[num_] = func(*args, **kwargs)
+                        self.cache_time[num_] = time.time()
+            return self.cache[num_]
         return _dec
