747c947f09a0037ee9fa551d02736eac4a9014b1
aleksandr <mac@MacBook-Air-Mas.local>
2019-10-05 17:05:22 +0300

    fix hashmap

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -15,25 +15,28 @@ class HashMap:
             :param key: ключ
             :param value: значение
             """
-            self.key = key
-            self.value = value
+            self._key = key
+            self._value = value
 
         def get_key(self):
-            # TODO возвращаем ключ
-            return self.key
+            #  возвращаем ключ
+            return self._key
 
         def get_value(self):
-            # TODO возвращаем значение
-            return self.value
+            #  возвращаем значение
+            return self._value
 
         def __eq__(self, other):
-            # TODO реализовать функцию сравнения
-            return self.value == other.get_key()
+            #  реализовать функцию сравнения
+            return self._key == other.get_key()
+
+        def set_value(self, value):
+            self._value = value
 
     class ItemIterator:
-        def __init__(self, hash_map):
+        def __init__(self, hash_map, keys):
             self.hash_map = hash_map
-            self.keys = hash_map.keys_list
+            self.keys = keys
             self.seek = 0
 
         def __iter__(self):
@@ -41,27 +44,24 @@ class HashMap:
 
         def __next__(self):
             if self.seek < len(self.keys):
-                key = self.keys[self.seek]
                 self.seek += 1
-                return key, self.hash_map.get(key)
+                return self.keys[self.seek - 1], self.hash_map.get(self.keys[self.seek - 1])
             else:
                 raise StopIteration
 
     class ValueIterator(ItemIterator):
         def __next__(self):
             if self.seek < len(self.keys):
-                key = self.keys[self.seek]
                 self.seek += 1
-                return self.hash_map.get(key)
+                return self.hash_map.get(self.keys[self.seek - 1])
             else:
                 raise StopIteration
 
     class KeyIterator(ItemIterator):
         def __next__(self):
             if self.seek < len(self.keys):
-                key = self.keys[self.seek]
                 self.seek += 1
-                return key
+                return self.keys[self.seek - 1]
             else:
                 raise StopIteration
 
@@ -70,88 +70,73 @@ class HashMap:
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
         """
-        self.hash_map = [[] for _ in range(bucket_num)]
         self.size = bucket_num
+        self.hash_map = [[] for _ in range(self.size)]
         self.elem_num = 0
         self.keys_list = []
         self.LOAD_K = 0.66
         self.INCREASE = 2
 
     def get(self, key, default_value=None):
-        # TODO метод get, возвращающий значение,
+        #  метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
         ind = self._get_index(self._get_hash(key))
-        if not self.hash_map[ind]:
-            return default_value
         for entry in self.hash_map[ind]:
             if key == entry.get_key():
                 return entry.get_value()
-            return default_value
+        return default_value
 
     def put(self, key, value):
-        # TODO метод put, кладет значение по ключу,
+        #  метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
         ind = self._get_index(self._get_hash(key))
-        if not self.hash_map[ind]:
-            self.elem_num += 1
-            self.keys_list.append(key)
-            self.hash_map[ind].append(self.Entry(key, value))
-            if self.elem_num > self.LOAD_K * self.elem_num:
-                self._resize()
-            return None
-        for i, entry in enumerate(self.hash_map[ind]):
+        for entry in self.hash_map[ind]:
             if entry.get_key() == key:
-                self.hash_map[ind][i] = self.Entry(key, value)
+                entry.set_value(value)
                 return None
+        self.hash_map[ind].append(self.Entry(key, value))
         self.elem_num += 1
         self.keys_list.append(key)
-        self.hash_map[ind].append(self.Entry(key, value))
-        if self.elem_num > self.LOAD_K * self.elem_num:
+        if self.elem_num > self.LOAD_K * self.size:
             self._resize()
-        return None
 
     def __len__(self):
-        # TODO Возвращает количество Entry в массиве
+        # Возвращает количество Entry в массиве
         return self.elem_num
 
     def _get_hash(self, key):
-        # TODO Вернуть хеш от ключа,
+        #  Вернуть хеш от ключа,
         #  по которому он кладется в бакет
         return hash(key)
 
     def _get_index(self, hash_value):
-        # TODO По значению хеша вернуть индекс элемента в массиве
+        #  По значению хеша вернуть индекс элемента в массиве
         return hash_value % self.size
 
     def values(self):
-        # TODO Должен возвращать итератор значений
-        return self.ValueIterator(self)
+        #  Должен возвращать итератор значений
+        return self.ValueIterator(self, self.keys_list)
 
     def keys(self):
-        # TODO Должен возвращать итератор ключей
-        return self.KeyIterator(self)
+        #  Должен возвращать итератор ключей
+        return self.KeyIterator(self, self.keys_list)
 
     def items(self):
-        # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        return self.ItemIterator(self)
+        #  Должен возвращать итератор пар ключ и значение (tuples)
+        return self.ItemIterator(self, self.keys_list)
 
     def _resize(self):
-        # TODO Время от времени нужно ресайзить нашу хешмапу
-        self.size *= self.INCREASE
-        values = [self.get(key) for key in self.keys_list]
-        old_elem_num = self.elem_num
-        self.elem_num = 0
-        del self.hash_map
-        self.hash_map = [[] for _ in range(self.size)]
-        items = zip(self.keys_list, values)
-        for key, value in items:
-            self.put(key, value)
-        self.elem_num = old_elem_num
+        #  Время от времени нужно ресайзить нашу хешмапу
+        new_size = self.size * self.INCREASE
+        new_map = HashMap(new_size)
+        for key, value in self.items():
+            new_map.put(key, value)
+        self.__dict__.update(new_map.__dict__)
 
     def __str__(self):
-        # TODO Метод выводит "buckets: {}, items: {}"
+        #  Метод выводит "buckets: {}, items: {}"
         return f'buckets: {self.size}, items: {self.elem_num}'
 
     def __contains__(self, item):
-        # TODO Метод проверяющий есть ли объект (через in)
+        #  Метод проверяющий есть ли объект (через in)
         return item in self.keys_list
