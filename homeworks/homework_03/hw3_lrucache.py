8f20a6730ac9afa15eed65fec1add8258a1d0a70
Maximenko Daniil <engmaxidan@gmail.com>
2019-10-05 22:48:54 +0400

    lrucache changes

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
 
 class LRUCacheDecorator:
 
     def __init__(self, maxsize, ttl):
-        '''
+        ''' 
         :param maxsize: максимальный размер кеша
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
@@ -17,22 +18,36 @@ class LRUCacheDecorator:
         def __call__(self, f):
             def wrapped(*args, **kwargs):
                 cache = str(args) + str(kwargs)
-                if cache in self.cache_bank.keys() and (time.time() - self.cache_bank[cache][1]) * 1000 < self.ttl:
-                    print('from cache_bank')
-                    return self.cache_bank[cache][0]
+                if cache in self.cache_bank.keys():
+                    if self.ttl:
+                        if (time.time() - self.cache_bank[cache][1]) * 1000 < self.ttl:
+                            print('from cache_bank')
+                            return self.cache_bank[cache][0]
+                        else:
+                            if len(self.cache_bank.keys()) == self.maxsize:
+                                print('from function')
+                                del [self.cache_bank[min(self.cache_bank.items(), key=lambda x: x[1][1])[0]]]
+                                res = f(*args, **kwargs)
+                                self.cache_bank[cache] = [res, time.time()]
+                                return res
+                            else:
+                                print('from function')
+                                res = f(*args, **kwargs)
+                                self.cache_bank[cache] = [res, time.time()]
+                                return res
+                    else:
+                        print('from cache_bank')
+                        return self.cache_bank[cache][0]
                 else:
                     if len(self.cache_bank.keys()) == self.maxsize:
                         print('from function')
-                        min = min(self.cache_bank.items(), key=lambda x: x[1][1])[0]
-                        del[self.cache_bank[min]]
+                        del[self.cache_bank[min(self.cache_bank.items(), key=lambda x: x[1][1])[0]]]
                         res = f(*args, **kwargs)
-                        cur_time = time.time()
-                        self.cache_bank[cache] = [res, cur_time]
+                        self.cache_bank[cache] = [res, time.time()]
                         return res
                     else:
                         print('from function')
                         res = f(*args, **kwargs)
-                        cur_time = time.time()
-                        self.cache_bank[cache] = [res, cur_time]
+                        self.cache_bank[cache] = [res, time.time()]
                         return res
             return wrapped
