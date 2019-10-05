e995b653768d98ccf01e2d97a765c6c432d7443d
Maximenko Daniil <engmaxidan@gmail.com>
2019-10-05 22:10:44 +0400

    lrucache changes

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -10,10 +10,29 @@ class LRUCacheDecorator:
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache_bank = dict()
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+        def __call__(self, f):
+            def wrapped(*args, **kwargs):
+                cache = str(args) + str(kwargs)
+                if cache in self.cache_bank.keys() and (time.time() - self.cache_bank[cache][1]) * 1000 < self.ttl:
+                    print('from cache_bank')
+                    return self.cache_bank[cache][0]
+                else:
+                    if len(self.cache_bank.keys()) == self.maxsize:
+                        print('from function')
+                        min = min(self.cache_bank.items(), key=lambda x: x[1][1])[0]
+                        del[self.cache_bank[min]]
+                        res = f(*args, **kwargs)
+                        cur_time = time.time()
+                        self.cache_bank[cache] = [res, cur_time]
+                        return res
+                    else:
+                        print('from function')
+                        res = f(*args, **kwargs)
+                        cur_time = time.time()
+                        self.cache_bank[cache] = [res, cur_time]
+                        return res
+            return wrapped
