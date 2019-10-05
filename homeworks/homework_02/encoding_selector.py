ff9c1099886ec797de5173d7cd6506897535f5b7
Anatoly Rubanov <anatolijrubanov@gmail.com>
2019-10-05 13:43:39 +0300

    delete files of homework_02

--- homeworks/homework_02/encoding_selector.py
+++
@@ -1,8 +0,0 @@
-def detect_encoding(filename):
-    with open(filename, 'rb') as file:
-        data = file.read()
-        try:
-            data.decode("UTF-8")
-            return "UTF-8"
-        except UnicodeDecodeError:
-            return "cp1251"
