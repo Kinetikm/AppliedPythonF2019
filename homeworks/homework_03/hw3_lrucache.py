5b025e96588d1ffd806b4d3d0e1eaeeee672400a
Hunte9999 <v.morozow@mail.ru>
2019-10-05 21:49:29 +0300

    added lrucache

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,51 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
+
 
 class LRUCacheDecorator:
 
     def __init__(self, maxsize, ttl):
-        '''
+        """
         :param maxsize: максимальный размер кеша
         :param ttl: время в млсек, через которое кеш
-                    должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+                    должен исчезнуть"""
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache = {}
+        self.sentinel = object()
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, func):
+        if self.maxsize == 0:
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
+        else:
+            def wrapper(*args, **kwargs):
+                key = (args, tuple(kwargs))
+                result = self.cache.get(key, (self.sentinel, ))
+                if result[0] is not self.sentinel:
+                    if self.ttl is not None:
+                        if (time.time() - result[1]) * 1000 <= self.ttl:
+                            self.cache[key] = (result[0], time.time())
+                            return result[0]
+                        else:
+                            self.cache.pop(key)
+                    else:
+                        self.cache[key] = (result[0], time.time())
+                        return result[0]
+                result = func(*args, **kwargs)
+                self.cache[key] = (result, time.time())
+                if len(self.cache.keys()) > self.maxsize:
+                    min_k, min_t = None, None
+                    for k in self.cache.keys():
+                        if min_t:
+                            if min_t > self.cache[k][1]:
+                                min_t = self.cache[k][1]
+                                min_k = k
+                        else:
+                            min_t = self.cache[k][1]
+                            min_k = k
+                    self.cache.pop(min_k)
+                return result
+        return wrapper
