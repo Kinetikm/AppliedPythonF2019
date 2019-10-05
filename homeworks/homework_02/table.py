a5938f9d12dbb465e1ce86c6aa400c05e25ec886
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-10-05 22:30:44 +0300

    Update table.py

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,19 @@
-import sys
+#!/usr/bin/env python
+# coding: utf-8
 
-# Ваши импорты
+
+from Error import *
+from Print_table import *
+from open_file import *
+
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
-
-    # Ваш код
+    fname = sys.argv[1]
+    try:
+        coding = define_coding(fname)
+        data = data(filename, coding)
+        table(data)
+    except FileNotFoundError:
+        print("Файл не валиден")
+    except WrongFormatException:
+        print("Формат не валиден")
