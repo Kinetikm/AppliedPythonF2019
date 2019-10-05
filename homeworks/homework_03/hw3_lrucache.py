3a60d23ce50fb4984d29300be161a2deff166fd6
Maximenko Daniil <engmaxidan@gmail.com>
2019-10-05 23:21:13 +0400

    lrucache changes

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -20,34 +20,28 @@ class LRUCacheDecorator:
         def wrapped(*args, **kwargs):
             cache = str(args) + str(kwargs)
             if cache in self.cache_bank.keys():
-                if self.ttl != None:
+                if self.ttl:
                     if (time.time() - self.cache_bank[cache][1]) * 1000 < self.ttl:
-                        print('from cache_bank')
                         return self.cache_bank[cache][0]
                     else:
                         if len(self.cache_bank.keys()) == self.maxsize:
-                            print('from function')
                             del [self.cache_bank[min(self.cache_bank.items(), key=lambda x: x[1][1])[0]]]
                             res = f(*args, **kwargs)
                             self.cache_bank[cache] = [res, time.time()]
                             return res
                         else:
-                            print('from function')
                             res = f(*args, **kwargs)
                             self.cache_bank[cache] = [res, time.time()]
                             return res
                 else:
-                    print('from cache_bank')
                     return self.cache_bank[cache][0]
             else:
                 if len(self.cache_bank.keys()) == self.maxsize:
-                    print('from function')
                     del[self.cache_bank[min(self.cache_bank.items(), key=lambda x: x[1][1])[0]]]
                     res = f(*args, **kwargs)
                     self.cache_bank[cache] = [res, time.time()]
                     return res
                 else:
-                    print('from function')
                     res = f(*args, **kwargs)
                     self.cache_bank[cache] = [res, time.time()]
                     return res
