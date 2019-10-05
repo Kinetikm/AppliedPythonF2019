7e1fef9329e4c6b5a8183d0f23463ffb0c10fe47
ttimur2 <hunter220900@gmail.com>
2019-10-05 20:16:11 +0300

    first try

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
+from collections import OrderDict
 
 class LRUCacheDecorator:
 
@@ -10,10 +12,43 @@ class LRUCacheDecorator:
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.function_result = OrderedDict()
+        self.time_in_cache = OrderedDict()
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def del_old(self, *args):
+        self.function_result.popitem(*args)
+        self.time_in_cache.popitem(*args)
+
+    def update_cache(self, arg,result):
+        self.function_result[arg] = result
+        self.time_in_cache[arg] = time.time()
+
+    def __call__(self, function):
+        def lru_function(*args, **kwargs):
+            list = args
+            if self.function_result.get(list):#есть ли в хеше
+                if self.ttl is not None:
+                    if self.ttl < (time.time() - self.time_in_cache[list])
+                        self.del_old(list)
+                        newresult = function(*args, **kwargs)
+                        self.update_cache(list, newresult)
+                        return newresult
+                    else:
+                        return self.function_result[list]
+                else:
+                    return self.function_result[list]
+            else:#нет в хеше
+                if self.maxsize > len(self.function_result):#хеш не забит
+                    newresult = function(*args, **kwargs)
+                    self.update_cache(list, newresult)
+                    return newresult
+                else:#хеш забит
+                    self.del_old(list)
+                    newresult = function(*args, **kwargs)
+                    self.update_cache(list, newresult)
+                    return newresult
+        return lru_function
+
+
