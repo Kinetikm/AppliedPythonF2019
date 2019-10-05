1ec307bc42071bbd9fb571ac4a607ba2bb652e3c
EvgeniyMist <wertygeny@gmail.com>
2019-10-05 14:01:18 +0300

    lru first try

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,50 @@
-#!/usr/bin/env python
-# coding: utf-8
+from time import time
 
 
 class LRUCacheDecorator:
 
-    def __init__(self, maxsize, ttl):
-        '''
-        :param maxsize: максимальный размер кеша
-        :param ttl: время в млсек, через которое кеш
-                    должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+    class Entry:
+        def __init__(self, inf, first_call, last_call):
+            self.inf = inf
+            self.first_call = first_call
+            self.last_call = last_call
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __init__(self, maxsize, ttl):
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache = {}
+
+    def __get_hash(self, arg):
+        try:
+            return hash(arg)
+        except TypeError:
+            return hash(tuple(arg))
+
+    def __get_full_hash(self, *args, **kwargs):
+        h_args = ''.join([self.__get_hash(arg) for arg in args])
+        h_kwargs = ''.join([self.__get_hash(kwargs[key]) for key in kwargs])
+        return h_args + ' ' + h_kwargs
+
+    def __call__(self, func):
+        def caccess(*args, **kwargs):
+            h = self.__get_full_hash(*args, **kwargs)
+            if h in self.cache:
+                if (self.ttl is not None and
+                   (time() - self.cache[h].first_call) * 1000 > self.ttl):
+                    self.cache[h].inf = func(*args, **kwargs)
+                    self.cache[h].first_call = time()
+                    self.cache[h].last_call = self.cache[h].first_call
+                    return self.cache[h].inf
+                else:
+                    self.cache[h].last_call = time()
+                    return self.cache[h].inf
+            if len(self.cache) == self.maxsize:
+                lst_of_keys = list(self.cache.keys())
+                lst_of_keys = sorted(lst_of_keys, key=lambda k:
+                                     self.cache[k].last_call)
+                self.cache.pop(lst_of_keys[0])
+            inf = func(*args, **kwargs)
+            ctime = time()
+            self.cache.update({h: self.Entry(inf, ctime, ctime)})
+            return inf
+        return caccess
