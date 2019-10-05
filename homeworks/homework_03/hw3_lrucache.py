b4510327b727c8a957b177be87f17a004e802212
advatrix <trojan.advatrix@gmail.com>
2019-10-05 18:43:35 +0300

    add lrucache

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from time import time
+
 
 class LRUCacheDecorator:
 
@@ -10,10 +12,37 @@ class LRUCacheDecorator:
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self._maxsize = maxsize
+        self._ttl = ttl
+        self._cach = {}
+        self._times = {}
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, func):
+        def cach(*args, **kwargs):
+            args = ((args, str(kwargs)))
+            if args in self._cach:
+                if self._ttl and (time() - self._times[args])*1000 > self._ttl:
+                    result = func(*args, **kwargs)
+                    self._cach[args] = result
+                    self._time[args] = time()
+                else:
+                    result = self._cach[args]
+                    self._time[args] = time()
+            else:
+                result = func(*args, **kwargs)
+                if len(self._cach) >= self._maxsize:
+                    self._pop()
+                self._cach[args] = result
+                self._time[args] = time()
+
+    def _pop(self):
+        oldness = time()
+        for args in self._cach.keys():
+            tmp_time = self._time[args]
+            if tmp_time < oldness:
+                oldness = tmp_time
+                pop_args = args
+        self._cach.pop(pop_args)
+        self._time.pop(pop_args)
+        
+                
