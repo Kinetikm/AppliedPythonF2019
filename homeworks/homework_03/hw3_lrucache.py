9325a85de4c1d729a018f3124c0d11dbc6910897
aleksandr <mac@MacBook-Air-Mas.local>
2019-10-06 01:13:45 +0300

    lru cache

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
 # coding: utf-8
+from time import time
 
 
 class LRUCacheDecorator:
 
     def __init__(self, maxsize, ttl):
-        '''
+        """
         :param maxsize: максимальный размер кеша
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        """
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache = {
+            'key_value': {},
+            'time': {}
+        }
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, func):
+        def inner(*args, **kwargs):
+            key = hash(str(args) + str(kwargs))
+            if key not in self.cache['key_value']:
+                out = func(*args, **kwargs)
+                self.cache['key_value'][key] = out
+                self.cache['time'][key] = time()
+                if len(self.cache['key_value']) > self.maxsize:
+                    items = sorted(self.cache['time'].items(), key=lambda x: x[1])
+                    del self.cache['key_value'][items[0][0]]
+                    del self.cache['time'][items[0][0]]
+            elif self.ttl and (time() - self.cache['time'][key]) * 1000 > self.ttl:
+                out = func(*args, **kwargs)
+                self.cache['time'][key] = time()
+            else:
+                self.cache['time'][key] = time()
+                out = self.cache['key_value'][key]
+            return out
+        return inner
