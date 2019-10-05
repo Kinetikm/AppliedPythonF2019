91b53f30eff9a110ce7f1c8019e494f8d7c7ccbd
MarksmanM <MarksmanMarin@gmail.com>
2019-10-05 16:47:12 +0300

    lru

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import time
 
 class LRUCacheDecorator:
 
-    def __init__(self, maxsize, ttl):
+    def __init__(self, maxsize, ttl = None, func):
         '''
         :param maxsize: максимальный размер кеша
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
+        #  TODO инициализация декоратора
         #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache = {}
+        self.func = func
 
     def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+        #  TODO вызов функции
+        key = (args, tuple(kwargs))
+        if key not in self.cache:
+            if len(self.cache) < self.maxsize:
+                self.cache[key] = [self.func(*args, **kwargs), time.time()]
+            else:
+                max = 0
+                for i in self.cache:
+                    if (time.time() - self.cache[i][1]) > max:
+                        max = time.time() - self.cache[i][1]
+                        key_max = k
+                self.cache.pop(key_max)
+                self.cache[key] = [self.func(*args, **kwargs), time.time()]
+            return self.func(*args, **kwargs)
+        else:
+            if self.ttl is not None:
+                if (time.time() - self.cache[key][1])*1000 > self.ttl:
+                    self.cache.pop(key)
+                    self.cache[key] = [self.func(*args, **kwargs), time.time()]
+                    return self.func(*args, **kwargs)
+            return self.func(*args, **kwargs)
