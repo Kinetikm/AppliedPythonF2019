082b692eaac46ca98b7979204e31a6a767b7eecd
Aigul <sirazovaaigul@gmail.com>
2019-10-07 02:11:54 +0300

    first try lru

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+import time
 
 
 class LRUCacheDecorator:
@@ -12,8 +13,36 @@ class LRUCacheDecorator:
         '''
         # TODO инициализация декоратора
         #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self._maxsize = maxsize
+        self._ttl = ttl
+        self._cache = {} # {key: [val, time]}
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, func, *args, **kwargs):
         # TODO вызов функции
-        raise NotImplementedError
+        def wrapper(*args, **kwargs):
+            key = (args, tuple(kwargs))
+            if key not in self._cache:
+                result = func(*args, **kwargs)
+                if len(self._cache) < self._maxsize:
+                    self._cache[key] = [result, time.time()]
+                else:
+                    max_time = -1
+                    for t in self._cache:
+                        if (time.time() - self._cache[t][1]) > max_time:
+                            old = t
+                            max_time = (time.time() - self._cache[t][1])
+                    self._cache.pop(old)
+                    self._cache[key] = [result, time.time()]
+                return result
+            else:
+                if not(self._ttl is None):
+                    if (time.time() - self._cache[key][1]) * 1000 > self._ttl:
+                        result = func(*args, **kwargs)
+                        self._cache.pop(key)
+                        self._cache[key] = [result, time.time()]
+                        return result
+                self._cache[key][1] = time.time()
+                result = self._cache[key][0]
+                return result
+
+        return wrapper
