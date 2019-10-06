727717813138f38a3cc2d42b23a6413eb85343cd
VladislavKuzevanov <vlad.kuzevanov@mail.ru>
2019-10-06 18:24:09 +0300

    вторая задача

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -2,18 +2,33 @@
 # coding: utf-8
 
 
+import time
+import heapq
+
+
 class LRUCacheDecorator:
 
     def __init__(self, maxsize, ttl):
-        '''
-        :param maxsize: максимальный размер кеша
-        :param ttl: время в млсек, через которое кеш
-                    должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cash = {}
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, function):
+        def wrapped(*args, **kwargs):
+            if (args in self.cash) and (self.ttl is not None) \
+            and (abs(time.time() - self.cash.get(args)[1]) > self.ttl):
+                self.cash.pop(args)
+            if args in self.cash:
+                cur_time = time.time()
+                self.cash.get(args)[1] = cur_time
+                return self.cash.get(args)[0]
+            if len(self.cash) == self.maxsize:
+                old = (min(self.cash.items(), key=operator.itemgetter(1))[0])
+                self.cash.pop(old)
+            result = function(*args, **kwargs)
+            self.cash[args] = []
+            self.cash.get(args).append(result)
+            cur_time = time.time()
+            self.cash.get(args).append(cur_time)
+            return result
+        return wrapped
