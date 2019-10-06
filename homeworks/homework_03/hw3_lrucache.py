b02a3989f1a918478d238e00a88ad7a8a60d204e
Chudvan <Mr.Chudvan@yandex.ru>
2019-10-07 01:41:55 +0300

    HW3_LRUCache

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+import time
 
 
 class LRUCacheDecorator:
@@ -12,8 +13,50 @@ class LRUCacheDecorator:
         '''
         # TODO инициализация декоратора
         #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache = {}
+        self.has_func = False
 
     def __call__(self, *args, **kwargs):
         # TODO вызов функции
-        raise NotImplementedError
+        if not self.has_func:
+            self.func = args[0]
+            self.has_func = True
+            return self
+        parameters = (args, tuple(kwargs.items()))
+        if parameters in self.cache:
+            cur_time = time.time()
+            if self.ttl is not None:
+                if cur_time - self.cache[parameters][1] < self.ttl:
+                    res = self.cache[parameters][0]
+                    self.cache[parameters] = res, cur_time
+                    return res
+                res = self.func(*args, **kwargs)
+                self.cache[parameters] = res, cur_time
+                return res
+            res = self.cache[parameters][0]
+            self.cache[parameters] = res, cur_time
+            return res
+        elif len(self.cache) != self.maxsize:
+            cur_time = time.time()
+            res = self.func(*args, **kwargs)
+            self.cache[parameters] = res, cur_time
+            return res
+        else:
+            sorted_cache = sorted(self.cache.items(), key=lambda t: -t[1][1])
+            if self.ttl is not None:
+                old_keys = [k for k, v in sorted_cache if time.time() - v[1] >= self.ttl]
+                for key in old_keys:
+                    del self.cache[key]
+                if len(self.cache) != self.maxsize:
+                    cur_time = time.time()
+                    res = self.func(*args, **kwargs)
+                    self.cache[parameters] = res, cur_time
+                    return res
+            last_key, last_val = sorted_cache[-1]
+            del self.cache[last_key]
+            cur_time = time.time()
+            res = self.func(*args, **kwargs)
+            self.cache[parameters] = res, cur_time
+            return res
