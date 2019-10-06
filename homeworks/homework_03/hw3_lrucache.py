73c29a5b59c44f0660678776fdfe8cdecf201d96
Smirnov Ilya <box53i@mail.ru>
2019-10-06 20:46:34 +0300

    lru

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import time
 
 class LRUCacheDecorator:
 
     def __init__(self, maxsize, ttl):
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache = {}
+        self.times = {}
+        self.size = 0
         '''
         :param maxsize: максимальный размер кеша
         :param ttl: время в млсек, через которое кеш
@@ -14,6 +19,30 @@ class LRUCacheDecorator:
         #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
         raise NotImplementedError
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def _make_key(self, args, kwargs):
+        key = args
+        if kwargs:
+            key += kwargs
+        return key
+
+    def __call__(self, func):
+        def cache(*args, **kwargs):
+            key = self._make_key(args, kwargs)
+            if key in self.cache:
+                if self.ttl is not None and time.time() - self.times[key] < self.ttl:
+                    self.times[key] = time.time()
+                    return self.cache[key]
+                elif self.ttl is None:
+                    self.times[key] = time.time()
+                    return self.cache[key]
+            res = func(*args, **kwargs)
+            if self.size == self.maxsize:
+                del_key = min(self.times, key=self.times.get)
+                del self.cache[del_key]
+                del self.times[del_key]
+                self.size -= 1
+            self.times[key] = time.time()
+            self.cache[key] = res
+            self.size += 1
+            return res
+        return cache
