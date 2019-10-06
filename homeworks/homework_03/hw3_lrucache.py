fb60424de562ab66fe3ad73a5c4ae92adc940fb9
ciplatov <siplatov-kg@mail.ru>
2019-10-06 15:28:48 +0300

    hw3_lrucach

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+import time
+import operator
 
 
 class LRUCacheDecorator:
@@ -10,10 +12,35 @@ class LRUCacheDecorator:
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        if ttl is not None:
+            self.ttl = int(ttl)
+        else:
+            self.ttl = None
+        self.cash = {}
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, fn):
+        def decorated(*args, **kwargs):
+            if args in self.cash:
+                if self.ttl:
+                    if (abs(time.time() - self.cash.get(args)[1]) >
+                            self.ttl):
+                        self.cash.pop(args)
+            if args in self.cash:
+                time.sleep(0.00001)
+                time_return_call = time.time()
+                self.cash.get(args)[1] = time_return_call
+                return self.cash.get(args)[0]
+            if len(self.cash) == self.maxsize:
+                help_dict = {}
+                for _ in self.cash:
+                    help_dict[_] = self.cash.get(_)[1]
+                old = (min(help_dict.items(), key=operator.itemgetter(1))[0])
+                self.cash.pop(old)
+            result = fn(*args, **kwargs)
+            self.cash[args] = []
+            self.cash.get(args).append(result)
+            time_first_call = time.time()
+            self.cash.get(args).append(time_first_call)
+            return result
+        return decorated
