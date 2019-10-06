a019e5a6438a273c2c67316cc169ae73cf795a0a
Ilya-Verendeev <55409291+Ilya-Verendeev@users.noreply.github.com>
2019-10-06 20:21:48 +0300

    Update hw3_lrucache.py

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -5,32 +5,27 @@ import time
 
 
 class LRUCacheDecorator:
+
     def __init__(self, maxsize, ttl):
         self.maxsize = maxsize
         self.time = ttl
         self.cache = {}
         self.realsize = 0
 
-    def __call__(self, function):
-
-        def Time():
-            return time.time() * 1000
-
-        def internal(*args, **kwargs):
-            key = args + tuple(sorted(kwargs.items()))
-            if key not in self.cache.keys():
-                if self.realsize < self.maxsize:
-                    self.realsize += 1
-                else:
-                    index = min(self.cache.items(),
-                                key=lambda i: i[1][1])
-                    del self.cache[index[0]]
+    def __call__(self, function, *args, **kwargs):
+        key = args + tuple(sorted(kwargs.items()))
+        if key not in self.cache.keys():
+            if self.realsize < self.maxsize:
+                self.realsize += 1
             else:
-                item = self.cache[key]
-                print(self.cache)
-                if self.time is None or Time() - item[1] <= self.time:
-                    self.cache[key] = (item[0], Time())
-                    return item[0]
-            self.cache[key] = (function(*args, **kwargs), Time())
-            return self.cache[key][0]
-        return internal
+                index = min(self.cache.items(),
+                            key=lambda i: i[1][1])
+                del self.cache[index[0]]
+        else:
+            item = self.cache[key]
+            print(self.cache)
+            if self.time is None or time.time() * 1000 - item[1] <= self.time:
+                self.cache[key] = (item[0], time.time() * 1000)
+                return item[0]
+        self.cache[key] = (function(*args, **kwargs), time.time() * 1000)
+        return self.cache[key][0]
