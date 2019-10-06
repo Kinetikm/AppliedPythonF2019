1a2e73ac349fdf5d54994cf94ddb1da569012d4f
Danil Andreev <d2234@mail.ru>
2019-10-06 10:19:43 +0300

    1,2,4 done

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -4,6 +4,34 @@
 
 class HashMap:
 
+    class Iterator:
+        def __init__(self, map, _type):
+            self.ptr_bucket = 0
+            self.ptr_entry = 0
+            self.map = map
+            self.type_ = _type
+
+        def __iter__(self):
+            return self
+
+        def return_val(self, item):
+            if self.type_ == 'keys':
+                return item.get_key()
+            elif self.type_ == 'values':
+                return item.get_value()
+            elif self.type_ == 'items':
+                return item.get_key(), item.get_value()
+
+        def __next__(self):
+            while self.ptr_bucket < len(self.map):
+                if len(self.map[self.ptr_bucket]) > 0 and self.ptr_entry < len(self.map[self.ptr_bucket]):
+                    el = self.return_val(self.map[self.ptr_bucket][self.ptr_entry])
+                    self.ptr_entry += 1
+                    return el
+                self.ptr_bucket += 1
+                self.ptr_entry = 0
+            raise StopIteration
+
     class Entry:
         def __init__(self, key, value):
             self.key = key
@@ -21,34 +49,24 @@ class HashMap:
     def __init__(self, bucket_num=64):
         self.len = 0
         self.bucket_num = bucket_num
-        self.map = [None for _ in range(bucket_num)]
+        self.map = [[] for _ in range(bucket_num)]
 
     def get(self, key, default_value=None):
         _id = self._get_index(self._get_hash(key))
-        arr = self.map[_id]
-        if arr is None:
-            return default_value
-        else:
-            for el in arr:
-                if el.get_key() == key:
-                    return el.get_value()
-            return default_value
+        for el in self.map[_id]:
+            if el.get_key() == key:
+                return el.get_value()
+        return default_value
 
     def put(self, key, value):
         _id = self._get_index(self._get_hash(key))
-        arr = self.map[_id]
-        if arr is None:
-            self.map[_id] = [self.Entry(key, value)]
-            self.len += 1
-            return True
-        else:
-            for el in arr:
-                if el.get_key() == key:
-                    el.value = value
-                    return True
-            self.map[_id].append(self.Entry(key, value))
-            self.len += 1
-            return True
+        for el in self.map[_id]:
+            if el.get_key() == key:
+                el.value = value
+                return True
+        self.map[_id].append(self.Entry(key, value))
+        self.len += 1
+        return True
 
     def __len__(self):
         return self.len
@@ -60,31 +78,13 @@ class HashMap:
         return hash_value % self.bucket_num
 
     def values(self):
-        val_list = []
-        for arr in self.map:
-            if arr is None:
-                continue
-            for el in arr:
-                val_list.append(el.get_value())
-        return val_list
+        return self.Iterator(self.map, 'values')
 
     def keys(self):
-        key_list = []
-        for arr in self.map:
-            if arr is None:
-                continue
-            for el in arr:
-                key_list.append(el.get_key())
-        return key_list
+        return self.Iterator(self.map, 'keys')
 
     def items(self):
-        items_list = []
-        for arr in self.map:
-            if arr is None:
-                continue
-            for el in arr:
-                items_list.append((el.get_key(), el.get_value()))
-        return items_list
+        return self.Iterator(self.map, 'items')
 
     def _resize(self):
         self.map += [None for _ in range(self.bucket_num)]
@@ -92,28 +92,22 @@ class HashMap:
         i = 0
         while i < self.bucket_num:
             arr = self.map[i]
-            if arr is not None:
-                j, length = 0, len(arr)
-                while j < length:
-                    new_id = self._get_index(self._get_hash(arr[j].get_key()))
-                    if new_id != i:
-                        self.put(arr[j].get_key(), arr[j].get_value())
-                        arr.pop(j)
-                        length = len(arr)
-                    j += 1
+            j, length = 0, len(arr)
+            while j < length:
+                new_id = self._get_index(self._get_hash(arr[j].get_key()))
+                if new_id != i:
+                    self.put(arr[j].get_key(), arr[j].get_value())
+                    arr.pop(j)
+                    length = len(arr)
+                j += 1
             i += 1
 
     def __str__(self):
-        # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        return "buckets: {}, items: {}".format(self.bucket_num, self.len)
 
     def __contains__(self, item):
         _id = self._get_index(self._get_hash(item))
-        arr = self.map[_id]
-        if arr is None:
-            return False
-        else:
-            for el in arr:
-                if el.get_key() == item:
-                    return True
-            return False
+        for el in self.map[_id]:
+            if el.get_key() == item:
+                return True
+        return False
--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
+import time
+from collections import OrderedDict
 
 
 class LRUCacheDecorator:
 
     def __init__(self, maxsize, ttl):
-        '''
-        :param maxsize: максимальный размер кеша
-        :param ttl: время в млсек, через которое кеш
-                    должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache = OrderedDict()
+        self.foo = None
 
     def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+        if self.foo is None:
+            self.foo = args[0]
+            return self
+        else:
+            arg = args[0]
+            if arg in self.cache and (self.ttl is None or time.time() - self.cache[arg][1] <= self.ttl):
+                self.cache[arg][1] = time.time()
+                self.cache.move_to_end(arg)
+                return self.cache[arg][0]
+
+            res = self.foo(*args, **kwargs)
+
+            if len(self.cache) == self.maxsize:
+                self.cache.popitem(last=False)
+            self.cache[arg] = [res, time.time()]
+            return res
