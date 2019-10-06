90ccfb814a1a8baa8a45394405f194ea93e835ae
Terekhin Artyom <artem15mulo@gmail.com>
2019-10-07 01:51:31 +0300

    “hash_and_cache_v1”

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -14,69 +14,107 @@ class HashMap:
             :param key: ключ
             :param value: значение
             """
+            self.key = key
+            self.value = value
 
         def get_key(self):
             # TODO возвращаем ключ
-            raise NotImplementedError
+            return self.key
 
         def get_value(self):
             # TODO возвращаем значение
-            raise NotImplementedError
+            return self.value
 
         def __eq__(self, other):
             # TODO реализовать функцию сравнения
-            raise NotImplementedError
+            return self.key == other.key
 
     def __init__(self, bucket_num=64):
         """
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
         """
-        raise NotImplementedError
+        self.length = bucket_num
+        self._bucket = [None for i in range(self.length)]
+        self.capacity = 0
+        self.free_place = 0.66
 
     def get(self, key, default_value=None):
         # TODO метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
-        raise NotImplementedError
+        index = self._get_index(self._get_hash(key))
+        if self._bucket[index] is not None:
+            for var in self._bucket[index]:
+                if var.key == key:
+                    default_value = var.value
+        return default_value
 
     def put(self, key, value):
         # TODO метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
+        if key not in self.keys():
+            index = self._get_index(self._get_hash(key))
+            if self._bucket[index] is None:
+                self._bucket[index] = [self.Entry(key, value)]
+            else:
+                self._bucket[index].append(self.Entry(key, value))
+            self.capacity += 1
+        else:
+            index = self._get_index(self._get_hash(key))
+            if self._bucket[index]:
+                for var in self._bucket[index]:
+                    if var.key == key:
+                        var.value = value
+        if self.capacity / self.length > self.threshold:
+            self._resize()
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        return self.capacity
 
     def _get_hash(self, key):
         # TODO Вернуть хеш от ключа,
         #  по которому он кладется в бакет
-        raise NotImplementedError
+        return hash(key)
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+        return hash_value % self.length
 
     def values(self):
         # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+        return list(map(lambda x: x[1], self.items()))
 
     def keys(self):
         # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+        return list(map(lambda x: x[0], self.items()))
 
     def items(self):
         # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+        result = []
+        for bucket in self._bucket:
+            if bucket:
+                for var in bucket:
+                    result.append((var.key, var.value))
+        return result
 
     def _resize(self):
         # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+        self._bucket.extend([None for _ in range(self.length)])
+        self.length *= 2
+        tmp_buckets = []
+        for bucket in self._bucket:
+            if len(bucket) != 0:
+                for entry in bucket:
+                    tmp_buckets.append(entry)
+            bucket = None
+        for entry in tmp_buckets:
+            self.put(entry.key, entry.value)
 
     def __str__(self):
         # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        return f"buckets: {self._bucket}, items: {self.items()}"
 
     def __contains__(self, item):
         # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+        return item in self.keys()
--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
+from collections import OrderedDict
+
 
 class LRUCacheDecorator:
 
     def __init__(self, maxsize, ttl):
-        '''
+        """
         :param maxsize: максимальный размер кеша
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
-        '''
+        """
         # TODO инициализация декоратора
         #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.results = OrderedDict()
+        self.time_in_cache = OrderedDict()
+
+    def clean_tail(self, *args):
+        self.results.popitem(*args)
+        self.time_in_cache.popitem(*args)
+
+    def refresh(self, arg, result):
+        self.results[arg] = result
+        self.time_in_cache[arg] = time.time()
 
     def __call__(self, *args, **kwargs):
         # TODO вызов функции
-        raise NotImplementedError
+        list = args
+        if self.results.get(list):
+            if self.ttl < (time.time() - self.time_in_cache[list]):
+                self.clean_tail(list)
+                new_result = function(*args, **kwargs)
+                self.refresh(list, new_result)
+                return new_result
+            return self.results[list]
+        else:
+            if self.maxsize <= len(self.function_result):
+                self.clean_tail(list)
+            new_result = function(*args, **kwargs)
+            self.refresh(list, new_result)
+            return new_result
