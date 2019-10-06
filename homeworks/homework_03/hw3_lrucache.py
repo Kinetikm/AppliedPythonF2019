ab669ad3d0afcd9133f4a05262d21f561b8f9be9
Sinitcyn Dmitriy <sinitcyn.dmitriy@mail.ru>
2019-10-06 21:06:36 +0300

    lru_cache

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
+
 
 class LRUCacheDecorator:
 
@@ -10,10 +12,33 @@ class LRUCacheDecorator:
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.time_lim = ttl
+        self.cache = {}
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, func):
+
+        def decor(*args, **kwargs):
+            if self.time_lim is not None:  # Чищу от старых значений в кеше
+                list_to_del = []
+                for key, value in self.cache.items():
+                    if time.time() * 1000 - value[1] > self.time_lim:
+                        list_to_del.append(key)
+                for key in list_to_del:
+                    del self.cache[key]
+
+            cache_key = hash(str(args)+str(kwargs))
+
+            if cache_key not in self.cache:    # если от такого значения ещё не вычисляли
+                result = func(*args, **kwargs)
+                if len(self.cache) < self.maxsize:      # если есть место
+                    self.cache[cache_key] = [result, time.time()*1000]
+                else:       # если места нет, то выбросим редкоиспользуемый
+                    key_less = max(self.cache, key=lambda x: time.time()*1000 - self.cache[x][1])
+                    del self.cache[key_less]
+                    self.cache[cache_key] = [result, time.time()*1000]
+                return result
+            else:   # если значение вычислялось
+                self.cache[cache_key][1] = time.time()*1000
+                return self.cache[cache_key][0]
+        return decor
