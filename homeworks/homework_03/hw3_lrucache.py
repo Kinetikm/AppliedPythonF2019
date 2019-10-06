73e0be2795334f948fc65320afa59e12940d31dc
Ilya-Verendeev <55409291+Ilya-Verendeev@users.noreply.github.com>
2019-10-06 20:06:47 +0300

    Update hw3_lrucache.py

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
+
 
 class LRUCacheDecorator:
-
+    
     def __init__(self, maxsize, ttl):
-        '''
-        :param maxsize: максимальный размер кеша
-        :param ttl: время в млсек, через которое кеш
-                    должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.time = ttl
+        self.cache = {}
+        self.realsize = 0
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, function):
+
+        def Time():
+            return time.time() * 1000
+
+        def internal(*args, **kwargs):
+            key = args + tuple(sorted(kwargs.items()))
+            if key not in self.cache.keys():
+                if self.realsize < self.maxsize:
+                    self.realsize += 1
+                else:
+                    index = min(self.cache.items(),
+                                key=lambda i: i[1][1])
+                    del self.cache[index[0]]
+            else:
+                item = self.cache[key]
+                print(self.cache)
+                if self.time is None or Time() - item[1] <= self.time:
+                    self.cache[key] = (item[0], Time())
+                    return item[0]
+            self.cache[key] = (function(*args, **kwargs), Time())
+            return self.cache[key][0]
+        return internal
