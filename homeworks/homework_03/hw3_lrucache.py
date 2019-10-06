e37e75151bab3e3b963316be4560f4a39f8cd20f
Артём <artem@Artems-MacBook-Pro.local>
2019-10-06 16:39:57 +0300

    Complete_LRUCAche

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,53 @@
 #!/usr/bin/env python
 # coding: utf-8
+from time import time
 
 
 class LRUCacheDecorator:
 
-    def __init__(self, maxsize, ttl):
-        '''
-        :param maxsize: максимальный размер кеша
-        :param ttl: время в млсек, через которое кеш
-                    должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+    __slots__ = ["maxsize", "ttl", "cache", "record_time"]
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __init__(self, maxsize, ttl):
+        self.maxsize = maxsize
+        self.ttl = ttl
+
+        self.cache = dict()
+        self.record_time = dict()
+
+    def __call__(self, func):
+
+        def wrapper(*args, **kwargs):
+            cur_time = time()
+
+            packet = tuple((args, str(kwargs)))
+
+            if packet in self.cache:
+                if self.ttl is not None:
+                    if (cur_time - self.record_time[packet]) < self.ttl:
+                        self.record_time[packet] = cur_time
+
+                        return self.cache[packet]
+                    else:
+                        result = func(*args, **kwargs)
+
+                        self.cache[packet] = result
+                        self.record_time[packet] = cur_time
+                else:
+                    self.record_time[packet] = cur_time
+
+                    return self.cache[packet]
+            else:
+                while len(self.cache) >= self.maxsize:
+                    key_packet = min(self.record_time, key=lambda unit: self.record_time[unit])
+
+                    del self.record_time[key_packet]
+                    del self.cache[key_packet]
+
+                result = func(*args, **kwargs)
+
+                self.cache[packet] = result
+                self.record_time[packet] = cur_time
+
+            return result
+
+        return wrapper
