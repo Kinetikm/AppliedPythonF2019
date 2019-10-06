44fb74f861e87716286e27a2cc9d1c7952c26ecc
MaximAbramenko <gotosleepmcht@gmail.com>
2019-10-06 16:11:50 +0300

    hw3

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -3,80 +3,103 @@
 
 
 class HashMap:
-    """
+    '''
     Давайте сделаем все объектненько,
      поэтому внутри хешмапы у нас будет Entry
-    """
+    '''
     class Entry:
         def __init__(self, key, value):
-            """
-            Сущность, которая хранит пары ключ-значение
+            '''
+            Сущнос4ть, которая хранит пары ключ-значение
             :param key: ключ
             :param value: значение
-            """
+            '''
+            self.key = key
+            self.value = value
 
         def get_key(self):
             # TODO возвращаем ключ
-            raise NotImplementedError
+            return self.key
 
         def get_value(self):
             # TODO возвращаем значение
-            raise NotImplementedError
+            return self.value
 
         def __eq__(self, other):
             # TODO реализовать функцию сравнения
-            raise NotImplementedError
+            return self.key == other.key
 
     def __init__(self, bucket_num=64):
-        """
+        '''
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
-        """
-        raise NotImplementedError
+        '''
+        self.size = bucket_num
+        self.hash_bucket = [None] * self.size
 
     def get(self, key, default_value=None):
         # TODO метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
-        raise NotImplementedError
+        x = self._get_hash(key)
+        y = self._get_index(x)
+        if self.hash_bucket[y] is not None:
+            for i in self.hash_bucket[y]:
+                if i.get_key() == key:
+                    return i.get_value()
+        return default_value
 
     def put(self, key, value):
         # TODO метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
+        x = self._get_hash(key)
+        y = self._get_index(x)
+        if self.hash_bucket[y] is None:
+            self.hash_bucket[y] = [self.Entry(key, value)]
+        elif self.Entry(key, value) in self.hash_bucket[y]:
+            for new_value in self.hash_bucket[y]:
+                if new_value.key == key:
+                    new_value.value = value
+        else:
+            self.hash_bucket[y].append(self.Entry(key, value))
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        k = 0
+        for i in self.hash_bucket:
+            if i is not None:
+                k += len(i)
+        return k
 
     def _get_hash(self, key):
         # TODO Вернуть хеш от ключа,
         #  по которому он кладется в бакет
-        raise NotImplementedError
+        return hash(key)
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+        return hash_value % self.size
 
     def values(self):
         # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+        return list(j.value for i in self.hash_bucket if i is not None for j in i)
 
     def keys(self):
         # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+        return list(j.key for i in self.hash_bucket if i is not None for j in i)
 
     def items(self):
         # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+        return list((j.key, j.value) for i in self.hash_bucket if i is not None
+                    for j in i)
 
     def _resize(self):
         # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+        self.hash_bucket += [None] * (self.size // 2)
 
     def __str__(self):
         # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        return "buckets: {}, items: {}".format(self.hash_bucket, self.items())
 
     def __contains__(self, item):
         # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+        return item in self.keys()
--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
+
 
 class LRUCacheDecorator:
 
@@ -10,10 +12,33 @@ class LRUCacheDecorator:
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.time = ttl
+        self.cache = {}
+        self.realsize = 0
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, function):
+
+        def Time():
+            return time.time() * 1000
+
+        def internal(*args, **kwargs):
+            key = args + tuple(sorted(kwargs.items()))
+            if key not in self.cache.keys():
+                if self.realsize < self.maxsize:
+                    self.realsize += 1
+                else:
+                    index = min(self.cache.items(),
+                                key=lambda i: i[1][1])
+                    del self.cache[index[0]]
+            else:
+                item = self.cache[key]
+                print(self.cache)
+                if self.time is None or Time() - item[1] <= \
+                        self.time:
+                    self.cache[key] = (item[0], Time())
+                    return item[0]
+            self.cache[key] = (function(*args, **kwargs), Time())
+            return self.cache[key][0]
+
+        return internal
