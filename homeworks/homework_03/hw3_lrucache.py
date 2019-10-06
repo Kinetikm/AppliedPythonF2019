94c0f74c9ce5466410a6e41fa849352572725036
Mikhail <zojloverr@gmail.com>
2019-10-06 23:14:43 +0300

    lrucache

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,37 @@
-#!/usr/bin/env python
-# coding: utf-8
+from time import time
 
 
 class LRUCacheDecorator:
-
     def __init__(self, maxsize, ttl):
-        '''
-        :param maxsize: максимальный размер кеша
-        :param ttl: время в млсек, через которое кеш
-                    должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache = {}
+        self.time = {}
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, func):
+        def func_sur(*args, **kwargs):
+            key = (args, str(kwargs), str(kwargs.values()))
+            if key in self.cache:
+                if self.ttl:
+                    if (time() - self.time[key]) * 1000 > self.ttl:
+                        self.cache.pop(key)
+                        self.time.pop(key)
+                        res = func(*args, **kwargs)
+                        self.cache[key] = res
+                        self.time[key] = time()
+                        return res
+                self.time[key] = time()
+                return self.cache[key]
+            else:
+                res = func(*args, **kwargs)
+                if len(self.cache) < self.maxsize:
+                    self.cache[key] = res
+                    self.time[key] = time()
+                else:
+                    oldst = max(self.time, key=lambda x: time() - self.time[x])
+                    self.cache.pop(oldst)
+                    self.time.pop(oldst)
+                    self.cache[key] = res
+                    self.time[key] = time()
+                return res
+        return func_sur
