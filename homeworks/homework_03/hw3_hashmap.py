bd2ae074850a9ac3446c42be901a778361e26c4a
Pavel Lomtev <pavellomtev2012@yandex.ru>
2019-10-07 00:33:31 +0300

    upd

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -14,69 +14,93 @@ class HashMap:
             :param key: ключ
             :param value: значение
             """
+            self.key = key
+            self.value = value
 
         def get_key(self):
             # TODO возвращаем ключ
-            raise NotImplementedError
+            return self.key
 
         def get_value(self):
             # TODO возвращаем значение
-            raise NotImplementedError
+            return self.value
 
         def __eq__(self, other):
-            # TODO реализовать функцию сравнения
-            raise NotImplementedError
+            return self.key == other.get_key()
+
+        def __iter__(self):
+            yield self.key
+            yield self.value
 
     def __init__(self, bucket_num=64):
         """
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
         """
-        raise NotImplementedError
+        self.bucket_num = bucket_num
+        self.hash_map = [[] for i in range(int(self.bucket_num))]
 
     def get(self, key, default_value=None):
+        index = self._get_index(self._get_hash(key))
+        for entry in self.hash_map[index]:
+            if entry.get_key() == key:
+                return entry.get_value()
+        return default_value
         # TODO метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
-        raise NotImplementedError
 
     def put(self, key, value):
+        index = self._get_index(self._get_hash(key))
+        p_entry = self.Entry(key, value)
+        for i, entry in enumerate(self.hash_map[index]):
+            if entry == p_entry:
+                self.hash_map[index].pop(i)
+                break
+        self.hash_map[index].append(p_entry)
+        app = self.bucket_num * 0.75
+
+        if len([lst for lst in self.hash_map if lst]) > app:
+            self._resize()
         # TODO метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        return len(self.items())
 
     def _get_hash(self, key):
         # TODO Вернуть хеш от ключа,
         #  по которому он кладется в бакет
-        raise NotImplementedError
+        return hash(key)
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+        return hash_value % self.bucket_num
 
     def values(self):
         # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+        return [entry.get_value() for entry in self.items()]
 
     def keys(self):
         # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+        return [entry.get_key() for entry in self.items()]
 
     def items(self):
         # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+        return [entry for lst_of_entry in self.hash_map for entry in lst_of_entry]
 
     def _resize(self):
         # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+        self.bucket_num *= 2
+        items = self.items()
+        self.hash_map = [[] for i in range(self.bucket_num)]
+        for entry in items:
+            self.put(entry.get_key(), entry.get_value())
 
     def __str__(self):
         # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        return 'buckets: {}, items: {}'.format(self.bucket_num, len(self))
 
     def __contains__(self, item):
         # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+        return item in self.keys()
--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,48 @@
 #!/usr/bin/env python
 # coding: utf-8
+from time import time
 
 
 class LRUCacheDecorator:
 
+    class Results():
+        def __init__(self, res, time):
+            self.result = res
+            self.time = time
+
     def __init__(self, maxsize, ttl):
-        '''
-        :param maxsize: максимальный размер кеша
-        :param ttl: время в млсек, через которое кеш
-                    должен исчезнуть
-        '''
         # TODO инициализация декоратора
         #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
         raise NotImplementedError
+        self.maxsize = maxsize
+        self.time = ttl
+        self.cache = {}
 
-    def __call__(self, *args, **kwargs):
+    def get_key(self, *args, **kwargs):
+        s = ''.join(str(arg) for arg in args)
+        s.join(str(kwarg) for kwarg in kwargs)
+        return s
+
+    def __call__(self, func):
         # TODO вызов функции
-        raise NotImplementedError
+        def inner(*args, **kwargs):
+            key = self.get_key(*args, **kwargs)
+            if key in self.cache:
+                if self.time:
+                    if (time() - self.cache[key].time)*1000 > self.time:
+                        del self.cache[key]
+                        result = func(*args, **kwargs)
+                        self.cache[key] = self.Results(result, time())
+                        return result
+                self.cache[key].time = time()
+                return self.cache[key].result
+            else:
+                result = func(*args, *kwargs)
+                if len(self.cache) < self.maxsize:
+                    self.cache[key] = self.Results(result, time())
+                else:
+                    minimalkey = min(self.cache, key=lambda x: self.cache[x].time)
+                    del self.cache[minimalkey]
+                    self.cache[key] = self.Results(result, time())
+                return result
+        return inner
