605e3675d7596acf9692baa918257500cd47be2f
DanilTremasov <treasovdanil@gmail.com>
2019-10-06 23:01:54 +0300

    frs_try

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -3,44 +3,40 @@
 
 
 class HashMap:
-    """
-    Давайте сделаем все объектненько,
-     поэтому внутри хешмапы у нас будет Entry
-    """
     class Entry:
         def __init__(self, key, value):
-            self._key = key
-            self._value = value
+            self.key = key
+            self.value = value
 
         def get_key(self):
-            return self.value
-
-        def get_value(self):
             return self.key
 
+        def get_value(self):
+            return self.value
+
         def __eq__(self, other):
-            return self._key == other.get_key
+            return self.get_key() == other.get_key()
 
     def __init__(self, bucket_num=64):
-        self.bucket_num = bucket_num
-        self.vals = [[] for l in range(bucket_num)]
+        self.size = bucket_num
+        self.list = [[] for s in range(self.size)]
 
     def get(self, key, default_value=None):
-        indx = self._get_index(self._get_hash(key))
-        for l in self.vals[indx]:
-            if l.get_key() == key:
-                return l.get_value()
+        index = self._get_index(self._get_hash(key))
+        for s in self.list[index]:
+            if s.get_key() == key:
+                return s.get_value()
         return default_value
 
     def put(self, key, value):
-        indx = self._get_index(self._get_hash(key))
-        while len(self.vals) < self.bucket_num:
-            self.vals.append([])
+        index = self._get_index(self._get_hash(key))
+        while len(self.list) < self.size:
+            self.list.append([])
         new = self.Entry(key, value)
-        if new in self.vals[indx]:
-            self.vals[indx].remove(new)
-        self.vals[indx].append(new)
-        if len([1 for l in self.vals if i]) > self.bucket_num * 0.5:
+        if new in self.list[index]:
+            self.list[index].remove(new)
+        self.list[index].append(new)
+        if len([1 for s in self.list if s]) > self.size * 0.5:
             self._resize
 
     def __len__(self):
@@ -50,26 +46,27 @@ class HashMap:
         return hash(key)
 
     def _get_index(self, hash_value):
-        return hash_value % self.bucket_num
+        return hash_value % self.size
 
     def values(self):
-        return ([l.get_value() for case in self.vals for l in case])
+        return ([s.get_value() for case in self.list for s in case])
 
     def keys(self):
-        return ([l.get_key() for case in self.vals for l in case])
+        return ([s.get_key() for case in self.list for s in case])
 
     def items(self):
-        return ([(l.get_key(), l.get_value()) for case in self.vals for s in case])
+        return ([(s.get_key(), s.get_value()) for case in self.list for s in case])
 
     def _resize(self):
-        self.bucket_num *= 2
+       self.size *= 2
         entries = self.items()
-        self.vals = [[] for i in range(self.bucket_num)]
-        for j in entries:
-            self.put(*j)
+        self.list = [[] for i in range(self.size)]
+        for s in entries:
+            self.put(*s)
 
     def __str__(self):
-        return 'buckets: {}, items: {}'.format(self.bucket_num, len(self))
+        return ("buckets: {}, items: {}".
+                format(self.size, len(self.items())))
 
     def __contains__(self, item):
         return item in self.keys()
