53c4b8dcb6ba26b34058779e2b733ee221adc2e0
mnpks <fuglu@mail.ru>
2019-10-07 02:25:21 +0300

    hashmap and lru

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -14,69 +14,106 @@ class HashMap:
             :param key: ключ
             :param value: значение
             """
+            self.key = key
+            self.value = value
 
         def get_key(self):
             # TODO возвращаем ключ
-            raise NotImplementedError
+            return self.key
 
         def get_value(self):
             # TODO возвращаем значение
-            raise NotImplementedError
+            return self.value
 
         def __eq__(self, other):
             # TODO реализовать функцию сравнения
-            raise NotImplementedError
+            return self.key == other.get_key()
 
     def __init__(self, bucket_num=64):
         """
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
         """
-        raise NotImplementedError
+        self.ar = [[] for i in range(bucket_num)]
+        self.buckets = bucket_num
+        self.elements = 0
 
     def get(self, key, default_value=None):
         # TODO метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
-        raise NotImplementedError
+        bucket = self._get_index(self._get_hash(key))
+        for i in self.ar[bucket]:
+            if i.get_key() == key:
+                return i.get_value()
+        else:
+            return default_value
 
     def put(self, key, value):
         # TODO метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
+        i = self._get_index(self._get_hash(key))
+        new_elem = self.Entry(key, value)
+        for elem in self.ar[i]:
+            if elem.get_key() == new_elem.get_key():
+                elem.value = value
+                return
+        self.ar[i].append(new_elem)
+        self.elements += 1
+        # if self.elements > self.buckets * 2 / 3:
+        #     self._resize()
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        return self.elements
 
     def _get_hash(self, key):
         # TODO Вернуть хеш от ключа,
         #  по которому он кладется в бакет
-        raise NotImplementedError
+        return hash(key)
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+        return hash_value % self.buckets
 
     def values(self):
         # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+        iterator = []
+        for buc in self.ar:
+            iterator.extend([elem.get_value() for elem in buc])
+        return iter(iterator)
 
     def keys(self):
         # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+        iterator = []
+        for buc in self.ar:
+            iterator.extend([elem.get_key() for elem in buc])
+        return iter(iterator)
 
     def items(self):
         # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+        iterator = []
+        for buc in self.ar:
+            iterator.extend([(elem.get_key(), elem.get_value()) for elem in buc])
+        return iter(iterator)
 
     def _resize(self):
         # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+        self.buckets *= 2
+        new = [[] for i in range(self.buckets)]
+        for buc in self.ar:
+            for elem in buc:
+                i = self._get_index(self._get_hash(elem.key))
+                new[i].append(elem)
+        self.ar = new
 
     def __str__(self):
         # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        return f"buckets: {self.ar}, items: {self.items()}"
 
     def __contains__(self, item):
         # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+        for buc in self.ar:
+            for elem in buc:
+                if elem.get_key() == item:
+                    return True
+        return False
--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+import time
 
 
 class LRUCacheDecorator:
@@ -10,10 +11,36 @@ class LRUCacheDecorator:
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache = dict()
+        self.time = dict()
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, func):
+        def dec(*args, **kwargs):
+            k = (args, tuple(kwargs))
+            if k in self.cache.keys():
+                if self.ttl:
+                    if (time.time() - self.time[k]) * 1000 > self.ttl:
+                        del self.cache[k]
+                        func(*args, **kwargs)
+                        self.cache[k] = func(*args, **kwargs)
+                        del self.time[k]
+                        self.time[k] = time.time()
+                        return self.cache[k]
+                del self.time[k]
+                self.time[k] = time.time()
+                return self.cache[k]
+            else:
+                if self.maxsize:
+                    if len(self.cache) < self.maxsize:
+                        self.cache[k] = func(*args, **kwargs)
+                        self.time[k] = time.time()
+                    else:
+                        d = max(self.time, key=lambda elem: time.time() - self.time[elem])
+                        del self.time[d]
+                        del self.cache[d]
+                        self.cache[k] = func(*args, **kwargs)
+                        self.time[k] = time.time()
+                return self.cache[k]
+        return dec
