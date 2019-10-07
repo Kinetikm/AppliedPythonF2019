a83724d29ca80873b5b427cd927689e477445ed3
PolinaPonomar <ponomareva.polia2013@yandex.ru>
2019-10-08 01:40:11 +0300

    hashmap

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -7,6 +7,7 @@ class HashMap:
     Давайте сделаем все объектненько,
      поэтому внутри хешмапы у нас будет Entry
     """
+
     class Entry:
         def __init__(self, key, value):
             """
@@ -14,69 +15,151 @@ class HashMap:
             :param key: ключ
             :param value: значение
             """
+            self.key = key
+            self.value = value
 
         def get_key(self):
             # TODO возвращаем ключ
-            raise NotImplementedError
+            return self.key
 
         def get_value(self):
             # TODO возвращаем значение
-            raise NotImplementedError
+            return self.value
 
         def __eq__(self, other):
             # TODO реализовать функцию сравнения
-            raise NotImplementedError
+            return self.key == other.get_key()
+
+    class Special_Iterator:
+
+        def __iter__(self):
+            return self
+
+        def __init__(self, some_list, need):
+            self.some_list = some_list
+            self.counter1 = 0
+            self.counter2 = 0
+            self.need = need
+
+        def __next__(self):
+
+            while self.counter1 < len(self.some_list):
+                if self.some_list[self.counter1] != []:
+                    if len(self.some_list[self.counter1]) == 1:
+                        z = self.some_list[self.counter1][self.counter2]
+                        self.counter2 = 0
+                        self.counter1 += 1
+                        if self.need == 'keys':
+                            return z.get_key()
+                        elif self.need == 'values':
+                            return z.get_value()
+                        elif self.need == 'items':
+                            return z.get_key(), z.get_value()
+                    else:
+                        if self.counter2 < len(self.some_list[self.counter1]):
+                            z = self.some_list[self.counter1][self.counter2]
+                            self.counter2 += 1
+                            if self.need == 'keys':
+                                return z.get_key()
+                            elif self.need == 'values':
+                                return z.get_value()
+                            elif self.need == 'items':
+                                return z.get_key(), z.get_value()
+                        else:
+                            self.counter1 += 1
+                            self.counter2 = 0
+
+                else:
+                    self.counter1 += 1
+            raise StopIteration
 
     def __init__(self, bucket_num=64):
         """
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
         """
-        raise NotImplementedError
+        self.bucket_num = bucket_num
+        slots = []
+        for i in range(self.bucket_num):
+            slots.append([])
+        self.slots = slots
+        self.keys_list = []
 
     def get(self, key, default_value=None):
         # TODO метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
-        raise NotImplementedError
+        if key in self.keys_list:
+            for x in self.slots[self._get_index(self._get_hash(key))]:
+                if x.get_key() == key:
+                    return x.get_value()
+        else:
+            return default_value
 
     def put(self, key, value):
         # TODO метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
+        i = 0
+        for bucket in self.slots:
+            if bucket != []:
+                i += 1
+                break
+        if i == 0:
+            self._resize()
+
+        item = self.Entry(key, value)
+        if self.__contains__(key):
+            for x in self.slots[self._get_index(self._get_hash(item.get_key()))]:
+                if x.get_key() == item.get_key():
+                    x.value = item.get_value()
+        else:
+            self.slots[self._get_index(self._get_hash(item.get_key()))].append(item)
+            self.keys_list.append(item.get_key())
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        l = 0
+        for item in self.items():
+            l += 1
+        return l
 
     def _get_hash(self, key):
         # TODO Вернуть хеш от ключа,
         #  по которому он кладется в бакет
-        raise NotImplementedError
+        return hash(key)
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+        return hash_value % self.bucket_num
 
     def values(self):
         # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+        return self.Special_Iterator(self.slots, 'values')
 
     def keys(self):
         # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+        return self.Special_Iterator(self.slots, 'keys')
 
     def items(self):
         # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+        return self.Special_Iterator(self.slots, 'items')
 
     def _resize(self):
         # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+        M = []
+        for x in self.items():
+            M.append(x)
+        self.bucket_num *= 2
+        slots = []
+        for i in range(self.bucket_num):
+            slots.append([])
+        self.slots = slots
+        for item in M:
+            self.put(item[0], item[1])
 
     def __str__(self):
         # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        return f'buckets: {{{self.bucket_num}}}, items: {{{self.__len__()}}}'
 
     def __contains__(self, item):
         # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+        return item in self.keys_list
