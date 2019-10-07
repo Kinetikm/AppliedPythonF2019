d117e7a1bdf809cc49482e328d1df7702f736682
Mikhail <zojloverr@gmail.com>
2019-10-07 04:16:17 +0300

    tensor

--- homeworks/homework_03/hw3_dense_tensor.py
+++ homeworks/homework_03/hw3_dense_tensor.py
@@ -1,31 +1,209 @@
-#!/usr/bin/env python
-# coding: utf-8
+import copy
 
 
 class Tensor:
-    """
-    Your realisation of numpy tensor.
-
-    Must be implemented:
-    1. Getting and setting element by indexes of row and col.
-    a[i, j] = v -- set value in i-th row and j-th column to value.
-    b = a[i, j] -- get value from i-th row and j-th column.
-    2. Pointwise operations.
-    c = a + b -- sum of two Tensors of the same shape or sum with scalar.
-    c = a - b -- difference --//--.
-    c = a * b -- product --//--.
-    c = a / alpha -- divide Tensor a by nonzero scalar alpha.
-    c = a ** b -- raises each element of a to the power b.
-    3. Sum, mean, max, min, argmax, argmin by axis.
-    if axis is None then operation over all elements.
-    4. Transpose by given axes (by default reverse dimensions).
-    5. Swap two axes.
-    6. Matrix multiplication for tensors with dimension <= 2.
-    """
-
     def __init__(self, init_matrix_representation):
-        """
-        :param init_matrix_representation: list of lists
-        """
+        self.matr = copy.deepcopy(init_matrix_representation)
 
-        raise NotImplementedError
+    def __getitem__(self, index):
+        if type(index) == tuple:
+            return self.matr[index[0]][index[1]]
+
+    def __setitem__(self, key, value):
+        self.matr[key[0]][key[1]] = value
+
+    def __add__(self, other):
+        if type(other) == float or type(other) == int:
+            out = copy.deepcopy(self)
+            for i in range(len(out.matr)):
+                for j in range(len(out.matr[i])):
+                    out[i, j] += other
+            return out
+        elif type(other) == Tensor:
+            if (len(self.matr) == len(other.matr)) and (len(self.matr[0]) == len(other.matr[0])):
+                out = copy.deepcopy(self)
+                for i in range(len(out.matr)):
+                    for j in range(len(out.matr[i])):
+                        out[i, j] += other[i, j]
+                return out
+            else:
+                raise ValueError
+        raise ValueError
+
+    def __sub__(self, other):
+        if type(other) == float or type(other) == int:
+            out = copy.deepcopy(self)
+            for i in range(len(out.matr)):
+                for j in range(len(out.matr[i])):
+                    out[i, j] += other
+            return out
+        elif type(other) == Tensor:
+            if (len(self.matr) == len(other.matr)) and (len(self.matr[0]) == len(other.matr[0])):
+                out = copy.deepcopy(self)
+                for i in range(len(out.matr)):
+                    for j in range(len(out.matr[i])):
+                        out[i, j] -= other[i, j]
+                return out
+            else:
+                raise ValueError
+        raise ValueError
+
+    def __mul__(self, other):
+        if type(other) == float or type(other) == int:
+            out = copy.deepcopy(self)
+            for i in range(len(out.matr)):
+                for j in range(len(out.matr[i])):
+                    out[i, j] += other
+            return out
+        elif type(other) == Tensor:
+            if (len(self.matr) == len(other.matr)) and (len(self.matr[0]) == len(other.matr[0])):
+                out = copy.deepcopy(self)
+                for i in range(len(out.matr)):
+                    for j in range(len(out.matr[i])):
+                        out[i, j] = out[i, j] * other[i, j]
+                return out
+            else:
+                raise ValueError
+        raise ValueError
+
+    def __truediv__(self, other):
+        if other == 0:
+            raise ValueError
+        out = copy.deepcopy(self)
+        for i in range(len(out.matr)):
+            for j in range(len(out.matr[i])):
+                out[i, j] = out[i, j]/other
+        return out
+
+    def __pow__(self, power, modulo=None):
+        out = copy.deepcopy(self)
+        for i in range(len(out.matr)):
+            for j in range(len(out.matr[i])):
+                if out[i, j] != 0:
+                    out[i, j] = out[i, j]**power
+        return out
+
+    def sum(self, *axis):
+        s = 0
+        if len(axis) == 0:
+            for i in range(len(self.matr)):
+                for j in range(len(self.matr[i])):
+                    s += self[i, j]
+            return s
+        else:
+            for j in range(len(self.matr[axis[0]])):
+                s += self[axis[0], j]
+            return s
+
+    def min(self, *axis):
+        if len(axis) == 0:
+            s = self[0, 0]
+            for i in range(len(self.matr)):
+                for j in range(len(self.matr[i])):
+                    if self[i, j] < s:
+                        s = self[i, j]
+            return s
+        else:
+            s = self[axis[0], 0]
+            for j in range(len(self.matr[axis[0]])):
+                if self[axis[0], j] < s:
+                    s = self[axis[0], j]
+            return s
+
+    def max(self, *axis):
+        if len(axis) == 0:
+            s = self.matr[0, 0]
+            for i in range(len(self.matr)):
+                for j in range(len(self.matr[i])):
+                    if self[i, j] > s:
+                        s = self[i, j]
+            return s
+        else:
+            s = self[axis[0], 0]
+            for j in range(len(self.matr[axis[0]])):
+                if self[axis[0], j] > s:
+                    s = self[axis[0], j]
+            return s
+
+    def argmin(self, *axis):
+        if len(axis) == 0:
+            s = self.matr[0, 0]
+            p = 0
+            for i in range(len(self.matr)):
+                for j in range(len(self.matr[i])):
+                    if self[i, j] < s:
+                        s = self[i, j]
+                        p = j
+            return p
+
+        m = self[axis[0], 0]
+        m_i = 0
+        for j in range(len(self.matr[axis[0]])):
+            if self[axis[0], j] < m:
+                m = self[axis[0], j]
+                m_i = j
+        return m_i
+
+    def argmax(self, *axis):
+        if len(axis) == 0:
+            s = self.matr[0, 0]
+            p = 0
+            for i in range(len(self.matr)):
+                for j in range(len(self.matr[i])):
+                    if self[i, j] > s:
+                        s = self[i, j]
+                        p = j
+            return p
+
+        m = self[axis[0], 0]
+        m_i = 0
+        for j in range(len(self.matr[axis[0]])):
+            if self[axis[0], j] > m:
+                m = self[axis[0], j]
+                m_i = j
+        return m_i
+
+    def mean(self, *axis):
+        s = 0
+        if len(axis) == 0:
+            return self.sum()/(len(self.matr)*len(self.matr[0]))
+        else:
+            return self.sum(*axis)/(len(self.matr[0]))
+
+    def transpose(self, *args):
+        if (args is None) or ((args[0] == 1) and (args[1] == 0) == 0):
+            new_m = []
+            for i in range(len(self.matr[0])):
+                new_m.append([0] * len(self.matr))
+            for i in range(len(self.matr)):
+                for j in range(len(self.matr[i])):
+                    new_m[j][i] = self[i, j]
+            self.matr = copy.deepcopy(new_m)
+
+    def swap(self, ax1, ax2):
+        self.matr[ax1], self.matr[ax2] = self.matr[ax2], self.matr[ax1]
+
+    def __matmul__(self, other):
+        if (len(other.matr) == 1) and (len(other.matr[0]) == len(self.matr[0])):
+            m = [[0] * len(self.matr)]
+            for row in range(len(self.matr)):
+                for i in range(len(self.matr[0])):
+                    m[0][row] += self[row, i] * other[0, i]
+            out = Tensor(m)
+            return out
+
+        if len(self.matr[0]) != len(other.matr):
+            raise ValueError
+        m = []
+        for i in range(len(self.matr)):
+            m.append([0] * len(other.matr[0]))
+        out = Tensor(m)
+
+        for row in range(len(self.matr)):
+            for col in range(len(other.matr[0])):
+                for i in range(len(self.matr[0])):
+                    out[row, col] += self[row, i] * other[i, col]
+        return out
+
+    def get(self):
+        return self.matr
