31c60d906496faaff9f4a712d50421210a2a1cdb
Iv4nuwka <iv4nuwka@mail.ru>
2019-10-07 08:12:13 +0300

    csr (init,to_dense,get)

--- homeworks/homework_03/hw3_csr_matrix.py
+++ homeworks/homework_03/hw3_csr_matrix.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 import numpy as np
 
 
@@ -34,17 +33,88 @@ class CSRMatrix:
             where data, row_ind and col_ind satisfy the relationship:
             a[row_ind[k], col_ind[k]] = data[k]
         """
+        self.a = []
+        self.ia = [0]
+        self.ja = []
+        self.nnz = 0
+        self.cols_num = 0
+        self.rows_num = 0
+        flag = True
         if isinstance(init_matrix_representation, tuple) and len(init_matrix_representation) == 3:
-            pass
+            for i in range(2):
+                flag *= (len(init_matrix_representation[i+1]) == len(init_matrix_representation[i]))
+            if flag:
+                for i in range(3):
+                    for j in range(len(init_matrix_representation[0])):
+                        if init_matrix_representation[i][j] != 0:
+                            self.a.append(init_matrix_representation[i][j])
+                            self.nnz += 1
+                            self.ja.append(j)
+                    self.ia.append(self.nnz)
+                self.rows_num = 3
+                self.cols_num = len(init_matrix_representation[0])
+                del flag
+            else:
+                raise ValueError
+
         elif isinstance(init_matrix_representation, np.ndarray):
-            pass
+            init_matr = init_matrix_representation
+            init_matr = init_matr.tolist()
+            init_matr.sort(key=lambda i: (i[0], i[1]))
+            self.rows_num = max(r[0] for r in init_matr)+1
+            self.cols_num = max(c[1] for c in init_matr)+1
+            nnz = 0
+            tmp_str_num = 0
+            for item in init_matr:
+                if item[2] != 0:
+                    if item[0] > tmp_str_num:
+                        for i in range(item[0] - tmp_str_num):
+                            self.ia.append(nnz)
+                        tmp_str_num = item[0]
+                    self.a.append(item[2])
+                    self.ja.append(item[1])
+                    nnz += 1
+            self.ia.append(nnz)
         else:
             raise ValueError
 
-        raise NotImplementedError
+    def __getitem__(self, item):
+        row = item[0]
+        col = item[1]
+        rows_num = len(self.ia) -1
+        cols_num = max(self.ja) + 1
+        els_in_row = self.ia[row+1] - self.ia[row]
+        if (0 <= row < rows_num) and (0 <= col < cols_num):
+            if els_in_row == 0:
+                return 0
+            else:
+                if col not in self.ja:
+                    return 0
+                else:
+                    els_behind = self.ia[row]
+                    for i in range(els_in_row):
+                        if self.ja[els_behind + i] == col:
+                            return self.a[els_behind + i]
+                    return 0
+        else:
+            raise IndexError
 
     def to_dense(self):
         """
-        Return dense representation of matrix (2D np.array).
+            Return dense representation of matrix (2D np.array).
         """
-        raise NotImplementedError
+        mas = [[] for i in range(self.rows_num)]
+        for i in range(self.rows_num):
+            for j in range(self.cols_num):
+                mas[i].append(0)
+        els_passed = 0
+        for i in range(1, len(self.ia)):
+            while els_passed < self.ia[i]:
+                mas[i-1][self.ja[els_passed]] = self.a[els_passed]
+                els_passed += 1
+        return mas
+
+
+matr = CSRMatrix(np.array([(0, 1, 1), (0, 0, 2), (0, 2, 3),
+                           (1, 0, 0), (1, 1, 0), (1, 2, 6),
+                           (2, 0, 7), (2, 1, 8), (2, 2, 9)]))
