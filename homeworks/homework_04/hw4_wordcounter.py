eacb782e9138603e97c5a2c7a28a9bdc5d5ab06a
Koltsa Ilya <koltsa.ilya@mail.ru>
2019-10-08 21:57:04 +0300

    еределал

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
 import os
 import math
 
@@ -11,23 +11,26 @@ def word_count_inference(path_to_dir):
     files = os.listdir(path_to_dir)
     manager = Manager()
     dct = manager.dict()
-    dct['total'] = 0
-    procs = []
+    queue = manager.Queue()
     for fl in files:
-        proc = Process(target=count, args=((path_to_dir, fl), dct))
-        proc.start()
-        procs.append(proc)
-
-    for pr in procs:
-        pr.join()
+        queue.put((path_to_dir, fl))
+    dct['total'] = 0
+    PROCESSES_COUNT = 3
+    pool = Pool(PROCESSES_COUNT)
+    for i in range(queue.qsize()):
+        pool.apply_async(count, (queue, dct))
+    pool.close()
+    pool.join()
+    for key in dct:
+        if key != 'total':
+            dct['total'] += dct[key]
     return dct
 
 
-def count(path_to_file, dct):
-    path = path_to_file[0] + '/' + path_to_file[1]
-    path_to_file = path_to_file[1]
-    dct[path_to_file] = 0
+def count(q, dct):
+    pth = q.get()
+    path = pth[0] + '/' + pth[1]
+    dct[pth[1]] = 0
     with open(path, 'r', encoding="utf-8") as file:
         for ln in file:
-            dct[path_to_file] += len(ln.split())
-        dct['total'] += dct[path_to_file]
+            dct[pth[1]] += len(ln.split())
