4e6e81dfe47265e1b8e5b1f4f8fdbe3714fb2f62
Laggystick <a.lavrenov@skib-dozen.org>
2019-10-08 21:52:56 +0300

    done wordcounter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Manager, Pool
 import os
+
+
+def producer_func(file, queue, path_to_file):
+    res = func(file, path_to_file)
+    queue.put({file: res})
+
+
+def consumer_func(queue):
+    result = {'total': 0}
+    while True:
+        res = queue.get()
+        if res == 'kill-9':
+            queue.put(result)
+            break
+        a = 0
+        for i in res.values():
+            a = i
+        result.update(res)
+        result.update({'total': result['total'] + a})
+
+
+def func(file, path_to_dir):
+    with open(f'{path_to_dir}/{file}', 'r') as f:
+        return len(f.read().strip().split())
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +40,21 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(os.cpu_count())
+
+    pool.apply_async(consumer_func, args=(queue,))
+
+    to_process_list = os.listdir(path_to_dir)
+
+    jobs = []
+    for file in to_process_list:
+        job = pool.apply_async(producer_func, args=(file, queue, path_to_dir,))
+        jobs.append(job)
+    for job in jobs:
+        job.get()
+
+    queue.put('kill-9')
+    result = queue.get()
+    return result
