3fbe5467b13b517690e7c1bf6e66b580790974e1
root <root@localhost.localdomain>
2019-10-08 23:05:09 +0300

    fix table

--- homeworks/homework_02/parse.py
+++ homeworks/homework_02/parse.py
@@ -8,50 +8,44 @@ def read_data(f, enc, type_f):
     if type_f == 'json':
         data = json.load(file)
         res.append(list(data[0].keys()))
-        i = len(data[0].keys()) - 1
-        for j in range(i):
+        for j in range(len(data)):
+            if res[0] != list(data[j].keys()):
+                raise
             l = []
             for x in data[j].values():
-                if type(x) != str:
+                if not isinstance(x, str):
                     l.append(str(x))
                 else:
                     l.append(x)
             res.append(l)
     else:
         data = csv.reader(file, delimiter="\t")
+        flag = False
         for i in data:
-            res.append(i)
+            if flag and len(i) != len(res[-1]):
+                raise
+            flag = True
+            if i:
+                res.append(i)
+            else:
+                raise
     return res
 
 
 def print_data(data):
     size = []
-    i = len(data)
-    l = len(data[0])
-    indent = 3
-    for j in range(l):
+    for j in range(len(data[0])):
         s = 0
-        for k in range(i):
+        for k in range(len(data)):
             if s < len(data[k][j]):
                 s = len(data[k][j])
-        size.append(s + 2 * indent)
-    s = sum(size) - (l - 1)
-    print(s * '-')
-    head = ''
-    for j in range(l):
-        spaces = int((size[j] - len(data[0][j]) - 2) / 2)
-        head += '|' + spaces * ' ' + data[0][j] + spaces * ' '
-    head += '|'
-    print(head)
-    spaces = 2
-    for x in range(1, i):
-        string = ''
-        for j in range(l):
-            last = size[j] - len(data[x][j]) - 2 * spaces
-            if j != l - 1:
-                string += '|' + spaces * ' ' + data[x][j] + last * ' '
-            else:
-                string += '|' + last * ' ' + data[x][j] + spaces * ' '
-        string += '|'
-        print(string)
-    print(s * '-')
+        size.append(s)
+    first = 5 * len(size) + sum(size) + 1
+    print(first * '-')
+    for i, line in enumerate(data):
+        flag = '^' if i == 0 else '<'
+        for j, value in enumerate(line):
+            flag = '>' if j == 3 else flag
+            line[j] = ("{:" + flag + str(size[j]) + "}").format(value)
+        print("|  {}  |".format("  |  ".join(line)))
+    print(first * '-')
--- homeworks/homework_02/type_is.py
+++ homeworks/homework_02/type_is.py
@@ -3,23 +3,14 @@ import csv
 
 
 def json_or_csv(f, enc):
-    l = 0
     try:
         file = open(f, encoding=enc)
         data = json.load(file)
-        for i in data:
-            if not (l == 0 or l == len(i)):
-                return None
-            l = len(i)
         return 'json'
     except:
         try:
             file = open(f, encoding=enc)
             data = csv.reader(file, delimiter="\t")
-            for i in data:
-                if not (l == 0 or l == len(i)):
-                    return None
-                l = len(i)
             return 'csv'
         except:
             raise
