5ffd3ad34a835159797fa51d4a304075de31f26d
Konstantin Kuritsyn <kuritsyn.1998@inbox.ru>
2019-10-09 19:48:08 +0300

    add wordcounter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Manager, Pool
 import os
+
+
+def sum_word(path, queue):
+    with open(path) as f:
+        lines = 0
+        words = 0
+        letters = 0
+        for line in f:
+            lines += 1
+            letters += len(line)
+            pos = 'out'
+            for letter in line:
+                if letter != ' ' and pos == 'out' and letter != '\n':
+                    words += 1
+                    pos = 'in'
+                elif letter == ' ':
+                    pos = 'out'
+    name = path.split('/')[-1]
+    queue.put({name: words})
+
+
+def total(queue):
+    tot = []
+    w = {}
+    while True:
+        q = queue.get()
+        if q == 'sosat':
+            break
+        tot += q.values()
+        w.update(q)
+    tot1 = sum(tot)
+    w["total"] = tot1
+    return w
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +49,15 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    a = os.listdir(path=path_to_dir)
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool()
+    result = pool.apply_async(total, (queue,))
+    for file in a:
+        c = path_to_dir + '/' + file
+        poc = pool.apply_async(sum_word, (c, queue))
+    poc.get()
+    queue.put('sosat')
+    pool.close()
+    return result.get()
