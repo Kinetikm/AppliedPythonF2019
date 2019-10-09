3c363f752ee25122cf1004a025c87a3726df52f9
advatrix <trojan.advatrix@gmail.com>
2019-10-09 11:09:14 +0300

    add wordcounter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
 import os
+
+
+PROCESSES_COUNT = os.cpu_count()
+
+
+def file_word_count(path, filename, queue):
+    count = 0
+    with open(path + '/' + filename, 'r') as f:
+        for line in f:
+            count += len(line.split())
+    item = {filename: count}
+    queue.put(item)
+
+
+def consumer_func(queue):
+    words_dict = {'total': 0}
+    while True:
+        res = queue.get()
+        if res == '*kill*':
+            break
+        words_dict.update(res)
+    words_dict['total'] = sum(words_dict.values())
+    return words_dict
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +39,22 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(PROCESSES_COUNT)
+
+    word_dict = pool.apply_async(consumer_func, (queue,))
+    jobs = []
+    for file in os.listdir(path_to_dir):
+        if os.path.isfile(path_to_dir + '/' + file):
+            job = pool.apply_async(file_word_count, (path_to_dir, file, queue,))
+            jobs.append(job)
+
+    for job in jobs:
+        job.get()
+
+    queue.put('*kill*')
+    pool.close()
+    pool.join()
+
+    return word_dict.get()
