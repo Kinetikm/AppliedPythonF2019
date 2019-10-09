cccff9b09f55d495290f27d2776549213bc5644c
golovnyakpa <pgolovnyak@gmail.com>
2019-10-09 22:06:41 +0300

    done word_counter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
 import os
 
 
-def word_count_inference(path_to_dir):
+def producer_func(file_, path_to_dir, queue):
+    try:
+        with open(os.path.join(path_to_dir, file_), 'r') as f:
+            ctr = 0
+            for line in f:
+                for word in line.strip().split():
+                    ctr += 1
+        queue.put({file_: ctr})
+    except IsADirectoryError:
+        pass
+
+
+def consumer_func(queue):
+    res_dict = {}
+    total = 0
+    while True:
+        new_val = queue.get()
+        if new_val == 'stop iteration':
+            break
+        for value in new_val.values():
+            total += value
+        res_dict.update(new_val)
+    res_dict['total'] = total
+    return res_dict
+
+
+def word_count_inference(path_to_dir, process_num=4):
     '''
     Метод, считающий количество слов в каждом файле из директории
     и суммарное количество слов.
@@ -16,4 +42,18 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(process_num)
+    files = os.listdir(path_to_dir)
+    cons_job = pool.apply_async(consumer_func, (queue,))
+    jobs = []
+    for file_ in files:
+        job = pool.apply_async(producer_func, (file_, path_to_dir, queue))
+        jobs.append(job)
+    for job in jobs:
+        job.get()
+    queue.put('stop iteration')
+    result = cons_job.get()
+    pool.close()
+    return result
