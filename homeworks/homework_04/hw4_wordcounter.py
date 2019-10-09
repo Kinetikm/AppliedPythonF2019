a4854d1ea62403bf2ea58582851337dcfad9ab5f
Filatov Ilya <ilya_filatovv@mail.ru>
2019-10-10 02:21:53 +0300

    wordcounter changed

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -4,16 +4,15 @@ import multiprocessing
 from multiprocessing import Pool, Manager, Queue
 
 
-def count_words(queue, path):
-    q = queue.get()
-    name = path + '/' + q
+def count_words(queue, path, file_name):
+    name = path + '/' + file_name
     try:
         with open(name, 'r', encoding='utf8') as file:
             n_words = 0
             for line in file:
                 words_list = line.split()
                 n_words += len(words_list)
-            queue.put((q, n_words))
+            queue.put((file_name, n_words))
     except (OSError, UnicodeDecodeError):
         queue.put((q, 0))
 
@@ -25,19 +24,15 @@ def word_count_inference(path_to_dir):
     queue = manager.Queue()
     pool = Pool(process_count)
     jobs = []
-    len_ = 0
-    for f in listdir(path_to_dir):
-        if isfile(join(path_to_dir, f)):
-            queue.put(f)
-            len_ += 1
-    for _ in range(len_):
-        job = pool.apply_async(count_words, (queue, path_to_dir))
+    onlyfiles = [f for f in listdir(path_to_dir) if isfile(join(mypath, f))]
+    for i in onlyfiles:
+        job = pool.apply_async(count_words, (queue, path_to_dir, i))
         jobs.append(job)
     for job in jobs:
         job.get()
     pool.close()
     pool.join()
-    for _ in range(len_):
+    for _ in range(len(onlyfiles)):
         q = queue.get()
         dict_[q[0]] = q[1]
         dict_['total'] += q[1]
