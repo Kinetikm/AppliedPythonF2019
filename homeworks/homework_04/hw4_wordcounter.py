9eae1b76a6341594e423ca64fc61adf8356718a7
root <root@localhost.localdomain>
2019-10-09 18:34:24 +0300

    hw4 1 file

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -2,18 +2,41 @@
 # coding: utf-8
 
 from multiprocessing import Process, Manager
+from multiprocessing import Pool
 import os
 
 
+def words_in_file(queue, path):
+    with open(path) as infile:
+        words = 0
+        for line in infile:
+            words += len(line.split())
+    queue.put((path, words))
+
+
+def get_result(queue):
+    result = {}
+    while True:
+        value = queue.get()
+        if value:
+            result[value[0].split('/')[-1]] = value[1]
+        else:
+            result['total'] = sum(result.values())
+            return result
+
+
 def word_count_inference(path_to_dir):
-    '''
-    Метод, считающий количество слов в каждом файле из директории
-    и суммарное количество слов.
-    Слово - все, что угодно через пробел, пустая строка "" словом не считается,
-    пробельный символ " " словом не считается. Все остальное считается.
-    Решение должно быть многопроцессным. Общение через очереди.
-    :param path_to_dir: путь до директории с файлами
-    :return: словарь, где ключ - имя файла, значение - число слов +
-        специальный ключ "total" для суммы слов во всех файлах
-    '''
-    raise NotImplementedError
+    q = Manager().Queue()
+    processes = []
+    files = os.listdir(path_to_dir)
+    pool = Pool(5)
+    result = pool.apply_async(get_result, args=(q,))
+    for value in files:
+        p = pool.apply_async(words_in_file, args=(q, path_to_dir + '/' + value,))
+        processes.append(p)
+    for p in processes:
+        p.get()
+    q.put(0)
+    pool.close()
+    pool.join()
+    return result.get()
