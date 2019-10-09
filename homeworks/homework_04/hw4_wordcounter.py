6050f3dd93f0c3a9ccae02a51faccfe58f014c10
ChesnovAE <aechesnov@yandex.ru>
2019-10-09 17:36:31 +0300

    Add wordcounter and fixe pep8 process_manager

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+
+from multiprocessing import Pool, Manager, Queue
 import os
+
+
+def producer(file_path, queue):
+    sum_words = 0
+    with open(file_path, 'r') as f:
+        sum_words = len(f.read().strip().split())
+        queue.put((file_path.split('/')[-1], sum_words))
+
+
+def consumer(queue):
+    dict_ = {}
+    total = 0
+    while True:
+        result = queue.get()
+        if result == 'kill':
+            break
+        dict_[result[0]] = result[1]
+        total += result[1]
+    dict_['total'] = total
+    return dict_
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +37,21 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    PROCESSES_COUNT = 4
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(PROCESSES_COUNT)
+    list_files = os.listdir(path_to_dir)
+
+    cons = pool.apply_async(consumer, (queue, ))
+    jobs = []
+    for file in list_files:
+        job = pool.apply_async(producer, (path_to_dir + '/' + file, queue))
+        jobs.append(job)
+    for job in jobs:
+        job.get()
+    queue.put('kill')
+    res = cons.get()
+    pool.close()
+    pool.join()
+    return res
