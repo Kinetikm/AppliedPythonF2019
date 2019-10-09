8bc2447e41c75d42c6661054fc46582ba2ce26bc
Filatov Ilya <ilya_filatovv@mail.ru>
2019-10-09 07:46:24 +0300

    wordcounter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,19 +1,44 @@
-#!/usr/bin/env python
-# coding: utf-8
+from os import listdir
+from os.path import isfile, join
+import multiprocessing
+from multiprocessing import Pool, Manager, Queue
 
-from multiprocessing import Process, Manager
-import os
+
+def count_words(queue, path):
+    q = queue.get()
+    name = path + '/' + q
+    try:
+        with open(name, 'r', encoding='utf8') as file:
+            n_words = 0
+            for line in file:
+                words_list = line.split()
+                n_words += len(words_list)
+            queue.put((q, n_words))
+    except (OSError, UnicodeDecodeError):
+        queue.put((q, 0))
 
 
 def word_count_inference(path_to_dir):
-    '''
-    Метод, считающий количество слов в каждом файле из директории
-    и суммарное количество слов.
-    Слово - все, что угодно через пробел, пустая строка "" словом не считается,
-    пробельный символ " " словом не считается. Все остальное считается.
-    Решение должно быть многопроцессным. Общение через очереди.
-    :param path_to_dir: путь до директории с файлами
-    :return: словарь, где ключ - имя файла, значение - число слов +
-        специальный ключ "total" для суммы слов во всех файлах
-    '''
-    raise NotImplementedError
+    dict_ = {'total': 0}
+    process_count = 4
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(process_count)
+    jobs = []
+    len_ = 0
+    for f in listdir(path_to_dir):
+        if isfile(join(path_to_dir, f)):
+            queue.put(f)
+            len_ += 1
+    for _ in range(len_):
+        job = pool.apply_async(count_words, (queue, path_to_dir))
+        jobs.append(job)
+    for job in jobs:
+        job.get()
+    pool.close()
+    pool.join()
+    for _ in range(len_):
+        q = queue.get()
+        dict_[q[0]] = q[1]
+        dict_['total'] += q[1]
+    return dict_
