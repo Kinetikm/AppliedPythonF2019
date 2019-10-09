a11da707091c06dc74d64a9ab8ad6ee7bb0fd797
Ilya Utkin <ilyaphys@yandex.ru>
2019-10-09 12:05:00 +0300

    wc done, pm in progress

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process
+from multiprocessing import Process, Pool, Manager
+import time
+import os
+import copy
 
 
 class Task:
@@ -23,24 +26,34 @@ class Task:
         raise NotImplementedError
 
 
-class TaskProcessor:
+class TaskProcessor(Process):
     """
     Воркер-процесс. Достает из очереди тасок таску и делает ее
     """
-    def __init__(self, tasks_queue):
+    def __init__(self, tasks_queue, time_dict):
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
         """
-        raise NotImplementedError
+        self.ta_queue = tasks_queue
+        self.t_dict = time_dict
 
     def run(self):
         """
         Старт работы воркера
         """
-        raise NotImplementedError
+        while True:
+            task = ta_queue.get()
+
+            task_s_time = time.time()
+            pid = os.getpid()
+            self.t_dict[pid] = task_s_time
+
+            task.perform()
 
 
-class TaskManager:
+
+
+class TaskManager(Process):
     """
     Мастер-процесс, который управляет воркерами
     """
@@ -50,11 +63,46 @@ class TaskManager:
         :param n_workers: кол-во воркеров
         :param timeout: таймаут в секундах, воркер не может работать дольше, чем timeout секунд
         """
-        raise NotImplementedError
+        self.t_queue = tasks_queue
+        self.n_workers = n_workers
+        self.timeout = timeout
 
     def run(self):
         """
         Запускайте бычка! (с)
+        Мастер следит, что число воркеров всегда было равно n_workers (в случае смерти пересоздает их).
         """
-        raise NotImplementedError
+        # создали пул рабочих, создали рабочих, передали в них очередь, запустили
+        # pool = Pool(self.n_workers)
+        # создать дикт из pid, TaskProccessor
+        # создать queue для отслеживания времени выполнения, в который дочерний процесс будет записывать pid процесса, который
+        # работает дольше положенного
+        manager = Manager()
+        wid_s_time = manager.dict()
+        workers = {}
+
+        for _ in range(self.n_workers):
+            worker = TaskProcessor(self.t_queue, wid_s_time)
+            worker.start()
+            workers[worker.pid] = worker
+
+        while True:
+            # restart
+            for wid, w in workers.items():
+                if not w.is_alive():
+                    # если не пусто в множестве тасков!
+                    new_w = TaskProcessor(self.t_queue, wid_s_time)
+                    new_w.start()
+                    workers[wid] = new_w
+            # kill         
+            wid_s_time_now = copy.deepcopy(wid_s_time)
+            current_time = time.time()
+            for wid, task_start_time in wid_s_time_now.items():
+                if current_time - task_start_time > timeout:
+                    timeouted_worker = workers[wid]
+                    timeouted_worker.close()
+                    timeouted_worker.join()
+
+    # QUEUE
+    # завершение мастер процесса, когда все таски выполнены!                
 
--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Lock, Pool
 import os
+import sys
+
+PROCESSES_COUNT = 5
+
+
+def get_files_from_path(path) -> list:
+    files_gen = os.walk(path)
+    path_dirs_files = next(files_gen)
+    files = path_dirs_files[2]
+    return files
+
+
+def get_words_count(file):
+    with open(file) as f:
+        text = f.read()
+    lines = text.split('\n')
+    plain_text = " ".join(lines)
+    words = [word for word in plain_text.split(' ') if len(word) > 0]
+    return len(words)
+
+
+def consumer_func(path, queue, words_in_files):
+    while True:
+        filename = queue.get()
+        if filename == 'kill':
+            break
+        words_count = get_words_count(f"{path}/{filename}")
+        words_in_files[filename] = words_count
+        # print(f"Wow,{os.getpid()} I found {filename} in queue with {words_count} words!")
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +45,26 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+
+    if not os.path.isdir(path_to_dir):
+        sys.exit("Path is not a directory")
+
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(PROCESSES_COUNT)
+    words_in_files = manager.dict()
+
+    files = get_files_from_path(path_to_dir)
+
+    for file in files:
+        queue.put(file)
+
+    pool.apply_async(consumer_func, args=(path_to_dir, queue, words_in_files))
+
+    queue.put('kill')
+
+    pool.close()
+    pool.join()
+
+    words_in_files['total'] = sum(list(words_in_files.values()))
+    return words_in_files
