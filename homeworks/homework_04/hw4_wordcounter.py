d0b1012109d995d737b48db4376ced8abc29b6ea
Diroman <roman.dubatov@gmail.com>
2019-10-09 18:27:11 +0300

    counter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Pool, Manager
 import os
+
+
+def counter(f_name, q, res):
+    with open(f_name, 'r') as f:
+        cnt = 0
+        for line in f:
+            cnt += len(line.split())
+
+        res[f_name.split('/')[-1]] = cnt
+        q.put(cnt)
+
+
+def total_count(q, res):
+    total = 0
+    while True:
+        cnt = q.get()
+        if cnt == -1:
+            break
+        total += cnt
+
+    res['total'] = total
+    return dict(res)
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +38,19 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+
+    files = [path_to_dir + "/" + f for f in os.listdir(path_to_dir)]
+
+    manager = Manager()
+    queue = manager.Queue()
+    result = manager.dict()
+    pool = Pool(len(files) + 1)
+
+    total_proc = pool.apply_async(total_count, (queue, result))
+
+    for file in files:
+        proc = pool.apply_async(counter, (file, queue, result))
+        proc.get()
+
+    queue.put(-1)
+    return total_proc.get()
