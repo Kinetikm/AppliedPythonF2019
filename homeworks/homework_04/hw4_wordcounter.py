a157e478881cbf89d9cdfc91cd373718a9f36cc1
VladislavKuzevanov <vlad.kuzevanov@mail.ru>
2019-10-09 19:07:15 +0300

    first try for wordcounter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -5,15 +5,39 @@ from multiprocessing import Process, Manager
 import os
 
 
+def words_in_file(path_to_dir, filename, queue):
+    sum = 0
+    temp_dict = {}
+    with open(path_to_dir + "/" + filename, 'r') as f:
+        sum = len(f.read().strip().split())
+    temp_dict[filename] = sum
+    queue.put(temp_dict)
+
+
+def queue_switching(queue):
+    major_dict = {}
+    while True:
+        res = queue.get()
+        if res == 'kill':
+            break
+        major_dict.update(res)
+    major_dict['total'] = sum(major_dict.values())
+
+
 def word_count_inference(path_to_dir):
-    '''
-    Метод, считающий количество слов в каждом файле из директории
-    и суммарное количество слов.
-    Слово - все, что угодно через пробел, пустая строка "" словом не считается,
-    пробельный символ " " словом не считается. Все остальное считается.
-    Решение должно быть многопроцессным. Общение через очереди.
-    :param path_to_dir: путь до директории с файлами
-    :return: словарь, где ключ - имя файла, значение - число слов +
-        специальный ключ "total" для суммы слов во всех файлах
-    '''
-    raise NotImplementedError
+    PROCESSES_COUNT = os.cpu_count()
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(PROCESSES_COUNT)
+    pool.apply_async(queue_switching, (queue, ))
+    jobs = []
+    for filename in os.listdir(path_to_dir):
+        job = pool.apply_async(words_in_file, (path_to_dir, filename, queue))
+        jobs.append(job)
+
+    for job in jobs:
+        job.get()
+
+    queue.put('kill')
+    pool.close()
+    pool.join()
