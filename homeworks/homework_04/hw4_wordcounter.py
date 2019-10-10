02f0bf8de7ea4e4bd464d58aa02e531594950f02
francuz-v <vera.frantsuzova@yandex.ru>
2019-10-10 09:45:30 +0300

    wordcounter test passed

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -5,15 +5,28 @@ from multiprocessing import Process, Manager
 import os
 
 
+def read_file(path_to_dir, queue, d):
+    filename = queue.get()
+    word_list = []
+    # print(filename, time.time(), os.getpid())
+    full_name = str(path_to_dir) + '/' + str(filename)
+    with open(full_name) as f:
+        for line in f:
+            word_list += [word for word in line.split()]
+    d[filename] = len(word_list)
+
+
 def word_count_inference(path_to_dir):
-    '''
-    Метод, считающий количество слов в каждом файле из директории
-    и суммарное количество слов.
-    Слово - все, что угодно через пробел, пустая строка "" словом не считается,
-    пробельный символ " " словом не считается. Все остальное считается.
-    Решение должно быть многопроцессным. Общение через очереди.
-    :param path_to_dir: путь до директории с файлами
-    :return: словарь, где ключ - имя файла, значение - число слов +
-        специальный ключ "total" для суммы слов во всех файлах
-    '''
-    raise NotImplementedError
+    manager = Manager()
+    queue = manager.Queue()
+    tasks = []
+    d = manager.dict()
+    for filename in os.listdir(path_to_dir):
+        queue.put(filename)
+        proc = Process(target=read_file, args=(path_to_dir, queue, d))
+        tasks.append(proc)
+        proc.start()
+    for task in tasks:
+        task.join()
+    d['total'] = sum(d.values())
+    return d
