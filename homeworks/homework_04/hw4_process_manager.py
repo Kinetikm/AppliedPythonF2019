05f71099a6c0992d3c439d843f463956c8fc5401
Filatov Ilya <ilya_filatovv@mail.ru>
2019-10-10 09:20:40 +0300

    + process manager

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -1,59 +1,99 @@
-#!/usr/bin/env python
-# coding: utf-8
+import multiprocessing
+import time
+import os
+from abc import ABC, abstractmethod
+from multiprocessing import Manager
 
-from multiprocessing import Process
 
+class Task(ABC):
 
-class Task:
-    """
-    Задача, которую надо выполнить.
-    В идеале, должно быть реализовано на достаточном уровне абстракции,
-    чтобы можно было выполнять "неоднотипные" задачи
-    """
-    def __init__(self, ...):
-        """
-        Пофантазируйте, как лучше инициализировать
-        """
-        raise NotImplementedError
-
+    @abstractmethod
     def perform(self):
-        """
-        Старт выполнения задачи
-        """
-        raise NotImplementedError
+        pass
 
 
 class TaskProcessor:
-    """
-    Воркер-процесс. Достает из очереди тасок таску и делает ее
-    """
+
     def __init__(self, tasks_queue):
-        """
-        :param tasks_queue: Manager.Queue с объектами класса Task
-        """
-        raise NotImplementedError
+        self.task = tasks_queue.get()
+        self.start_time = None
+        self.proc = None
 
     def run(self):
-        """
-        Старт работы воркера
-        """
-        raise NotImplementedError
+        self.proc = multiprocessing.Process(target=self.task.perform)
+        self.proc.start()
+        self.start_time = time.time()
 
 
 class TaskManager:
-    """
-    Мастер-процесс, который управляет воркерами
-    """
+
     def __init__(self, tasks_queue, n_workers, timeout):
-        """
-        :param tasks_queue: Manager.Queue с объектами класса Task
-        :param n_workers: кол-во воркеров
-        :param timeout: таймаут в секундах, воркер не может работать дольше, чем timeout секунд
-        """
-        raise NotImplementedError
+        self.tasks_queue = tasks_queue
+        self.n_workers = n_workers
+        self.timeout = timeout
 
     def run(self):
-        """
-        Запускайте бычка! (с)
-        """
-        raise NotImplementedError
+        jobs = []
+        sleep_time = 1 / self.n_workers
+        if self.tasks_queue.qsize() < self.n_workers:
+            self.n_workers = self.tasks_queue.qsize()
+        for _ in range(self.n_workers):
+            task_proc = TaskProcessor(self.tasks_queue)
+            jobs.append(task_proc)
+            task_proc.run()
+        while not self.tasks_queue.empty():
+            k = 0
+            for j in range(len(jobs)):
+                if jobs[j - k].proc.is_alive():
+                    if time.time() - jobs[j - k].start_time > self.timeout:
+                        os.system("kill -9 {}".format(jobs[j - k].proc.pid))
+                        print("Time exceeded for Queue[{}]".format(jobs[j - k].task.size), ". Process was killed")
+                        del jobs[j - k]
+                        k += 1
+                        if not self.tasks_queue.empty():
+                            task_proc = TaskProcessor(self.tasks_queue)
+                            jobs.append(task_proc)
+                            task_proc.run()
+                else:
+                    del jobs[j - k]
+                    k += 1
+                    if not self.tasks_queue.empty():
+                        task_proc = TaskProcessor(self.tasks_queue)
+                        jobs.append(task_proc)
+                        task_proc.run()
+            time.sleep(sleep_time)
+
+        while len(jobs) != 0:
+            k = 0
+            for j in range(len(jobs)):
+                if jobs[j - k].proc.is_alive():
+                    if time.time() - jobs[j - k].start_time > self.timeout:
+                        print("Time exceeded for Queue[{}]".format(jobs[j - k].task.size), ". Process was killed")
+                        os.system("kill -9 {}".format(jobs[j - k].proc.pid))
+                        del jobs[j - k]
+                        k += 1
+                else:
+                    del jobs[j - k]
+                    k += 1
+                time.sleep(sleep_time)
+
+
+# Для проверки:
+'''
+class TaskExample(Task):
+    def __init__(self, size):
+        self.size = size
+
+    def perform(self):
+        print(str(self.size) + " started")
+        time.sleep(self.size % 10)
+
+
+if __name__ == '__main__':
+    manager = Manager()
+    queue = manager.Queue()
+    for i in range(50):
+        queue.put(TaskExample(i))
+    TM = TaskManager(queue, 4, 7)
+    TM.run()
+'''
