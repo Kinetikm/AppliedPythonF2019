b16455f53b6131e73e408be8aa9a0164e38c71c7
verbarius <verbarius@mail.ru>
2019-10-11 02:42:45 +0300

    word_counter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
 import os
+
+
+def num_of_words(filename):
+    with open(filename, 'r') as f:
+        return len(f.read().split())
+
+
+def producer_func(path_to_dir, filename, queue):
+    res = (filename, num_of_words(path_to_dir + '/' + filename))
+    queue.put(res)
+
+
+def consumer_func(queue):
+    dict_ = {'total': 0}
+    while True:
+        res = queue.get()
+        if res == 'kill':
+            return dict_
+        dict_[res[0]] = res[1]
+        dict_['total'] += res[1]
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +36,24 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    file_names = os.listdir(path_to_dir)
+    PROCESSES_COUNT = len(file_names)
+
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(PROCESSES_COUNT)
+
+    cons = pool.apply_async(consumer_func, (queue, ))
+
+    jobs = []
+    for filename in file_names:
+        job = pool.apply_async(producer_func, (path_to_dir, filename, queue))
+        jobs.append(job)
+    for job in jobs:
+        job.get()
+
+    queue.put('kill')
+    pool.close()
+    pool.join()
+
+    return cons.get()
