456ebede927e25f11e8b7d936ad3153ae120eda8
vlad1m1r_17 <zamyatovskiy.vi@gmail.com>
2019-10-11 09:01:15 +0300

    Counter done

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
 import os
 
 
@@ -16,4 +16,46 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+
+    files = [f for f in os.listdir(path_to_dir)
+             if os.path.isfile(os.path.join(path_to_dir, f))]
+    print(files)
+    process_count = 8
+    queue = Manager().Queue()
+    proc_pool = Pool(process_count)
+    summary = proc_pool.apply_async(calculate_total, (queue,))
+
+    file_workers = []
+    for file in files:
+        worker = proc_pool.apply_async(calculate_in_file, (path_to_dir, file, queue))
+        file_workers.append(worker)
+    for worker in file_workers:
+        worker.get()
+    queue.put("end")
+
+    retval = summary.get()
+    proc_pool.close()
+    proc_pool.join()
+    return retval
+
+
+def calculate_in_file(path_to_dir, filename, q):
+    cnt = 0
+    with open(os.path.join(path_to_dir, filename), 'r') as f:
+        for line in f:
+            cnt += len(line.strip().split())
+    q.put((filename, cnt))
+
+
+def calculate_total(q):
+    d = {"total": 0}
+    while True:
+        val = q.get()
+        if val == "end":
+            break
+        d[val[0]] = val[1]
+        d["total"] += val[1]
+    return d
+
+
+
