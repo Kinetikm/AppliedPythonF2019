407ab080859a685dd81d9eac9c689cb176021e02
Anton35925 <Anton35925@gmail.com>
2019-10-11 16:51:03 +0200

    wordcounter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-from multiprocessing import Process, Manager
+from multiprocessing import Pool, Manager
 import os
+
+
+def word_count(path_to_dir, filename, queue):
+    words = 0
+    with open("{}/{}".format(path_to_dir, filename), 'r') as f:
+        for line in f:
+            wordslist = line.split()
+            words += len(wordslist)
+    item = {filename: words}
+    queue.put(item)
+
+
+def consumer_func(queue):
+    words_dict = dict()
+    total = 0
+    while True:
+        elem = queue.get()
+        if elem == 'kill':
+            break
+        words_dict.update(elem)
+    for k in words_dict:
+        total += words_dict[k]
+    words_dict['total'] = total
+    return words_dict
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +39,17 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(os.cpu_count())
+    task = pool.apply_async(consumer_func, (queue, ))
+    jobs = []
+    for filename in os.listdir(path_to_dir):
+        job = pool.apply_async(word_count, (path_to_dir, filename, queue))
+        jobs.append(job)
+    for job in jobs:
+        job.get()
+    queue.put('kill')
+    pool.close()
+    pool.join()
+    return task.get()
