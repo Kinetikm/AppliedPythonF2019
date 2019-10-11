a940669b73d8eb59b7cf3af584fc0c7dc42c7eab
Hunte9999 <v.morozow@mail.ru>
2019-10-11 17:45:45 +0300

    add process manager

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process
+from multiprocessing import Process, Manager
+import time
+import os
 
 
 class Task:
@@ -10,34 +12,58 @@ class Task:
     В идеале, должно быть реализовано на достаточном уровне абстракции,
     чтобы можно было выполнять "неоднотипные" задачи
     """
-    def __init__(self, ...):
+    def __init__(self, func, *args, **kwargs):
         """
         Пофантазируйте, как лучше инициализировать
         """
-        raise NotImplementedError
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
 
     def perform(self):
         """
         Старт выполнения задачи
         """
-        raise NotImplementedError
+        if not self.kwargs:
+            if not self.args:
+                r = self.func()
+            else:
+                r = self.func(*self.args)
+        else:
+            if not self.args:
+                r = self.func(**self.kwargs)
+            else:
+                r = self.func(*self.args, **self.kwargs)
+        return r
 
 
-class TaskProcessor:
+class TaskProcessor(Process):
     """
     Воркер-процесс. Достает из очереди тасок таску и делает ее
     """
-    def __init__(self, tasks_queue):
+    def __init__(self, tasks_queue, timeout):
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
         """
-        raise NotImplementedError
+        super().__init__()
+        self.queue = tasks_queue
+        self.timeout = timeout
 
     def run(self):
         """
         Старт работы воркера
         """
-        raise NotImplementedError
+        while True:
+            proc = self.queue.get()
+            proces = Process(target=proc.perform, args=())
+            proces.start()
+            time.sleep(self.timeout)
+            if proces.is_alive():
+                proces.terminate()
+                print('terminated ' + str(proc))
+            else:
+                proces.join()
+
 
 
 class TaskManager:
@@ -50,10 +76,50 @@ class TaskManager:
         :param n_workers: кол-во воркеров
         :param timeout: таймаут в секундах, воркер не может работать дольше, чем timeout секунд
         """
-        raise NotImplementedError
+        self.__queue = tasks_queue
+        self.__workers = []
+        self.__num_workers = n_workers
+        self.__timeout = timeout
 
     def run(self):
         """
         Запускайте бычка! (с)
         """
-        raise NotImplementedError
+        while True:
+            try:
+                self.__workers.extend([TaskProcessor(self.__queue, self.__timeout).start()
+                                       for _ in range(self.__num_workers - len(self.__workers))])
+            except Exception:
+                continue
+
+
+def func(a):
+    print(a)
+
+
+def func_sleep(a, b):
+    time.sleep(a)
+    print(b)
+
+
+queue = Manager().Queue()
+for i in range(5):
+    queue.put(Task(func_sleep, i, i*i))
+    print(f'put {i}')
+
+
+tm = TaskManager(queue, 5, 12)
+proc = Process(target=tm.run, args=())
+proc.start()
+
+
+for i in range(5, 10):
+    queue.put(Task(func_sleep, a=i, b=i*i))
+    print(f'put {i}')
+
+for i in range(10, 15):
+    queue.put(Task(func_sleep, i, b=i*i))
+    print(f'put {i}')
+
+
+proc.join()
