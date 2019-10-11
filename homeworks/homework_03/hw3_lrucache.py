24de9475c60b9e1300426c9377a3ae383fccc035
Anatoly Rubanov <anatolijrubanov@gmail.com>
2019-10-11 15:09:57 +0300

    lru_cache fixed

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -24,7 +24,8 @@ class LRUCacheDecorator:
     def _make_key(self, args, kwds, kwd_mark=(object(),)):
         key = args
         if kwds:
-            key += kwd_mark
+            key += kwd_mark  # добавляем разделитель между args и kwargs, чтобы для вызовы
+            # decorated_func(1, 2, 3, 'a', 'x')  decorated_func(1, 2, 3, a='x') получались разные ключи
             for item in kwds.items():
                 key += item
         return key
@@ -33,38 +34,24 @@ class LRUCacheDecorator:
         def cache(*args, **kwargs):
             key = self._make_key(args, kwargs)
             if key in self._results_dict:
-                if self._max_time is not None and time.time() - self._times_dict[key] < self._max_time:
+                if self._max_time is None or time.time() - self._times_dict[key] < self._max_time:
+                    # в кэше есть достаточно свежий элемент, либо нас устраивает элемент любой давности
+                    # times_dict упорядочен по времени обращения к функции, поэтому заменяем элемент,
+                    #  к которому обращаемся чтобы он был в конце
+                    del self._times_dict[key]
                     self._times_dict[key] = time.time()
                     return self._results_dict[key]
-                elif self._max_time is None:
-                    self._times_dict[key] = time.time()
-                    return self._results_dict[key]
+            # в кэше нужного элемента нет, либо он слишком старый
             res = func(*args, **kwargs)
             if self._current_size == self._maxsize:
-                del_key = min(self._times_dict, key=self._times_dict.get)
+                # размер кэша достиг максимального, поэтому выкидываем самый старый элемент (первый)
+                del_key = next(iter(self._times_dict.keys()))
                 del self._results_dict[del_key]
                 del self._times_dict[del_key]
                 self._current_size -= 1
+            # теперь добавим в кэш полученный резултат
             self._times_dict[key] = time.time()
             self._results_dict[key] = res
             self._current_size += 1
             return res
         return cache
-
-
-if __name__ == '__main__':
-    @LRUCacheDecorator(maxsize=3, ttl=None)
-    def get_sq(s):
-        time.sleep(2)
-        return s ** 2
-
-    get_sq(1)
-    get_sq(2)
-    get_sq(3)
-    get_sq(1)
-    get_sq(4)
-    get_sq(5)
-
-    t_start = time.time()
-    get_sq(1)
-    assert time.time() - t_start < 0.5
