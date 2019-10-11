4f02f11e30de907d3133ebe610f4c26a9ad32295
verbarius <verbarius@mail.ru>
2019-10-12 00:57:45 +0300

    processer norm version

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -3,6 +3,25 @@
 
 from multiprocessing import Process, Manager, Queue
 from time import time, sleep
+import signal
+from contextlib import contextmanager
+
+
+class TimeoutException(Exception):
+    pass
+
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.setitimer(signal.ITIMER_REAL, seconds, 0)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
 
 class Task:
     """
@@ -18,7 +37,6 @@ class Task:
         self.args = args
         self.kwargs = kwargs
 
-
     def perform(self):
         """
         Старт выполнения задачи
@@ -30,25 +48,36 @@ class TaskProcessor(Process):
     """
     Воркер-процесс. Достает из очереди тасок таску и делает ее
     """
-    def __init__(self, tasks_queue, res_queue):
+    def __init__(self, tasks_queue, res_queue, timeout):
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
         """
         super().__init__()
         self.tasks_queue = tasks_queue
         self.res_queue = res_queue
-        self.flag = 0
+        self.timeout = timeout
 
     def run(self):
         """
         Старт работы воркера
         """
+
         item = self.tasks_queue.get()
-        res = item.perform()
-        if res is not None:
-            self.res_queue.put(res)
+        if self.timeout is not None:
+            try:
+                with time_limit(self.timeout):
+                    res = item.perform()
+            except TimeoutException:
+                res = 'Timed out!'
         else:
+            res = item.perform()
+
+        if res is None:
             self.res_queue.put('Function does not return anything!')
+        elif res == 'Timed out!':
+            self.res_queue.put('Function`s performance was not end')
+        else:
+            self.res_queue.put(res)
 
 
 class TaskManager(Process):
@@ -67,31 +96,52 @@ class TaskManager(Process):
         self.timeout = timeout
         self.res_queue = res_queue
 
-
     def run(self):
         """
         Запускайте бычка! (с)
         """
         jobs = []
-        times = []
 
         while True:
-            print(len(jobs))
-            sleep(1)
             while len(jobs) < self.n_workers:
-                job = TaskProcessor(self.tasks_queue, self.res_queue)
-                if self.tasks_queue.qsize():
-                    flag = 1
-                else:
-                    flag = 0
+                job = TaskProcessor(self.tasks_queue, self.res_queue, self.timeout)
                 jobs.append(job)
                 job.start()
-                times.append([time(), flag])
 
             for i, job in enumerate(jobs):
-                if times[i][1]:
-                    if time() - times[i][0] > self.timeout:
-                        jobs[i].kill()
                 if not job.is_alive():
                     del jobs[i]
-                    del times[i]
+
+
+# Первый аргумент - время сна, остальное по желанию
+
+def func(*args, **kwargs):
+    s = 0
+    sleep(args[0])
+    for arg in args:
+        s += arg
+    print(f'What a wonderful homework! I will sleep for {args[0]} second(s)')
+    for kw in kwargs.values():
+        s += kw
+    return s
+
+
+n_workers = 2
+time_out = 3.1
+queue_0 = Manager().Queue()
+queue_1 = Manager().Queue()
+a = TaskManager(queue_0, queue_1, n_workers, time_out)
+a.start()
+
+queue_0.put(Task(func, 1, 7))
+queue_0.put(Task(func, 1, 7, 0.5, c=2))
+queue_0.put(Task(func, 1, 7, 0.5, c=2))
+queue_0.put(Task(func, 1, 7, 0.5, c=2))
+queue_0.put(Task(func, 1, 7, c=2, d=3))
+queue_0.put(Task(func, 5, 7, c=2, d=4))
+queue_0.put(Task(func, 3, 7, c=2.3, d=4))
+
+
+while True:
+    while queue_1.qsize():
+        print(queue_1.get())
