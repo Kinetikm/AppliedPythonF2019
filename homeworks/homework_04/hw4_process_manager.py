5433856b05ecfe131a14fdf26e8eb3335840e9d7
iturovskiy <iturovskiy2497@gmail.com>
2019-10-11 16:21:07 +0300

    yet another process manager version

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -2,8 +2,8 @@
 # coding: utf-8
 
 from queue import Empty
-from multiprocessing import Process, Manager
-from time import sleep
+from multiprocessing import Process, Manager, Queue
+from time import time, sleep
 
 
 class Task:
@@ -24,7 +24,7 @@ class Task:
         """
         Старт выполнения задачи
         """
-        self._target(*self._args, **self._kwargs)
+        return self._target(*self._args, **self._kwargs)
 
 
 class TaskProcessor:
@@ -37,7 +37,7 @@ class TaskProcessor:
         """
         self._tasks_queue = tasks_queue
 
-    def run(self, timeout):
+    def run(self, result_queue):
         """
         Старт работы воркера
         """
@@ -46,14 +46,8 @@ class TaskProcessor:
                 task = self._tasks_queue.get(block=False)
             except Empty:
                 break
-            # Изначально пытался сделать Thread внутри воркер-процесса
-            # Но потоки не могут в terminate()
-            # пока остается использовать Process
-            thread = Process(target=task.perform)
-            thread.start()
-            thread.join(timeout)
-            if thread.is_alive():
-                thread.terminate()
+            result = task.perform()
+            result_queue.put((task, result))
 
 
 class TaskManager:
@@ -75,20 +69,29 @@ class TaskManager:
         Запускайте бычка! (с)
         """
         active_workers_p = []
+        result_queue = Queue(self._queue.qsize())
         while not self._queue.empty() or len(active_workers_p) != 0:
             last = self._queue.qsize()
             available = self._workers_num - len(active_workers_p)
             for worker in range(min(last, available)):
                 processor = TaskProcessor(self._queue)
-                active_worker = Process(target=processor.run, args=(self._timeout, ))
+                active_worker = Process(target=processor.run, args=(result_queue, ))
                 active_worker.start()
-                active_workers_p.append(active_worker)
+                active_workers_p.append((active_worker, time()))
             new_active_workers_p = []
-            for active_worker in active_workers_p:
+            for active_worker, start_time in active_workers_p:
                 if active_worker.is_alive():
-                    new_active_workers_p.append(active_worker)
+                    if time() - start_time < self._timeout:
+                        new_active_workers_p.append((active_worker, start_time))
+                    else:
+                        active_worker.terminate()
             active_workers_p = new_active_workers_p
             sleep(0.1)
+        work_result = {}
+        while not result_queue.empty():
+            key, value = result_queue.get_nowait()
+            work_result[key] = value
+        return work_result
 
 
 if __name__ == "__main__":
@@ -98,6 +101,7 @@ if __name__ == "__main__":
         print(f"Start: {i}")
         sleep(stime)
         print(f"Finish: {i}")
+        return i, stime
 
     # отваливаемся по timeout
     manager = Manager()
@@ -108,4 +112,26 @@ if __name__ == "__main__":
         queue.put(Task(sleeper, it + n, 2))      # args
 
     task_manager = TaskManager(queue, 4, 1)
-    task_manager.run()
+    res = task_manager.run()
+    assert len(res) == 0
+    print(res)
+
+    # не отваливаемся по timeout
+    for it in range(n):
+        queue.put(Task(sleeper, i=it, stime=2))  # kwargs
+        queue.put(Task(sleeper, it + n, 2))      # args
+
+    task_manager = TaskManager(queue, 4, 4)
+    res = task_manager.run()
+    assert len(res) == n * 2
+    print(res)
+
+    # не отваливаемся по timeout но часть воркеров не успеваю дообработать таски
+    for it in range(n):
+        queue.put(Task(sleeper, i=it, stime=2))  # kwargs
+        queue.put(Task(sleeper, it + n, 2))  # args
+
+    task_manager = TaskManager(queue, 4, 3)
+    res = task_manager.run()
+    print(res)
+    assert len(res) == 4
