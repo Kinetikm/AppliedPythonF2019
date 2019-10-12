72ae7ac5cebf1f2d941c1a5fd89d24986ba47ada
Anna Maximova <anna.maximova.14.07@mail.ru>
2019-10-13 00:39:01 +0300

    lrucache and csrmatrix change

--- homeworks/homework_03/hw3_csr_matrix.py
+++ homeworks/homework_03/hw3_csr_matrix.py
@@ -36,26 +36,26 @@ class CSRMatrix:
             a[row_ind[k], col_ind[k]] = data[k]
         """
         if isinstance(init_matrix_representation, tuple) and len(init_matrix_representation) == 3:
-            self.A = init_matrix_representation[2]
-            self.JA = init_matrix_representation[1]
-            self.IA = [0]
+            self.a = init_matrix_representation[2]
+            self.ja = init_matrix_representation[1]
+            self.ia = [0]
             for i in range(max(init_matrix_representation[0]) + 1):
-                self.IA += [self.IA[i]]
-                self.IA[i + 1] += init_matrix_representation[0].count(i)
-            self._nnz = len(self.A)
+                self.ia += [self.ia[i]]
+                self.ia[i + 1] += init_matrix_representation[0].count(i)
+            self._nnz = len(self.a)
         elif isinstance(init_matrix_representation, np.ndarray):
-            self.A = []
-            self.IA = [0]
-            self.JA = []
+            self.a = []
+            self.ia = [0]
+            self.ja = []
             for i in range(init_matrix_representation.shape[0]):
-                self.IA += [self.IA[i]]
+                self.ia += [self.ia[i]]
                 for j in range(init_matrix_representation.shape[1]):
                     a = init_matrix_representation[i][j]
                     if a != 0:
-                        self.A += [a]
-                        self.IA[i + 1] += 1
-                        self.JA += [j]
-            self._nnz = len(self.A)
+                        self.a += [a]
+                        self.ia[i + 1] += 1
+                        self.ja += [j]
+            self._nnz = len(self.a)
         else:
             raise ValueError
 
@@ -67,122 +67,122 @@ class CSRMatrix:
         """
         Return dense representation of matrix (2D np.array).
         """
-        mtrx = np.zeros((len(self.IA) - 1, max(self.JA) + 1))
-        for i in range(len(self.IA) - 1):
-            for j in range(self.IA[i], self.IA[i + 1]):
-                mtrx[i][self.JA[j]] = self.A[j]
+        mtrx = np.zeros((len(self.ia) - 1, max(self.ja) + 1))
+        for i in range(len(self.ia) - 1):
+            for j in range(self.ia[i], self.ia[i + 1]):
+                mtrx[i][self.ja[j]] = self.a[j]
         print(mtrx)
         return mtrx
 
     def __getitem__(self, tup):
         i, j = tup
-        for ind in range(self.IA[i], self.IA[i + 1]):
-            if self.JA[ind] == j:
-                return self.A[ind]
+        for ind in range(self.ia[i], self.ia[i + 1]):
+            if self.ja[ind] == j:
+                return self.a[ind]
         return 0.
 
     def __setitem__(self, tup, value):
         i, j = tup
         if value != 0:
-            for ind in range(self.IA[i], self.IA[i + 1]):
-                if self.JA[ind] == j:
-                    self.A[ind] = value
-                    return self.A[ind]
-            self.A += [0]
-            self.JA += [0]
+            for ind in range(self.ia[i], self.ia[i + 1]):
+                if self.ja[ind] == j:
+                    self.a[ind] = value
+                    return self.a[ind]
+            self.a += [0]
+            self.ja += [0]
             temp1 = j
             temp2 = value
-            tempJA = self.JA[self.IA[i + 1]:]
-            for ind in range(self.IA[i], self.IA[i + 1]):
-                if temp1 < self.JA[ind]:
-                    self.JA[ind], temp1 = temp1, self.JA[ind]
-            temp1, self.JA[self.IA[i + 1]] = self.JA[self.IA[i + 1]], temp1
-            self.JA[self.IA[i + 1] + 1:] = tempJA
-            self.JA.pop()
-            tempA = self.A[self.IA[i + 1]:]
-            for k in range(i + 1, len(self.IA)):
-                self.IA[k] += 1
-            for ind in range(self.IA[i], self.IA[i + 1]):
-                if self.JA[ind] == j:
+            temp_ja = self.ja[self.ia[i + 1]:]
+            for ind in range(self.ia[i], self.ia[i + 1]):
+                if temp1 < self.ja[ind]:
+                    self.ja[ind], temp1 = temp1, self.ja[ind]
+            temp1, self.ja[self.ia[i + 1]] = self.ja[self.ia[i + 1]], temp1
+            self.ja[self.ia[i + 1] + 1:] = temp_ja
+            self.ja.pop()
+            #temp_a = self.a[self.ia[i + 1]:]
+            for k in range(i + 1, len(self.ia)):
+                self.ia[k] += 1
+            for ind in range(self.ia[i], self.ia[i + 1]):
+                if self.ja[ind] == j:
                     break
-            for idx in range(ind, len(self.A)):
-                self.A[idx], temp2 = temp2, self.A[idx]
-            self._nnz = len(self.A)
+            for idx in range(ind, len(self.a)):
+                self.a[idx], temp2 = temp2, self.a[idx]
+            self._nnz = len(self.a)
         else:
             on_nonzero_place = False
-            for ind in range(self.IA[i], self.IA[i + 1]):
-                if self.JA[ind] == j:
-                    del self.JA[ind]
-                    del self.A[ind]
+            for ind in range(self.ia[i], self.ia[i + 1]):
+                if self.ja[ind] == j:
+                    del self.ja[ind]
+                    del self.a[ind]
                     on_nonzero_place = True
                     break
             if on_nonzero_place:
-                for ind in range(i + 1, len(self.IA)):
-                    self.IA[ind] -= 1
-            self._nnz = len(self.A)
+                for ind in range(i + 1, len(self.ia)):
+                    self.ia[ind] -= 1
+            self._nnz = len(self.a)
             return self
 
     def __add__(self, mtrx):
         res = copy.deepcopy(self)
         all_nnz_plc = []
-        for i in range(len(mtrx.IA) - 1):
-            for j in range(mtrx.IA[i], mtrx.IA[i + 1]):
-                all_nnz_plc.append([i, mtrx.JA[j]])
+        for i in range(len(mtrx.ia) - 1):
+            for j in range(mtrx.ia[i], mtrx.ia[i + 1]):
+                all_nnz_plc.append([i, mtrx.ja[j]])
         res.to_dense()
         for i, j in all_nnz_plc:
             value = mtrx[i, j]
             added = False
-            for ind in range(res.IA[i], res.IA[i + 1]):
-                if res.JA[ind] == j:
-                    if res.A[ind] + value == 0:
+            for ind in range(res.ia[i], res.ia[i + 1]):
+                if res.ja[ind] == j:
+                    if res.a[ind] + value == 0:
                         res[i, j] = 0
                     else:
-                        res.A[ind] += value
+                        res.a[ind] += value
                     added = True
                     break
             if not added:
                 res[i, j] = value
-        res._nnz = len(res.A)
+        res._nnz = len(res.a)
         return res
 
     def __sub__(self, mtrx):
         rev_mtrx = mtrx * (-1)
         res = self + rev_mtrx
-        res._nnz = len(res.A)
+        res._nnz = len(res.a)
         return res
 
     def __mul__(self, mtrx):
         if type(mtrx) != type(self):
             res = copy.deepcopy(self)
             if mtrx != 0:
-                res.A = [i * mtrx for i in res.A]
+                res.a = [i * mtrx for i in res.a]
             else:
-                res.A = []
-                res.IA = []
-                res.JA = []
+                res.a = []
+                res.ia = []
+                res.ja = []
                 res._nnz = 0
             return res
         else:
-            if len(self.IA) == len(mtrx.IA):
+            if len(self.ia) == len(mtrx.ia):
                 res = copy.deepcopy(self)
                 all_nnz_plc_1 = []
-                for i in range(len(mtrx.IA) - 1):
-                    for j in range(mtrx.IA[i], mtrx.IA[i + 1]):
-                        all_nnz_plc_1.append([i, mtrx.JA[j]])
+                for i in range(len(mtrx.ia) - 1):
+                    for j in range(mtrx.ia[i], mtrx.ia[i + 1]):
+                        all_nnz_plc_1.append([i, mtrx.ja[j]])
                 all_nnz_plc_2 = []
                 all_z_plc = []
-                for i in range(len(res.IA) - 1):
-                    for j in range(res.IA[i], res.IA[i + 1]):
-                        if [i, res.JA[j]] in all_nnz_plc_1:
-                            all_nnz_plc_2.append([i, res.JA[j]])
+                for i in range(len(res.ia) - 1):
+                    for j in range(res.ia[i], res.ia[i + 1]):
+                        if [i, res.ja[j]] in all_nnz_plc_1:
+                            all_nnz_plc_2.append([i, res.ja[j]])
                         else:
-                            all_z_plc.append([i, res.JA[j]])
+                            all_z_plc.append([i, res.ja[j]])
                 for i, j in all_nnz_plc_2:
                     value = mtrx[i, j] * res[i, j]
                     res[i, j] = value
                 for i, j in all_z_plc:
                     res[i, j] = 0
-                res._nnz = len(res.A)
+                res._nnz = len(res.a)
                 return res
             else:
                 print('Shapes dont match')
@@ -191,40 +191,40 @@ class CSRMatrix:
     def __rmul__(self, a):
         if a != 0:
             res = copy.deepcopy(self)
-            res.A = [i * a for i in res.A]
+            res.a = [i * a for i in res.a]
         else:
-            res.A = []
-            res.IA = []
-            res.JA = []
+            res.a = []
+            res.ia = []
+            res.ja = []
             res._nnz = 0
         return res
 
     def __truediv__(self, a):
         if a != 0:
             res = copy.deepcopy(self)
-            res.A = [i / a for i in res.A]
+            res.a = [i / a for i in res.a]
             return res
         print('Divide CSR matrix a by zero scalar alpha')
         return self
 
     def __matmul__(self, mtrx):
-        shape_left = (len(self.IA) - 1, max(self.JA) + 1)
-        shape_right = (max(mtrx.JA) + 1, len(mtrx.IA) - 1)
+        shape_left = (len(self.ia) - 1, max(self.ja) + 1)
+        shape_right = (max(mtrx.ja) + 1, len(mtrx.ia) - 1)
         if shape_left != shape_right:
             raise ValueError
         else:
             res = np.zeros((shape_left[0], shape_left[0]))
             temp_mtrx = mtrx.to_dense()
-            trans_mtrx = np.zeros((max(mtrx.JA) + 1, len(mtrx.IA) - 1))
-            for i in range(max(mtrx.JA) + 1):
-                for j in range(len(mtrx.IA) - 1):
+            trans_mtrx = np.zeros((max(mtrx.ja) + 1, len(mtrx.ia) - 1))
+            for i in range(max(mtrx.ja) + 1):
+                for j in range(len(mtrx.ia) - 1):
                     trans_mtrx[i][j] = temp_mtrx[j, i]
             trans_mtrx = CSRMatrix(trans_mtrx)
             for k in range(shape_left[0]):
                 for i in range(shape_left[0]):
                     r = 0
-                    for ind in range(self.IA[k], self.IA[k + 1]):
-                        r += self[k, self.JA[ind]] * trans_mtrx[i, self.JA[ind]]
+                    for ind in range(self.ia[k], self.ia[k + 1]):
+                        r += self[k, self.ja[ind]] * trans_mtrx[i, self.ja[ind]]
                     res[k][i] = r
             res = CSRMatrix(res)
             return res
--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -3,6 +3,8 @@
 
 
 from time import time
+from functools import wraps
+from collections import deque
 
 
 class LRUCacheDecorator:
@@ -13,34 +15,37 @@ class LRUCacheDecorator:
         :param ttl: время в млсек, через которое кеш
                     должен исчезнуть
         '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
         self.maxsize = maxsize
-        self.ttl = ttl
+        if ttl:
+            self.ttl = ttl / 1000
+        else:
+            self.ttl = None
         self.cache = {}
-        self.cache_time = {}
+        #self.cache_time = {}
+        self.key_time_queue = deque()
 
     def __call__(self, func):
-        # TODO вызов функции
+        @wraps(func)
         def inner(*args, **kwargs):
             key = str(args) + str(kwargs)
             if key not in self.cache:
                 if len(self.cache) == self.maxsize:
-                    later_time = time()
-                    for k in self.cache_time:
-                        if self.cache_time[k] < later_time:
-                            later_time = self.cache_time[k]
-                            del_key = k
-                    del self.cache[k]
-                    del self.cache_time[k]
-                self.cache[key] = func(*args, **kwargs)
-                self.cache_time[key] = time()
-                return self.cache[key]
+                    del_key = self.key_time_queue.popleft()
+                    del self.cache[del_key]
+                    #del self.cache_time[del_key]
+                self.cache[key] = [func(*args, **kwargs), time()]
+                #self.cache_time[key] = time()
+                self.key_time_queue.append(key)
+                return self.cache[key][0]
             else:
-                if self.ttl and (time() - self.cache_time[key]) * 1000 > self.ttl:
-                    self.cache_time[key] = time()
-                    self.cache[key] = func(*args, **kwargs)
+                if self.ttl and (time() - self.cache[key][1]) > self.ttl:
+                    #self.cache_time[key] = time()
+                    self.cache[key] = [func(*args, **kwargs), time()]
+                    self.key_time_queue.remove(key)
+                    self.key_time_queue.append(key)
                 else:
-                    self.cache_time[key] = time()
-                return self.cache[key]
+                    self.cache[key][1] = time()
+                    self.key_time_queue.remove(key)
+                    self.key_time_queue.append(key)
+                return self.cache[key][0]
         return inner
