6f76227f74978fdd908dc5b218e0b47dc291c38f
DanilTremasov <treasovdanil@gmail.com>
2019-10-12 21:02:58 +0300

    1

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Queue
 import os
 
 
+def counter(file_name, word_dict):
+    counter_word = 0
+    with open(file_name, 'r') as f:
+        for line in f:
+            counter_word += len(line.split())
+    word_dict.update({file_name: counter_word})
+
+
 def word_count_inference(path_to_dir):
-    '''
-    Метод, считающий количество слов в каждом файле из директории
-    и суммарное количество слов.
-    Слово - все, что угодно через пробел, пустая строка "" словом не считается,
-    пробельный символ " " словом не считается. Все остальное считается.
-    Решение должно быть многопроцессным. Общение через очереди.
-    :param path_to_dir: путь до директории с файлами
-    :return: словарь, где ключ - имя файла, значение - число слов +
-        специальный ключ "total" для суммы слов во всех файлах
-    '''
-    raise NotImplementedError
+    manager = Manager()
+    tmp_queue = manager.Queue()
+    tmp_dict = manager.dict()
+    for i in os.listdir(path_to_dir):
+        tmp_queue.put(i)
+    tmp_queue.put("end")
+    pool = Pool(3)
+    while True:
+        a = tmp_queue.get()
+        if a == "end":
+            break
+        p = pool.apply_async(counter, (a, tmp_dict))
+        p.get()
+    tmp_queue.put("Happy end")
+    for key in tmp_dict:
+        summ += tmp_dict[key]
+    tmp_dict.update({total: summ})
+    result = dict(tmp_dict)
+    return result
