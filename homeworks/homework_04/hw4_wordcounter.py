6f4d17731529f1a1b5145c5ad74960e4d7ac4b83
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2019-10-12 20:10:52 +0300

    filecounter is done

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import multiprocessing
 from multiprocessing import Process, Manager
 import os
+
+
+def count_words(short_fname="", queue: multiprocessing.Queue=None, full_name=""):
+    wcount = 0
+
+    with open(full_name, "r") as f:
+        for line in f:
+            wcount += len([None for w in line.strip().split(" ") if len(w) > 0])
+
+    queue.put((short_fname, wcount))
+    return
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +28,30 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+
+    result = dict()
+    proc_list = []
+    manager = Manager()
+    queue = manager.Queue()
+    for f in os.listdir(path_to_dir):
+        full_fname = os.path.join(path_to_dir, f)
+        if not os.path.isfile(full_fname):
+            continue
+
+        proc = Process(target=count_words, kwargs=dict(queue=queue, full_name=full_fname, short_fname=f))
+        proc_list.append(proc)
+        proc.start()
+
+    for p in proc_list:
+        wcount = queue.get()
+        result[wcount[0]] = wcount[1]
+
+    for p in proc_list:
+        p.join()
+
+    if 'total' in result:
+        raise Exception("В задании не было такого кейса")
+
+    result['total'] = sum(result.values())
+    print(result)
+    return result
