2447501e56d223f1012b7905844259615c5ed7ea
Aigul <sirazovaaigul@gmail.com>
2019-10-12 15:42:25 +0300

    wordcounter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -5,6 +5,13 @@ from multiprocessing import Process, Manager
 import os
 
 
+def word_count(queue, dict_f, path_to_dir):
+    fn = queue.get()
+    with open(path_to_dir + '/' + fn) as f:
+        dict_f[fn] = len(f.read().split())
+        print(fn, dict_f)
+
+
 def word_count_inference(path_to_dir):
     '''
     Метод, считающий количество слов в каждом файле из директории
@@ -16,4 +23,24 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    files_list = os.listdir(path_to_dir)
+    f_num = len(files_list)
+    manager = Manager()
+    q = manager.Queue()
+    dict_f = manager.dict()
+
+    for fname in files_list:
+        q.put(fname)
+
+    proc_list = []
+    for i in range(f_num):
+        proc = Process(target=word_count, args=(q, dict_f, path_to_dir, ))
+        proc_list.append(proc)
+        proc.start()
+
+    for p in proc_list:
+        p.join()
+
+    dict_f['total'] = sum(dict_f.values())
+
+    return dict_f
