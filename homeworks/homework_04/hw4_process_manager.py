df5eec12ad88c6f11335552b9e4277e25c0879fa
Dmitry Zakharov <zakhar343@yandex.ru>
2019-10-12 15:00:57 +0300

    Beta HW4

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process
+from multiprocessing import Process, Manager
+import time
 
 
 class Task:
@@ -10,34 +11,48 @@ class Task:
     В идеале, должно быть реализовано на достаточном уровне абстракции,
     чтобы можно было выполнять "неоднотипные" задачи
     """
-    def __init__(self, ...):
+    def __init__(self, func, *args, **kwargs):
         """
         Пофантазируйте, как лучше инициализировать
         """
-        raise NotImplementedError
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.ret = None
 
     def perform(self):
         """
         Старт выполнения задачи
         """
-        raise NotImplementedError
+        self.ret = self.func(*self.args, **self.kwargs)
 
 
 class TaskProcessor:
     """
     Воркер-процесс. Достает из очереди тасок таску и делает ее
     """
-    def __init__(self, tasks_queue):
+    def __init__(self, tasks_queue, num):
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
         """
-        raise NotImplementedError
+        self.queue = tasks_queue
+        self.num = num
 
-    def run(self):
+    def run(self, dct):
         """
         Старт работы воркера
         """
-        raise NotImplementedError
+        while True:
+            item = self.queue.get()
+            dct[self.num] = time.time()
+            if item == 'kill':
+                raise SystemExit
+            elif True:
+                try:
+                    item.perform()
+                except Exception:
+                    item.ret = "Execution Error"
+                self.queue.put(item.ret)
 
 
 class TaskManager:
@@ -50,10 +65,55 @@ class TaskManager:
         :param n_workers: кол-во воркеров
         :param timeout: таймаут в секундах, воркер не может работать дольше, чем timeout секунд
         """
-        raise NotImplementedError
+        self.queue = tasks_queue
+        self.num_workers = n_workers
+        self.timeout = timeout
+        for i in range(self.num_workers):
+            self.queue.put('kill')
 
     def run(self):
         """
         Запускайте бычка! (с)
         """
-        raise NotImplementedError
+        procs = Manager().dict()
+        proc = Process(target=self.control, args=(procs,))
+        proc.start()
+        proc.join()
+        lst = []
+        for i in range(self.queue.qsize()):
+            lst.append(self.queue.get())
+        return lst
+
+    def control(self, procs):
+        pr = []
+        workers = [TaskProcessor(self.queue, i) for i in range(self.num_workers)]
+        for i in range(self.num_workers):
+            proc = Process(target=workers[i].run, args=(procs,))
+            procs[i] = 0
+            pr.append(proc)
+            proc.start()
+        time.sleep(self.timeout)
+        while True:
+            for i in range(len(procs)):
+                if time.time() - procs[i] > self.timeout:
+                    pr[i].kill()
+                    pr[i].join()
+                    self.queue.put('Timeout Error')
+                    pr[i] = Process(target=workers[i].run, args=(procs,))
+                    procs[i] = time.time()
+                    pr[i].start()
+            if all([not i.is_alive() for i in pr]):
+                break
+        for i in pr:
+            i.join()
+
+
+def f(n, t):
+    time.sleep(t)
+    return 1/n
+lst = [Task(f, i, i) for i in range(6)]
+q = Manager().Queue()
+for i in lst:
+    q.put(i)
+a = TaskManager(q, 3, 5)
+print(a.run())
--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
 import os
+import time
+
+PROCESSES_COUNT = 4
+
+
+def producer_func(path, to_process, queue):
+    with open(path + to_process, 'r') as f:
+        res = len(f.read().split())
+        queue.put((to_process, res))
+    time.sleep(1)
+
+
+def consumer_func(queue):
+    data = dict()
+    tot = 0
+    while True:
+        res = queue.get()
+        if res == 'kill':
+            break
+        tot += res[1]
+        data[res[0]] = res[1]
+    data['total'] = tot
+    return data
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +39,18 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(PROCESSES_COUNT)
+    to_process_list = os.listdir(path_to_dir)
+    res = pool.apply_async(consumer_func, (queue, ))
+    jobs = []
+    for to_process in to_process_list:
+        job = pool.apply_async(producer_func, (path_to_dir + '/', to_process, queue))
+        jobs.append(job)
+    for job in jobs:
+        job.get()
+    queue.put('kill')
+    pool.close()
+    pool.join()
+    return res.get()
