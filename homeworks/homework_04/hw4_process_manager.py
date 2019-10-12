b4c93500c424097a1e956f49ba05fc036a0e1cd5
artfawl <stark050600@mail.ru>
2019-10-13 01:15:23 +0300

    HomW4

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process
+from multiprocessing import Process, Manager
+import time
 
 
 class Task:
@@ -10,50 +11,141 @@ class Task:
     В идеале, должно быть реализовано на достаточном уровне абстракции,
     чтобы можно было выполнять "неоднотипные" задачи
     """
-    def __init__(self, ...):
+
+    def __init__(self, func, *args, **kwargs):
         """
         Пофантазируйте, как лучше инициализировать
         """
-        raise NotImplementedError
+        # Сами определяем функцию и кладём её в Таск
+        self.func = func
+        self.ar = args
+        self.kwar = kwargs
 
-    def perform(self):
+    def perform(self, ans_dict, num):
         """
         Старт выполнения задачи
         """
-        raise NotImplementedError
+        # Результат выполнения кладём в словарь(можно в очередь
+        # num - номер Таска
+        ans_dict[num] = self.func(*self.ar, **self.kwar)
 
 
 class TaskProcessor:
     """
     Воркер-процесс. Достает из очереди тасок таску и делает ее
     """
-    def __init__(self, tasks_queue):
+
+    def __init__(self, tasks_queue, num):
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
         """
-        raise NotImplementedError
+        self.tasks = tasks_queue
+        self.num = num
+        self.ans_dict = Manager().dict()
 
     def run(self):
         """
         Старт работы воркера
         """
-        raise NotImplementedError
+        task = self.tasks.get()
+        self.proc = Process(target=task.perform, args=(self.ans_dict, self.num))
+        self.time = time.time()
+        self.proc.start()
 
 
 class TaskManager:
     """
     Мастер-процесс, который управляет воркерами
     """
+
     def __init__(self, tasks_queue, n_workers, timeout):
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
         :param n_workers: кол-во воркеров
         :param timeout: таймаут в секундах, воркер не может работать дольше, чем timeout секунд
         """
-        raise NotImplementedError
+        self.tasks = tasks_queue
+        self.slaves = n_workers
+        self.timeout = timeout
+        self.working = True  # флажок
 
     def run(self):
         """
         Запускайте бычка! (с)
         """
-        raise NotImplementedError
+        result = dict()  # словарь с результатами выполнения Тасков
+        chislo_rabotyag = 0  # отвечает за количество работающих воркеров
+        tsk_done = 0  # Номер Таска (для передачи в дальнейшие функции и запись в словарь с результатами)
+        rabotyagi = dict()  # Тут хранятся воркеры для быстрого доступа к ним (можно было использовать обычный лист)
+        start_time = time.time()
+        while (self.working):
+            #  пока ативен флажок
+            if (self.tasks.empty()) and (chislo_rabotyag == 0):  # нет тасков и работающих воркеров
+                start_time = time.time()  # для своевременного отключения
+            while (self.tasks.empty()) and (len(rabotyagi) == 0):  # нет тасков и работающих воркеров
+                if time.time() - start_time > 5:  # если время ожидания велико, то отключаемся
+                    self.working = False
+                    break
+                print('wait for tasks')
+                time.sleep(1)
+            while (chislo_rabotyag < self.slaves) and (not self.tasks.empty()):  # если есть свободные работяги и работа
+                print('new process open')
+                rabotyagi[tsk_done] = TaskProcessor(self.tasks, tsk_done)
+                rabotyagi[tsk_done].run()
+                chislo_rabotyag += 1
+                tsk_done += 1
+            keys = list(rabotyagi.keys())
+            for key in keys:
+                if not rabotyagi[key].proc.is_alive():  # выключение завершённых процессов
+                    print('process close')
+                    result.update(rabotyagi[key].ans_dict)
+                    rabotyagi[key].proc.join()
+                    rabotyagi.pop(key)
+                    chislo_rabotyag -= 1
+                elif (time.time() - rabotyagi[key].time) > self.timeout:  # выключение долгих процессов
+                    print('process close cause of time is up')
+                    result.update({key: "time is up"})
+                    rabotyagi[key].proc.terminate()
+                    rabotyagi.pop(key)
+                    chislo_rabotyag -= 1
+
+        return result
+
+
+# ну а тут всякие тестики
+'''def sum(a, b):  # функция с принтом
+    print(a + b)
+
+
+def timesum(a, b):  # с задержкой
+    time.sleep(5)
+    return a + b
+
+
+def mul(a, b):  # просто функция с результатом
+    return a * b
+
+
+task1 = Task(sum, *[1, 2])
+task2 = Task(mul, *[4, 7])
+task3 = Task(sum, *[10, 20])
+task4 = Task(timesum, *[4, 5])
+task5 = Task(sum, *[100, 200])
+
+
+def adding(n, queue):  # функция для добавления Тасков в очередь (реализуется через другой процесс)
+# Не знаю как реализовать добавление Тасков в очередь одновременно с работающим менеджером в одном процессе
+# вряд ли это возможно (возможно, но не реалистично, да и не особо красиво)
+    for i in range(n):
+        queue.put(Task(mul, *[i, i]))
+        queue.put(Task(timesum, *[4, 5]))
+
+
+if __name__ == "__main__":
+    tsk_qu = Manager().Queue()
+    subproc = Process(target=adding, args=(5, tsk_qu)) #
+    subproc.start()
+    m = TaskManager(tsk_qu, 3, 1)
+    an = m.run()
+    subproc.join()
+    print(an)'''
--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
 import os
+
+
+def counter(list):  # list=[path, filename, queue
+    number = 0
+    dct = dict()
+    with open(list[0] + '/' + list[1], 'r', encoding='utf-8') as file:
+        for line in file:
+            number += len(line.split())
+    dct[list[1]] = number
+    list[2].put(dct)
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +25,29 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    files = os.listdir(path=path_to_dir)
+    result = dict()
+    sum = 0
+    manager = Manager()
+    queue = manager.Queue()
+    forpool = Pool(4)  # сначала реализовал способ: для каждого файла свой процесс (4, потому что 4 ядра )
+    mas = []  # но в беседе увидел, что надо небольшое количество процессов, поэтому переделал
+    for file in files:
+        mas.append([path_to_dir, file, queue])
+    forpool.map(counter, mas)
+    '''procs = []  # оставил старую реализацию
+    for file in files:
+        proc = Process(target=counter, args=([path_to_dir, file, queue],))  # пришлось обернуть в лист для новой реализ
+        procs.append(proc)
+        proc.start()'''
+
+    while not queue.empty():
+        res = queue.get()
+        result.update(res)
+        sum += list(res.values())[0]
+
+    '''for process in procs:
+        process.join()'''
+    result['total'] = sum
+
+    return result
