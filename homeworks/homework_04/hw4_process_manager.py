13ddc06202f5470d9ea288f124d9914a03712c0f
Justegoriy <egoregor424@gmail.com>
2019-10-12 19:56:27 +0300

    hw4

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process
+from multiprocessing import Process, Queue, Manager
+from abc import ABC, abstractmethod
+from time import time, sleep
 
+class Task(ABC):
 
-class Task:
-    """
-    Задача, которую надо выполнить.
-    В идеале, должно быть реализовано на достаточном уровне абстракции,
-    чтобы можно было выполнять "неоднотипные" задачи
-    """
-    def __init__(self, ...):
-        """
-        Пофантазируйте, как лучше инициализировать
-        """
-        raise NotImplementedError
-
+    @abstractmethod
     def perform(self):
-        """
-        Старт выполнения задачи
-        """
-        raise NotImplementedError
-
+        pass
 
 class TaskProcessor:
     """
@@ -31,14 +19,20 @@ class TaskProcessor:
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
         """
-        raise NotImplementedError
+        self.tasks_queue = tasks_queue
+        self.start_t = 0
+        self.process = None
+        self.pid = None
 
     def run(self):
         """
         Старт работы воркера
         """
-        raise NotImplementedError
-
+        Task = self.tasks_queue.get()
+        self.process = Process(target=Task.perform, args=())
+        self.process.start()
+        self.start_t = time()
+        self.pid = self.process.pid
 
 class TaskManager:
     """
@@ -50,11 +44,51 @@ class TaskManager:
         :param n_workers: кол-во воркеров
         :param timeout: таймаут в секундах, воркер не может работать дольше, чем timeout секунд
         """
-        raise NotImplementedError
+        self.tasks_queue = tasks_queue
+        self.n_workers = n_workers
+        self.timeout = timeout
+
+    def add_process(self, w_list):
+        worker = TaskProcessor(self.tasks_queue)
+        w_list.append(worker)
+        worker.run()
+        print(f'Процесс {worker.pid} запущен.')
 
     def run(self):
-        """
-        Запускайте бычка! (с)
-        """
-        raise NotImplementedError
-
+        work_list = []
+        sleeptime = 1
+        for _ in range(self.n_workers):
+           self.add_process(work_list)
+        while not self.tasks_queue.empty():
+            k = 0
+            for i in range(len(work_list)):
+                pid = work_list[i - k].pid
+                if work_list[i - k].process.is_alive():
+                    if time() - work_list[i - k].start_t > self.timeout:
+                        print(f'Процесс {pid} удален по превышению времени.')
+                        del work_list[i - k]
+                        k += 1
+                        if not self.tasks_queue.empty():
+                            self.add_process(work_list)
+                else:
+                    print(f'Процесс {pid} удален по выполнению задачи.')
+                    del work_list[i - k]
+                    k += 1
+                    if not self.tasks_queue.empty():
+                        self.add_process(work_list)
+            sleep(sleeptime)
+        while len(work_list) != 0:
+            k = 0
+            for i in range(len(work_list)):
+                pid = work_list[i - k].pid
+                if work_list[i - k].process.is_alive():
+                    if time() - work_list[i - k].start_t > self.timeout:
+                        print(f'Процесс {pid} удален по превышению времени.')
+                        del work_list[i - k]
+                        k += 1
+                else:
+                    print(f'Процесс {pid} удален по выполнению задачи.')
+                    del work_list[i - k]
+                    k += 1
+            sleep(sleeptime)
+        print("Дело сделано.")
--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Queue, Pool
 import os
 
+def worker(q, ret_d, path_to_dir):
+    File = q.get()
+    with open(path_to_dir + '/' + File, 'r', encoding='utf-8') as fin:
+        count = 0
+        for line in fin.readlines():
+            count += len(line.split())
+        ret_d[File] = count
 
 def word_count_inference(path_to_dir):
     '''
@@ -16,4 +23,19 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    files = os.listdir(path_to_dir)
+    Process_num = 3
+    manager = Manager()
+    ret_d = manager.dict()
+    q = manager.Queue()
+    for File in files:
+        q.put(File)
+    pool = Pool(processes=Process_num)
+    for _ in range(len(files)):
+        pool.apply_async(worker, (q, ret_d, path_to_dir))
+    pool.close()
+    pool.join()
+    ret_d["total"] = 0
+    for v in ret_d.values():
+        ret_d["total"] += v
+    return ret_d
