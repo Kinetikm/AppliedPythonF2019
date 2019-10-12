e438f89b27abd520da2c7d7e444b14c81aa7383e
Kirill <kirill.lego00@mail.ru>
2019-10-13 01:16:25 +0300

    process_manager

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -10,17 +10,19 @@ class Task:
     В идеале, должно быть реализовано на достаточном уровне абстракции,
     чтобы можно было выполнять "неоднотипные" задачи
     """
-    def __init__(self, ...):
+    def __init__(self, function, *args, **kwargs):
         """
         Пофантазируйте, как лучше инициализировать
         """
-        raise NotImplementedError
+        self.args = args
+        self.kwargs = kwargs
+        self.function = function
 
     def perform(self):
         """
         Старт выполнения задачи
         """
-        raise NotImplementedError
+        self.function(*self.args, **self.kwargs)
 
 
 class TaskProcessor:
@@ -31,13 +33,20 @@ class TaskProcessor:
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
         """
-        raise NotImplementedError
+        self.tasks_queue = tasks_queue
 
-    def run(self):
+    def run(self, timeout):
         """
         Старт работы воркера
         """
-        raise NotImplementedError
+        while True:
+            if self.tasks_queue.empty():
+                print("end")
+                break
+            task = self.tasks_queue.get()
+            work = Process(target=task.perform())
+            work.start()
+            work.join(timeout=timeout)
 
 
 class TaskManager:
@@ -50,10 +59,24 @@ class TaskManager:
         :param n_workers: кол-во воркеров
         :param timeout: таймаут в секундах, воркер не может работать дольше, чем timeout секунд
         """
-        raise NotImplementedError
+        self.tasks_queue = tasks_queue
+        self.n_workers = n_workers
+        self.timeout = timeout
+        self.alive_procs = []
 
     def run(self):
         """
         Запускайте бычка! (с)
         """
-        raise NotImplementedError
+        tasks = []
+        for i in range(n_workers):
+            tasks.append(TaskProcessor(self.tasks_queue))
+        for task in tasks:
+            work = Process(target=task.run(timeout=self.timeout))
+            self.alive_procs.append(work)
+            work.start()
+        while not self.tasks_queue.empty():
+            for i in range(len(self.alive_procs)):
+                if not self.alive_procs[i].is_alive():
+                    work = Process(target=self.alive_procs[i].run(self.timeout))
+                    work.start()
