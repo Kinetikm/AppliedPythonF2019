7c67a2d3969cbaa259013fa22da301fce42f62c4
Ильнур Зиннуров <vip.zinnurov@mail.ru>
2019-10-12 14:51:40 +0300

    Update hw4_wordcounter.py
    Сделан счётчик слов

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,19 +1,57 @@
-#!/usr/bin/env python
-# coding: utf-8
+from multiprocessing import Manager, Queue
+from multiprocessing import Pool
+from os import listdir
 
-from multiprocessing import Process, Manager
-import os
+PROCESSES_COUNT = 2
 
 
-def word_count_inference(path_to_dir):
-    '''
-    Метод, считающий количество слов в каждом файле из директории
-    и суммарное количество слов.
-    Слово - все, что угодно через пробел, пустая строка "" словом не считается,
-    пробельный символ " " словом не считается. Все остальное считается.
-    Решение должно быть многопроцессным. Общение через очереди.
-    :param path_to_dir: путь до директории с файлами
-    :return: словарь, где ключ - имя файла, значение - число слов +
-        специальный ключ "total" для суммы слов во всех файлах
-    '''
-    raise NotImplementedError
+def func(x, path_to_dir="../homeworks/homework_04/test_data"):
+    with open(path_to_dir + '/' + x, 'r', encoding='utf-8') as f:
+        words = len(f.read().split())
+    return words
+
+
+def new_producer(path, queue):
+    directory = path
+    to_process_list = listdir(directory)
+    for file in to_process_list:
+        queue.put(file)
+    queue.put('kill')
+
+
+def new_consumer(results, queue: Queue):
+    while True:
+        file = queue.get()
+        if file == 'kill':
+            break
+        num = func(file)
+        results.put([num, file])
+
+
+def returner(results):
+    answer = {}
+    total = 0
+    while True:
+        res = results.get()
+        if res[0] == "kill":
+            answer['total'] = total
+            return answer
+        answer[res[1]] = res[0]
+        total += res[0]
+
+
+def word_count_inference(path_to_dir="./test_data"):
+    manager = Manager()
+    queue = manager.Queue()
+    results = manager.Queue()
+    pool = Pool(PROCESSES_COUNT)
+
+    pool.apply_async(new_consumer, (results, queue,))
+
+    pool.apply_async(new_producer, (path_to_dir, queue)).get()
+
+    pool.close()
+    pool.join()
+    results.put(["kill", "kill"])
+
+    return returner(results)
