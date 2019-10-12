86308f375d7949542d95cd208e1b94e68b687f5f
Alexander Zimokos <alexander.zimokos@mail.ru>
2019-10-12 11:14:38 +0300

    add wordcount

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-from multiprocessing import Process, Manager
+from multiprocessing import Manager, Pool
 import os
+
+
+def word_count(filepath, queue):
+    with open(filepath, 'r') as f:
+        queue.put({filepath.split('/')[-1]: len(f.read().strip().split())})
+
+
+def result_func(queue):
+    res_dict = {}
+    total = 0
+    while True:
+        result = queue.get()
+        if result == "The end":
+            break
+        total += list(result.values())[0]
+        res_dict.update(result)
+    res_dict["total"] = total
+    return res_dict
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +31,16 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+
+    NUMBER_OF_PROCESSES = 8
+    manager = Manager()
+    queue = manager.Queue()
+    files = [os.path.join(path_to_dir, i) for i in os.listdir(path_to_dir)]
+    pool = Pool(NUMBER_OF_PROCESSES)
+    cons = pool.apply_async(result_func, (queue,))
+    tot = [pool.apply_async(word_count, (filepath, queue)) for filepath in files]
+    for elem in tot:
+        elem.get()
+    queue.put("The end")
+    return cons.get()
+
