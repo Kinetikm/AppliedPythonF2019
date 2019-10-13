180362ee283ce565e4fa795874bf7918f765e85a
MaximAbramenko <gotosleepmcht@gmail.com>
2019-10-13 04:45:25 +0300

    kk

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -16,4 +16,26 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    q = Queue()
+    words = Manager().dict()
+    tasks = []
+    for file in os.listdir(path_to_dir):
+        q.put(path_to_dir+'/'+file)
+    for _ in range(2):
+        task = Process(target=f, args=(q, words))
+        tasks.append(task)
+        task.start()
+    for task in tasks:
+        task.join()
+    words['total'] = sum(words.values())
+    return words
+
+
+def f(queue_of_files, words_in_file):
+    while True:
+        item = queue_of_files.get_nowait()
+        if item is not None:
+            with open(item, 'r') as f_id:
+                words_in_file[item.split('/')[-1]] = len(f_id.read().split())
+        else:
+            break
