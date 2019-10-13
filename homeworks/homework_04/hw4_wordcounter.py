a431fc5246a023df3915973731dcc94069acf54f
mnpks <fuglu@mail.ru>
2019-10-13 04:24:05 +0300

    wordcounter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from queue import Queue
+from multiprocessing import Process
 import os
+
+
+def word_counter(directory, file, dic):
+    path = directory + '/' + file
+    with open(path, "r", encoding="utf-8") as f:
+        words = 0
+        for i in f:
+            words += len(i.split())
+        f.close()
+    dic[file] = words
+    dic['total'] += words
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +28,14 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    d = {'total': 0}
+    all_processes = Queue()
+    all_files = os.listdir(path_to_dir)
+    for file in all_files:
+        process = Process(target=word_counter(path_to_dir, file, d))
+        all_processes.put(process)
+        process.start()
+    while not all_processes.empty():
+        all_processes.get().join()
+    return d
+
