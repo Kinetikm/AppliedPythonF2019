d071e761148e082b573aef6fc2442cd1dad8539f
getkekked <gettopkekked@gmail.com>
2019-10-13 15:52:43 +0300

    Fixed LRUCache

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 import time
+from functools import wraps
+from collections import OrderedDict
 
 
 class LRUCacheDecorator:
 
     def __init__(self, maxsize, ttl):
         self.maxsize = maxsize
-        self.ttl = ttl
-        self.cache = {}
+        self.ttl = ttl / 1000
+        self.cache = OrderedDict()
 
     def __call__(self, func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             key = hash(str(args) + str(tuple(kwargs)) + str(tuple(kwargs.values())))
             if key not in self.cache:
@@ -18,17 +21,17 @@ class LRUCacheDecorator:
                 if len(self.cache) < self.maxsize:
                     self.cache[key] = [result, time.time()]
                 else:
-                    keymax = max(self.cache,  key=lambda x: time.time() - self.cache[x][1])
-                    self.cache.pop(keymax)
+                    self.cache.popitem(last=False)
                     self.cache[key] = [result, time.time()]
                 return result
             else:
                 if self.ttl:
-                    if (time.time() - self.cache[key][1]) * 1000 > self.ttl:
-                        self.cache.pop(key)
+                    if (time.time() - self.cache[key][1]) > self.ttl:
                         result = func(*args, **kwargs)
-                        self.cache[key] = [result, time.time()]
+                        self.cache[key][0] = result
                         return result
+                self.cache.move_to_end(key, last=True)
                 self.cache[key][1] = time.time()
                 return self.cache[key][0]
         return wrapper
+
