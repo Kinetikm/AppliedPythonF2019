f4854d307c132b45b52e1ccd9bea6fd7b732113d
Daniil <tretyakov.daniil.edcuation@gmail.com>
2019-10-13 03:19:39 +0300

    Added wordcount and process_manager

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process
+from multiprocessing import Process, Queue, current_process
+import time
+import random
 
 
 class Task:
@@ -10,17 +12,25 @@ class Task:
     В идеале, должно быть реализовано на достаточном уровне абстракции,
     чтобы можно было выполнять "неоднотипные" задачи
     """
-    def __init__(self, ...):
+    def __init__(self, func, args=(), kwargs={}):
         """
-        Пофантазируйте, как лучше инициализировать
+        :param func: функция, которую надо выполнить
+        :param args: позиционные аргументы функции в виде списка
+        :param kwargs: именнованные аргументы функции в виде словаря
         """
-        raise NotImplementedError
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.name = func.__name__
 
     def perform(self):
         """
         Старт выполнения задачи
         """
-        raise NotImplementedError
+        return self.func(*self.args, **self.kwargs)
+
+    def __str__(self):
+        return f'Task obj: func = {self.name}, args = {self.args}, kwargs = {self.kwargs}'
 
 
 class TaskProcessor:
@@ -30,14 +40,38 @@ class TaskProcessor:
     def __init__(self, tasks_queue):
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
+        :param done_queue: Manager.Queue со строками - результами выполнения таски
         """
-        raise NotImplementedError
+        self.tasks_queue = tasks_queue
+        self.start_time = time.time()
+        self.p = None
+        self.name = None
+
+    def calculate(self):
+        for task in iter(self.tasks_queue.get, 'STOP'):
+            result = task.perform()
+            args_str = ', '.join(str(i) for i in task.args)
+            kwargs_str = ', '.join('='.join((str(k), str(v))) for k, v in task.kwargs.items())
+            # попытка красиво вывести результат выполнения воркера
+            job_info = f'{current_process().name} says that {task.name}({args_str} {kwargs_str}) = {result}'
+            print(job_info)
 
     def run(self):
         """
         Старт работы воркера
         """
-        raise NotImplementedError
+        self.p = Process(target=self.calculate)
+        self.name = self.p.name
+        self.p.start()
+
+    def suicide(self):
+        self.p.terminate()
+
+    def finish(self):
+        self.p.join()
+
+    def is_alive(self):
+        return self.p.is_alive()
 
 
 class TaskManager:
@@ -50,10 +84,75 @@ class TaskManager:
         :param n_workers: кол-во воркеров
         :param timeout: таймаут в секундах, воркер не может работать дольше, чем timeout секунд
         """
-        raise NotImplementedError
+        self.tasks_queue = tasks_queue
+        self.n_workers = n_workers
+        self.timeout = timeout
+        self.workers = []
+
+    def start_worker(self):
+        p = TaskProcessor(self.tasks_queue)
+        self.workers.append(p)
+        p.run()
 
     def run(self):
         """
         Запускайте бычка! (с)
         """
-        raise NotImplementedError
+        for _ in range(self.n_workers):
+            self.start_worker()
+
+        # Проверяем, не работает ли какой-нибудь воркер дольше таймаута
+        while not self.tasks_queue.empty():
+            for worker in self.workers:
+                if worker.is_alive():
+                    if time.time() - worker.start_time >= self.timeout:
+                        print(f'{worker.name} timeout! termiate')
+                        worker.suicide()
+                        worker.finish()
+                        self.workers.remove(worker)
+
+                        self.start_worker()
+            time.sleep(0.1)
+
+    def stop(self):
+        for _ in range(self.n_workers):
+            self.tasks_queue.put('STOP')
+
+        for w in self.workers:
+            w.finish()
+
+
+if __name__ == "__main__":
+    def mul(a, b):
+        # Эта функция может работать за время большее, чем таймаут
+        time.sleep(2*random.random())
+        return a * b
+
+    def plus(a, b):
+        time.sleep(0.5*random.random())
+        return a + b
+
+    def say_hello(name):
+        time.sleep(0.5*random.random())
+        return 'Hello ' + name
+
+    tasks_queue = Queue()
+
+    # Заполним очередь первой таской
+    for i in [Task(mul, (j, 7)) for j in range(20)]:
+        tasks_queue.put(i)
+
+    # создадим и запустим менеджера
+    NUMBER_OF_PROCESSES = 4
+    TIMEOUT = 1
+    tm = TaskManager(tasks_queue, NUMBER_OF_PROCESSES, TIMEOUT)
+    tm.run()
+
+    # Добавим в очередь вторую таску
+    for i in [Task(plus, (j, 7)) for j in range(20)]:
+        tasks_queue.put(i)
+    # Добавим в очередь третью таску
+    for i in [Task(say_hello, kwargs={'name': j}) for j in ('Vasya', 'Petya', 'Kolya')]:
+        tasks_queue.put(i)
+
+    tm.stop()
--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Pool, Manager
 import os
+
+
+PROCESSES_COUNT = os.cpu_count()
+
+
+def worker(queue, shared_dict):
+    while True:
+        res = queue.get()
+        if res == -1:
+            break
+
+        counter = 0
+        with open(res, 'r') as f:
+            for line in f:
+                counter += len(line.split())
+        print(res, counter)
+
+        shared_dict[res.split('/')[-1]] = counter
+        shared_dict['total'] += counter
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +35,19 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    manager = Manager()
+    queue = manager.Queue()
+    pool = Pool(PROCESSES_COUNT)
+    shared_dict = manager.dict()
+    shared_dict['total'] = 0
+
+    pool.apply_async(worker, (queue, shared_dict))
+
+    for filename in os.listdir(path_to_dir):
+        queue.put(path_to_dir + '/' + filename)
+
+    queue.put(-1)
+    pool.close()
+    pool.join()
+
+    return shared_dict
