fd79b0d1bc456cf02cdbbec968cac0051b82c076
LoskutovPD <48155308+LoskutovPD@users.noreply.github.com>
2019-10-13 09:19:26 +0300

    Hw4

--- homeworks/homework_04/hw4_process_manager.py
+++ homeworks/homework_04/hw4_process_manager.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process
+from multiprocessing import Process, Queue
+import time
 
 
 class Task:
@@ -10,50 +11,83 @@ class Task:
     В идеале, должно быть реализовано на достаточном уровне абстракции,
     чтобы можно было выполнять "неоднотипные" задачи
     """
-    def __init__(self, ...):
+
+    def __init__(self, function, *args, **kwargs):
         """
         Пофантазируйте, как лучше инициализировать
         """
-        raise NotImplementedError
-
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+    
     def perform(self):
         """
         Старт выполнения задачи
         """
-        raise NotImplementedError
+        return self.function(*self.args, **self.kwargs)
 
 
 class TaskProcessor:
     """
     Воркер-процесс. Достает из очереди тасок таску и делает ее
     """
-    def __init__(self, tasks_queue):
+
+    def __init__(self, tasks_queue, timeout_task):
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
         """
-        raise NotImplementedError
+        self.tasks_queue = tasks_queue
+        self.timeout_task = timeout_task
 
     def run(self):
         """
         Старт работы воркера
         """
-        raise NotImplementedError
+        while not self.tasks_queue.empty():
+            trg = self.tasks_queue.get()
+            proc = Process(target=trg.perform)
+            proc.start()
+            try:
+                proc.join(timeout=self.timeout_task)
+            except:
+                proc.terminate()
 
 
 class TaskManager:
     """
     Мастер-процесс, который управляет воркерами
     """
+
     def __init__(self, tasks_queue, n_workers, timeout):
         """
         :param tasks_queue: Manager.Queue с объектами класса Task
         :param n_workers: кол-во воркеров
         :param timeout: таймаут в секундах, воркер не может работать дольше, чем timeout секунд
+
+        "Если какая-то задача выполняется дольше таймаута, то её выполнение должно прекратиться."
+        потому добавим еще один параметр - timeout_task
+        А значит, процессы в процессах, ибо время важно для воркера и для таски
         """
-        raise NotImplementedError
+        self.tasks_queue = tasks_queue
+        self.n_workers = n_workers
+        self.timeout = timeout
+        self.workers = [
+            Process(target=TaskProcessor(tasks_queue=self.tasks_queue,
+                                         timeout_task=self.timeout).run) for _
+            in range(self.n_workers)]
 
     def run(self):
         """
         Запускайте бычка! (с)
         """
-        raise NotImplementedError
+        for worker in self.workers:
+            worker.start()
+        while True:
+            for i, worker in enumerate(self.workers):
+                if not worker.is_alive():
+                    self.workers.pop(i)
+                    tmp_proc = Process(
+                        target=TaskProcessor(tasks_queue=self.tasks_queue,
+                                             timeout_task=self.timeout).run)
+                    tmp_proc.run()
+                    self.workers.append(tmp_proc)
--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
 import os
+
+counts = Manager().dict()
+
+
+def counter(filename):
+    x = 0
+    try:
+        with open(filename, "r", encoding="utf8") as f:
+            for line in f:
+                x += len(line.split())
+    except:
+        print("Something wrong!")
+        return
+    counts[filename.split("/")[-1]] = x
 
 
 def word_count_inference(path_to_dir):
@@ -16,4 +30,9 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    pl = Pool()
+    pl.map(counter, [path_to_dir + "/" + x for x in os.listdir(path_to_dir)])
+    pl.close()
+    pl.join()
+    counts["total"] = sum(counts.values())
+    return counts
