6a628a1d178ffa4ae3bf77f7c09a49a61b51ea16
mnpks <55459685+mnpks@users.noreply.github.com>
2019-10-13 14:57:52 +0300

    improved lru

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 import time
+from functools import wraps
 
 
 class LRUCacheDecorator:
@@ -12,20 +13,21 @@ class LRUCacheDecorator:
                     должен исчезнуть
         '''
         self.maxsize = maxsize
-        self.ttl = ttl
+        if ttl:
+            self.ttl = ttl / 1000
+        else:
+            self.ttl = ttl
         self.cache = dict()
         self.time = dict()
 
     def __call__(self, func):
+        @wraps(func)
         def dec(*args, **kwargs):
             k = (args, tuple(kwargs))
             if k in self.cache.keys():
                 if self.ttl:
-                    if (time.time() - self.time[k]) * 1000 > self.ttl:
-                        del self.cache[k]
-                        func(*args, **kwargs)
+                    if time.time() - self.time[k] > self.ttl:
                         self.cache[k] = func(*args, **kwargs)
-                        del self.time[k]
                         self.time[k] = time.time()
                         return self.cache[k]
                 del self.time[k]
@@ -37,7 +39,7 @@ class LRUCacheDecorator:
                         self.cache[k] = func(*args, **kwargs)
                         self.time[k] = time.time()
                     else:
-                        d = max(self.time, key=lambda elem: time.time() - self.time[elem])
+                        d = next(iter(self.time))
                         del self.time[d]
                         del self.cache[d]
                         self.cache[k] = func(*args, **kwargs)
