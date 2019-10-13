bfd15dcd1f0b5e3b63c62d1c9b5dc2894184c624
MaximAbramenko <gotosleepmcht@gmail.com>
2019-10-13 03:14:03 +0300

    letsgo

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -5,6 +5,16 @@ from multiprocessing import Process, Manager
 import os
 
 
+def word_counter(path_to_file, filename, out_dict):
+    out = 0
+    with open("{}/{}".format(path_to_file, filename)) as file:
+        for line in file:
+            out += len(line.split())
+        file.close()
+    out_dict[filename] = out
+    out_dict["total"] += out
+
+
 def word_count_inference(path_to_dir):
     '''
     Метод, считающий количество слов в каждом файле из директории
@@ -16,4 +26,13 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    manager = Manager()
+    out, out["total"] = manager.dict(), 0
+    ps = []
+    for file in os.listdir(path_to_dir):
+        p = Process(target=word_counter(path_to_dir, file, out))
+        ps.append(p)
+        p.start()
+    for p in ps:
+        p.join()
+    return out
