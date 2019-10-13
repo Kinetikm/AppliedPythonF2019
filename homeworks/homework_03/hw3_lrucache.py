8c95c1f7648d6f6099f85988176b4553f8f520b6
Aleksandr Kondakov <bimboterminator@gmail.com>
2019-10-13 18:35:58 +0300

    fix lru with requirements

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -9,9 +9,9 @@ class LRUCacheDecorator:
     class LRUCacheItem:
         """Data structure of items stored in cache"""
 
-        def __init__(self, args):
+        def __init__(self, args, key):
             self.args_str = str(args)
-            self.key = hash(self.args_str)
+            self.key = key
             self.cached_result = None
             self.timestamp = time.time()
 
@@ -19,7 +19,7 @@ class LRUCacheDecorator:
             return self.args_str
 
         def __repr__(self):
-            return self.args_str
+            return self.args_str + ", time=" + str(self.timestamp)
 
         def __hash__(self):
             return self.key
@@ -33,7 +33,6 @@ class LRUCacheDecorator:
             self.timestamp = time.time()
 
         def get_cached_result(self):
-            self.timestamp = time.time()
             return self.cached_result
 
     def __init__(self, maxsize, ttl):
@@ -46,49 +45,39 @@ class LRUCacheDecorator:
         #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
         self.maxsize = maxsize
         self.ttl = ttl
-        self.item_list = []
+        self.items = {}
 
     def __call__(self, *args, **kwargs):
 
         func = args[0]
 
         def decorator(*args, **kwargs):
-            item = self.LRUCacheItem(args)
-            print(self.item_list)
-            if item in self.item_list:
-                item_index = self.item_list.index(item)
-                item = self.item_list[item_index]
+            key = hash(str(args))
+            if key in self.items:
+                item = self.items[key]
                 if not self.ttl_is_valid(item):
                     item.set_cached_result(func(*args, **kwargs))
                 return item.get_cached_result()
             else:
-                if len(self.item_list) + 1 > self.maxsize:
+                if len(self.items) + 1 > self.maxsize:
                     self.validate_cache()
+                item = self.LRUCacheItem(args, key)
                 item.set_cached_result(func(*args, **kwargs))
-                self.item_list.insert(0, item)
+                self.items[key] = item
                 return item.cached_result
 
         return decorator
 
     def remove_item(self, item):
-        del self.item_list[self.item_list.index(item)]
+        del self.items[item.key]
 
     def validate_cache(self):
         if self.ttl:
-            for item in self.item_list:
+            for item in self.items.values():
                 if not self.ttl_is_valid(item):
                     self.remove_item(item)
         else:
-            # now = time.time()
-            # max_delta = -1
-            # remove_candidate = None
-            # for item in self.item_list:
-            #     if (now - item.timestamp) > max_delta:
-            #         max_delta = now - item.timestamp
-            #         remove_candidate = item
-            # if remove_candidate:
-            #     self.remove_item(remove_candidate)
-            self.remove_item(self.item_list[0])
+            self.items.popitem()
 
     def ttl_is_valid(self, item):
         if self.ttl:
