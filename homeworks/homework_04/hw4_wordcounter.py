e2489da7d2ecbab621c72ab3ec6672cf6ef11960
MaximAbramenko <gotosleepmcht@gmail.com>
2019-10-13 03:55:25 +0300

    letsgo

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -2,37 +2,29 @@
 # coding: utf-8
 
 from multiprocessing import Process, Manager
-import os
+from os import listdir
 
 
-def word_counter(path_to_file, filename, out_dict):
-    out = 0
-    with open("{}/{}".format(path_to_file, filename)) as file:
+def words_counter(path_to_file, filename, output):
+    cnt = 0
+    with open(path_to_file + '/' + filename) as file:
         for line in file:
-            out += len(line.split())
+            cnt += len(line.split())
         file.close()
-    out_dict[filename] = out
-    out_dict["total"] += out
+    output[filename] = cnt
+    output["total"] += cnt
 
 
 def word_count_inference(path_to_dir):
-    '''
-    Метод, считающий количество слов в каждом файле из директории
-    и суммарное количество слов.
-    Слово - все, что угодно через пробел, пустая строка "" словом не считается,
-    пробельный символ " " словом не считается. Все остальное считается.
-    Решение должно быть многопроцессным. Общение через очереди.
-    :param path_to_dir: путь до директории с файлами
-    :return: словарь, где ключ - имя файла, значение - число слов +
-        специальный ключs "total" для суммы слов во всех файлах
-    '''
-    manager = Manager()
-    out, out["total"] = manager.dict(), 0
-    ps = []
-    for file in os.listdir(path_to_dir):
-        p = Process(target=word_counter(path_to_dir, file, out))
-        ps.append(p)
+    man = Manager()
+    output = man.dict({"total": 0})
+
+    q = []
+    for file in listdir(path_to_dir):
+        p = Process(target=words_counter(path_to_dir, file, output))
+        q.append(p)
         p.start()
-    for p in ps:
-        p.join()
-    return out
+
+    for el in q:
+        el.join()
+    return output
