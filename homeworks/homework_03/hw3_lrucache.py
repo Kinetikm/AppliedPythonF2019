77577843a5d608a5903ddc7f592010d08a64f00f
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2019-10-13 13:47:36 +0300

    fix logic error

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -6,38 +6,32 @@ import time
 import collections
 import functools
 
+
 class LRUCacheDecorator:
-    
+
     class CacheEntity:
         def __init__(self, function, arguments=(), kw_arguments=dict(), ttl=None):
-            self.last_use_time = time.time()
             # self.ttl = None if ttl is None else ttl / 1000
             self.ttl = ttl
             self.function = function
             self.arguments = arguments
             self.kw_arguments = kw_arguments
             self.result = None
-            self.recalc_result()
+            self.recalc_time = None
 
         def expired(self):
             if self.ttl is None:
                 return False
-            return self.ttl < time.time() - self.last_use_time
+            return self.ttl < time.time() - self.recalc_time
 
         def get_result(self):
             if self.expired():
                 self.recalc_result()
-            self.last_use_time = time.time()
             return self.result
 
         def recalc_result(self):
+            self.recalc_time = time.time()
             self.result = self.function(*self.arguments, **self.kw_arguments)
-
-        def __eq__(self, other):
-            return self.last_use_time == other.last_use_time
-
-        def __lt__(self, other):
-            return self.last_use_time < other.last_use_time
 
         def __repr__(self):
             return "{} {}".format(self.ttl, self.last_use_time)
@@ -68,5 +62,6 @@ class LRUCacheDecorator:
                     cached_calls.popitem(last=False)
                 ce = self.CacheEntity(func, arguments=args, kw_arguments=kwargs, ttl=self.ttl)
                 cached_calls[args_key] = ce
+                ce.recalc_result()
 
         return cached_call
