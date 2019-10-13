ec9f58f76912f11c875bdbc8faab7813c467f6d5
Anna Maximova <anna.maximova.14.07@mail.ru>
2019-10-13 12:20:19 +0300

    correct search of the oldest record

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -21,7 +21,6 @@ class LRUCacheDecorator:
         else:
             self.ttl = None
         self.cache = {}
-        #self.cache_time = {}
         self.key_time_queue = deque()
 
     def __call__(self, func):
@@ -32,14 +31,11 @@ class LRUCacheDecorator:
                 if len(self.cache) == self.maxsize:
                     del_key = self.key_time_queue.popleft()
                     del self.cache[del_key]
-                    #del self.cache_time[del_key]
                 self.cache[key] = [func(*args, **kwargs), time()]
-                #self.cache_time[key] = time()
                 self.key_time_queue.append(key)
                 return self.cache[key][0]
             else:
                 if self.ttl and (time() - self.cache[key][1]) > self.ttl:
-                    #self.cache_time[key] = time()
                     self.cache[key] = [func(*args, **kwargs), time()]
                     self.key_time_queue.remove(key)
                     self.key_time_queue.append(key)
