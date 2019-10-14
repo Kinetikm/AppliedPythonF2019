2d3ccd148417dacb4c7729b9c670121cf0204d73
Ильнур Зиннуров <vip.zinnurov@mail.ru>
2019-10-14 09:49:33 +0300

    hw3 fixed Ilnur Zinnurov
    В надежде на то, что всё же смогу пройти блок

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,19 +1,49 @@
-#!/usr/bin/env python
-# coding: utf-8
+import time
+from functools import wraps
+from collections import OrderedDict
+
+
+def make_key(args, kwds):
+    kwd_mark = (object(),)
+    key = args
+    if kwds:
+        key += kwd_mark
+        for item in kwds.items():
+            key += item
+
+    return key
 
 
 class LRUCacheDecorator:
-
     def __init__(self, maxsize, ttl):
-        '''
+        """
         :param maxsize: максимальный размер кеша
-        :param ttl: время в млсек, через которое кеш
-                    должен исчезнуть
-        '''
-        # TODO инициализация декоратора
-        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+         :param ttl: время в млсек, через которое кеш
+                     должен исчезнуть
+                     МАксимально возможно для своих знаний оптимизировал декоратор
+        """
+        self.maxsize = maxsize
+        self.ttl = ttl
+        self.cache = OrderedDict()
+        self.times = {}
 
-    def __call__(self, *args, **kwargs):
-        # TODO вызов функции
-        raise NotImplementedError
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            key = make_key(args, kwargs)
+            value = self.cache.get(key, None)
+            if value is not None and self.ttl is not None and (time.time() - self.times[key]) > self.ttl:
+                self.cache.pop(key)
+                value = None
+
+            if value is None:
+                value = func(*args, **kwargs)
+                if len(self.cache) == self.maxsize:
+                    self.cache.popitem()
+                self.cache[key] = value
+
+            self.times[key] = time.time()
+
+            return value
+
+        return wrapped
