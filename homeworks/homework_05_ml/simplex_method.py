92b6d2ceab54d78d22a6e4e46ab6c27f7ad31976
EvgeniyMist <wertygeny@gmail.com>
2019-10-19 13:31:28 +0300

    simplex method first try

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -1,23 +1,30 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import numpy as np
 
 
 def simplex_method(a, b, c):
-    """
-    Почитать про симплекс метод простым языком:
-    * https://  https://ru.wikibooks.org/wiki/Симплекс-метод._Простое_объяснение
-    Реализацию алгоритма взять тут:
-    * https://youtu.be/gRgsT9BB5-8 (это ссылка на 1-ое из 5 видео).
-
-    Используем numpy и, в целом, векторные операции.
-
-    a * x.T <= b
-    c * x.T -> max
-    :param a: np.array, shape=(n, m)
-    :param b: np.array, shape=(n, 1)
-    :param c: np.array, shape=(1, m)
-    :return x: np.array, shape=(1, m)
-    """
-    raise NotImplementedError
+    c *= -1
+    a = a.astype(np.float)
+    b = b.astype(np.float)
+    c = c.astype(np.float)
+    n, m = a.shape
+    corr = -1*np.ones(n, dtype=np.int16)  # correspondence between var and row
+    while True:
+        c_min = np.min(c)
+        if c_min >= 0:
+            break
+        pivot_col = np.argmin(c)
+        pivot_row = np.argmin(b / a[:, pivot_col])
+        pivot = a[pivot_row][pivot_col]
+        b[pivot_row] /= pivot
+        a[pivot_row] /= pivot
+        for i in range(n):
+            if i != pivot_row:
+                b[i] -= b[pivot_row] * a[i][pivot_col]
+                a[i] -= a[pivot_row] * a[i][pivot_col]
+        c -= a[pivot_row]*c[pivot_col]
+        corr[pivot_row] = pivot_col
+    result = np.zeros(m)
+    for i in range(n):
+        if corr[i] != -1:
+            result[corr[i]] = b[i]
+    return result
