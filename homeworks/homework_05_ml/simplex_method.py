45a9c6e797a2cf293103a67232cda8d0fcfb0730
verbarius <verbarius@mail.ru>
2019-10-20 19:20:26 +0300

    simplex_method

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -11,13 +11,6 @@ oper_vec = np.vectorize(oper)
 
 def simplex_method(a, b, c):
     """
-    Почитать про симплекс метод простым языком:
-    * https://  https://ru.wikibooks.org/wiki/Симплекс-метод._Простое_объяснение
-    Реализацию алгоритма взять тут:
-    * https://youtu.be/gRgsT9BB5-8 (это ссылка на 1-ое из 5 видео).
-
-    Используем numpy и, в целом, векторные операции.
-
     a * x.T <= b
     c * x.T -> max
     :param a: np.array, shape=(n, m)
