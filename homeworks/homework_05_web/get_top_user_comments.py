42707c4ad420435792b0e41271086259ff092cfc
ttimur2 <hunter220900@gmail.com>
2019-10-20 13:57:56 +0300

    3

--- homeworks/homework_05_web/get_top_user_comments.py
+++ homeworks/homework_05_web/get_top_user_comments.py
@@ -1,51 +1,15 @@
-import asyncio
-import aiohttp
-import csv
 import sys
 
 # Ваши импорты
-from collections import Counter
-
-from bs4 import BeautifulSoup
 
 
-async def get_html(link):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(link) as response:
-            return link, await response.text()
-
-
-async def main_coro(links):
-    return await asyncio.gather(*[
-        get_html(link) for link in links
-    ])
-
-
-def parse_html(text):
-    html = BeautifulSoup(text, 'html.parser')
-    return Counter((
-        span.getText()
-        for span in html.findAll('span', attrs={'class': 'user-info__nickname_comment'})
-    )).most_common()
-
-
-def write_csv(data, filename):
-    with open(filename, 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-
-
-def main(links, filename):
-    lines = [['link', 'username', 'count_comment']]
-    for link, text in asyncio.run(main_coro(links)):
-        for username, count_comment in parse_html(text):
-            lines.append([link, username, count_comment])
-    write_csv(lines)
+def main(filename, links):
+    # Ваш код
+    raise NotImplementedError
 
 
 if __name__ == '__main__':
     filename = 'top_user_comments.csv'
     links = sys.argv[1:4]
 
-    # Ваш код
-    main(links, filename)
+    main(filename, links)
