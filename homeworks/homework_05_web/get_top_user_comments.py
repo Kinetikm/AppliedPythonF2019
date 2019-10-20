083aa19288f1816732bd07952373b43f17988f2c
tsapen <dennytsap@gmail.com>
2019-10-20 13:50:47 +0300

    Update homework 5

--- homeworks/homework_05_web/get_top_user_comments.py
+++ homeworks/homework_05_web/get_top_user_comments.py
@@ -5,28 +5,31 @@ from aiohttp import ClientSession
 from bisect import insort
 import asyncio
 
+
 async def do_request(link, session):
     async with session.get(link) as resp:
         return await resp.read()
+
 
 def main(filename, links):
     loop = asyncio.get_event_loop()
     future = asyncio.ensure_future(run(filename, links))
     loop.run_until_complete(future)
 
+
 async def run(filename, links):
     requests = []
     result = []
     responses = []
     with open(filename, 'w') as file:
-        file.write("{},{},{}\n".format("link","username","count_comment"))
+        file.write("{},{},{}\n".format("link", "username", "count_comment"))
     try:
         async with ClientSession() as session:
             for link in links:
                 req = asyncio.ensure_future(do_request(link, session))
                 requests.append(req)
             responses = await asyncio.gather(*requests)
-    except:
+    except BaseException:
         return
     for num_link, resp in enumerate(responses):
         resp_cnt = Counter()
@@ -42,7 +45,7 @@ async def run(filename, links):
                 continue
             result_resp[resp_cnt[key]] = [key]
         result.append({'link': links[num_link], 'result': result_resp})
-    result.sort(key = lambda a: a['link'])
+    result.sort(key=lambda a: a['link'])
 
     with open(filename, 'a') as file:
         for link_result in result:
@@ -51,7 +54,11 @@ async def run(filename, links):
             for cnt in counts_list:
                 link_result['result'][cnt].sort()
                 for username in link_result['result'][cnt]:
-                    file.write("{},{},{}\n".format(link_result['link'],username,cnt)) 
+                    file.write(
+                        "{},{},{}\n".format(
+                            link_result['link'],
+                            username,
+                            cnt))
 
 
 if __name__ == '__main__':
