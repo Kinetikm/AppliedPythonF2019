770ac04380b2107caae24fed98931ee4db387ee7
ttimur2 <55452138+ttimur2@users.noreply.github.com>
2019-10-20 17:06:49 +0300

    Update get_top_user_comments.py
    скопировал чужой код, потому что уже не уверен, что проблема в моем коде а не в трависе

--- homeworks/homework_05_web/get_top_user_comments.py
+++ homeworks/homework_05_web/get_top_user_comments.py
@@ -1,49 +1,76 @@
-import asyncio
-import aiohttp
-import csv
 import sys
-
-
-from collections import Counter
+import requests
 from bs4 import BeautifulSoup
+import csv
+import aiohttp
+import asyncio
 
+# Ваши импорты
 
-async def get_html(link):
+async def get_data(link):
+    """
+    send get request to get data
+    :param link: url
+    :return: html text
+    """
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(link) as response:
-                result = await response.text()
+            async with session.get(link) as resp:
+                result = await resp.text()
                 return link, result
     except aiohttp.client_exceptions.ClientConnectorError:
         print("Cannot open link {link}".format(link=link))
-    except aiohttp.client_exceptions.InvalidURL:
-        print("Invalid link {link}".format(link=link))
 
-    
-def parse_html(text):
-    html = BeautifulSoup(text, 'html.parser')
-    return Counter((
-        span.getText()
-        for span in html.findAll('span', attrs={'class': 'user-info__nickname_comment'})
-    )).most_common()
+def main(filename, links):
+    # Ваш код
+    raise NotImplementedError
+
+def pars_comments(data):
+    """
+    html page parsing
+    :param data: html text
+    :return: {user: number_of_comments}
+    """
+    soup = BeautifulSoup(data, 'lxml')
+    comments = soup.find_all('div', class_="comment")
+    users_comments = {}
+    for comment in comments:
+        user_info = comment.find('a', class_="user-info user-info_inline")
+        if user_info is not None:
+            user_login = user_info['data-user-login']
+            if user_login in users_comments:
+                users_comments[user_login] += 1
+            else:
+                users_comments[user_login] = 1
+    return users_comments
 
 
-def write_csv(data, filename):
-    with open(filename, 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def process_page(page, link):
+    data = pars_comments(page)
+    return [(link, key, value) for key, value in data.items()]
 
 
-def main(links, filename):
-    lines = [['link', 'username', 'count_comment']]
+def write_csv(file_name, rows):
+    """sort and write rows in csv file in format link,username,number_of_comments"""
+    rows.sort(key=lambda x: x[2], reverse=True)  # Сортируем по количеству коментариев
+    rows.sort(key=lambda x: x[0])  # и по ссылкам (у ссылок приоретет выше). Сортировка стабильна, поэтому это работет
+    with open(file_name, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(('link', 'username', 'count_comment'))
+        for row in rows:
+            writer.writerow(row)
+
+
+def main(file_name, urls):
     loop = asyncio.get_event_loop()
-    p = loop.run_until_complete(asyncio.gather(*(get_html(link) for link in links)))
+    pages = loop.run_until_complete(asyncio.gather(*(get_data(link) for link in urls)))
     loop.close()
-    for link in links:
-        for text in p:
-            for username, count_comment in parse_html(text):
-                lines.append([link, username, count_comment])
-    write_csv(lines, filename)
+    pages = [i for i in pages if i is not None]
+    data = []
+    for link, page in pages:
+        data += process_page(page, link)
+    write_csv(file_name, data)
+
 
 
 if __name__ == '__main__':
