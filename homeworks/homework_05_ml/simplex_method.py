5a96d2e4ce65b4abc219ef2cadd82878d743fd84
root <root@localhost.localdomain>
2019-10-20 16:12:18 +0300

    Saturday

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -4,6 +4,20 @@
 import numpy as np
 
 
+def step(m):
+    col = np.where(m[-1] == m[-1].min())[0][0]
+    n = 0
+    ind = 0
+    for x in range(0, len(m) - 1):
+        if m[x, col]:
+            if not n:
+                n = m[x, -1] / m[x, col]
+            elif n > (m[x, -1] / m[x, col]):
+                n = m[x, -1] / m[x, col]
+                ind = x
+    return ind, col
+
+
 def simplex_method(a, b, c):
     """
     Почитать про симплекс метод простым языком:
@@ -20,4 +34,31 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    eye = np.concatenate([np.eye(a.shape[0]), np.zeros((1, a.shape[0]))])
+    b1 = np.concatenate([b.reshape((b.shape[0], 1)), [[0]]])
+    a1 = np.concatenate([a, [c * (-1)]])
+    matrix = np.concatenate([a1, eye, b1], axis=1)
+    res = np.zeros_like(c) - 1
+    return solve(matrix, res)
+
+
+def solve(matrix, res):
+    indexes = step(matrix)
+    matrix[indexes[0]] = matrix[indexes[0]] / matrix[indexes]
+    for i in range(len(matrix)):
+        if i != indexes[0]:
+            matrix[i] = matrix[i] - matrix[indexes[0]] * matrix[i, indexes[1]]
+    if indexes[0] in res:
+        for i in range(len(res)):
+            if res[i] == indexes[0]:
+                res[i] = -1
+    res[indexes[1]] = indexes[0]
+    if matrix[-1].min() >= 0:
+        result = np.zeros(res.shape[0])
+        for i in range(len(res)):
+            if res[i] > -1:
+                result[i] = matrix[res[i], -1]
+        return result
+    else:
+        return solve(matrix, res)
+
