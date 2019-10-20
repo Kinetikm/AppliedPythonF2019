514f865833ae23c37c9914fd92c6b2d0bd78c660
verbarius <verbarius@mail.ru>
2019-10-20 19:19:14 +0300

    simplex_method

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -4,6 +4,11 @@
 import numpy as np
 
 
+def oper(x, y, k):
+    return k*x + y
+oper_vec = np.vectorize(oper)
+
+
 def simplex_method(a, b, c):
     """
     Почитать про симплекс метод простым языком:
@@ -20,4 +25,17 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    n, m = a.shape
+    table = np.hstack((np.vstack((a, c*(-1))), np.eye(n+1), np.vstack((b.reshape(n, 1), np.array([0])))))
+    x = np.zeros((1, m))[0]
+    while np.min(table[-1, :]) < 0:
+        mincol = table[-1, :].argmin()
+        minrow = (table[:-1, -1]/table[:-1, mincol]).argmin()
+        table[minrow, :] = table[minrow, :]/table[minrow, mincol]
+        for i in range(n+1):
+            if i != minrow:
+                table[i, :] = oper_vec(table[minrow, :], table[i, :], -table[i, mincol]/table[minrow, mincol])
+    ind = np.where(table[:-1, :m] == 1)
+    for i in range(len(ind[1])):
+        x[ind[1][i]] = table[ind[0][i], -1]
+    return x
