e6f31fff425773ac1e6abc184aa1137e701e8dae
Ильнур Зиннуров <vip.zinnurov@mail.ru>
2019-10-23 18:46:39 +0300

    исправил все недочёты
    Exception переделал
    С response is None  переосмыслил, подправил
    var += 1 пофиксил
    Условия также упросил
    Нормальную версию текста прикрепил

--- homeworks/homework_05_web/get_top_user_comments.py
+++ homeworks/homework_05_web/get_top_user_comments.py
@@ -6,53 +6,44 @@ from bs4 import BeautifulSoup
 
 
 def opener(link):
-    response = None
     try:
         response = requests.get(link)
-    except ConnectionRefusedError:
-        print('Connection err {}'.format(link))
-        return None
-    except urllib3.exceptions.NewConnectionError:
-        print('Connection err {}'.format(link))
-        return None
-    except requests.exceptions.ConnectionError:
+    except (ConnectionRefusedError, urllib3.exceptions.NewConnectionError, requests.exceptions.ConnectionError):
         print('Connection err {}'.format(link))
         return None
 
-    if response is not None:
-        html = BeautifulSoup(response.text, 'html.parser')
-        names = {}
+    html = BeautifulSoup(response.text, 'html.parser')
+    names = {}
 
-        comments = html.find_all("span", attrs={"class": [
-            "user-info__nickname user-info__nickname_small "
-            "user-info__nickname_comment"]})
+    comments = html.find_all("span", attrs={"class": [
+        "user-info__nickname user-info__nickname_small "
+        "user-info__nickname_comment"]})
 
-        for comment in comments:
-            if comment.text in names:
-                names[comment.text] = names[comment.text] + 1
-            else:
-                names[comment.text] = 1
+    for comment in comments:
+        if comment.text in names:
+            names[comment.text] += 1
+        else:
+            names[comment.text] = 1
 
-        return sorted(names.items(), key=lambda kv: kv[1])
+    return sorted(names.items(), key=lambda kv: kv[1])
 
 
 def main(filename, links):
     for ind, link in enumerate(links):
         result = opener(link)
-        if ind == 0 and result is not None:
-            with open(filename, 'w') as file:
-                for keys in result:
-                    file.write("link, username, count_comment\n")
-                    file.write("{},{},{}\n".format(link, keys[0], keys[1]))
-                file.close()
-        elif result is not None:
-            with open(filename, 'a') as file:
-                for keys in result:
-                    file.write("{},{},{}\n".format(link, keys[0], keys[1]))
-                file.close()
-        else:
+        if result is None:
             with open(filename, 'w') as file:
                 file.write("link,username,count_comment\n")
+        else:
+            if ind == 0:
+                with open(filename, 'w') as file:
+                    file.write("link, username, count_comment\n")
+                    for keys in result:
+                        file.write("{},{},{}\n".format(link, keys[0], keys[1]))
+            else:
+                with open(filename, 'a') as file:
+                    for keys in result:
+                        file.write("{},{},{}\n".format(link, keys[0], keys[1]))
 
 
 if __name__ == '__main__':
