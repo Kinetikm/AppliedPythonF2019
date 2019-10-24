8c26d13a7cc6d90e5fb2e8f51827d1e672724558
Gulyaev Egor <egoregor424@gmail.com>
2019-10-24 20:48:24 +0300

    'hw5'

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -5,19 +5,43 @@ import numpy as np
 
 
 def simplex_method(a, b, c):
-    """
-    Почитать про симплекс метод простым языком:
-    * https://  https://ru.wikibooks.org/wiki/Симплекс-метод._Простое_объяснение
-    Реализацию алгоритма взять тут:
-    * https://youtu.be/gRgsT9BB5-8 (это ссылка на 1-ое из 5 видео).
-
-    Используем numpy и, в целом, векторные операции.
-
-    a * x.T <= b
-    c * x.T -> max
-    :param a: np.array, shape=(n, m)
-    :param b: np.array, shape=(n, 1)
-    :param c: np.array, shape=(1, m)
-    :return x: np.array, shape=(1, m)
-    """
-    raise NotImplementedError
+    c = c * -1
+    n = a.shape[0]
+    m = a.shape[1]
+    b = b.reshape(n, 1)
+    a = a.astype('float64')
+    b = b.astype('float64')
+    c = c.astype('float64')
+    b = np.vstack((b, np.array([0])))
+    table = np.vstack((a, c))
+    table = np.hstack((table, b))
+    table[-1, -1] = 0
+    x_row = []
+    for i in range(m):
+        x_row.append((i, 'x'))
+    y_column = []
+    for i in range(n):
+        y_column.append((i, 'y'))
+    n += 1
+    m += 1
+    while min(table[-1, :-1]) < 0:
+        col = np.argmin(table[-1, :-1])
+        sim_col = np.copy(table[:-1, -1])
+        for i in range(n - 1):
+            sim_col[i] = sim_col[i] / table[i, col]
+        row = np.argmin(sim_col)
+        pivot = table[row, col]
+        for i in range(m):
+            table[row, i] /= pivot
+        pivot = table[row, col]
+        for i in range(n):
+            if i != row:
+                elem = table[i, col]
+                for j in range(m):
+                    table[i, j] -= table[row, j] * elem
+        x_row[col], y_column[row] = y_column[row], x_row[col]
+    x = [0] * (m - 1)
+    for i in range(n - 1):
+        if y_column[i][1] == 'x':
+            x[y_column[i][0]] = table[i, -1]
+    return np.array(x)
