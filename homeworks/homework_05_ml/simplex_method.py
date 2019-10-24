96b0d85353fa18ee3f8c9239b1e133d670f558de
VladislavKuzevanov <vlad.kuzevanov@mail.ru>
2019-10-24 23:47:29 +0300

    simplex method

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -21,16 +21,17 @@ def simplex_method(a, b, c):
     :return x: np.array, shape=(1, m)
     """
 
-    #Блок первоначальной подгтовки симплекс матрицы
-    simplex_mat = np.vstack((a,c))
+    # Блок первоначальной подгтовки симплекс матрицы
+    simplex_mat = np.vstack((a, c))
     simplex_mat = np.hstack((simplex_mat, np.eye(b.shape[0] + 1)))
-    temp_mat = np.vstack((b, [0]))
+    adding_b_mat = np.array
+    temp_mat = np.vstack((b, np.zeros(1)))
     simplex_mat = np.hstack((simplex_mat, temp_mat))
 
-    #Блок основных вычисления
+    # Блок основных вычисления
     accessory_b_mat = b.copy()
     temp_row = [-1 for _ in range(simplex_mat.shape[0] - 1)]
-    #Пробегаемся по поcледней строке в поисках разрежающего столбца и строки
+    # Пробегаемся по поcледней строке в поисках разрежающего столбца и строки
     while simplex_mat[simplex_mat.shape[0] - 1, :].min() < 0:
         pivot_column = np.argmin(simplex_mat[simplex_mat.shape[0] - 1, :])
         for i in range(simplex_mat.shape[0] - 1):
@@ -56,8 +57,7 @@ def simplex_method(a, b, c):
     return x
 
 
-a = np.array([[1, 2], [3, 4], [5, 6]])
-b = np.array([[1], [0], [2]])
+a = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+b = np.array([[1], [0], [2], [5]])
 c = np.array([5, 7])*(-1)
-
 simplex_method(a, b, c)
