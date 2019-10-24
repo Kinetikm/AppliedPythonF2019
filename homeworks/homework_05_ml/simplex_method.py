4a7adea42914ae50a4f2f4720b4f336b06679dec
Артём <artem@Artems-MacBook-Pro.local>
2019-10-24 21:42:10 +0300

    Add some part hw

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,36 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+
+    a = a.astype(dtype=float)
+    b = b.astype(dtype=float)
+    c = -c.astype(dtype=float)
+    
+    n, m = a.shape
+
+    simplex_table = np.append(a, [c], axis=0)
+    simplex_table = np.append(simplex_table, np.eye(n + 1), axis=1)
+    b_column = np.append(b, [0], axis=0).reshape(n + 1, 1)
+    simplex_table = np.append(simplex_table, b_column, axis=1)
+
+    while min(simplex_table[-1, :]) < 0:
+        indices_pivot_column = np.argmin(simplex_table[-1, :], axis=0)
+
+        indices_pivot_row = np.argmin(simplex_table[:, -1][:-1] / simplex_table[:, indices_pivot_column][:-1])
+
+        simplex_table[indices_pivot_row, :] /= simplex_table[indices_pivot_row, indices_pivot_column]
+        for i in range(n + 1):
+            if i != indices_pivot_row:
+                simplex_table[i, :] -= simplex_table[indices_pivot_row, :] * simplex_table[i, indices_pivot_column]
+
+        result = np.array([])
+        for i in range(m):
+            if len(np.nonzero(simplex_table[:, i])[0]) == 1:
+                indices_first_non_zero = np.nonzero(simplex_table[:, i])[0]
+                result = np.append(result, simplex_table[indices_first_non_zero, -1])
+            else:
+                result = np.append(result, [0])
+
+    return result
+
+        
