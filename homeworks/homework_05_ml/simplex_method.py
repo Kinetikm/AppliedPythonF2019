b65f21953bd00229b13fd34e7aa35923d74f312a
Koltsa Ilya <koltsa.ilya@mail.ru>
2019-10-24 20:06:40 +0300

    hw5_ml

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -4,20 +4,28 @@
 import numpy as np
 
 
-def simplex_method(a, b, c):
-    """
-    Почитать про симплекс метод простым языком:
-    * https://  https://ru.wikibooks.org/wiki/Симплекс-метод._Простое_объяснение
-    Реализацию алгоритма взять тут:
-    * https://youtu.be/gRgsT9BB5-8 (это ссылка на 1-ое из 5 видео).
+def simplex_method(a:np.array, b:np.array, c:np.array):
 
-    Используем numpy и, в целом, векторные операции.
-
-    a * x.T <= b
-    c * x.T -> max
-    :param a: np.array, shape=(n, m)
-    :param b: np.array, shape=(n, 1)
-    :param c: np.array, shape=(1, m)
-    :return x: np.array, shape=(1, m)
-    """
-    raise NotImplementedError
+    n = a.shape[0]
+    m = a.shape[1]
+    b1 = b.reshape(n, 1)
+    c1 = -c.reshape(1, m)
+    matrix = np.concatenate((a, c1))
+    matrix = np.concatenate((matrix, np.eye(n + 1)), axis=1)
+    matrix = np.concatenate((matrix, np.concatenate((b1, [[0]]))), axis=1)
+    column = list(zip(['s'] * n, range(n)))
+    row = list(zip(['x'] * m, range(m)))
+    while matrix[-1].min() < 0:
+        j = matrix[-1].argmin()
+        i = (matrix[:-1, -1] / matrix[:-1, j]).argmin()
+        matrix[i, :] /= matrix[i, j]
+        for k in range(n + 1):
+            if k == i:
+                continue
+            matrix[k, :] -= matrix[i, :] * (matrix[k ,j] / matrix[i, j])
+        column[i] = row[j]
+    x = np.array([0] * m)
+    for i in range(len(column)):
+        if column[i][0] == 'x':
+            x[column[i][1]] = matrix[i, -1]
+    return x
