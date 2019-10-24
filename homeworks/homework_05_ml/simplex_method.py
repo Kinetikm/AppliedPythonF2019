bc97525d9298dde89e4428254040710ae86fd863
vlad1m1r_17 <zamyatovskiy.vi@gmail.com>
2019-10-24 15:21:14 +0300

    simplex done

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,24 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    n, m = a.shape
+    col_labels = np.array(range(n + m + 1))
+    row_labels = np.array([m + i for i in range(n)])
+    a = np.concatenate((a, c.reshape(1, m) * (-1)), axis=0)
+    a = np.concatenate((a, np.eye(n + 1)), axis=1)
+    b = np.vstack((b.reshape(n, 1), np.array([[0]])))
+    a = np.hstack((a, b))
+    pivot_col = np.argmin(a[-1, :-1])
+    el = a[-1, pivot_col]
+    while el < 0:
+        pivot_row = np.argmin(a[:-1, -1] / a[:-1, pivot_col])
+        row_labels[pivot_row], col_labels[pivot_col] = col_labels[pivot_col], row_labels[pivot_row]
+        a[pivot_row] = a[pivot_row, :] / a[pivot_row, pivot_col]
+        r = np.copy(a[pivot_row])
+        a = a + (-a[:, pivot_col]).reshape(n+1, 1) * r.reshape(1, n + m + 1 + 1)
+        a[pivot_row] = r
+        pivot_col = np.argmin(a[-1, :-1])
+        el = a[-1, pivot_col]
+    x = np.zeros(m + n)
+    np.put(x, row_labels, a[:-1, -1])
+    return x[:m]
