35f752c3f31e5f9745f9922547d1ec331e50bc96
kepolol <kepolol@gmail.com>
2019-10-24 20:35:29 +0300

    first iteration

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -7,7 +7,7 @@ import numpy as np
 def simplex_method(a, b, c):
     """
     Почитать про симплекс метод простым языком:
-    * https://  https://ru.wikibooks.org/wiki/Симплекс-метод._Простое_объяснение
+    * https://ru.wikibooks.org/wiki/Симплекс-метод._Простое_объяснение
     Реализацию алгоритма взять тут:
     * https://youtu.be/gRgsT9BB5-8 (это ссылка на 1-ое из 5 видео).
 
@@ -20,4 +20,23 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    ans = np.zeros(a.shape[1])
+    x = np.zeros(max(a.shape[1], a.shape[0]))
+    e = np.eye(len(a) + 1)
+    b_sim_matrix = np.hstack((b, 0)).reshape(len(b) + 1, 1)
+    matrix = np.hstack((np.vstack((a, (-1 * c))), e))
+    sim_matrix = np.hstack((matrix, b_sim_matrix))
+    while min(sim_matrix[-1, :]) < 0:
+        lead_col = sim_matrix[-1, :].argmin()
+        lead_row = (sim_matrix[:-1, -1] / sim_matrix[:-1, lead_col]).argmin()
+        key_element = sim_matrix[lead_row, lead_col]
+        sim_matrix[lead_row, :] = sim_matrix[lead_row, :] / key_element
+        for i in range(sim_matrix.shape[0]):
+            if i != lead_row:
+                sim_matrix[i, :] = sim_matrix[i, :] - \
+                                   sim_matrix[lead_row, :] * sim_matrix[i, lead_col]
+        x[lead_row] = lead_col + 1
+    for i in range(len(x)):
+        if int(x[i]) != 0:
+            ans[int(x[i]) - 1] = sim_matrix[i, -1]
+    return ans
