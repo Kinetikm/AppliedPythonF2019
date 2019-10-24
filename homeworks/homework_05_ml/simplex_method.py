98a9c8d356b60352468f8e95b63cbe2ecc8260f1
ChesnovAE <aechesnov@yandex.ru>
2019-10-24 23:55:48 +0300

    Add simplex_method

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,22 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    x = np.zeros((1, a.shape[1]))
+    # Create a table
+    b = b.reshape(-1, 1)
+    b = np.vstack((b, np.zeros((1, 1))))
+    table = np.vstack((a, -c))
+    table = np.hstack((table, np.eye(table.shape[0])))
+    table = np.hstack((table, b))
+    while not (table[-1, :] >= 0).prod():
+        idx_col = table[-1, :].argmin()
+        idx_row = (table[:-1, -1] / table[:-1, idx_col]).reshape(-1, 1).argmin()
+        table[idx_row, :] = table[idx_row, :] / table[idx_row, idx_col]
+        for row in range(table.shape[0]):
+            if row != idx_row:
+                table[row, :] += table[idx_row, :] * (-table[row, idx_col])
+    for row in range(table.shape[0] - 1):
+        for col in range(a.shape[1]):
+            if table[row, col] == 1.0:
+                x[0, col] = table[row, -1]
+    return x.reshape(-1,)
