03f72be7e165ce5c8344e4d9bbd142acb248cdbe
artfawl <stark050600@mail.ru>
2019-10-24 22:50:56 +0300

    hw5_ml

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,28 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    n = a.shape[0]
+    len_x = a.shape[1]
+    simp_table = np.vstack((a, -1 * c))
+    simp_table = np.hstack((simp_table, np.eye(n + 1)))
+    b = np.append(b, [0])
+    b = b.reshape((n + 1, 1))
+    simp_table = np.hstack((simp_table, b))
+    n = simp_table.shape[0] - 1
+    m = simp_table.shape[1] - 1
+    y_j = np.arange(m)
+    x_i = y_j[len_x:-1:]
+    ans = np.zeros((len_x,))
+    while simp_table[n, 0:m].min() < 0:
+        j = simp_table[n].argmin()
+        i = (simp_table[0:n, m] / simp_table[0:n, j]).argmin()
+        y_j[j], x_i[i] = x_i[i], y_j[j]
+        simp_table[i] /= simp_table[i, j]
+        for iter in range(n + 1):
+            if iter != i:
+                simp_table[iter] -= simp_table[iter, j] * simp_table[i]
+    for i in range(len(x_i)):
+        if x_i[i] < len_x:
+            print(x_i[i])
+            ans[x_i[i]] = simp_table[i, m]
+    return ans
