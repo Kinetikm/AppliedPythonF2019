938b521e6211edd8a3ac3e1379ee13895a15bb20
Чистов Иван <ivan_chistov2001@mail.ru>
2019-10-24 22:58:42 +0300

    Fifth message:)

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,29 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    n, m = len(a), len(c)
+    a = a.astype(np.float128)
+    b = b.astype(np.float128)
+    c = -1 * c.astype(np.float128)
+    x = np.array([0.0 for _ in range(m)])
+    dict_x = [i + m for i in range(n)]
+    while True:
+        row = c.argmin()
+        if c[row] >= 0:
+            break
+        with np.errstate(divide='ignore'):
+            column = (b / a[:, row]).argmin()
+        dict_x[column] = row
+        b[column] /= a[column][row]
+        a[column] = a[column] / a[column][row]
+        for i in range(n):
+            if i == column:
+                continue
+            b[i] -= a[i][row] * b[column]
+            a[i] -= a[i][row] * a[column]
+        c -= c[row] * a[column]
+    for i in range(n):
+        if dict_x[i] >= m:
+            continue
+        x[dict_x[i]] = b[i]
+    return x
