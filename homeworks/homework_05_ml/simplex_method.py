7e2d03b10a2d8d54abab19c72ed0f54a66b46036
Daniil <tretyakov.daniil.edcuation@gmail.com>
2019-10-25 06:49:22 +0300

    all tasks done

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -4,6 +4,45 @@
 import numpy as np
 
 
+def simplex_table(a, b, c):
+    n, m = a.shape
+    table = np.zeros((n + 1, m + n + 1 + 1), dtype=float)
+
+    table[0:n, 0:m] = a
+    table[n, 0:m] = -c
+    table[0:, m:m + n + 1] = np.eye(n + 1)
+    table[0:n, -1] = b[:]
+
+    return table
+
+
+def min_idx(ar):
+    imin = np.argmin(ar)
+    return ar[imin], imin
+
+
+def pivot_num(table):
+    b = np.copy(table[:-1, -1])
+
+    col = np.argmin(table[-1])  # получим pivot column
+
+    for i in range(table.shape[0]-1):
+        b[i] /= table[i, col]
+
+    row = np.argmin(b)  # пересечение row и col будет pivot number
+    return row, col
+
+
+def row_elimination(table, prow, pcol):
+    k = table[prow, pcol]
+    table[prow] /= k
+
+    for i in range(table.shape[0]):
+        if i == prow:
+            continue
+        table[i] += table[prow] * -np.sign(table[i, pcol]) * abs(table[i, pcol])
+
+
 def simplex_method(a, b, c):
     """
     Почитать про симплекс метод простым языком:
@@ -20,4 +59,17 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    np.set_printoptions(suppress=True)
+    table = simplex_table(a, b, c)
+    _, m = a.shape
+    pnumbers = {}  # запоминаем, какой строке соответствует какой Х
+    while table[-1].min() < 0:
+        prow, pcol = pivot_num(table)
+        pnumbers[prow] = pcol
+        row_elimination(table, prow, pcol)
+
+    x = [0] * m
+    for num in pnumbers:
+        x[pnumbers[num]] = table[:, -1][num]
+
+    return np.array(x)
