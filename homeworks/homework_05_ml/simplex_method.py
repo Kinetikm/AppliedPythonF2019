c5eaf8ddb0557abf8cd7a3db0bc9a41fe7096230
mnpks <fuglu@mail.ru>
2019-10-25 06:33:22 +0300

    simplex method

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,31 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    n = a.shape[0]
+    mat = np.vstack((a, -c))
+    mat = np.hstack((mat, np.eye(n + 1)))
+    mat = np.hstack((mat, np.vstack((b.reshape(-1, 1), np.zeros((1, 1))))))
+    l = len(mat[n, :])
+    while (mat[n, :] < 0).any():
+        m = mat[n, 0]
+        ind_1 = 0
+        for i in range(l):
+            if mat[n, i] < m:
+                ind_1 = i
+                m = mat[n, i]
+        temp = mat[0, l - 1] / mat[0, ind_1]
+        ind_2 = 0
+        for i in range(n):
+            if mat[i, l - 1] / mat[i, ind_1] < temp:
+                temp = mat[i, l - 1] / mat[i, ind_1]
+                ind_2 = i
+        mat[ind_2, :] = mat[ind_2, :] / mat[ind_2, ind_1]
+        for row in range(n + 1):
+            if row != ind_2:
+                mat[row, :] += -1 * mat[ind_2, :] * mat[row, ind_1]
+    x = np.zeros((1, a.shape[1]))
+    for row in range(n):
+        for col in range(a.shape[1]):
+            if mat[row, col] == 1.0:
+                x[0, col] = mat[row, l - 1]
+    return x[0]
