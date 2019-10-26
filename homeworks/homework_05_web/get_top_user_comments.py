4c4150764872e09ea5b04810b5219832225864c1
tsapen <dennytsap@gmail.com>
2019-10-26 18:14:17 +0300

    Update homework 5

--- homeworks/homework_05_web/get_top_user_comments.py
+++ homeworks/homework_05_web/get_top_user_comments.py
@@ -2,64 +2,50 @@ import sys
 from collections import Counter
 from bs4 import BeautifulSoup
 from aiohttp import ClientSession
-from bisect import insort
 import asyncio
+import csv
 
 
 async def do_request(link, session):
     async with session.get(link) as resp:
-        return await resp.read()
+        return link, await resp.read()
 
 
 def main(filename, links):
     loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(run(filename, links))
-    loop.run_until_complete(future)
-
-
-async def run(filename, links):
-    requests = []
+    responses = asyncio.ensure_future(run(links))
+    loop.run_until_complete(responses)
     result = []
-    responses = []
+    if responses.result() is not None:
+        for response in responses.result():
+            resp_cnt = Counter()
+            html = BeautifulSoup(response[1], 'html.parser')
+            for tag in html.findAll('a'):
+                if 'data-user-login' in tag.attrs:
+                    resp_cnt[tag.attrs['data-user-login']] += 1
+
+            resp_cnt = [[comm, resp_cnt[comm]] for comm in resp_cnt]
+            resp_cnt.sort(key=lambda a: (a[1], a[0]), reverse=True)
+            result.append({'link': response[0], 'stat': resp_cnt})
+        result.sort(key=lambda a: a['link'])
     with open(filename, 'w') as file:
-        file.write("{},{},{}\n".format("link", "username", "count_comment"))
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow(["link", "username", "count_comment"])
+        for link_result in result:
+            for stat in link_result['stat']:
+                writer.writerow([link_result['link'], stat[0], stat[1]])
+
+
+async def run(links):
     try:
+        requests = []
         async with ClientSession() as session:
             for link in links:
                 req = asyncio.ensure_future(do_request(link, session))
                 requests.append(req)
-            responses = await asyncio.gather(*requests)
+            return await asyncio.gather(*requests)
     except BaseException:
-        return
-    for num_link, resp in enumerate(responses):
-        resp_cnt = Counter()
-        html = BeautifulSoup(resp, 'html.parser')
-        for tag in html.findAll('a'):
-            if 'data-user-login' in tag.attrs:
-                resp_cnt[tag.attrs['data-user-login']] += 1
-
-        result_resp = {}
-        for key in resp_cnt:
-            if resp_cnt[key] in result_resp:
-                result_resp[resp_cnt[key]] += [key]
-                continue
-            result_resp[resp_cnt[key]] = [key]
-        result.append({'link': links[num_link], 'result': result_resp})
-    result.sort(key=lambda a: a['link'])
-
-    with open(filename, 'a') as file:
-        for link_result in result:
-            counts_list = [key for key in link_result['result']]
-            counts_list.sort()
-            for cnt in counts_list:
-                link_result['result'][cnt].sort()
-                for username in link_result['result'][cnt]:
-                    file.write(
-                        "{},{},{}\n".format(
-                            link_result['link'],
-                            username,
-                            cnt))
-
+        return None
 
 if __name__ == '__main__':
     filename = 'top_user_comments.csv'
