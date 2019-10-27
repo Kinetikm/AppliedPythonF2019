48d89964701d60ada51240cd4389f5ae18c17f17
vlad1m1r_17 <zamyatovskiy.vi@gmail.com>
2019-10-27 19:17:00 +0300

    Before switch to another brunch

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+import numpy as np
+from itertools import cycle
 
 
 class LinearRegression:
@@ -11,7 +13,44 @@ class LinearRegression:
         :param batch_size: num sample per one model parameters update
         :param max_iter: maximum number of parameters updates
         """
-        raise NotImplementedError
+        self.lambda_coef = lambda_coef
+        self.regularization = regulatization
+        self.reg_coef = alpha
+        self.batch_size = batch_size
+        self.max_iter = max_iter
+    @staticmethod
+    def grad(a, x, y):
+        def mae_i_j_grad(a_j, x_i_j, y_i):
+            if a_j * x_i_j - y_i > 0:
+                return x_i_j
+            elif a_j * x_i_j - y_i < 0:
+                return -x_i_j
+            else:
+                return 0
+
+        def l1_j_grad(a_j):
+            if a_j > 0:
+                return 1
+            elif a_j < 0:
+                return -1
+            else:
+                return 0
+
+        def l2_j_grad(a_j):
+            return 2 * a_j
+
+        mae_i_grad = np.vectorize(mae_i_j_grad)
+        l1_grad = np.vectorize(l1_j_grad)
+        l2_grad = np.vectorize(l2_j_grad)
+        x = np.hstack((np.ones((x.shape[0], 1)), x))
+        a = a.reshape(1, a.shape[0])
+        y = y.reshape(y.shape[0], 1)
+        mae_matrix_grad = mae_i_grad(a, x, y)
+        mae_grad_vec = np.mean(mae_matrix_grad, axis=0)
+        l1_grad_vec = l1_grad(a)
+        l2_grad_vec = l2_grad(a)
+        print(l2_grad_vec)
+        return mae_grad_vec + l1_grad_vec + l2_grad_vec
 
     def fit(self, X_train, y_train):
         """
@@ -20,7 +59,18 @@ class LinearRegression:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        dim = X_train.shape[1]
+        gamma = 0.9
+        l1_coef = 0.9
+        l2_coef = 0.1
+        E_g_2 = np.zeros(dim)
+        eps = 0.00000001
+        a = np.zeros(dim)
+        X = np.array_split(X_train, self.batch_size)
+        for batch in cycle(X):
+            g_t = LinearRegression.grad(a, batch, y_train)
+            E_g_2 = gamma * E_g_2 + (1 - gamma)
+
 
     def predict(self, X_test):
         """
@@ -36,3 +86,7 @@ class LinearRegression:
         :return: weights array
         """
         pass
+
+a = LinearRegression()
+xx = np.array([[1, 3], [6, 9]])
+a.grad(np.array([1, 2, -5]), xx, np.array([8, 9]))
