039a7be059896db7e47afb3d75307201d13be986
Gulyaev Egor <egoregor424@gmail.com>
2019-10-31 15:14:43 +0300

    Linear Regression with SGD, MSE and L1

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -1,38 +1,58 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+from sklearn.utils import shuffle
 
 class LinearRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5, batch_size=50, max_iter=100):
-        """
-        :param lambda_coef: constant coef for gradient descent step
-        :param regulatization: regularizarion type ("L1" or "L2") or None
-        :param alpha: regularizarion coefficent
-        :param batch_size: num sample per one model parameters update
-        :param max_iter: maximum number of parameters updates
-        """
-        raise NotImplementedError
+    def __init__(self, lambda_coef=1.0, regulation=None, alpha=1, batch_size=50, max_iter=100):
+        self.l_coef = lambda_coef
+        self.reg = regulation
+        self.alpha = alpha
+        self.batch = batch_size
+        self.max_iter = max_iter
+        self.weights = None
 
     def fit(self, X_train, y_train):
-        """
-        Fit model using gradient descent method
-        :param X_train: training data
-        :param y_train: target values for training data
-        :return: None
-        """
-        pass
+        np.random.seed(1)
+        self.normalization(X_train)
+        y_train = y_train.reshape(-1, 1)
+        table = np.hstack((np.ones((X_train.shape[0], 1)), X_train, y_train))
+        self.weights = 2 * np.random.random((1, np.shape(X_train)[1] + 1)) - 1
+        eps = 1e-3
+        for _ in range(self.max_iter):
+            table = shuffle(table)
+            for i in range(0, X_train.shape[0], self.batch):
+                sample = table[i : i + self.batch]
+                x = sample[:, : -1]
+                y = sample[:, -1].reshape(-1, 1)
+                for i in range(x.shape[0]):
+                    old_w = np.copy(self.weights)
+                    self.weights -= self.l_coef * self.grad_step(x, y, i) / self.batch
+                ex = True
+                for i in range(len(self.weights)):
+                    if ex:
+                        ex = abs(self.weights[0, i] - old_w[0, i]) < eps
+                    else:
+                        break
+                if ex:
+                    break
+                
+    def normalization(self, X):
+        means, stds = np.mean(X, axis=0), np.std(X, axis=0)
+        for num in range(len(X)):
+            for i in range(X.shape[1]):
+                X[num, i]=(X[num, i] - means[i])/stds[i]
+
+    def grad_step(self, x, y, i):
+        L1 = self.alpha*(np.sign(self.weights))
+        pred = x[i].dot(self.weights.T).reshape(-1, 1)
+        grad = 2 * x[i].reshape(-1, 1).dot(pred - y[i]) + L1.T
+        return grad.T
 
     def predict(self, X_test):
-        """
-        Predict using model.
-        :param X_test: test data for predict in
-        :return: y_test: predicted values
-        """
-        pass
+        test = np.hstack((np.ones((X_test.shape[0], 1)), X_test))
+        return test.dot(self.weights.T)
 
     def get_weights(self):
-        """
-        Get weights from fitted linear model
-        :return: weights array
-        """
-        pass
+        return self.weights
