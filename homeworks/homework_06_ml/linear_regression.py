c2ef1d958dfb62bc624987fb539fe58e84fa4bbe
Koltsa Ilya <koltsa.ilya@mail.ru>
2019-10-31 21:06:22 +0300

    linear_regr

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -6,9 +6,8 @@ import numpy as np
 
 
 class LinearRegression:
-    def __init__(self, lambd_coef=0.9, alpha=0.5, batch_size=50, max_iter=100):
-        self.coef = lambd_coef
-        self.reg = 'l2'
+    def __init__(self, lmbd_coef=0.9, alpha=0.01, batch_size=50, max_iter=100):
+        self.coef = lmbd_coef
         self.a = alpha
         self.max = max_iter
         self.size = batch_size
@@ -16,13 +15,14 @@ class LinearRegression:
     def fit(self, X_train, y_train):
         y_train = y_train.reshape((-1, 1))
         matrix = np.hstack((np.ones((X_train.shape[0], 1)), X_train, y_train))
-        self.w = np.random.normal(size=(1, X_train.shape[1] + 1))
+        self.w = np.random.normal(scale=20, size=(1, X_train.shape[1] + 1))
         # adadelta
         e_g = 0
         e_w = 0
         eps = 1e-8
         g = self.coef
         grad = 0
+        rms_w = 100
         for _ in range(self.max):
             ind = np.random.permutation(np.arange(matrix.shape[0]))
             for i in range(int(X_train.shape[0] / self.size)):
@@ -37,9 +37,9 @@ class LinearRegression:
                 # adadelta
                 e_g = g * e_g + (1 - g) * (n_grad**2)
                 rms_g = np.sqrt(e_g + eps)
-                rms_w = np.sqrt(e_w + eps)
                 delta_w = - (rms_w / rms_g) * n_grad
                 e_w = g * e_w + (1 - g) * (delta_w ** 2)
+                rms_w = np.sqrt(e_w + eps)
                 self.w = self.w + delta_w.T
 
     def predict(self, X_test):
