6f775d32ee41ac4fae9ff6d7b7c37dd92ed4a062
root <root@localhost.localdomain>
2019-10-31 15:14:23 +0300

    hw6_ml

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
+import numpy as np
 
 
 class LinearRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5, batch_size=50, max_iter=100):
+    def __init__(self, lambda_coef=0.1, alpha=0.5, l1_ratio=0.5, batch_size=50, max_iter=100):
         """
         :param lambda_coef: constant coef for gradient descent step
         :param regulatization: regularizarion type ("L1" or "L2") or None
@@ -11,7 +12,13 @@ class LinearRegression:
         :param batch_size: num sample per one model parameters update
         :param max_iter: maximum number of parameters updates
         """
-        raise NotImplementedError
+        self._lc = lambda_coef
+        self._alpha = alpha
+        self._l1_ratio = l1_ratio
+        self._batch_size = batch_size
+        self._max_iter = max_iter
+        self._weight = np.array([])
+        self._loss = []
 
     def fit(self, X_train, y_train):
         """
@@ -20,7 +27,27 @@ class LinearRegression:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        if X_train.shape[0] != y_train.shape[0]:
+            raise AssertionError
+
+        X_train = np.hstack((np.ones((X_train.shape[0], 1)), X_train))
+        rows = X_train.shape[0]
+        cols = X_train.shape[1]
+        self._weight = np.random.normal(size=cols)
+        for epoch in range(self._max_iter):
+            data = np.hstack((X_train, y_train.reshape((-1, 1))))
+            np.random.shuffle(data)
+            X_train = data[:, : -1]
+            y_train = data[:, -1].flatten()
+            for i in np.arange(0, rows, self._batch_size):
+                batch = X_train[i:i + self._batch_size]
+                y = y_train[i:i + self._batch_size]
+                pred = batch @ self._weight.T
+                f_error = pred - y
+                gradient = 2 * f_error.dot(batch) + self._alpha * self._l1_ratio * np.sign(
+                    self._weight) + self._alpha * (1 - self._l1_ratio) * self._weight
+                self._weight -= self._lc * gradient / batch.shape[0]
+            self._loss.append(self.mse(X_train @ self._weight.T, y_train))
 
     def predict(self, X_test):
         """
@@ -28,11 +55,17 @@ class LinearRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        data = np.hstack((np.ones((X_test.shape[0], 1)), X_test))
+        if data.shape[1] != self._weight.T.shape[0]:
+            raise AssertionError
+        return data @ self._weight.T
+
+    def mse(self, y1, y2):
+        return np.mean((y1 - y2) ** 2)
 
     def get_weights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
-        pass
+        return self._weight
