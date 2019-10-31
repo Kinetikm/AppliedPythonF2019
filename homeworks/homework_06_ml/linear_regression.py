f0530a8104539bf82c6e40a0b31689dbe647ea50
Mikhail <zojloverr@gmail.com>
2019-11-01 00:34:27 +0300

    regression

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -1,38 +1,48 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import numpy as np
+from random import randint
 
 class LinearRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5, batch_size=50, max_iter=100):
-        """
-        :param lambda_coef: constant coef for gradient descent step
-        :param regulatization: regularizarion type ("L1" or "L2") or None
-        :param alpha: regularizarion coefficent
-        :param batch_size: num sample per one model parameters update
-        :param max_iter: maximum number of parameters updates
-        """
-        raise NotImplementedError
+    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=1, batch_size=50, max_iter=100):
+        self.lambda_coef = lambda_coef
+        self.alpha = alpha
+        self.batch_size = batch_size
+        self.max_iter = max_iter
+        self.weights = np.array([])
+
+    def grad_mae(self, x, y):
+        x_n = np.hstack([x, np.ones((self.batch_size, 1))])
+        w = np.transpose(self.weights)
+        grad = np.zeros((1, x_n.shape[1]))
+        for i in range(x_n.shape[1]):
+            for j in range (x_n.shape[0]):
+                grad[0, i] -= np.sign(y[j] - (x_n[j, ::] @ w)) * x_n[j, i] / self.batch_size
+            grad[0, i] -= np.sign(w[i, 0]) * self.alpha
+        return grad
 
     def fit(self, X_train, y_train):
-        """
-        Fit model using gradient descent method
-        :param X_train: training data
-        :param y_train: target values for training data
-        :return: None
-        """
-        pass
+        m = np.zeros((1, X_train.shape[1] + 1))
+        v = np.zeros((1, X_train.shape[1] + 1))
+        w = np.zeros((1, X_train.shape[1] + 1))
+        self.weights = w
+        b_n = randint(0, X_train.shape[0] - self.batch_size)
+        batch_x = X_train[b_n:b_n + self.batch_size, ::]
+        batch_y = y_train[b_n:b_n + self.batch_size]
+
+        for t in range(1, self.max_iter):
+            g = self.grad_mae(batch_x, batch_y)
+            m = 0.9 * m + (1 - 0.9) * g
+            v = 0.999 * v + (1 - 0.999) * np.power(g, 2)
+            m_hat = m / (1 - np.power(0.9, t))
+            v_hat = v / (1 - np.power(0.999, t))
+            w = w - self.lambda_coef * m_hat / (np.sqrt(v_hat) + 0.00000001)
+            self.weights = w
 
     def predict(self, X_test):
-        """
-        Predict using model.
-        :param X_test: test data for predict in
-        :return: y_test: predicted values
-        """
-        pass
+        X = np.hstack([X_test, np.ones((X_test.shape[0], 1))])
+        w_t = np.transpose(self.weights)
+        return X @ w_t
 
     def get_weights(self):
-        """
-        Get weights from fitted linear model
-        :return: weights array
-        """
-        pass
+        return self.weights
