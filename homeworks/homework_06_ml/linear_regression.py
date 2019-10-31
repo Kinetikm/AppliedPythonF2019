70c59af8043cec037dffe9bf2ad832be4eded1d5
Koltsa Ilya <koltsa.ilya@mail.ru>
2019-10-31 14:21:20 +0300

    linear_redression

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -2,37 +2,52 @@
 # coding: utf-8
 
 
+import numpy as np
+
+
 class LinearRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5, batch_size=50, max_iter=100):
-        """
-        :param lambda_coef: constant coef for gradient descent step
-        :param regulatization: regularizarion type ("L1" or "L2") or None
-        :param alpha: regularizarion coefficent
-        :param batch_size: num sample per one model parameters update
-        :param max_iter: maximum number of parameters updates
-        """
-        raise NotImplementedError
+    def __init__(self, lambd_coef=0.9, alpha=0.5, batch_size=50, max_iter=100):
+        self.coef = lambd_coef
+        self.reg = 'l2'
+        self.a = alpha
+        self.max = max_iter
+        self.size = batch_size
 
     def fit(self, X_train, y_train):
-        """
-        Fit model using gradient descent method
-        :param X_train: training data
-        :param y_train: target values for training data
-        :return: None
-        """
-        pass
+        y_train = y_train.reshape((-1, 1))
+        matrix = np.hstack((np.ones((X_train.shape[0], 1)), X_train, y_train))
+        self.w = np.random.normal(size=(1, X_train.shape[1] + 1))
+        # adadelta
+        e_g = 0
+        e_w = 0
+        eps = 1e-8
+        g = self.coef
+        grad = 0
+        for _ in range(self.max):
+            ind = np.random.permutation(np.arange(matrix.shape[0]))
+            for i in range(int(X_train.shape[0] / self.size)):
+                perm = ind[i * self.size: (i + 1) * self.size]
+                x = matrix[perm, :-1]
+                y = matrix[perm, -1]
+                # gradient
+                sign = np.sign(x.dot(self.w.T) - y.reshape(-1, 1))
+                l2 = self.a / 2 * self.w.T
+                n_grad = (np.apply_along_axis(self.gradient, 0, x, sign))
+                n_grad = n_grad.reshape(-1, 1) + l2.reshape(-1, 1)
+                # adadelta
+                e_g = g * e_g + (1 - g) * (n_grad**2)
+                rms_g = np.sqrt(e_g + eps)
+                rms_w = np.sqrt(e_w + eps)
+                delta_w = - (rms_w / rms_g) * n_grad
+                e_w = g * e_w + (1 - g) * (delta_w ** 2)
+                self.w = self.w + delta_w.T
 
     def predict(self, X_test):
-        """
-        Predict using model.
-        :param X_test: test data for predict in
-        :return: y_test: predicted values
-        """
-        pass
+        x = np.hstack((np.ones((X_test.shape[0], 1)), X_test))
+        return x @ self.w.T
 
     def get_weights(self):
-        """
-        Get weights from fitted linear model
-        :return: weights array
-        """
-        pass
+        return self.w
+
+    def gradient(self, x, sign):
+        return np.sum(x.reshape(-1, 1) * sign)
