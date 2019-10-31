030f88ade18caaff484293dc66ac38084cf8c5d7
Kashin Danil <potatochka007@gmail.com>
2019-10-31 23:55:44 +0300

    hw_ml_06

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -1,38 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
+import numpy as np
 
+
+# Loss: MSE, Regularization: elastic, Optim: NAG
 
 class LinearRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5, batch_size=50, max_iter=100):
-        """
-        :param lambda_coef: constant coef for gradient descent step
-        :param regulatization: regularizarion type ("L1" or "L2") or None
-        :param alpha: regularizarion coefficent
-        :param batch_size: num sample per one model parameters update
-        :param max_iter: maximum number of parameters updates
-        """
-        raise NotImplementedError
+    def __init__(self, lambda_coef=0.005, regulatization='elastic', alpha=0.5, batch_size=50, max_iter=100):
+        self.grad_coef = lambda_coef
+        self.alpha = alpha
+        self.batch = batch_size
+        self.iter = max_iter
+        self.weights = np.array(1)
 
     def fit(self, X_train, y_train):
-        """
-        Fit model using gradient descent method
-        :param X_train: training data
-        :param y_train: target values for training data
-        :return: None
-        """
-        pass
+        gamma = 0.1  # коэффициент сохранения
+        speed_last = 0
+        y_train = y_train[:, np.newaxis]
+        X_train = np.hstack((np.ones((X_train.shape[0], 1)), X_train))  # добавляем столбец единиц для свободных весов
+        self.weights = np.random.rand(np.shape(X_train)[1], 1)
+        for i in range(self.iter):
+            np.random.shuffle(X_train)  # на каждой итерации я перемещиваю датасет и беру первый бач,
+            predict_f = (X_train[:self.batch] @ self.weights)  # это не оригинальный sgd, но как один из вариантов
+            regulariz = self.alpha * np.sign(
+                self.weights) + self.alpha * self.weights  # регуляриацию вынес в отдельную
+            # переменную
+            gradient = X_train[:self.batch].T @ (predict_f - y_train[:self.batch]) + regulariz  # подсчет градиента
+            speed_n = gamma * speed_last + self.grad_coef * gradient
+            self.weights -= speed_n  # а это как раз оптимизация NAG
+            speed_last = speed_n
 
     def predict(self, X_test):
-        """
-        Predict using model.
-        :param X_test: test data for predict in
-        :return: y_test: predicted values
-        """
-        pass
+        X_test = np.hstack((np.ones((X_test.shape[0], 1)), X_test))
+        return X_test @ self.weights
 
     def get_weights(self):
-        """
-        Get weights from fitted linear model
-        :return: weights array
-        """
-        pass
+        return self.weights
