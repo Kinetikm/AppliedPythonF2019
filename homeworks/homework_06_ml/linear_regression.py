f9c51347a8a3547488de165cc7336f6fdb21228f
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2019-11-01 15:02:24 +0300

    homework_06_ml fix pep8 and initial verison

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -1,17 +1,64 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Вар 14
+# Loss: MAE, Regularization: elastic, Optim: Adam
+
+import numpy as np
+
 
 class LinearRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5, batch_size=50, max_iter=100):
+    def __init__(self, lambda_coef=1.0, regulatization='elastic', alpha=0.5,
+                 batch_size=50, max_iter=100, eps=1e-6, l1_ratio=0.5, betta1=0.01, betta2=0.01):
+
         """
         :param lambda_coef: constant coef for gradient descent step
-        :param regulatization: regularizarion type ("L1" or "L2") or None
+        :param regulatization: regularizarion type ("L1", "L2", "elastic") or None
         :param alpha: regularizarion coefficent
         :param batch_size: num sample per one model parameters update
         :param max_iter: maximum number of parameters updates
         """
-        raise NotImplementedError
+
+        self.lambda_coef = lambda_coef
+        self.regulatization = regulatization
+        self.alpha = alpha
+        self.batch_size = batch_size
+        self.max_iter = max_iter
+
+        self.l1_ratio = l1_ratio
+
+        self.eps = eps
+        self.betta1 = betta1
+        self.betta2 = betta2
+
+        return
+
+    def get_mae_loss(self, y_pred, y):
+        return sum(abs(y-y_pred)) / y_pred.shape[0]
+
+    def get_grad(self, X_train, y_pred, y):
+        grad = np.zeros_like(self.weights)
+
+        n = X_train.shape[0]
+        m = X_train.shape[1]
+
+        for j in range(m):
+            jgrad = np.sum([X_train[i, j] * np.sign(y_pred - y[i]) for i in range(n)])
+            jgrad /= n
+
+            if self.regulatization == 'elastic':
+                jgrad += self.alpha * self.l1_ratio \
+                    * abs(self.weights[j]) + self.alpha * (1-self.l1_ratio)/2 * self.weights[j] ** 2
+
+            elif self.regulatization == 'L1':
+                jgrad += self.alpha * abs(self.weights[j])
+            elif self.regulatization == 'L2':
+                jgrad += self.alpha * (self.weights[j]) ** 2
+            else:
+                raise Exception("Unknown regularisation")
+
+            grad[j] = jgrad
+        return grad
 
     def fit(self, X_train, y_train):
         """
@@ -20,7 +67,36 @@ class LinearRegression:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+
+        n = X_train.shape[0]
+        m = X_train.shape[1]
+
+        self.weights = np.random.normal(scale=0.001, size=(m,))
+
+        opt_m = 0
+        opt_v = 0
+        priv_loss = 0
+        for i in range(self.max_iter):
+            loss = 0
+            for i in range(0, n, self.batch_size):
+                X_batch = X_train[i:max(n, i+self.batch_size)]
+                y_batch = y_train[i:max(n, i+self.batch_size)]
+
+                y_pred = self.predict(X_batch)
+                loss += self.get_mae_loss(y_pred, y_batch)
+                grad = self.get_grad(X_batch, y_pred, y_batch)
+
+                # adam optimisation
+                opt_m = self.betta1 * opt_m + (1-self.betta1) * grad
+                opt_v = self.betta2 * opt_v + (1-self.betta1) * grad**2
+                m_hat = opt_m / (1 - self.betta1 ** i)
+                v_hat = opt_v / (1 - self.betta2 ** i)
+                self.weights -= self.lambda_coef * m_hat / (np.sqrt(v_hat) + self.eps)
+
+            if abs(priv_loss - loss) < 1e-5:
+                print("Loss eps reached")
+                break
+            print(loss)
 
     def predict(self, X_test):
         """
@@ -28,11 +104,11 @@ class LinearRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        return X_test @ self.weights.T
 
-    def get_weights(self):
+    def getweightseights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
-        pass
+        return self.weights
