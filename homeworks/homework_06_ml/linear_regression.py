6ce7ae8f4394e636a197f96d8c05c2e081361540
Марин Денис <55432164+MarksmanM@users.noreply.github.com>
2019-11-02 19:17:00 +0300

    regr

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -34,7 +34,7 @@ class LinearRegression:  # Реализация для варианта 1
         self.theta = np.random.sample((self.n_features, 1))
         self.X_train = np.hstack((np.ones((self.n_samples, 1)), X_train))
         A_train = np.zeros((self.n_samples, self.n_features+1))
-        A_train[:, :-2] = X_train[:, :]
+        A_train[:, :-1] = X_train[:, :]
         A_train[:, -1] = self.y
         shuffle(A_train[:])
         self.X_train = A_train[:, :-2]
