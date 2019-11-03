2446b3a7c5b17621dcae7aea0754042c7e9fd681
Марин Денис <55432164+MarksmanM@users.noreply.github.com>
2019-11-03 22:05:46 +0300

    logreg

--- homeworks/homework_07_ml/logistic_regression.py
+++ homeworks/homework_07_ml/logistic_regression.py
@@ -24,7 +24,7 @@ class LogisticRegression:
         self.theta = []
 
     def add_penalty(self):
-        return self.alpha * np.sign(self.theta) + 0.6 * self.get_weights()
+        return self.alpha * np.sign(self.get_weights()) + 0.6 * self.get_weights()
 
     def get_next_batch(self, X, Y, batch):
         index = np.random.choice(self.n_samples, batch, replace=False)
@@ -53,11 +53,10 @@ class LogisticRegression:
         for i in range(self.max_iter):
             batch_X, batch_y = self.get_next_batch(self.X_train, self.y, self.batch)
             # Считаем градиент и обновляем тетту
-            print(self.theta.shape)
-            print(batch_X.shape)
             gr = np.zeros((self.n_features, 1)).reshape(1, -1)
             for j in range(self.n_features):
                 gr += (batch_y[j] - 1 / (1 + np.exp((-1)*self.theta.T @ batch_X.T[:, j]))) * batch_X[j, :]
+            gr = gr.reshape(-1, 1)
             print(gr.shape)
             gr += self.add_penalty()
             speed_n = self.gamma * speed_l + self.etta * gr
