2069179f0f1d8e9283e351f0a9fa0715f3aefe0f
Koltsa Ilya <koltsa.ilya@mail.ru>
2019-11-03 21:35:53 +0300

    regression

--- homeworks/homework_07_ml/logistic_regression.py
+++ homeworks/homework_07_ml/logistic_regression.py
@@ -3,21 +3,19 @@
 
 
 import numpy as np
-from sklearn import linear_model
-from sklearn.metrics import log_loss
-from sklearn.model_selection import train_test_split
 
 
 class LogisticRegression:
-    def __init__(self, lambd_coef=1, alpha=0.001, batch_size=50, max_iter=100):
-        self.coef = lambd_coef
+    def __init__(self, lmbd_coef=1e-2, a=1e-3, batch_size=50, max_iter=100):
+        self.coef = lmbd_coef
         # regilarization = l1
-        self.a = alpha
+        self.a = a
         self.size = batch_size
         self.max = max_iter
-        self.min_proba = 0.9
+        self.min_proba = 0.95
 
     def fit(self, X_train, y_train):
+        X_train = self.normalize(X_train)
         y_train = y_train.reshape((-1, 1))
         matrix = np.hstack((np.ones((X_train.shape[0], 1)), X_train, y_train))
         self.w = np.random.normal(size=(1, X_train.shape[1] + 1))
@@ -29,13 +27,13 @@ class LogisticRegression:
                 x = matrix[perm, :-1]
                 y = matrix[perm, -1]
                 # gradient
-                '''cost = self.cost_func(x, y)
-                old_cost = cost'''
+                cost = self.cost_func(x, y)
+                old_cost = cost
                 self.w -= self.coef * self.log_gradient(x, y)
-                '''cost = self.cost_func(x, y)
+                cost = self.cost_func(x, y)
                 diff = old_cost - cost
                 if diff > eps:
-                    break'''
+                    break
 
     def predict(self, X_test):
         proba = self.predict_proba(X_test)
@@ -43,6 +41,7 @@ class LogisticRegression:
         return np.squeeze(pred_value)
 
     def predict_proba(self, X_test):
+        X_test = self.normalize(X_test)
         X_test = np.hstack((np.ones((X_test.shape[0], 1)), X_test))
         return np.squeeze(self.logistic_func(X_test))
 
@@ -60,43 +59,12 @@ class LogisticRegression:
     def cost_func(self, X, y):
         log_func_v = np.squeeze(self.logistic_func(X))
         y = np.squeeze(y)
-        step1 = y * np.log(log_func_v)
-        step2 = (1-y) * np.log(1-log_func_v)
-        final = -step1 - step2
+        final = -(y * np.log(log_func_v)) - ((1-y) * np.log(1-log_func_v))
         return np.mean(final)
 
-
-size = 5000
-n_feat = 20
-
-np.random.seed(0)
-
-C1 = np.random.randn(n_feat, n_feat) * 5
-C2 = np.random.randn(n_feat, n_feat) * 5
-gauss1 = np.dot(np.random.randn(size, n_feat) + np.random.randn(n_feat) * 0.3, C1)
-gauss2 = np.dot(np.random.randn(size, n_feat) + np.random.randn(n_feat) * 0.3, C2)
-
-x = np.vstack([gauss1, gauss2])
-y = np.r_[np.ones(size), np.zeros(size)]
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
-
-lr1 = linear_model.LogisticRegression()
-lr2 = LogisticRegression(batch_size=500, max_iter=1000)
-
-lr1.fit(x_train, y_train)
-lr2.fit(x_train, y_train)
-
-# check predict works
-assert lr2.predict(x_test).shape == y_test.shape
-
-loss1 = log_loss(y_test, lr1.predict_proba(x_test))
-loss2 = log_loss(y_test, lr2.predict_proba(x_test))
-proba = lr2.predict_proba(x_test)
-predict = np.where(lr2.predict_proba(x_test) != y_test, 1, 0)
-su = 0
-for i in predict:
-    if i == 1:
-        su += 1
-print(su)
-
-assert loss2 < loss1 * 2
+    def normalize(self, X):
+        mins = np.min(X, axis=0)
+        maxs = np.max(X, axis=0)
+        rng = maxs - mins
+        norm = 1 - ((maxs - X) / rng)
+        return norm
