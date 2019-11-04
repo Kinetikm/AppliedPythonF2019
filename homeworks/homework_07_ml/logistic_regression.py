768e8293b238a703301d39aa4e62f9e3d97dfdf2
Марин Денис <55432164+MarksmanM@users.noreply.github.com>
2019-11-04 13:19:12 +0300

    logreg

--- homeworks/homework_07_ml/logistic_regression.py
+++ homeworks/homework_07_ml/logistic_regression.py
@@ -91,7 +91,7 @@ class LogisticRegression:
         :return: y_test: predicted probabilities
         """
         X_test = np.hstack((np.ones((X_test.shape[0], 1)), X_test))
-        return 1 - 1/(1 + np.exp(X_test.dot(self.theta)))
+        return 1/(1 + np.exp((-1)*X_test.dot(self.theta)))
 
     def get_weights(self):
         """
