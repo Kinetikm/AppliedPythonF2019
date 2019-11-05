0b26f10f2c166632dab9d77c88efb8bed28ecef8
Filatov Ilya <ilya_filatovv@mail.ru>
2019-11-05 07:26:50 +0300

    lin_regr

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -17,12 +17,10 @@ class LinearRegression:
         self.intercept_ = None
 
     def normalize(self, x_train):
-        mean = np.mean(x_train, axis=0)
-        std = np.std(x_train, axis=0)
-        return (x_train - mean) / std
+        return (x_train - np.mean(x_train)) / np.std(x_train)
 
     def mae(self, x_test, y):
-        return sum(abs(np.sum(self.intercept_ + self.coef_ * x_test, axis=1) - y))
+        return sum(abs(np.sum(self.coef_ * x_test, axis=1) - y))
 
     def grad_func(self, x_train, y_train, coef_last, regulator):
         return -2 * (1 / x_train.shape[0]) * x_train.transpose().dot(y_train) + 2 * (
@@ -38,18 +36,19 @@ class LinearRegression:
         assert x_train.shape[0] == y_train.shape[0], 'Invalid dimensions'
         x_train = self.normalize(x_train)
         x_train = np.hstack([np.ones((x_train.shape[0], 1)), x_train])
+        np.random.seed(346346)
+        x = np.array_split(x_train, self.batch_size)
+        y = np.array_split(y_train, self.batch_size)
         self.coef_ = np.random.randn(x_train.shape[1])
         l2 = self.alpha * sum(self.coef_ ** 2)
         tmp_err = np.inf
-        x = np.split(x_train, self.batch_size, axis=0)
-        y = np.split(y_train, self.batch_size, axis=0)
-        indexes = list(range(x.shape[0]))
+        indexes = list(range(len(x)))
         np.random.shuffle(indexes)
         sqrs = np.zeros(x_train.shape[1])
         deltas = np.zeros(x_train.shape[1])
         for _ in range(self.max_iter):
             for idx in indexes:
-                eps_stable = 1e-5
+                eps_stable = 1e-8
                 g = self.grad_func(x[idx], y[idx], self.coef_, l2) / x[idx].shape[0]
                 sqrs = self.ada_coef * sqrs + (1 - self.ada_coef) * np.square(g)
                 cur_delta = np.sqrt(deltas + eps_stable) / np.sqrt(sqrs + eps_stable) * g
@@ -64,9 +63,9 @@ class LinearRegression:
         self.coef_ = self.coef_[1:]
 
     def predict(self, x_test):
-        assert self.coef_ is None, 'Model is not fitted'
+        assert self.coef_ is not None, 'Model is not fitted'
         return np.sum(self.intercept_ + self.coef_ * x_test, axis=1)
 
     def get_weights(self):
-        assert self.coef_ is None, 'Model is not fitted'
+        assert self.coef_ is not None, 'Model is not fitted'
         return np.hstack((self.intercept_, self.coef_))
