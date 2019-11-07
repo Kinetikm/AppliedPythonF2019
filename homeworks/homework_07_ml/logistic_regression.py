19c8abdc4eed7ea6fd8d4e4d45098b9fb0da23a8
Hunte9999 <v.morozow@mail.ru>
2019-11-08 00:35:13 +0300

    Added logistic regression with NAG and l2 reg (6 var)

--- homeworks/homework_07_ml/logistic_regression.py
+++ homeworks/homework_07_ml/logistic_regression.py
@@ -14,7 +14,27 @@ class LogisticRegression:
         :param batch_size: num sample per one model parameters update
         :param max_iter: maximum number of parameters updates
         """
-        raise NotImplementedError
+        self._lr = lambda_coef
+        if regulatization == 'L2':
+            raise NotImplementedError
+        else:
+            self._regul = regulatization
+        self._alpha = alpha
+        self._batch_size = batch_size
+        self._miter = max_iter
+        self._theta = None
+        self._treshold = 0.5
+
+    @staticmethod
+    def __batch(iterable, n=1):
+        current_batch = []
+        for item in iterable:
+            current_batch.append(item)
+            if len(current_batch) == n:
+                yield current_batch
+                current_batch = []
+        if current_batch:
+            yield current_batch
 
     def fit(self, X_train, y_train):
         """
@@ -23,7 +43,28 @@ class LogisticRegression:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        self._theta = np.random.normal(scale=0.1, size=X_train.shape[1])
+        vt = np.zeros_like(self._theta)
+        x_t = X_train.copy()
+        y_t = y_train.copy()
+        for epoch in range(self._miter):
+            for xbatch, ybatch in zip(self.__batch(x_t, self._batch_size), self.__batch(y_t, self._batch_size)):
+                xbatch = np.array(xbatch)
+                ybatch = np.array(ybatch)
+                vt = 0.2 * vt + self._lr * self.grad(xbatch, ybatch) / X_train.shape[0]
+                self._theta -= vt
+            sh_nums = np.arange(len(y_t))
+            np.random.shuffle(sh_nums)
+            sh_x_t = np.array([x_t[i] for i in sh_nums])
+            x_t = sh_x_t
+            sh_y_t = np.array([y_t[i] for i in sh_nums])
+            y_t = sh_y_t
+
+    def grad(self, x, y):
+        grad = (x.T @ (self.predict_proba(x) - y)) / x.shape[0]
+        if self._regul:
+            grad += 2 * self._alpha * self._theta * np.sign(self._theta)
+        return grad
 
     def predict(self, X_test):
         """
@@ -31,7 +72,8 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        y_pred = map(lambda x: 1 if x >= self._treshold else 0, self.predict_proba(X_test))
+        return np.array([*y_pred])
 
     def predict_proba(self, X_test):
         """
@@ -39,11 +81,12 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted probabilities
         """
-        pass
+        y_prob = map(lambda x: 1 / (1 + np.exp(-x)), X_test @ self._theta)
+        return np.array([*y_prob])
 
     def get_weights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
-        pass
+        return self._theta
