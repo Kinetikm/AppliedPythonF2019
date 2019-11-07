9215f4de64b6e3c7a92f4b94d6fcf770b9d2a760
Kashin Danil <potatochka007@gmail.com>
2019-11-08 00:20:20 +0300

    hw_7

--- homeworks/homework_07_ml/logistic_regression.py
+++ homeworks/homework_07_ml/logistic_regression.py
@@ -5,45 +5,52 @@
 import numpy as np
 
 
+# Regularization: l1, Optim: Adam
+
 class LogisticRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5, batch_size=50, max_iter=100):
-        """
-        :param lambda_coef: constant coef for gradient descent step
-        :param regulatization: regularizarion type ("L1" or "L2") or None
-        :param alpha: regularizarion coefficent
-        :param batch_size: num sample per one model parameters update
-        :param max_iter: maximum number of parameters updates
-        """
-        raise NotImplementedError
+    def __init__(self, lambda_coef=0.05, regulatization=None, alpha=0.5, batch_size=100, max_iter=100):
+        self.grad_coef = lambda_coef
+        self.alpha = alpha
+        self.batch = batch_size
+        self.iterations = max_iter
+        self.weights = np.array(1)
 
     def fit(self, X_train, y_train):
-        """
-        Fit model using gradient descent method
-        :param X_train: training data
-        :param y_train: target values for training data
-        :return: None
-        """
-        pass
+        X_train = np.hstack((np.ones((X_train.shape[0], 1)), X_train))  # добавим фиктивный столбец
+        beta1 = 0.9
+        beta2 = 0.999  # константы для оптимизатора adam
+        m_last = 0
+        v_last = 0
+        eps = 1e-8
+        self.weights = np.random.rand(X_train.shape[1])
+        for i in range(self.iterations):
+            index = np.random.choice(X_train.shape[0], self.batch, replace=False)
+            X_batch = X_train[index]
+            y_batch = y_train[index]
+            y_predict = 1 / (1 + np.exp(-(X_batch @ self.weights)))
+            grad = (X_batch.T @ (y_predict - y_batch) + self.alpha * np.sign(self.weights))  # градиент
+            m_new = m_last * beta1 + (1 - beta1) * grad
+            v_new = v_last * beta2 + (1 - beta2) * (grad * grad)
+            m_hat = m_new / (1 - np.power(beta1, i + 1))
+            v_hat = v_new / (1 - np.power(beta2, i + 1))
+            self.weights -= self.grad_coef * m_hat / np.sqrt(v_hat + eps)
+            m_last = m_new
+            v_last = v_new
 
     def predict(self, X_test):
-        """
-        Predict using model.
-        :param X_test: test data for predict in
-        :return: y_test: predicted values
-        """
-        pass
+        X_test = np.hstack((np.ones((X_test.shape[0], 1)), X_test))
+        y_predict = 1 / (1 + np.exp(-(X_test @ self.weights)))
+        for i in range(y_predict.shape[0]):
+            if i > 0.5:  # сделал порог ответа равный 0.5
+                y_predict[i] = 1
+            else:
+                y_predict[i] = 0
+        return y_predict
 
     def predict_proba(self, X_test):
-        """
-        Predict probability using model.
-        :param X_test: test data for predict in
-        :return: y_test: predicted probabilities
-        """
-        pass
+        X_test = np.hstack((np.ones((X_test.shape[0], 1)), X_test))
+        y_predict = 1 / (1 + np.exp(-(X_test @ self.weights)))
+        return y_predict
 
     def get_weights(self):
-        """
-        Get weights from fitted linear model
-        :return: weights array
-        """
-        pass
+        return self.weights
