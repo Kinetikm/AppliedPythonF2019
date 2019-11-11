998faaf36f3a9ac5df8fbe368eed3c44e27dcb00
Zarina Sayfullina <z.sayfullina@corp.mail.ru>
2019-11-11 15:15:46 +0300

    added doc_test

--- lectures/lecture_09_web/part1/doc_test.py
+++ lectures/lecture_09_web/part1/doc_test.py
@@ -1,1 +1,35 @@
+def factorial(n):
+    """Return the factorial of n, an exact integer >= 0.
+    >>> [factorial(n) for n in range(6)]
+    [1, 1, 2, 6, 24, 120]
+    >>> factorial(30)
+    265252859812191058636308480000000
+    >>> factorial(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be >= 0
+    >>> factorial(30.1)
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be exact integer
+    >>> factorial(30.0)
+    265252859812191058636308480000000
+    >>> factorial(1e100)
+    Traceback (most recent call last):
+        ...
+    OverflowError: n too large
+    """
 
+    import math
+    if not n >= 0:
+        raise ValueError("n must be >= 0")
+    if math.floor(n) != n:
+        raise ValueError("n must be exact integer")
+    if n+1 == n:  # catch a value like 1e300
+        raise OverflowError("n too large")
+    result = 1
+    factor = 2
+    while factor <= n:
+        result *= factor
+        factor += 1
+    return result
