df41ad97147ea1e0c9453d65c4c78d5a7c4fccf4
advatrix <trojan.advatrix@gmail.com>
2019-11-14 22:51:09 +0300

    edit

--- homeworks/homework_07_ml/logistic_regression.py
+++ homeworks/homework_07_ml/logistic_regression.py
@@ -26,10 +26,18 @@ class LogisticRegression:
         self._alpha = alpha
         self._batch_size = batch_size
         self._max_iter = max_iter
-        self._weights = [[]]
-        self._classnum = 1
+        self._beta = alpha
+        self._eps = 1e-7
+        self._threshold = 0.5
+        self._weights = None
 
-        raise NotImplementedError
+    def grad(self, x, y):
+        # grad = np.empty_like(self._weights)
+
+        grad = x.T @ (self.predict_proba(x) - y) + self._alpha * np.sign(self._weights)
+        grad += 2 * self._beta * self._weights * np.sign(self._weights)
+        grad /= x.shape[0]
+        return grad
 
     def fit(self, X_train, y_train):
         """
@@ -38,25 +46,14 @@ class LogisticRegression:
         :param y_train: target values for training data
         :return: None
         """
-        '''
-        implement softmax
-        if y_train.shape[1] > 1:
-            *softmax*
-
-
-        '''
-        self._classnum = y.shape[1]
-        self._weights = [_ for _ in range(self._classnum)]
-        beta_1 = 0.9
-        beta_2 = 0.9
-
-        for i in range(self._classnum):
-            self._weights[i] = np.random.rand(X_train.shape[1])
-            m = 0
-            v = 0
-            for j in range(self._max_iter):
-                for x_batch, y_batch in self.__batches(X_train, y_train[:,j]):
-                    g = self.__grad()
+        X_train = np.hstack((np.ones((X_train.shape[0], 1)), X_train))
+        G = 0
+        self._weights = np.random.rand(X_train.shape[1])
+        for j in range(self._max_iter):
+            for x_batch, y_batch in self.__batches(X_train, y_train):
+                gt = self.grad(x_batch, y_batch)
+                G += gt ** 2
+                self._weights -= self._lambda / np.sqrt(G + self._eps) * gt
 
     def predict(self, X_test):
         """
@@ -64,7 +61,7 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        return [X_test @ self._weights[i].T for i in range(self._classnum)]
+        return (self.predict_proba(X_test) >= self._threshold).astype(np.int)
 
     def predict_proba(self, X_test):
         """
@@ -72,7 +69,9 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted probabilities
         """
-        pass
+        if X_test.shape[1] != self._weights.shape[0]:
+            X_test = np.hstack((np.ones((X_test.shape[0], 1)), X_test))
+        return self.sigmoid(X_test @ self._weights)
 
     def get_weights(self):
         """
@@ -88,3 +87,70 @@ class LogisticRegression:
         end = self._batch_size * n[0] % x.shape[0]
         n[0] += 1
         yield (x[beg:end], y[beg:end])
+
+    @staticmethod
+    def sigmoid(z):
+        return 1 / (1 + np.exp(-z))
+
+
+class SoftmaxLogisticRegression(LogisticRegression):
+
+    @staticmethod
+    def softmax(x):
+        e = np.exp(x - np.max(x))
+        return e / np.sum(e)
+
+    def __init__(self, n):
+        super().__init__()
+        self._n_cls = n
+        self._weights = [[] for _ in range(n)]
+
+    def fit(self, x_train, y_train):
+        x_train = np.hstack((np.ones((x_train.shape[0], 1)), x_train))
+        for i in range(n):
+            G = 0
+            self._weights[i] = np.random.rand(x_train.shape[1] + 1)
+            for j in range(self._max_iter):
+                for x_batch, y_batch in self.__batches(x_train, y_train[:, j]):
+                    gt = self.grad(x_batch, y_batch)
+                    G += gt ** 2
+                    self._weights[i] -= self._lambda_coef / np.sqrt(G + self._eps) * gt
+
+
+    def predict_proba(self, X_test):
+        pr = super().predict_proba(X_test)
+        return softmax(pr)
+
+
+# import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import log_loss
+from sklearn.model_selection import train_test_split
+
+size = 5000
+n_feat = 20
+
+np.random.seed(0)
+
+C1 = np.random.randn(n_feat, n_feat) * 5
+C2 = np.random.randn(n_feat, n_feat) * 5
+gauss1 = np.dot(np.random.randn(size, n_feat) + np.random.randn(n_feat) * 0.3, C1)
+gauss2 = np.dot(np.random.randn(size, n_feat) + np.random.randn(n_feat) * 0.3, C2)
+
+x = np.vstack([gauss1, gauss2])
+y = np.r_[np.ones(size), np.zeros(size)]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+
+lr1 = linear_model.LogisticRegression()
+lr2 = LogisticRegression(batch_size=500, max_iter=1000)
+
+lr1.fit(x_train, y_train)
+lr2.fit(x_train, y_train)
+
+# check predict works
+
+loss1 = log_loss(y_test, lr1.predict_proba(x_test))
+loss2 = log_loss(y_test, lr2.predict_proba(x_test))
+
+
+
