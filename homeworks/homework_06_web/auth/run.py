7c7b39735de20a67b66916e7a6dc0dc4b75d5867
Anatoly Rubanov <anatolijrubanov@gmail.com>
2019-11-16 02:07:24 +0300

    fix validation

--- homeworks/homework_06_web/auth/run.py
+++ homeworks/homework_06_web/auth/run.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, g, abort
 from flask_login import LoginManager, login_required, current_user, login_user, logout_user
-from marshmallow import Schema, fields, post_load
+from marshmallow import Schema, fields, ValidationError, validates_schema
 from application.model import Users, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "123SECRET_KEY123"
 app.config['REMEMBER_COOKIE_NAME'] = 'flight_board'
 
-engine = create_engine('sqlite:///flights.db')
+engine = create_engine('sqlite:///../flights.db')
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
@@ -32,6 +32,10 @@ class RegistrationSchema(Schema):
     email = fields.Email(required=True)
     password = fields.String(required=True)
 
+    @validates_schema()
+    def validate(self, data, **kwargs):
+        pass
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -40,16 +44,20 @@ def load_user(user_id):
 
 @app.route('/registration', methods=['POST'])
 def registration():
-    data = RegistrationSchema().load(request.json).data
-    session = get_db()
-    if session.query(Users).filter(Users.username == data['login']):
-        abort(400, 'User exist')
-    user = Users(username=data['login'], email=data['email'])
-    user.set_password(data['password'])
+    try:
+        schema = RegistrationSchema()
+        data = schema.load(request.json)
+        session = get_db()
+        if session.query(Users).filter(Users.username == data['login']).first():
+            abort(400, 'User exist')
+        user = Users(username=data['login'], email=data['email'])
+        user.set_password(data['password'])
 
-    session.add(user)
-    session.commit()
-    return 'OK'
+        session.add(user)
+        session.commit()
+        return 'OK'
+    except ValidationError as e:
+        abort(400, str(e))
 
 
 @app.teardown_appcontext
