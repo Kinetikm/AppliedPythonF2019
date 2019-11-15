bc5907351cf09f463d875f7767032a59124ac3b5
Ilya Utkin <ilyaphys@yandex.ru>
2019-11-16 01:41:11 +0300

    done tree classifier :(

--- homeworks/homework_08_ml/tree.py
+++ homeworks/homework_08_ml/tree.py
@@ -3,20 +3,12 @@
 
 from collections import deque
 import numpy as np
+import time
 
-def get_class(sample):
-    # УБРАТЬ ИЗ КЛАССА!
-    # 1 or 0
-    print(sample[sample == 1])
-    return len(sample[sample == 1]) > len(sample[sample == 0])
-    
+
 def get_proba(sample):
     # возвращает вероятность единичек!
-    return len(sample[sample == 1]) / len(sample)    
-
-
-def is_numeric(feature_column):
-    return feature_column.dtype == int or feature_column.dtype == float
+    return len(sample[sample == 1]) / len(sample)
 
 
 def gini(target_sample):
@@ -31,24 +23,16 @@ def entropy(target_sample):
     return -(p_one * np.log2(p_one) + (1 - p_one) * np.log2(1 - p_one))
 
 
-# DECSISION TREE
-def get_leaf_samp(observation, tree):
+def classify(observation, tree):
     # Если нет фичи, значит это лист
-    if tree.feature == None:
-        return tree.result
+    if tree.feature is None:
+        return tree.proba
     feat_val = observation[tree.feature]
-    print('feat_val', feat_val)
-    if observation.dtype == int or observation.dtype == float:
-        if feat_val <= tree.threshold:
-            branch = tree.l_child
-        else:
-            branch = tree.r_child
+    if feat_val <= tree.threshold:
+        branch = tree.l_child
     else:
-        if feat_val == tree.threshold:
-            branch = tree.l_child
-        else:
-            branch = tree.r_child
-    return get_leaf_samp(observation, branch)
+        branch = tree.r_child
+    return classify(observation, branch)
 
 
 def sort_by_column(column, sample):
@@ -56,15 +40,21 @@ def sort_by_column(column, sample):
     return sample[sorted_idxs, :]
 
 
-def split_by_value(is_number, row, column, data):
-    value = data[row, column]
-    if is_number:
-        r_sample = data[data[:, column] > value]
-        l_sample = data[data[:, column] <= value]
-    else:
-        l_sample = data[data[:, column] == value]
-        r_sample = data[data[:, column] != value]
+def split_by_value(value, column, data):
+    mask = data[:, column] <= value
+    l_sample = data[mask]
+    r_sample = data[~mask]
     return l_sample, r_sample
+
+
+class DecisionTree:
+    def __init__(self):
+        self.feature = None
+        self.threshold = None
+        self.score = None
+        self.proba = None
+        self.l_child = None
+        self.r_child = None
 
 
 class Tree:
@@ -74,10 +64,9 @@ class Tree:
         :param max_depth: maximum depth of tree. If None depth of tree is not constrained
         :param min_samples_leaf: the minimum number of samples required to be at a leaf node
         """
-        self.criterion = gini if criterion.lower() == 'gini' else entropy ########
+        self.criterion = gini if criterion.lower() == 'gini' else entropy
         self.max_depth = max_depth if max_depth else -1
         self.min_samples_leaf = min_samples_leaf
-        self.tree_ = None
 
     def fit(self, X_train, y_train):
         """
@@ -87,10 +76,71 @@ class Tree:
         :return: None
         """
         data = np.c_[X_train, y_train]
-        
+
+        self.n_objects = X_train.shape[0]
         self.n_features = X_train.shape[1]
-        self.tree_ = DecisionTree(self.criterion, self.max_depth, self.min_samples_leaf)
-        self.tree_.build_tree(sample=data, initial_size=X_train.shape[0], depth=0)
+        self.tree_ = DecisionTree()
+        self.build_tree(data, self.tree_)
+
+    def build_tree(self, sample, node, depth=1):
+        target = sample[:, -1]
+        node.score = self.criterion(target)
+        is_leaf = (node.score == 0 or
+                   (depth == self.max_depth and self.max_depth != -1) or
+                   sample.shape[0] < 2 * self.min_samples_leaf)
+        if is_leaf:
+            node.proba = get_proba(target)
+            return
+
+        best_gain, l_samp, r_samp, best_criteria = self.get_best_params(sample, node.score)
+
+        if best_gain > 0:
+            node.feature = best_criteria[0]
+            node.threshold = best_criteria[1]
+
+            node.l_child = DecisionTree()
+            node.r_child = DecisionTree()
+
+            self.build_tree(l_samp, node.l_child, depth=depth + 1)
+            self.build_tree(r_samp, node.r_child, depth=depth + 1)
+
+            node.node_importance = sample.shape[0] / self.n_objects * best_gain
+        else:
+            node.proba = get_proba(target)
+
+    def get_best_params(self, sample, node_score):
+        columns_number = sample.shape[1]
+        best_gain = 0.0
+        # -1 так как последняя колонка - target
+        for col in range(columns_number - 1):
+            sample = sort_by_column(col, sample)
+            u, indices = np.unique(sample[:, col], return_index=True)
+            for i in range(len(indices) - 1):
+                idx = indices[i]
+                idx_next = indices[i + 1]
+                value = sample[idx, col]
+
+                target_curr = sample[idx, -1]
+                target_next = sample[idx_next, -1]
+                # считаем IG только при переходе через класс
+                if target_next != target_curr:
+                    l_sample, r_sample = split_by_value(value, col, sample)
+                    bad_split = (l_sample.shape[0] < self.min_samples_leaf or r_sample.shape[0] < self.min_samples_leaf)
+                    info_gain = self.get_gain(l_sample, r_sample, node_score) if not bad_split else 0.0
+
+                    if info_gain > best_gain:
+                        best_gain = info_gain
+                        best_l_samp = l_sample
+                        best_r_samp = r_sample
+                        best_criteria = (col, value)
+
+        return best_gain, best_l_samp, best_r_samp, best_criteria
+
+    def get_gain(self, l, r, score):
+        weight = l.shape[0] / (l.shape[0] + r.shape[0])
+        l_score = self.criterion(l[:, -1])
+        r_score = self.criterion(r[:, -1])
+        return score - weight * l_score - (1 - weight) * r_score
 
     def predict(self, X_test):
         """
@@ -101,8 +151,7 @@ class Tree:
         y_pred = np.empty(X_test.shape[0])
         # попробовать векторайзером еще!
         for row in range(X_test.shape[0]):
-            leaf_samp = get_leaf_samp(X_test[row], self.tree_)
-            y_pred[row] = get_class(leaf_samp)
+            y_pred[row] = int(classify(X_test[row], self.tree_) >= 0.5)
         return y_pred
 
     def get_feature_importance(self):
@@ -119,10 +168,11 @@ class Tree:
                 stack.appendleft(tree.l_child)
             if tree.r_child:
                 stack.appendleft(tree.r_child)
-            if tree.feature != None:
+            if tree.feature is not None:
                 self.feat_imp[tree.feature] += tree.node_importance
         self.feat_imp = self.feat_imp / np.sum(self.feat_imp)
         return self.feat_imp
+
 
 class TreeRegressor(Tree):
     def __init__(self, criterion='mse', max_depth=None, min_samples_leaf=1):
@@ -148,122 +198,5 @@ class TreeClassifier(Tree):
         """
         y_pred = np.empty(X_test.shape[0])
         for row in range(X_test.shape[0]):
-            leaf_samp = get_leaf_samp(X_test[row], self.tree_)
-            y_pred[row] = get_proba(leaf_samp)
+            y_pred[row] = classify(X_test[row], self.tree_)
         return y_pred
-
-
-class DecisionTree:
-    def __init__(self, criterion, max_depth, min_samples_leaf):
-        # описать!
-        self.criterion = criterion#self._gini if criterion.lower() == 'gini' else self._entropy
-        self.max_depth = max_depth
-        self.min_samples_leaf = min_samples_leaf
-        # закомментировать!
-        # колонка фичи, которая разделила
-        self.feature = None
-        # значение фичи
-        self.threshold = None
-        # entropy or gini score
-        self.score = None
-        # результат работы дерева(лист)
-        # заполняется только если ЛИСТ!
-        self.result = None
-        self.l_child = None
-        self.r_child = None
-        
-    def build_tree(self, sample, initial_size, depth=1):
-        self.score = self.criterion(sample[:, -1])
-        self.sample_size = sample.shape[0]
-
-        is_leaf = (self.score == 0 or 
-                   (depth == self.max_depth and self.max_depth != -1) or 
-                   sample.shape[0] < 2 * self.min_samples_leaf)
-        
-        if is_leaf:
-            # sample[:, 1] - целевая переменная!
-            self.result = sample[:, -1]
-            return
-        
-        best_gain, l_samp, r_samp, best_criteria = self.get_best_params(sample)
-        
-        is_leaf = (best_gain <= 0)
-        
-        if not is_leaf:
-            self.feature = best_criteria[0]
-            self.threshold = best_criteria[1]
-            
-            self.l_child = DecisionTree(self.criterion, self.max_depth, self.min_samples_leaf)
-            self.l_child.build_tree(l_samp, initial_size, depth=depth+1)
-            
-            self.r_child = DecisionTree(self.criterion, self.max_depth, self.min_samples_leaf)
-            self.r_child.build_tree(r_samp, initial_size, depth=depth+1)
-            
-            self.node_importance = self.get_ni(initial_size)
-        else:
-            self.result = sample[:, -1]
-    
-    def get_ni(self, initial_size):
-        """ node_j_importance = j_samples_size / initial_samples * node_j_impurity - 
-             - node_j_Lchild_samples_size / initial_samples * node_j_Lchild_impurity
-             - node_j_Rchild_samples_size / initial_samples * node_j_Rchild_impurity.
-        """
-        ni = self.sample_size / initial_size * self.score
-        ni_l = self.l_child.sample_size / initial_size * self.l_child.score
-        ni_r = self.r_child.sample_size / initial_size * self.r_child.score
-        return ni - ni_l - ni_r
-    
-    def get_best_params(self, sample):
-        # описать функцию!
-        rows_number = sample.shape[0]
-        columns_number = sample.shape[1]
-        
-        best_gain = 0.0
-        # -1 так как последняя колонка - target
-        for col in range(columns_number - 1):
-            col_is_num = is_numeric(sample[0, col])
-            sample = sort_by_column(col, sample)
-        
-            for row in range(self.min_samples_leaf - 1, rows_number - 1):
-                target_curr = sample[row, -1]
-                target_next = sample[row + 1, -1]
-                # считаем IG только при переходе через класс
-                if target_next != target_curr:
-                    value = sample[row, col]
-                    l_sample, r_sample = split_by_value(col_is_num, row, col, sample)
-                    
-                    bad_split = (l_sample.shape[0] < self.min_samples_leaf or
-                                 r_sample.shape[0] < self.min_samples_leaf)
-                    
-                    info_gain = self.get_gain(l_sample, r_sample) if not bad_split else 0.0
-                    
-                    if info_gain > best_gain:
-                        best_gain = info_gain
-                        best_l_samp = l_sample
-                        best_r_samp = r_sample
-                        best_criteria = (col, value)
-                        
-        return best_gain, best_l_samp, best_r_samp, best_criteria
-    
-    def get_gain(self, l_sample, r_sample):
-        # описать переменные!
-        l_size = l_sample.shape[0]
-        r_size = r_sample.shape[0]
-        
-        l_weight = l_size / (l_size + r_size)
-        l_score = self.criterion(l_sample[:, 1])
-        r_score = self.criterion(r_sample[:, 1])
-        gain = self.score - l_weight * l_score - (1 - l_weight) * r_score
-        return gain
-
-
-#X_train = np.arange(20)
-#X_train = X_train[:, np.newaxis]
-#y_train = np.array([0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,1]).T
-
-#tree = TreeClassifier()
-#tree.fit(X_train, y_train)
-
-#print(tree.predict(y_train))
-#print(tree.predict_proba(y_train))
-#print(tree.get_feature_importance())
