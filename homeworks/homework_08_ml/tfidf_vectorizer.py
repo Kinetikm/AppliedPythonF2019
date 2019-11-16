5a27d116a4d033d89a4c36c0e3d8e1df3d03bf65
Марин Денис <55432164+MarksmanM@users.noreply.github.com>
2019-11-16 15:59:00 +0300

    tr

--- homeworks/homework_08_ml/tfidf_vectorizer.py
+++ homeworks/homework_08_ml/tfidf_vectorizer.py
@@ -2,8 +2,12 @@
 # coding: utf-8
 
 
-import numpy
+from multiprocessing import Pool
+import os
+import numpy as np
 from scipy.sparse import csr_matrix
+from pymorphy2 import MorphAnalyzer
+import string
 
 
 class TfIdfVectorizer:
@@ -19,13 +23,10 @@ class TfIdfVectorizer:
         :param n_jobs: число процессов (не потоков), с которыми должны выполнятсья дальнейшие преобразования.
         Если n_jobs = -1, то n_jobs = кол-во ядер
         '''
-        self.min_df = min_df
-        self.max_df = max_df
-        self.ngram = ngram_range
-        if n_jobs == -1:
-            self.n_jobs = cpu_count()
-        else:
-            self.n_jobs = n_jobs
+        raise NotImplementedError
+
+    def idf(self, n, el):
+        return np.log(n / el)
 
     def fit(self, folder, working_folder):
         '''
@@ -37,7 +38,14 @@ class TfIdfVectorizer:
         в эту папку
         Работаем в числе потоков, равных n_jobs
         '''
-        pass
+        for key in list(words.keys()):
+            if words[key] / N < self.min_df or words[key] / N > self.max_df:
+                del words[key]
+            else:
+                words[key] = self.idf(N, words[key])
+        words = np.array([(key, words[key]) for key in sorted(words)])
+        with open(os.path.join(working_folder, "file"), 'wb') as f:
+            pickle.dump(words, f)
 
     def transform(self, text):
         '''
@@ -46,11 +54,29 @@ class TfIdfVectorizer:
         !НЕ забудьте предобработать =)
         :return: sparse vector (используем scipy.csr_matrix)
         '''
-        pass
+        for punct in string.punctuation:
+            text = text.replace(punct, ' ').lower()
+        text = text.strip().split()
+        text = [self.morph.parse(item)[0].normal_form for item in text]
+        ngrams = zip(*[text[i:] for i in range(self.ngram_range[1])])
+        text += [self.morph.parse("_".join(ngram))
+                 [0].normal_form for ngram in ngrams]
+        with open(os.path.join(self.working_folder, "file"), 'wb') as f:
+            idfs = pickle.load(f)
+        tf = Counter(text)
+        res = np.array([0 if word not in tf else tf[word] * idf for word, idf in idfs])
+        return csr_matrix(res)
 
     def idfs(self, output_file):
         '''
         Возвращаем все idfы у обученной модели, если она не обучена возвращаем пустой файл
         :param output_file: string, файл, в который нужно выгрузить idfы (слово \t idf)
         '''
-        pass
+        os.system(f"echo > {output_file}")
+        with open(os.path.join(self.working_folder, "file"), 'rb') as f:
+            words = pickle.load(f)
+        with open(output_file, "w") as f:
+            for word, idf in words:
+                res = f"{word}\t{idf}\n"
+                print(res)
+                f.write(res)
