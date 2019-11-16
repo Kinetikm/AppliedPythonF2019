7c24de4de56d2e0f7a39f9ef4e039cf01eb51e00
Чистов Иван <ivan_chistov2001@mail.ru>
2019-11-16 05:37:59 +0300

    Message :)

--- homeworks/homework_08_ml/tfidf_vectorizer.py
+++ homeworks/homework_08_ml/tfidf_vectorizer.py
@@ -2,6 +2,18 @@
 # coding: utf-8
 
 
+from multiprocessing import Pool
+from functools import reduce
+import os
+from collections import Counter, defaultdict
+from math import log
+import pickle
+import numpy as np
+from scipy.sparse import csr_matrix
+import string
+from pymorphy2 import MorphAnalyzer
+
+
 class TfIdfVectorizer:
 
     def __init__(self, min_df, max_df, ngram_range, n_jobs):
@@ -15,9 +27,17 @@ class TfIdfVectorizer:
         :param n_jobs: число процессов (не потоков), с которыми должны выполнятсья дальнейшие преобразования.
         Если n_jobs = -1, то n_jobs = кол-во ядер
         '''
-        raise NotImplementedError
+        self.min_df = min_df
+        self.max_df = max_df
+        self.ngram_range = ngram_range
+        if n_jobs == -1:
+            self.n_jobs = os.cpu_count()
+        else:
+            self.n_jobs = n_jobs
+        self.working_folder = None
+        self.morph = MorphAnalyzer()
 
-    def fit(self, folder, working_folder):
+    def fit(self, path_to_dir, working_folder):
         '''
         Считаем idf, формируем словарь итп
         :param folder: string, путь до папки, в которой лежат текстовые файлы. (1 строка - один текст)
@@ -27,7 +47,31 @@ class TfIdfVectorizer:
         в эту папку
         Работаем в числе потоков, равных n_jobs
         '''
-        pass
+        working_file = f"{working_folder}/textfile_1"
+        self.working_folder = working_folder
+        os.system(f"echo > {working_file}")
+        files = os.listdir(path_to_dir)
+        pool = Pool(self.n_jobs)
+        res = pool.map(self.read,
+                       map(lambda file: os.path.join(path_to_dir, file),
+                           files)
+                       )
+        words = reduce(lambda x, y: x + y[0], res, Counter())
+        N = reduce(lambda x, y: x + y[1], res, 0)
+
+        # print(words)
+        for key in list(words.keys()):
+            if words[key] / N < self.min_df or words[key] / N > self.max_df:
+                del words[key]
+            else:
+                words[key] = np.log(N / words[key])
+        # print(words)
+        words = np.array([(key, words[key]) for key in sorted(words)])
+        # print(words)
+        # print(type(words))
+        # raise AssertionError
+        with open(os.path.join(working_folder, "file"), 'wb') as f:
+            pickle.dump(words, f)
 
     def transform(self, text):
         '''
@@ -36,11 +80,47 @@ class TfIdfVectorizer:
         !НЕ забудьте предобработать =)
         :return: sparse vector (используем scipy.csr_matrix)
         '''
-        pass
+        text = self.preprocessing(text)
+        with open(os.path.join(self.working_folder, "file"), 'wb') as f:
+            idfs = pickle.load(f)
+        tf = Counter(text)
+        res = np.array([0 if word not in tf else tf[word] * idf for word, idf in idfs])
+        return csr_matrix(res)
+
+    def preprocessing(self, line):
+        # line = self.morph.parse(line)[0].normal_form
+        for punct in string.punctuation:
+            line = line.replace(punct, ' ').lower()
+        line = line.strip().split()
+        line = [self.morph.parse(item)[0].normal_form for item in line]
+        ngrams = zip(*[line[i:] for i in range(self.ngram_range[1])])
+        line += [self.morph.parse("_".join(ngram))
+                 [0].normal_form for ngram in ngrams]
+        # print(line)
+        return line
+
+    def read(self, read_file):
+        words = Counter()
+        i = 0
+        with open(read_file) as f:
+            for line in f:
+                line = self.preprocessing(line)
+                words += Counter(set(line))
+                i += 1
+        return words, i
 
     def idfs(self, output_file):
         '''
         Возвращаем все idfы у обученной модели, если она не обучена возвращаем пустой файл
         :param output_file: string, файл, в который нужно выгрузить idfы (слово \t idf)
         '''
-        pass
+        if self.working_folder is None:
+            return
+        os.system(f"echo > {output_file}")
+        with open(os.path.join(self.working_folder, "file"), 'rb') as f:
+            words = pickle.load(f)
+        with open(output_file, "w") as f:
+            for word, idf in words:
+                res = f"{word}\t{idf}\n"
+                print(res)
+                f.write(res)
--- homeworks/homework_08_ml/tree.py
+++ homeworks/homework_08_ml/tree.py
@@ -6,13 +6,18 @@ import numpy as np
 
 
 class Tree:
-    def __init__(self, criterion, max_depth, min_samples_leaf):
+    def __init__(self, criterion, max_depth, min_samples_leaf, tree_type):
         """
         :param criterion: method to determine splits
         :param max_depth: maximum depth of tree. If None depth of tree is not constrained
         :param min_samples_leaf: the minimum number of samples required to be at a leaf node
         """
-        raise NotImplementedError
+        self.criterion = criterion
+        self.max_depth = max_depth
+        self.min_samples_leaf = min_samples_leaf
+        self.value = None
+        self.left, self.right = None, None
+        self.tree_type = tree_type
 
     def fit(self, X_train, y_train):
         """
@@ -21,15 +26,72 @@ class Tree:
         :param y_train: target values for training data
         :return: None
         """
+        self.eps = 1e-3
+        N, features = X_train.shape
+        S_0 = self.entropy(y_train)
+        if S_0 < self.eps or self.max_depth == 0:
+            self.value = self.get_value(y_train)
+            return
+
+        else:
+            feature = None
+            best_dS = 0
+            for i in range(features):
+                data = np.argsort(X_train[:, i])
+                N_board = 0
+                for n in range(
+                        self.min_samples_leaf + 1,
+                        N - self.min_samples_leaf):
+
+                    if y_train[data[n - 1]] == y_train[data[n]]:
+                        continue
+
+                    left_entropy = self.entropy(y_train[data[0:n]])
+                    right_entropy = self.entropy(y_train[data[n:N]])
+
+                    dS = S_0 - (left_entropy * (n / N) +
+                                right_entropy * (N - n) / N)
+
+                    if dS > best_dS:
+                        best_dS = dS
+                        feature, board = i, n
+                    N_board += 1
+
+            if feature is None:
+                self.value = self.get_value(y_train)
+                return
+        self.feature = feature
+        self.board = X_train[board, self.feature]
+
+        data = np.argsort(X_train[:, feature])
+
+        self.left = self.tree_type(
+            self.criterion,
+            self.max_depth - 1,
+            self.min_samples_leaf)
+        self.right = self.tree_type(
+            self.criterion,
+            self.max_depth - 1,
+            self.min_samples_leaf)
+
+        self.left.fit(X_train[data[0:board]], y_train[data[0:board]])
+        self.right.fit(X_train[data[board:N]], y_train[data[board:N]])
+
         pass
 
+    def one_predict(self, x):
+        if self.value is None:
+            direction = False if x[self.feature] < self.board else True
+            if direction:
+                return self.right.one_predict(x)
+            else:
+                return self.left.one_predict(x)
+        else:
+            return self.value
+
     def predict(self, X_test):
-        """
-        Predict using model.
-        :param X_test: test data for predict in
-        :return: y_test: predicted values
-        """
-        pass
+        return np.array([self.one_predict(X_test[i, :])
+                         for i in range(X_test.shape[0])])
 
     def get_feature_importance(self):
         """
@@ -40,19 +102,45 @@ class Tree:
 
 
 class TreeRegressor(Tree):
-    def __init__(self, criterion='mse', max_depth=None, min_samples_leaf=1):
+    def __init__(self, criterion='mse', max_depth=np.inf, min_samples_leaf=1):
         """
         :param criterion: method to determine splits, 'mse' or 'mae'
         """
-        super().__init__(criterion, max_depth, min_samples_leaf)
+        super().__init__(criterion, max_depth, min_samples_leaf, TreeRegressor)
+
+    def entropy(self, y):
+        return ((y - y.mean()) ** 2).sum() / len(y)
+
+    def get_value(self, y):
+        return y.mean()
 
 
 class TreeClassifier(Tree):
-    def __init__(self, criterion='gini', max_depth=None, min_samples_leaf=1):
+    def __init__(self, criterion='gini', max_depth=np.inf, min_samples_leaf=1):
         """
         :param criterion: method to determine splits, 'gini' or 'entropy'
         """
-        super().__init__(criterion, max_depth, min_samples_leaf)
+        super().__init__(criterion, max_depth, min_samples_leaf, TreeClassifier)
+
+    def entropy(self, y):
+        H = 0
+        for elem in np.unique(y):
+            p = np.sum(y == elem)
+            H += p * p / len(y)
+        return 1 - H
+
+    def get_value(self, y):
+        p_max, value = 0, None
+        unique = np.unique(y)
+        for elem in unique:
+            p = np.sum(y == elem)
+            if p > p_max:
+                p_max, value = p, elem
+        return (p_max / len(y), value)
+
+    def predict(self, X_test):
+        return np.array([self.one_predict(X_test[i, :])[1]
+                         for i in range(X_test.shape[0])])
 
     def predict_proba(self, X_test):
         """
@@ -60,4 +148,5 @@ class TreeClassifier(Tree):
         :param X_test: test data for predict in
         :return: y_test: predicted probabilities
         """
-        pass
+        return np.array([self.one_predict(X_test[i, :])[0]
+                         for i in range(X_test.shape[0])])
