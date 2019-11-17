c46a04801d093edfdfbfa896402d7575c84291ca
francuz-v <vera.frantsuzova@yandex.ru>
2019-11-17 21:31:37 +0300

    boosting solution without using subsample

--- homeworks/homework_09_ml/boosting.py
+++ homeworks/homework_09_ml/boosting.py
@@ -2,32 +2,33 @@
 # coding: utf-8
 
 
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+
+
 class GradientBoosting:
     def __init__(self, n_estimators=100, learning_rate=1.0, max_depth=None,
                  min_samples_leaf=1, subsample=1.0, subsample_col=1.0):
-        """
-        :param n_estimators: number of trees in model
-        :param learning_rate: discount for gradient step
-        :param max_depth: maximum depth of tree. If None depth of tree is not constrained
-        :param min_samples_leaf: the minimum number of samples required to be at a leaf node
-        :param subsample: the fraction of samples to be used for fitting the individual base learners
-        :param subsample_col: the fraction of features to be used for fitting the individual base learners
-        """
-        raise NotImplementedError
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.min_samles_leaf = min_samples_leaf
+        self.subsample = subsample
+        self.subsample_col = subsample_col
+        self.tree_list = []
 
     def fit(self, X_train, y_train):
-        """
-        Fit model using gradient descent method
-        :param X_train: training data
-        :param y_train: target values for training data
-        :return: None
-        """
-        pass
+        tree = DecisionTreeRegressor(criterion='mse', max_depth=self.max_depth, min_samples_leaf=self.min_samles_leaf)
+        self.average = y_train.mean()
+        h = self.average
+        for i in range(self.n_estimators):
+            g = y_train - h
+            tree.fit(X_train, g)
+            self.tree_list.append(tree)
+            h += self.learning_rate * tree.predict(X_train)
 
     def predict(self, X_test):
-        """
-        Predict using model.
-        :param X_test: test data for predict in
-        :return: y_test: predicted values
-        """
-        pass
+        y_test = self.average
+        for tree in self.tree_list:
+            y_test += self.learning_rate * tree.predict(X_test)
+        return y_test
