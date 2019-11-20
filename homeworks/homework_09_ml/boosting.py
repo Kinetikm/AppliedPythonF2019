cdf5cb37ba4916e4d2f3af14f4324b34c6919201
Марин Денис <55432164+MarksmanM@users.noreply.github.com>
2019-11-20 17:39:34 +0300

    1st_try

--- homeworks/homework_09_ml/boosting.py
+++ homeworks/homework_09_ml/boosting.py
@@ -2,6 +2,11 @@
 # coding: utf-8
 
 
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+
+
 class GradientBoosting:
     def __init__(self, n_estimators=100, learning_rate=1.0, max_depth=None,
                  min_samples_leaf=1, subsample=1.0, subsample_col=1.0):
@@ -13,7 +18,13 @@ class GradientBoosting:
         :param subsample: the fraction of samples to be used for fitting the individual base learners
         :param subsample_col: the fraction of features to be used for fitting the individual base learners
         """
-        raise NotImplementedError
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.min_samples_leaf = min_samples_leaf
+        self.subsample = subsample
+        self.sub_col = subsample_col
+        self.trees = []
 
     def fit(self, X_train, y_train):
         """
@@ -22,7 +33,22 @@ class GradientBoosting:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        self.y_mean = np.mean(y_train)
+        X_train, x_test, y_train, y_test = train_test_split(X_train, y_train, test_size=1-self.subsample)
+        y_pred = np.zeros(X_train.shape[0], )
+        y_pred[:] = np.mean(y_train)
+
+        for i in range(self.n_estimators):
+            if i == 0:
+                grad = y_train
+            else:
+                grad = y_train - y_pred
+            tree = DecisionTreeRegressor(max_depth=self.max_depth, min_samples_leaf=self.min_samples_leaf)
+            tree.fit(X_train, grad)
+            predictions = tree.predict(X_train)
+            y_pred += self.learning_rate * predictions
+            self.trees.append(tree)
+
 
     def predict(self, X_test):
         """
@@ -30,4 +56,7 @@ class GradientBoosting:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        y_pred = np.ones(X_test.shape[0], ) * self.y_mean
+        for i, tree in enumerate(self.trees):
+            y_pred += self.learning_rate * tree.predict(X_test)
+        return y_pred
