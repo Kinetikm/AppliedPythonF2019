2b3c1d2ccc12d005a2209bfc8d68e5cbebcd9d68
Ilya Utkin <ilyaphys@yandex.ru>
2019-11-22 02:39:53 +0300

    WITHOUT WEIGHTS

--- homeworks/homework_09_ml/boosting.py
+++ homeworks/homework_09_ml/boosting.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+
+
+class MetaAlgorithm:
+    def __init__(self, n_estimators, learning_rate):
+        self.n_estimators = n_estimators
+        self.ensebmle = []
+        self.learning_rate = learning_rate
+    
+    def add_model(self, model):
+        self.ensebmle.append(model)
+    
+    def predict(self, X):
+        # предикт константной модели
+        y_hat = np.full((X.shape[0], 1), self.const)
+
+        for (i, tree) in enumerate(self.ensebmle):
+            y_hat += self.learning_rate * tree.predict(X).reshape(-1, 1)
+
+        return y_hat / (len(self.ensebmle) + 1)
 
 
 class GradientBoosting:
@@ -13,7 +34,12 @@ class GradientBoosting:
         :param subsample: the fraction of samples to be used for fitting the individual base learners
         :param subsample_col: the fraction of features to be used for fitting the individual base learners
         """
-        raise NotImplementedError
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth #
+        self.min_samples_leaf = min_samples_leaf
+        self.subsample = subsample
+        self.subsample_col = subsample_col
 
     def fit(self, X_train, y_train):
         """
@@ -22,20 +48,58 @@ class GradientBoosting:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        self.meta = MetaAlgorithm(self.n_estimators, self.learning_rate)
+        self.meta.const = np.mean(y_train, axis=0)
 
+        data = np.c_[X_train, y_train]
+
+        for i in range(self.n_estimators):
+            x_batch, y_batch = self.get_subsample(data)
+
+            meta_pred = self.meta.predict(x_batch)
+            residuals = y_batch - meta_pred
+
+            clf = DecisionTreeRegressor(max_depth=self.max_depth, min_samples_leaf=self.min_samples_leaf)
+            clf = clf.fit(x_batch, residuals)
+            
+            self.meta.add_model(clf)
+            # b = 0
+
+            # new_meta_pred = self.meta.predict(x_batch)
+
+            # clf_pred = clf.predict(x_batch)
+            # clf_pred_w = (clf_pred * b).reshape(-1, 1)
+            # dy = meta_pred + clf_pred_w - y_batch
+            # gradient = 2 * clf_pred.T @ dy
+            # print(gradient)
+            # b -= self.learning_rate * gradient
+            # self.meta._w.append(b)
+
+    def get_subsample(self, data):
+        # data[:,-1] - target
+        n_feat = data.shape[1] - 1
+        n_obj = data.shape[0]
+
+        col_lim = int(self.subsample_col * n_feat)
+        obj_lim = int(self.subsample * n_obj)
+    
+        idx_feat = np.arange(n_feat)
+        idx_obj = np.arange(n_obj)
+
+        np.random.shuffle(idx_feat)
+        np.random.shuffle(idx_obj)
+
+        obj_sub = data[idx_obj[:obj_lim], :]
+
+        target = obj_sub[:, -1]
+        feat_sub = obj_sub[:, idx_feat[:col_lim]]
+        return feat_sub, target[:, np.newaxis]
+    
     def predict(self, X_test):
         """
         Predict using model.
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
-
-# создать функцию для средней оценки
-# написать алгоритм, который использует градиентный спуск с оптимизацией для обучения базовых алгоритмов 
-# Используя написанное ранее дерево: переделать под регрессию!
-# создать список базовых алгоритмов, заполнить его константным для MSE
-#for i in range(T) # T = n_estimators
-#    for j in range(N):
-#        
+        return self.meta.predict(X_test)
+      
