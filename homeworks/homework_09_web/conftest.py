2d7248ec9e7872c00e3c216298a906f0b3761dce
Kirill <vk1r1ll@yandex.ru>
2019-11-22 00:11:48 +0300

    first pep

--- homeworks/homework_09_web/conftest.py
+++ homeworks/homework_09_web/conftest.py
@@ -1,1 +1,14 @@
-import pytest  # TODO
+import pytest
+from app.server import app
+
+@pytest.fixture(scope='module')
+def test_client():
+    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI_TEST']
+    testing_client = app.test_client()
+
+    ctx = app.app_context()
+    ctx.push()
+
+    yield testing_client
+
+    ctx.pop()
