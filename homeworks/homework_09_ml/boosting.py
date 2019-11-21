5827cfa3b155ca3920e072785ed9d066e4aa935e
vlad1m1r_17 <zamyatovskiy.vi@gmail.com>
2019-11-21 22:27:26 +0300

    Boosting done

--- homeworks/homework_09_ml/boosting.py
+++ homeworks/homework_09_ml/boosting.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+
 
 class GradientBoosting:
     def __init__(self, n_estimators=100, learning_rate=1.0, max_depth=None,
@@ -13,7 +17,28 @@ class GradientBoosting:
         :param subsample: the fraction of samples to be used for fitting the individual base learners
         :param subsample_col: the fraction of features to be used for fitting the individual base learners
         """
-        raise NotImplementedError
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.min_samples_leaf = min_samples_leaf
+        if subsample > 1 or subsample_col > 1:
+            raise ValueError
+        self.subsample_coef = subsample
+        self.subsample_col_coef = subsample_col
+        self.constant = None
+        self.trees = []
+        self.features_for_estimator = []
+        self.coefficients = []
+
+    @staticmethod
+    def get_constant(y):
+        return np.mean(y)
+
+    def anti_grad(self, X, y):
+        return 2 * (y - self.predict(X))
+
+    def calculate_coef(self, X, y, current_predictions):
+        return np.sum(y - self.predict(X) * current_predictions) / np.sum(current_predictions ** 2)
 
     def fit(self, X_train, y_train):
         """
@@ -22,7 +47,22 @@ class GradientBoosting:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        n_estimators = 0
+        self.constant = GradientBoosting.get_constant(y_train)
+        while n_estimators < self.n_estimators:
+            tree = DecisionTreeRegressor(max_depth=self.max_depth, min_samples_leaf=self.min_samples_leaf)
+            x_train_i, x_test, y_train_i, y_test = train_test_split(X_train, y_train, test_size=1 - self.subsample_coef)
+            columns = sorted(np.random.choice(x_train_i.shape[1], round(self.subsample_col_coef * x_train_i.shape[1]),
+                                              replace=False))
+            x_train_i_feature = np.hstack([x_train_i[:, i:i+1] for i in columns])
+            g_train = self.anti_grad(x_train_i, y_train_i)
+            tree.fit(x_train_i_feature, g_train)
+            current_predictions = tree.predict(x_train_i_feature)
+            b = self.calculate_coef(x_train_i, y_train_i, current_predictions)
+            self.trees.append(tree)
+            self.features_for_estimator.append(columns)
+            self.coefficients.append(b * self.learning_rate)
+            n_estimators += 1
 
     def predict(self, X_test):
         """
@@ -30,4 +70,8 @@ class GradientBoosting:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        result = [self.constant] * X_test.shape[0]
+        for i, tree in enumerate(self.trees):
+            X = np.hstack([X_test[:, k:k+1] for k in self.features_for_estimator[i]])
+            result += self.coefficients[i] * tree.predict(X)
+        return result
