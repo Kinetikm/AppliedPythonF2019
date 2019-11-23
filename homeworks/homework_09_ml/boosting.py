18babdbc2a35dc0f40ef5225c0cef3e042e14a1c
Daniil <tretyakov.daniil.edcuation@gmail.com>
2019-11-23 16:42:38 +0300

    boosting done

--- homeworks/homework_09_ml/boosting.py
+++ homeworks/homework_09_ml/boosting.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+
 
 class GradientBoosting:
     def __init__(self, n_estimators=100, learning_rate=1.0, max_depth=None,
@@ -13,7 +16,22 @@ class GradientBoosting:
         :param subsample: the fraction of samples to be used for fitting the individual base learners
         :param subsample_col: the fraction of features to be used for fitting the individual base learners
         """
-        raise NotImplementedError
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.min_samples_leaf = min_samples_leaf
+        self.subsample = subsample
+        self.subsample_col = subsample_col
+
+        self.trees = []
+        self.target_mean = 0
+        self.features = []
+
+    def get_next_batch(self, X, Y, batch_size):
+        index = np.random.choice(X.shape[0], batch_size, replace=False)
+        x_batch = X[index]
+        y_batch = Y[index]
+        return x_batch, y_batch
 
     def fit(self, X_train, y_train):
         """
@@ -22,7 +40,21 @@ class GradientBoosting:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        self.target_mean = np.mean(y_train)
+        pred = np.full(y_train.shape[0], np.mean(y_train))
+
+        for _ in range(self.n_estimators):
+            samples = np.random.choice(X_train.shape[0], int(X_train.shape[0]*self.subsample), replace=False)
+            features = np.random.choice(X_train.shape[1], int(X_train.shape[1]*self.subsample_col), replace=False)
+            self.features.append(features)
+            x = X_train[np.ix_(samples, features)]
+
+            grad = (y_train - pred)[samples]
+
+            tree = DecisionTreeRegressor(max_depth=self.max_depth, min_samples_leaf=self.min_samples_leaf)
+            tree.fit(x, grad)
+            pred[samples] += self.learning_rate * tree.predict(x)
+            self.trees.append(tree)
 
     def predict(self, X_test):
         """
@@ -30,4 +62,7 @@ class GradientBoosting:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        pred = np.ones([X_test.shape[0]]) * self.target_mean
+        for i in range(self.n_estimators):
+            pred += self.learning_rate * self.trees[i].predict(X_test[:, self.features[i]])
+        return pred
