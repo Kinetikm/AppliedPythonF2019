c83d1debd225238ee103be4480986c08c1946d90
sego <mister.sego@mail.ru>
2019-11-23 07:42:29 +0300

    boosting

--- homeworks/homework_09_ml/boosting.py
+++ homeworks/homework_09_ml/boosting.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+
 
 class GradientBoosting:
-    def __init__(self, n_estimators=100, learning_rate=1.0, max_depth=None,
+    def __init__(self, n_estimators=100, learning_rate=0.5, max_depth=None,
                  min_samples_leaf=1, subsample=1.0, subsample_col=1.0):
         """
         :param n_estimators: number of trees in model
@@ -13,16 +16,43 @@ class GradientBoosting:
         :param subsample: the fraction of samples to be used for fitting the individual base learners
         :param subsample_col: the fraction of features to be used for fitting the individual base learners
         """
-        raise NotImplementedError
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.min_samples_leaf = min_samples_leaf
+        self.subsample = subsample
+        self.subsample_col = subsample_col
+        self.const = 0
+        self.trees = []
+        self.features = []
+        self.coef_b = []
 
-    def fit(self, X_train, y_train):
+    def get_batch(self, shape, batch_size):
+        all_index = np.arange(shape)
+        np.random.shuffle(all_index)
+        return all_index[:int(batch_size * shape)]
+
+    def fit(self, x_train, y_train):
         """
-        Fit model using gradient descent method
-        :param X_train: training data
+        :param x_train: training data
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        self.const = np.mean(y_train)
+        for i in range(self.n_estimators):
+            tree = DecisionTreeRegressor(max_depth=self.max_depth, min_samples_leaf=self.min_samples_leaf)
+            rows = self.get_batch(x_train.shape[0], self.subsample)
+            cols = self.get_batch(x_train.shape[1], self.subsample_col)
+            x_train_i = x_train[rows]
+            x_train_i_without_feature = x_train_i[:, cols]
+            y_train_i = y_train[rows]
+            antigradient = y_train_i - self.predict(x_train_i)
+            tree.fit(x_train_i_without_feature, antigradient)
+            a_i = tree.predict(x_train_i_without_feature)
+            b_i = np.sum((antigradient - self.predict(x_train_i)) * a_i) / np.sum(a_i ** 2)
+            self.trees.append(tree)
+            self.features.append(cols)
+            self.coef_b.append(b_i * self.learning_rate)
 
     def predict(self, X_test):
         """
@@ -30,4 +60,8 @@ class GradientBoosting:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        h = [self.const] * X_test.shape[0]
+        for i, tree in enumerate(self.trees):
+            x_test = X_test[:, self.features[i]]
+            h += self.coef_b[i] * tree.predict(x_test)
+        return h
