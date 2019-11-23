5fa524116d5ee29906770917a6d67eb608a86a6a
Ramil.Chermanteev <ramil.chermanteev@x5.ru>
2019-11-23 17:09:46 +0300

    добавлено решение GradientBoosting

--- homeworks/homework_09_ml/boosting.py
+++ homeworks/homework_09_ml/boosting.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+
 
 class GradientBoosting:
-    def __init__(self, n_estimators=100, learning_rate=1.0, max_depth=None,
-                 min_samples_leaf=1, subsample=1.0, subsample_col=1.0):
+    def __init__(
+        self,
+        n_estimators=100,
+        learning_rate=0.01,
+        max_depth=None,
+        min_samples_leaf=1,
+        subsample=1.0,
+        subsample_col=1.0,
+    ):
         """
         :param n_estimators: number of trees in model
         :param learning_rate: discount for gradient step
@@ -13,7 +23,20 @@ class GradientBoosting:
         :param subsample: the fraction of samples to be used for fitting the individual base learners
         :param subsample_col: the fraction of features to be used for fitting the individual base learners
         """
-        raise NotImplementedError
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.min_samples_leaf = min_samples_leaf
+        self.subsample = subsample
+        self.subsample_col = subsample_col
+        self.trees = []
+        self.features = []
+
+    def _get_random_sample(self, x):
+        s, s_col = x.shape
+        sample_idx = np.random.choice(s, int(self.subsample * s))
+        sample_col_idx = np.random.choice(s_col, int(self.subsample_col * s_col))
+        return sample_idx, sample_col_idx
 
     def fit(self, X_train, y_train):
         """
@@ -22,7 +45,37 @@ class GradientBoosting:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        self.y = y_train
+
+        y_train = y_train.reshape(-1, 1)
+
+        sample_idx, _ = self._get_random_sample(X_train)
+
+        prediction = np.mean(y_train[sample_idx, :])
+
+        for i in range(self.n_estimators):
+            sample_idx, sample_col_idx = self._get_random_sample(X_train)
+
+            y_fit = y_train[sample_idx, :]
+            X_fit = X_train[sample_idx, :][:, sample_col_idx]
+
+            if not i:
+                anti_grad = y_fit
+            else:
+                anti_grad = y_fit - prediction.reshape(-1, 1)
+
+            tree = DecisionTreeRegressor(
+                criterion="mse",
+                max_depth=self.max_depth,
+                min_samples_leaf=self.min_samples_leaf,
+            )
+
+            tree.fit(X_fit, anti_grad)
+
+            self.trees.append(tree)
+            self.features.append(sample_col_idx)
+
+            prediction += self.learning_rate * tree.predict(X_fit)
 
     def predict(self, X_test):
         """
@@ -30,4 +83,9 @@ class GradientBoosting:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        prediction = np.mean(self.y)
+
+        for i in range(self.n_estimators):
+            prediction += self.learning_rate * self.trees[i].predict(X_test[:, self.features[i]])
+
+        return prediction
