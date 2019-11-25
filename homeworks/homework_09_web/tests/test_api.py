ab581f3c855d0d9e0e12b2f654ded87676114266
Kirill <vk1r1ll@yandex.ru>
2019-11-25 20:11:28 +0300

    fixture pep

--- homeworks/homework_09_web/tests/test_api.py
+++ homeworks/homework_09_web/tests/test_api.py
@@ -33,6 +33,43 @@ def check_response(response, data=None, status_code=200):
         assert response.json == data
 
 
+def test_create_flight(test_client, init_database):
+
+    response = test_client.post(
+        '/flights', json={
+            "departure": "21:31",
+            "arrival": "6:44",
+            "travel_time": "9:13",
+            "destination": "Mexico",
+            "aircraft_type": "Airbus A320"
+        })
+    check_response(response, True, 201)
+
+
+def test_get_flights(test_client, init_database):
+    data = {
+            "departure": "21:31",
+            "arrival": "6:44",
+            "travel_time": "9:13",
+            "destination": "Mexico",
+            "aircraft_type": "Airbus A320"
+        }
+    test_client.post('/flights', json=data)
+    response = test_client.get('/flights')
+    check_response(response, data, 201)
+
+
+def test_put_flight(test_client, init_database):
+    response = test_client.put(
+        "/flights/1", json={"aircraft_type": "Snoop-Dog Airbus A320 V2"})
+    check_response(response, True, 202)
+
+
+def test_delete_flight(test_client, init_database):
+    response = test_client.delete("/flights/1")
+    check_response(response, "1 is deleted.", 202)
+
+
 def test_create_flight2(test_client, init_database):
 
     response = test_client.post(
@@ -48,3 +85,20 @@ def test_put_flight2(test_client, init_database):
     response = test_client.put(
         "/flights/1", json={"aircraft_type1": "Snoop-Dog Airbus A320 V2"})
     check_response(response, "Error: wrong input", 400)
+
+
+def test_delete_flight2(test_client, init_database):
+    response = test_client.delete("/flights/101")
+    check_response(response, "101 not found.", 404)
+
+
+def test_registration(test_client, init_database):
+
+    response = test_client.post(
+        '/login', json={
+            "token": 'toekrrtgg234234-tkfdgmgkd543',
+            "login": 'Morty',
+            "password": 'password',
+            "email": "testin@mail.ru"
+        })
+    check_response(response, {"registration": "user created"}, 200)
